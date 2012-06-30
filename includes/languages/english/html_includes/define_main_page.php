05337233a3f617cddc45c39bd72e9c85d8ab5777
DrByte <drbyte@zen-cart.com>
2012-06-30 02:30:07 -0400

    ADMINISTRATIVE - fix Book link in distro

--- includes/languages/english/html_includes/define_main_page.php
+++ includes/languages/english/html_includes/define_main_page.php
@@ -1,4 +1,4 @@
-<a href="http://www.lulu.com/content/466605"><img src="images/large/e-start-book.gif" alt="get your manual today" title="Have you got yours yet? Join the 1000's of Zen Cart users that have bought the only comprehensive owners manual !" /></a>
+<a href="http://www.zen-cart.com/book"><img src="images/large/e-start-book.gif" alt="get your manual today" title="Have you got yours yet? Join the 1000's of Zen Cart users that have bought the only comprehensive owners manual !" /></a>
 <p>This content is located in the file at: <code> /languages/english/html_includes/YOUR_TEMPLATE/define_main_page.php</code></p>
 <p>You can quickly edit this content via Admin->Tools->Define Pages Editor, and select define_main_page from the pulldown.</p>
 <p><strong>NOTE: Always backup the files in<code> /languages/english/html_includes/your_template</code></strong></p>
