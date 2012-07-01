7cb1be3c24d95fad001c4b274ea4ad8c5643252d
Ian Wilson <ian@zen-cart.com>
2012-07-01 13:45:07 +0100

    CHANGE-202 compounding or multiple tax problems
    Oops - missed copyright header date

--- includes/classes/order.php
+++ includes/classes/order.php
@@ -3,7 +3,7 @@
  * File contains the order-processing class ("order")
  *
  * @package classes
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: order.php 19353 2011-08-19 19:06:45Z kuroi $
  */
