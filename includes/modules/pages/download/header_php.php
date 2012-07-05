fcd8d9ffc0fce003016633137939563b2c608a46
DrByte <drbyte@zen-cart.com>
2012-07-04 22:12:50 -0400

    CHANGE-220 Add optional constant 'DOWNLOADS_SKIP_DOT_PREFIX_ON_REDIRECT' to overcome rare problem on some servers which won't allow use of dot-prefixed temporary files Usage: define('DOWNLOADS_SKIP_DOT_PREFIX_ON_REDIRECT', TRUE); in /includes/extra_configures/ folder.

--- includes/modules/pages/download/header_php.php
+++ includes/modules/pages/download/header_php.php
@@ -3,7 +3,7 @@
  * download header_php.php
  *
  * @package page
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: header_php.php 18964 2011-06-22 19:58:38Z drbyte $
@@ -78,6 +78,7 @@ function zen_random_name()
 {
   $letters = 'abcdefghijklmnopqrstuvwxyz';
   $dirname = '.';
+  if (defined('DOWNLOADS_SKIP_DOT_PREFIX_ON_REDIRECT') && DOWNLOADS_SKIP_DOT_PREFIX_ON_REDIRECT === TRUE) $dirname = '';
   $length = floor(zen_rand(16,20));
   for ($i = 1; $i <= $length; $i++) {
     $q = floor(zen_rand(1,26));
