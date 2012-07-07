43cf2a6a5049329cafb6e7e1b58e9fb8ee1e1566
DrByte <drbyte@zen-cart.com>
2012-07-07 00:29:18 -0400

    CHANGE-240 - Add form auto-focus on advanced-search results and admin category lookups

--- includes/modules/pages/advanced_search_result/jscript_onload_helper.php
+++ includes/modules/pages/advanced_search_result/jscript_onload_helper.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * @package page
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id$
+ */
+?>
+
 <script type="text/javascript">
 function onloadFocus() {
 <?php
