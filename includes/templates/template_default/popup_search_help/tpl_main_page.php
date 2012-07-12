8de9e84f20e552e5f60efe0599678b89277a9164
DrByte <drbyte@zen-cart.com>
2012-07-12 12:12:23 -0400

    CHANGE-255 Fix missing [close]  from popup_search_help template file

--- includes/templates/template_default/popup_search_help/tpl_main_page.php
+++ includes/templates/template_default/popup_search_help/tpl_main_page.php
@@ -3,7 +3,7 @@
  * Override Template for common/tpl_main_page.php
  *
  * @package templateSystem
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_main_page.php 2870 2006-01-21 21:36:02Z birdbrain $
@@ -11,4 +11,5 @@
 ?>
 <body id="popupSearchHelp" onload="resize();">
 <div><?php echo TEXT_SEARCH_HELP ?></div>
+<p><?php echo '<a href="javascript:window.close()">' . TEXT_CURRENT_CLOSE_WINDOW . '</a>'; ?></p>
 </body>
