1bf3617fc5b4f9ce182641b49c0479e95e26a985
Ian Wilson <ian@zen-cart.com>
2012-07-17 13:37:03 +0100

    CHANGE-231 page_not_found on session timeout?
    change copyright date

--- includes/init_includes/init_sanitize.php
+++ includes/init_includes/init_sanitize.php
@@ -4,7 +4,7 @@
  * see {@link  http://www.zen-cart.com/wiki/index.php/Developers_API_Tutorials#InitSystem wikitutorials} for more details.
  *
  * @package initSystem
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: init_sanitize.php 18695 2011-05-04 05:24:19Z drbyte $
