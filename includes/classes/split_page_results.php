74bb8df5abef7036fe76b170c92fc364a9f969c6
Ian Wilson <ian@zen-cart.com>
2012-08-17 17:54:58 +0100

    ADMINISTRATIVE - header date update

--- includes/classes/split_page_results.php
+++ includes/classes/split_page_results.php
@@ -3,7 +3,7 @@
  * split_page_results Class.
  *
  * @package classes
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: split_page_results.php 18925 2011-06-13 04:35:37Z drbyte $
