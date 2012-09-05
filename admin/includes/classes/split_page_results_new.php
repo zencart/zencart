f0f3a5442e009e8a0f5eec0becd45f0e610443d3
Ian Wilson <ian@zen-cart.com>
2012-09-05 14:22:57 +0100

    CHANGE-283 Allow for a custom count query in split page results class
    The split page result classes parse the original query and rebuild it to attempt to add a count(*) in the query.
    This repurposed query is used to provide a count of all the results to use in pagination displays.
    
    The problem is that the parsing logic fails with some more complex queries. e.g. sub selects using GROUP BY clauses, and others
    
    These changes to the catalog class allow for the use of a separate query being passed to the class that will be used to provide the count operation.
    The change is backward compatible with previous versions.
    
    The admin class is not amenable to the same change, so a copy of the catalog class has been provided to allow developers/contributors to use if necessary.

--- admin/includes/classes/split_page_results_new.php
+++ admin/includes/classes/split_page_results_new.php
@@ -3,7 +3,7 @@
  * split_page_results Class.
  *
  * @package classes
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: split_page_results.php 18925 2011-06-13 04:35:37Z drbyte $
@@ -19,11 +19,11 @@ if (!defined('IS_ADMIN_FLAG')) {
  *
  * @package classes
  */
-class splitPageResults extends base {
+class splitPageResultsNew{
   var $sql_query, $number_of_rows, $current_page_number, $number_of_pages, $number_of_rows_per_page, $page_name;
 
   /* class constructor */
-  function splitPageResults($query, $max_rows, $count_key = '*', $page_holder = 'page', $debug = false, $countQuery = "") {
+  function splitPageResultsNew($query, $max_rows, $count_key = '*', $page_holder = 'page', $debug = false, $countQuery = "") {
     global $db;
     $max_rows = ($max_rows == '' || $max_rows == 0) ? 20 : $max_rows;
 
