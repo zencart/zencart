122d2138df0d566fdef76899927ff3035b0b0c16
ajeh <ajeh@zen-cart.com>
2012-10-21 00:12:37 -0400

    CHANGE-373 - Show message about customer basket when logging in and nothing was in the cart to start with Fix header date

--- includes/modules/pages/login/header_php.php
+++ includes/modules/pages/login/header_php.php
@@ -3,7 +3,7 @@
  * Login Page
  *
  * @package page
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2012 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: header_php.php 18695 2011-05-04 05:24:19Z drbyte $
