d633ca65adefe05741586317668385bc99f6be96
Erik <info@zen4all.nl>
2012-12-12 10:57:25 +0100

    Update includes/init_includes/init_sessions.php
    Spell error

--- includes/init_includes/init_sessions.php
+++ includes/init_includes/init_sessions.php
@@ -54,7 +54,7 @@ if (isset($_POST[zen_session_name()])) {
   zen_session_id($_GET[zen_session_name()]);
 }
 /**
- * need to tidy up $_SERVER['REMOTE_ADDR'] here beofre we use it any where else
+ * need to tidy up $_SERVER['REMOTE_ADDR'] here before we use it any where else
  * one problem we don't address here is if $_SERVER['REMOTE_ADDRESS'] is not set to anything at all
  */
 $ipAddressArray = explode(',', $_SERVER['REMOTE_ADDR']);
