29f970b092b1f669044acf70acb1ecaa40cb50f5
DrByte <drbyte@zen-cart.com>
2013-01-08 16:57:30 -0500

    CHANGE-398 - Debug log alert for /logs and /cache

--- admin/includes/debug_logs_checker.php
+++ admin/includes/debug_logs_checker.php
@@ -5,7 +5,7 @@
  * checks for debug logs in /logs/ and /cache/ folders
  *
  * @package admin
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: debug_logs_checker.php ver 1.51 by Linda McGrath 2012-01-19
