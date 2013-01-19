bd0234bff3a81e7a8f8778a8cb43998679926121
Ian Wilson <ian@zen-cart.com>
2013-01-19 11:40:41 +0000

    Dashboard Widgets for 1.6 Admin Home Page (including ajax infrastructure)
    Fix test for ajax in init_languages

--- admin/includes/init_includes/init_languages.php
+++ admin/includes/init_includes/init_languages.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Fri Jul 6 11:57:44 2012 -0400 Modified in v1.5.1 $
@@ -46,7 +46,7 @@ if (!defined('IS_ADMIN_FLAG')) {
     $current_page = basename($PHP_SELF);
   }
   
-  if ($ajax = TRUE)
+  if ($ajax == TRUE)
   {
     include(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_DEFAULT . '.php');
   }
