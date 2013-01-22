1e3882633257d899748c8ba66fef7377edd01e22
DrByte <drbyte@zen-cart.com>
2013-01-22 03:36:04 -0500

    CHANGE-432 - PHP 5.4 causing warnings in debug logs

--- includes/modules/payment/authorizenet_aim.php
+++ includes/modules/payment/authorizenet_aim.php
@@ -438,6 +438,7 @@ class authorizenet_aim extends base {
   function admin_notification($zf_order_id) {
     global $db;
     $output = '';
+    $aimdata = new stdClass;
     $aimdata->fields = array();
     require(DIR_FS_CATALOG . DIR_WS_MODULES . 'payment/authorizenet/authorizenet_admin_notification.php');
     return $output;
