182ea487ae2b2599da2e6664418182448711b29f
DrByte <drbyte@zen-cart.com>
2013-03-17 17:11:18 -0400

    CHANGE-470 - Fixed - /admin/orders.php missing closing table row

--- admin/orders.php
+++ admin/orders.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: orders.php 19530 2011-09-19 13:52:37Z ajeh $
@@ -437,6 +437,7 @@ function couponpopupWindow(url) {
             </td>
           </form>
 
+          </tr>
         </table></td>
       </tr>
 <!-- search -->
