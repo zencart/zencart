4e4b58d170e160cd03aaf886ba7e1883645a4bc8
DrByte <drbyte@zen-cart.com>
2013-03-20 16:46:29 -0400

    ADMINISTRATIVE - Remove trailing blank line after closing ?> tag on some older language files, since it sometimes causes problems with weird FTP setups

--- includes/languages/english/account_password.php
+++ includes/languages/english/account_password.php
@@ -1,24 +1,12 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
-// |                                                                      |   
-// | http://www.zen-cart.com/index.php                                    |   
-// |                                                                      |   
-// | Portions Copyright (c) 2003 osCommerce                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-// $Id: account_password.php 1969 2005-09-13 06:57:21Z drbyte $
-//
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: account_password.php 1969 2005-09-13 06:57:21Z drbyte $
+ */
+
 
 define('NAVBAR_TITLE_1', 'My Account');
 define('NAVBAR_TITLE_2', 'Change Password');
@@ -29,4 +17,3 @@ define('MY_PASSWORD_TITLE', 'My Password');
 
 define('SUCCESS_PASSWORD_UPDATED', 'Your password has been successfully updated.');
 define('ERROR_CURRENT_PASSWORD_NOT_MATCHING', 'Your Current Password did not match the password in our records. Please try again.');
-?>
