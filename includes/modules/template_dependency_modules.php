333613eed35a934f22fe9c57fc5b8080e090835a
DrByte <drbyte@zen-cart.com>
2013-05-31 16:34:12 -0400

    CHANGE-503 - Move call to "modules" files from top of html_header.php file to /index.php, thus keeping template more clean

---
+++ includes/modules/template_dependency_modules.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Load common dependencies required by templates
+ *
+ * @package initSystem
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version GIT: $Id: Author: DrByte  Fri Jul 6 11:57:44 2012 -0400 Modified in v1.5.1 $
+ */
+// must be called appropriately
+if (!defined('IS_ADMIN_FLAG')) {
+  die('Illegal Access');
+}
+
+require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
--- includes/templates/template_default/common/html_header.php
+++ includes/templates/template_default/common/html_header.php
@@ -5,15 +5,11 @@
  * outputs the html header. i,e, everything that comes before the \</head\> tag <br />
  *
  * @package templateSystem
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Tue Jul 17 16:02:00 2012 -0400 Modified in v1.5.1 $
  */
-/**
- * load the module for generating page meta-tags
- */
-require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 /**
  * output main page HEAD tag and related headers/meta-tags, etc
  */
