56915d63e382a5c03be2cd3b64d903000d915b35
DrByte <drbyte@zen-cart.com>
2013-06-10 15:35:55 -0400

    CHANGE-509 - Fix minor incorrect variable declaration in option_values_manager.php

--- admin/options_values_manager.php
+++ admin/options_values_manager.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Sat Jul 21 17:10:54 2012 -0400 Modified in v1.5.1 $
@@ -823,7 +823,7 @@ function go_option() {
       }
 
       $inputs = '';
-      $inputs = '';
+      $inputs2 = '';
       for ($i = 0, $n = sizeof($languages); $i < $n; $i ++) {
         $inputs .= $languages[$i]['code'] . ':&nbsp;<input type="text" name="value_name[' . $languages[$i]['id'] . ']" ' . zen_set_field_length(TABLE_PRODUCTS_OPTIONS_VALUES, 'products_options_values_name', 50) . '>&nbsp;<br />';
       }
