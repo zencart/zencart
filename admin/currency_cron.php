c4169e460de3aec87656f353eea89a881bfe07c6
DrByte <drbyte@zen-cart.com>
2013-06-20 16:08:23 -0400

    CHANGE-3 - Small update to previous commit for currency cron job support.

--- admin/currency_cron.php
+++ admin/currency_cron.php
@@ -6,13 +6,17 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: currency_cron.php $
  */
+if (PHP_SAPI != 'cli' && isset($_SERVER["REMOTE_ADDR"]) && ($_SERVER["REMOTE_ADDR"] != $_SERVER["SERVER_ADDR"])){
+  echo ' ERROR: Permission denied.';
+  exit(1);
+};
 
 // define('STRICT_ERROR_REPORTING', TRUE); // commented out for normal use
 // define('DEBUG_AUTOLOAD', TRUE);         // commented out for normal use
 
 define('IS_CLI', 'VERBOSE'); // options: VERBOSE will cause it to output informational messages. Anything else will suppress status messages other than caught errors.
 
-chdir( dirname ( realpath ( __FILE__ ) ) );
+chdir( __DIR__ );
 $loaderPrefix = 'currency_cron';
 $_SERVER['REMOTE_ADDR'] = 'cron';
 $result = require('includes/application_top.php');
@@ -21,9 +25,12 @@ $_SERVER['HTTP_USER_AGENT'] = 'Zen Cart update';
 // $_SERVER['REMOTE_ADDR'] = DB_SERVER;
 if (function_exists('zen_update_currencies'))
 {
+  if (PHP_SAPI != 'cli') echo '<br><pre>';
   if (IS_CLI == 'VERBOSE') echo 'Updating currencies... ' . "\n";
   zen_update_currencies(IS_CLI == 'VERBOSE');
   if (IS_CLI == 'VERBOSE') echo 'Done.' . "\n\n";
+  exit(0);
 } else {
-  die("Error: Function not found: zen_update_currencies().\nMake sure you have placed the currency_cron.php file in your (renamed) Admin folder.\n\n");
+  echo "Error: Function not found: zen_update_currencies().\nMake sure you have placed the currency_cron.php file in your (renamed) Admin folder.\n\n";
+  exit(1);
 }
