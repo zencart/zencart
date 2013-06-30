98d2176f9d93b8e0b2b918587a31e32617487208
ajeh <ajeh@zen-cart.com>
2013-06-30 13:21:37 -0400

    CHANGE-486: Gift Certificate should not allow purchase of Gift Certificates Fix header

--- admin/specials.php
+++ admin/specials.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: specials.php 19294 2011-07-28 18:15:46Z drbyte $
