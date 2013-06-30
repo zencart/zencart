037a1e2d2a4603a3a382c428b406780661e00bf9
ajeh <ajeh@zen-cart.com>
2013-06-29 22:37:25 -0400

    CHANGE-483 - Orders tables need weight Fix header

--- includes/classes/shipping.php
+++ includes/classes/shipping.php
@@ -3,7 +3,7 @@
  * shipping class
  *
  * @package classes
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Thu Aug 2 11:37:22 2012 -0400 Modified in v1.5.1 $
