1fc2c10560277cd665abd20231ece1a38ae44d9b
Ian Wilson <ian@zen-cart.com>
2013-07-12 13:36:55 +0100

    CHANGE-235 create_account_success doesn't honor session timeout
    remove page from navigation history, and forward user to timeout page, if necessary
    thanks to lat9

--- includes/modules/pages/create_account_success/header_php.php
+++ includes/modules/pages/create_account_success/header_php.php
@@ -16,6 +16,14 @@ require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
 $breadcrumb->add(NAVBAR_TITLE_1);
 $breadcrumb->add(NAVBAR_TITLE_2);
 
+//-BOF-lat9
+// Remove this page from the navigation history and if the customer returns to this page after time-out, redirect them to the time_out page
+$_SESSION['navigation']->remove_current_page();
+if (!$_SESSION['customer_id']) {
+  zen_redirect(zen_href_link(FILENAME_TIME_OUT, '', 'NONSSL'));
+}
+//-EOF-lat9
+
 if (sizeof($_SESSION['navigation']->snapshot) > 0) {
   $origin_href = zen_href_link($_SESSION['navigation']->snapshot['page'], zen_array_to_string($_SESSION['navigation']->snapshot['get'], array(zen_session_name())), $_SESSION['navigation']->snapshot['mode']);
   $_SESSION['navigation']->clear_snapshot();
