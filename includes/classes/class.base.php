0fdc68d9c6947371f032e3457f7513f04b1e1c80
DrByte <drbyte@zen-cart.com>
2013-07-17 01:29:34 -0400

    CHANGE-543 - Updates for PHP 5.5 Compatibility

--- includes/classes/class.base.php
+++ includes/classes/class.base.php
@@ -108,6 +108,6 @@ class base {
     {
       $rawName[0] = strtoupper($rawName[0]);
     }
-    return preg_replace('/[_-]([0-9,a-z])/e', "strtoupper('\\1')", $rawName);
+    return preg_replace_callback('/[_-]([0-9,a-z])/', create_function('$matches', 'return strtoupper($matches[1]);'), $rawName);
   }
 }
