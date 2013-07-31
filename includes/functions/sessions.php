ed960a62e3cd6de810b0c8d73b5cd661db75084d
Ian Wilson <ian@zen-cart.com>
2013-07-31 19:44:32 +0100

    Oops - I thought I had fixed this before the merge

--- includes/functions/sessions.php
+++ includes/functions/sessions.php
@@ -4,11 +4,7 @@
  * Session functions
  *
  * @package functions
-<<<<<<< HEAD
- * @copyright Copyright 2003-2011 Zen Cart Development Team
-=======
  * @copyright Copyright 2003-2013 Zen Cart Development Team
->>>>>>> 3bb09e7... CHANGE-549  PHP Notice: Object of class queryFactoryResult could not be converted to int
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id:
