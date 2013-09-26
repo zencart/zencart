2c66d4348022f6612969e5b3d22b702936c48d2f
DrByte <drbyte@zen-cart.com>
2013-09-26 16:34:25 -0400

    CHANGE-564 - Installer version stamping for 1.5.2

--- zc_install/version.php
+++ zc_install/version.php
@@ -23,4 +23,4 @@ define('PROJECT_VERSION_PATCH1_SOURCE', '');
 define('PROJECT_VERSION_PATCH2_SOURCE', '');
 define('NEW_VERSION_CHECKUP_URL','http://ping.zen-cart.com/version_id.txt');
 define('EXPECTED_DATABASE_VERSION_MAJOR', '1');
-define('EXPECTED_DATABASE_VERSION_MINOR', '5.0');
+define('EXPECTED_DATABASE_VERSION_MINOR', '5.2');
