99c73377b59c728385c8a496b33781cd8fff0eb7
DrByte <drbyte@zen-cart.com>
2013-10-30 18:45:38 -0400

    ISSUE-24 - Change in LOGIN_EXPIRY_NOTICE to reflect actual timeout setting for admin (SESSION_TIMEOUT_ADMIN).

--- admin/includes/languages/english/login.php
+++ admin/includes/languages/english/login.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2012 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Mon Jul 16 14:10:24 2012 -0400 Modified in v1.5.1 $
