329816beded34489fffff740a81dd999d5df791b
DrByte <drbyte@zen-cart.com>
2013-10-30 18:44:15 -0400

    CHANGE-616 For consistency and PHP 5.4 compatibility $_SESSION['shipping'] should always be treated as an array

--- includes/modules/pages/checkout_shipping_address/header_php.php
+++ includes/modules/pages/checkout_shipping_address/header_php.php
@@ -3,7 +3,7 @@
  * Page to let customer change their shipping address(ship to)
  *
  * @package page
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: header_php.php 4793 2006-10-20 05:25:20Z ajeh $
@@ -56,4 +56,3 @@ $addresses_count = zen_count_customer_address_book_entries();
 
 // This should be last line of the script:
 $zco_notifier->notify('NOTIFY_HEADER_END_CHECKOUT_SHIPPING_ADDRESS');
-?>
