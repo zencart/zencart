8e37cca76fa844a162e088553e4ad01016011c1b
DrByte <drbyte@zen-cart.com>
2013-11-02 01:21:28 -0400

    CHANGE-491 - Timezone patch -- consistency with 1.6.0

--- admin/includes/extra_configures/use_catalog_time_zone.php
+++ admin/includes/extra_configures/use_catalog_time_zone.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * @package patches
+ * @package admin
  * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id:  $
  */
-include (DIR_FS_CATALOG . 'includes/extra_configures/datetime_patch.php');
+// get time zone settings from catalog-side file
+include (DIR_FS_CATALOG . '/includes/extra_configures/set_time_zone.php');
--- includes/extra_configures/set_time_zone.php
+++ includes/extra_configures/set_time_zone.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package patches
+ * @package initSystem
  * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id:  $
@@ -19,7 +19,7 @@ if (version_compare(PHP_VERSION, 5.3, '>='))
    * MAKE NO CHANGES BELOW THIS LINE
    *
    * The following will take the timezone you specified above and apply it in your store.
-   * If you didn't specify one, it will try to use the datetime value from your PHP configuration
+   * If you didn't specify one, it will try to use the setting from your server's PHP configuration
    */
   if ($TZ == '') {
     $TZ = date_default_timezone_get();
