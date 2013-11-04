5e27d9b5c304ccc9f2d02874093b11b001a7e400
DrByte <drbyte@zen-cart.com>
2013-11-04 14:07:53 -0500

    CHANGE-285 - Change template_default to use the new css-js generator module, which can be extended via observers instead of hacking up html_output

---
+++ includes/modules/tpl_css_js_generator.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ * template css_js_generator module
+ *
+ * @package modules
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id$
+ */
+if (!defined('IS_ADMIN_FLAG')) {
+  die('Illegal Access');
+}
+// init the arrays, if not already set before this script fired or by the above notifier hook.
+foreach(array('stylesheets', 'jsfiles', 'jsfilesTop', 'jscripts', 'jscriptsTop', 'printsheets', 'inline_css', 'deduplicate') as $val) {
+  if (!isset($$val)) $$val = array();
+}
+if (isset($template_framework)) $template_framework = '';
+$bodyClasses = '';
+
+$zco_notifier->notify('NOTIFY_MODULE_START_CSS_JS_GENERATOR', array(), $css_js_handler, $use_default_handler, $bodyClasses);
+
+
+$manufacturers_id = (isset($_GET['manufacturers_id'])) ? $_GET['manufacturers_id'] : '';
+$tmp_products_id = (isset($_GET['products_id'])) ? (int)$_GET['products_id'] : '';
+$tmp_pagename = ($this_is_home_page) ? 'index_home' : $current_page_base;
+if ($current_page_base == 'page' && isset($ezpage_id)) $tmp_pagename = $current_page_base . (int)$ezpage_id;
+
+/** Built a list of classes to be assigned to the BODY tag for targeted styling */
+$bodyClasses .= ' ' . $_SESSION['language'];
+if ($tmp_pagename != '')        $bodyClasses .= ' ' . $tmp_pagename;
+if ($tmp_products_id != '')     $bodyClasses .= ' p' . $tmp_products_id;
+if ($manufacturers_id != '')    $bodyClasses .= ' m' . $manufacturers_id;
+if ($current_category_id != '') $bodyClasses .= ' c' . $current_category_id;
+if ($cPath != '')               $bodyClasses .= ' cPath' . $cPath;
+$zco_notifier->notify('NOTIFY_ADD_BODY_CLASSES', array(), $bodyClasses);
+
+
+
+
+// Now build all the CSS/JS components  -- or skip if overridden by an alternate handler
+if (!isset($use_default_handler) || (isset($use_default_handler) && $use_default_handler != FALSE) ) {
+  /**
+   * load the framework-specific stylesheet files
+   *
+   * To customize loading for a specific CSS framework, use an observer class watching NOTIFY_MODULE_CSS_JS_GENERATOR_FRAMEWORK_SELECT,
+   * and populate $stylesheets with the correct files for your framework.
+   *
+   * The intention here is to allow for specifying which stylesheets need to be loaded before everything else, ie: setting a dependency order
+   *
+   * The $template_framework variable is expected to be set in your template's template_info.php file, and is used by observer classes to help them make decisions on whether to load these dependencies.
+   */
+  $zco_notifier->notify('NOTIFY_MODULE_CSS_JS_GENERATOR_FRAMEWORK_SELECT', $css_js_handler, $template_framework, $stylesheets);
+
+  // prepare array for later deduplicating of stylesheet names
+  foreach($stylesheets as $val) {
+    if (preg_match('~.*/([^/]*\.css)$~', $val, $matches)) {
+      $deduplicate[] = $matches[1];
+    }
+  }
+
+  /**
+   * load all template-specific stylesheets, named as "stylesheet.css" and "stylesheet-responsive.css" and "font.css"
+   */
+  $sheets_array_primary = array(
+          '/' . 'stylesheet',
+          '/' . 'stylesheet-responsive',
+          '/' . 'font',
+  );
+  while(list ($key, $value) = each($sheets_array_primary)) {
+    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . $value . '.css';
+    if (file_exists($perpagefile)) {
+      $stylesheets[] = $perpagefile;
+      $deduplicate[] = ltrim($value . '.css', '/');
+    }
+  }
+
+  /**
+   * load all remaining template-specific stylesheets, alphabetically
+   * filter out duplicates
+   */
+  $directory_array = $template->get_template_part($template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css'), '/^[^(inline|print)]/', '.css');
+  sort($directory_array);
+  $directory_array  = array_diff($directory_array, $deduplicate);
+  while(list ($key, $value) = each($directory_array)) {
+    $stylesheets[] = $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . $value;
+  }
+
+  /**
+   * load 'inline' stylesheet content, named like "inline*.css", alphabetically
+   */
+  $directory_array = $template->get_template_part($template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css'), '/^inline/', '.css');
+  sort($directory_array);
+  while(list ($key, $value) = each($directory_array)) {
+    $inline_css[] = $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . $value;
+  }
+  $sheets_array = array('/inline_' . $_SESSION['language'] . '_stylesheet',
+          '/inline_' . $tmp_pagename,
+          '/inline_' . $_SESSION['language'] . '_' . $tmp_pagename,
+          '/inline_c_' . $cPath,
+          '/inline_' . $_SESSION['language'] . '_c_' . $cPath,
+          '/inline_m_' . $manufacturers_id,
+          '/inline_' . $_SESSION['language'] . '_m_' . (int)$manufacturers_id,
+          '/inline_p_' . $tmp_products_id,
+          '/inline_' . $_SESSION['language'] . '_p_' . $tmp_products_id,
+  );
+
+  $sheets_array = array_intersect($sheets_array_primary, $sheets_array);
+  while(list ($key, $value) = each($sheets_array)) {
+    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . $value . '.css';
+    if (file_exists($perpagefile)) $inline_css[] = $perpagefile;
+  }
+
+
+  /**
+   * load printer-friendly stylesheets -- named like "print*.css", alphabetically
+   */
+  $directory_array = $template->get_template_part($template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css'), '/^print/', '.css');
+  sort($directory_array);
+  while(list ($key, $value) = each($directory_array)) {
+    $printsheets[] = $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . $value;
+  }
+
+  /**
+   * load all site-wide jscript_*.php files from includes/templates/YOURTEMPLATE/jscript, alphabetically
+   */
+  $directory_array = $template->get_template_part($template->get_template_dir('.php',DIR_WS_TEMPLATE, $current_page_base,'jscript'), '/^jscript_/', '.php');
+  sort($directory_array);
+  while(list ($key, $value) = each($directory_array)) {
+    /**
+     * include content from all site-wide jscript_*.php files from includes/templates/YOURTEMPLATE/jscript, alphabetically.
+     * These .PHP files can be manipulated by PHP when they're called, and are copied in-full to the browser page
+     */
+    if (preg_match('~^.*_top.php$~', $value)) {
+      $jscriptsTop[] = $template->get_template_dir('.php',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value;
+    } else {
+      $jscripts[] = $template->get_template_dir('.php',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value;
+    }
+  }
+
+  /**
+   * include content from all page-specific jscript_*.php files from includes/modules/pages/PAGENAME, alphabetically.
+   */
+  $directory_array = $template->get_template_part($page_directory, '/^jscript_/');
+  sort($directory_array);
+  while(list ($key, $value) = each($directory_array)) {
+    /**
+     * include content from all page-specific jscript_*.php files from includes/modules/pages/PAGENAME, alphabetically.
+     * These .PHP files can be manipulated by PHP when they're called, and are copied in-full to the browser page
+     */
+    if (preg_match('~.*_top.php$~', $value)) {
+      $jscriptsTop[] = $page_directory . '/' . $value;
+    } else {
+      $jscripts[] = $page_directory . '/' . $value;
+    }
+  }
+
+  /**
+   * load all site-wide jscript_*.js files from includes/templates/YOURTEMPLATE/jscript, alphabetically
+   */
+  $directory_array = $template->get_template_part($template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript'), '/.*/', '.js');
+  sort($directory_array);
+  while(list ($key, $value) = each($directory_array)) {
+    if (preg_match('~^.*_top.js$~', $value)) {
+      $jsfilesTop[] = $template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value;
+    } else {
+      $jsfiles[] = $template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value;
+    }
+  }
+
+  /**
+   * load all page-specific jscript_*.js files from includes/modules/pages/PAGENAME, alphabetically
+   */
+  $directory_array = $template->get_template_part($page_directory, '/^.*/', '.js');
+  sort($directory_array);
+  while(list ($key, $value) = each($directory_array)) {
+    if (preg_match('~^.*_top.js$~', $value)) {
+      $jsfilesTop[] = $page_directory . '/' . $value;
+    } else {
+      $jsfiles[] = $page_directory . '/' . $value;
+    }
+  }
+
+// DEBUG
+// die('<pre>' . print_r($stylesheets, true) . print_r($inline_css, true). print_r($printsheets, true) . print_r($jsfilesTop, true) . print_r($jsfiles, true) . print_r($jscriptsTop, true) . print_r($jscripts, true));
+
+
+
+/**
+ * Now assemble stylesheets and linked scripts to prepare the default output HTML
+ * NOTE: the arrays are still available in case the template designer wants to do something different in the template
+ *       or in case an observer class is preferred and wants to override the generated content of $template_css_output and $template_js_output
+ *       ie: someone might want to hook the starting and ending notifiers for this page and use them to periodically cache/recache the generated content
+ * For those who want to minify/combine files, see the comments next to the notifier hook at the bottom of this file, below.
+ */
+$zco_notifier->notify('NOTIFY_MODULE_BUILD_STRINGS_IN_CSS_JS_GENERATOR', $css_js_handler, $stylesheets, $inline_css, $printsheets, $jsfilesTop, $jsfiles, $jscriptsTop, $jscripts);
+foreach ($stylesheets as $val) {
+  $template_css_output .= '    <link rel="stylesheet" type="text/css" href="' . $val . '" id="' . str_replace('.', '-', basename($val)) . '">'."\n";
+}
+// process "inline" as the "last loaded" ... in most cases this is just as efficient as actually loading "inline" ... and doing it this way allows for minifier/compiler scripts to override.
+foreach ($inline_css as $val) {
+  $template_css_output .= '    <link rel="stylesheet" type="text/css" href="' . $val . '" id="' . str_replace('.', '-', basename($val)) . '" />'."\n";
+}
+foreach ($printsheets as $val) {
+  $template_css_output .= '    <link rel="stylesheet" type="text/css" media="print" href="' . $val . '" id="' . str_replace('.', '-', basename($val)) . '">'."\n";
+}
+//js
+foreach ($jsfilesTop as $val) {
+  $template_js_output_top .= '    <script src="' . $val . '"></script>' . "\n";
+}
+foreach ($jsfiles as $val) {
+  $template_js_output_bottom .= '    <script src="' . $val . '"></script>' . "\n";
+}
+// NOTE: the $jscripts and $jscriptsTop arrays can't be processed into a string variable, since they use "require" statements which cause execution of other scripts.
+// Therefore the arrays are simply passed back to the template so it can loop thru the arrays to do the needed execution at the appropriate time.
+
+
+} else {
+  require(DIR_WS_MODULES . zen_get_module_directory($css_js_handler));
+}
+
+/**
+ * To minify or combine stylesheets and/or js files, hook the following notifier using an observer class and use the above arrays to feed your minify/combine script.
+ */
+// This should be last line of the script:
+$zco_notifier->notify('NOTIFY_MODULE_END_CSS_JS_GENERATOR');
--- includes/templates/template_default/common/html_header.php
+++ includes/templates/template_default/common/html_header.php
@@ -30,107 +30,23 @@
     <meta name="robots" content="noindex, nofollow"<?php if ($isRobotsMaintenanceMode) echo ' mode="maintenance"'; ?>>
 <?php } ?>
 
-<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER . DIR_WS_HTTPS_CATALOG : HTTP_SERVER . DIR_WS_CATALOG ); ?>" />
+    <base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER . DIR_WS_HTTPS_CATALOG : HTTP_SERVER . DIR_WS_CATALOG ); ?>">
 <?php if (isset($canonicalLink) && $canonicalLink != '') { ?>
-<link rel="canonical" href="<?php echo $canonicalLink; ?>" />
+    <link rel="canonical" href="<?php echo $canonicalLink; ?>">
 <?php } ?>
 
 <?php
-/**
- * load all template-specific stylesheets, named like "style*.css", alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css'), '/^style/', '.css');
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<link rel="stylesheet" type="text/css" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . $value . '" />'."\n";
-  }
-/**
- * load stylesheets on a per-page/per-language/per-product/per-manufacturer/per-category basis. Concept by Juxi Zoza.
- */
-  $manufacturers_id = (isset($_GET['manufacturers_id'])) ? $_GET['manufacturers_id'] : '';
-  $tmp_products_id = (isset($_GET['products_id'])) ? (int)$_GET['products_id'] : '';
-  $tmp_pagename = ($this_is_home_page) ? 'index_home' : $current_page_base;
-  if ($current_page_base == 'page' && isset($ezpage_id)) $tmp_pagename = $current_page_base . (int)$ezpage_id;
-  $sheets_array = array('/' . $_SESSION['language'] . '_stylesheet',
-                        '/' . $tmp_pagename,
-                        '/' . $_SESSION['language'] . '_' . $tmp_pagename,
-                        '/c_' . $cPath,
-                        '/' . $_SESSION['language'] . '_c_' . $cPath,
-                        '/m_' . $manufacturers_id,
-                        '/' . $_SESSION['language'] . '_m_' . (int)$manufacturers_id,
-                        '/p_' . $tmp_products_id,
-                        '/' . $_SESSION['language'] . '_p_' . $tmp_products_id
-                        );
-  while(list ($key, $value) = each($sheets_array)) {
-    //echo "<!--looking for: $value-->\n";
-    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . $value . '.css';
-    if (file_exists($perpagefile)) echo '<link rel="stylesheet" type="text/css" href="' . $perpagefile .'" />'."\n";
+  // output assembled stylesheet links (see modules/tpl_css_js_generator.php)
+  echo $template_css_output;
+  echo "\n";
+
+  // run dynamically-generated jscript_XXXXX.php files
+  foreach ($jscriptsTop as $val) {
+    require($page_directory . '/' . $val); echo "\n";
   }
 
-/**
- *  custom category handling for a parent and all its children ... works for any c_XX_XX_children.css  where XX_XX is any parent category
- */
-  $tmp_cats = explode('_', $cPath);
-  $value = '';
-  foreach($tmp_cats as $val) {
-    $value .= $val;
-    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . '/c_' . $value . '_children.css';
-    if (file_exists($perpagefile)) echo '<link rel="stylesheet" type="text/css" href="' . $perpagefile .'" />'."\n";
-    $perpagefile = $template->get_template_dir('.css', DIR_WS_TEMPLATE, $current_page_base, 'css') . '/' . $_SESSION['language'] . '_c_' . $value . '_children.css';
-    if (file_exists($perpagefile)) echo '<link rel="stylesheet" type="text/css" href="' . $perpagefile .'" />'."\n";
-    $value .= '_';
-  }
-
-/**
- * load printer-friendly stylesheets -- named like "print*.css", alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css'), '/^print/', '.css');
-  sort($directory_array);
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<link rel="stylesheet" type="text/css" media="print" href="' . $template->get_template_dir('.css',DIR_WS_TEMPLATE, $current_page_base,'css') . '/' . $value . '" />'."\n";
-  }
-
-/**
- * load all site-wide jscript_*.js files from includes/templates/YOURTEMPLATE/jscript, alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript'), '/^jscript_/', '.js');
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<script type="text/javascript" src="' .  $template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value . '"></script>'."\n";
-  }
-
-/**
- * load all page-specific jscript_*.js files from includes/modules/pages/PAGENAME, alphabetically
- */
-  $directory_array = $template->get_template_part($page_directory, '/^jscript_/', '.js');
-  while(list ($key, $value) = each($directory_array)) {
-    echo '<script type="text/javascript" src="' . $page_directory . '/' . $value . '"></script>' . "\n";
-  }
-
-/**
- * load all site-wide jscript_*.php files from includes/templates/YOURTEMPLATE/jscript, alphabetically
- */
-  $directory_array = $template->get_template_part($template->get_template_dir('.php',DIR_WS_TEMPLATE, $current_page_base,'jscript'), '/^jscript_/', '.php');
-  while(list ($key, $value) = each($directory_array)) {
-/**
- * include content from all site-wide jscript_*.php files from includes/templates/YOURTEMPLATE/jscript, alphabetically.
- * These .PHP files can be manipulated by PHP when they're called, and are copied in-full to the browser page
- */
-    require($template->get_template_dir('.php',DIR_WS_TEMPLATE, $current_page_base,'jscript') . '/' . $value); echo "\n";
-  }
-/**
- * include content from all page-specific jscript_*.php files from includes/modules/pages/PAGENAME, alphabetically.
- */
-  $directory_array = $template->get_template_part($page_directory, '/^jscript_/');
-  while(list ($key, $value) = each($directory_array)) {
-/**
- * include content from all page-specific jscript_*.php files from includes/modules/pages/PAGENAME, alphabetically.
- * These .PHP files can be manipulated by PHP when they're called, and are copied in-full to the browser page
- */
-    require($page_directory . '/' . $value); echo "\n";
-  }
-
-// DEBUG: echo '<!-- I SEE cat: ' . $current_category_id . ' || vs cpath: ' . $cPath . ' || page: ' . $current_page . ' || template: ' . $current_template . ' || main = ' . ($this_is_home_page ? 'YES' : 'NO') . ' -->';
-
-
+  // output src links to scripts
+  echo $template_js_output_top;
 ?>
     <!--[if lt IE 9]>
     <meta http-equiv="imagetoolbar" content="no">
@@ -140,7 +56,9 @@
   require($template->get_template_dir('tpl_favicon.php',DIR_WS_TEMPLATE, $current_page_base,'common'). '/tpl_favicon.php');
 ?>
 
-</head>
-<?php // NOTE: Blank line following is intended: ?>
+  </head>
 
+<?php
+  // DEBUG: echo '<!-- I SEE cat: ' . $current_category_id . ' || vs cpath: ' . $cPath . ' || page: ' . $current_page . ' || template: ' . $current_template . ' || main = ' . ($this_is_home_page ? 'YES' : 'NO') . ' -->';
+?>
 
