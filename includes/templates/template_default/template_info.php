343fc9dddc279d469bd352a9b47b5b6848faaede
DrByte <drbyte@zen-cart.com>
2013-11-04 15:19:10 -0500

    CHANGE-285 - Add $template_framework flag to the template_info.php file

--- includes/templates/template_default/template_info.php
+++ includes/templates/template_default/template_info.php
@@ -3,7 +3,7 @@
  * Template Information File
  *
  * @package templateSystem
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
@@ -13,4 +13,5 @@ $template_version = 'Version 1.0';
 $template_author = 'Zen Cart Team (c) 2003';
 $template_description = 'This template set is designed to be easily modified using only the style sheet to change colors, fonts, and the store logo. Three images are required; logo.jpg, header_bg.jpg, and tile_back.jpg.';
 $template_screenshot = 'scr_template_default.jpg';
-?>
+$designed_for_zencart_version = '1.6.0';
+$template_framework = 'None';
