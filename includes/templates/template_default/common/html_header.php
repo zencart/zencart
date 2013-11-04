ca3774c18744758be7499a8ec231fcd3d12c501e
DrByte <drbyte@zen-cart.com>
2013-11-04 14:02:52 -0500

    CHANGE-285 - Change template_default doctype to HTML5

--- includes/templates/template_default/common/html_header.php
+++ includes/templates/template_default/common/html_header.php
@@ -2,7 +2,7 @@
 /**
  * Common Template
  *
- * outputs the html header. i,e, everything that comes before the \</head\> tag <br />
+ * outputs the html header, eg the doctype and the entire [HEAD] section
  *
  * @package templateSystem
  * @copyright Copyright 2003-2013 Zen Cart Development Team
@@ -10,22 +10,24 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Tue Jul 17 16:02:00 2012 -0400 Modified in v1.5.1 $
  */
+
 /**
- * output main page HEAD tag and related headers/meta-tags, etc
+ * output main page HEAD tag and related headers etc
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php echo HTML_PARAMS; ?>>
-<head>
-<title><?php echo META_TAG_TITLE; ?></title>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>" />
-<meta name="keywords" content="<?php echo META_TAG_KEYWORDS; ?>" />
-<meta name="description" content="<?php echo META_TAG_DESCRIPTION; ?>" />
-<meta http-equiv="imagetoolbar" content="no" />
-<meta name="author" content="The Zen Cart&reg; Team and others" />
-<meta name="generator" content="shopping cart program by Zen Cart&reg;, http://www.zen-cart.com eCommerce" />
-<?php if (defined('ROBOTS_PAGES_TO_SKIP') && in_array($current_page_base,explode(",",constant('ROBOTS_PAGES_TO_SKIP'))) || $current_page_base=='down_for_maintenance' || $robotsNoIndex === true) { ?>
-<meta name="robots" content="noindex, nofollow" />
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" <?php echo HTML_PARAMS; ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php echo HTML_PARAMS; ?>> <!--<![endif]-->
+  <head>
+    <meta charset="<?php echo CHARSET; ?>">
+    <title><?php echo META_TAG_TITLE; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="<?php echo STORE_NAME ?>">
+    <meta name="generator" content="shopping cart program by Zen Cart(R), http://www.zen-cart.com eCommerce software">
+    <meta name="keywords" content="<?php echo META_TAG_KEYWORDS; ?>">
+    <meta name="description" content="<?php echo META_TAG_DESCRIPTION; ?>">
+<?php if ($robotsNoIndex === true) { ?>
+    <meta name="robots" content="noindex, nofollow"<?php if ($isRobotsMaintenanceMode) echo ' mode="maintenance"'; ?>>
 <?php } ?>
 <?php if (defined('FAVICON')) { ?>
 <link rel="icon" href="<?php echo FAVICON; ?>" type="image/x-icon" />
@@ -136,4 +138,5 @@
 ?>
 </head>
 <?php // NOTE: Blank line following is intended: ?>
+
 
