f9b5d3d882829501853dd87d83d48d4936e761e4
DrByte <drbyte@zen-cart.com>
2013-11-06 22:54:11 -0500

    CHANGE-225 - Handle use of comma as decimal point for Gift Voucher

--- includes/modules/pages/gv_send/header_php.php
+++ includes/modules/pages/gv_send/header_php.php
@@ -6,7 +6,7 @@
  * They can send up to the amount of GV accumlated in their account by way of purchased GV's or GV's sent to them.
  *
  * @package page
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: header_php.php 18695 2011-05-04 05:24:19Z drbyte $
