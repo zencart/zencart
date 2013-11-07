cff86bbc1db194165adca044ab6b685bd4ebd351
DrByte <drbyte@zen-cart.com>
2013-11-07 01:13:22 -0500

    CHANGE-285 - Add language file updates to use font icons

--- includes/languages/english/advanced_search.php
+++ includes/languages/english/advanced_search.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package languageDefines
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: advanced_search.php 3253 2006-03-25 17:26:14Z birdbrain $
@@ -19,14 +19,15 @@
   define('ENTRY_CATEGORIES', 'Limit to Category:');
   define('ENTRY_INCLUDE_SUBCATEGORIES', 'Include Subcategories');
   define('ENTRY_MANUFACTURERS', 'Limit to Manufacturer');
-define('ENTRY_PRICE_RANGE', 'Search by Price Range');
+  define('ENTRY_PRICE_RANGE', 'Search by Price Range');
   define('ENTRY_PRICE_FROM', 'Price From:');
   define('ENTRY_PRICE_TO', 'Price To:');
-define('ENTRY_DATE_RANGE', 'Search by Date Added');
+  define('ENTRY_DATE_RANGE', 'Search by Date Added');
   define('ENTRY_DATE_FROM', 'Date From:');
   define('ENTRY_DATE_TO', 'Date To:');
 
-  define('TEXT_SEARCH_HELP_LINK', 'Search Help [?]');
+  define('TEXT_SEARCH_HELP_LINK', '<i class="icon-help-circled"></i>');
+  define('TEXT_SEARCH_HELP_ALT', 'Search Help');
 
   define('TEXT_ALL_CATEGORIES', 'All Categories');
   define('TEXT_ALL_MANUFACTURERS', 'All Manufacturers');
@@ -50,5 +51,3 @@ define('ENTRY_DATE_RANGE', 'Search by Date Added');
   define('ERROR_PRICE_TO_MUST_BE_NUM', 'Price To must be a number.');
   define('ERROR_PRICE_TO_LESS_THAN_PRICE_FROM', 'Price To must be greater than or equal to Price From.');
   define('ERROR_INVALID_KEYWORDS', 'Invalid keywords.');
-
-?>
--- includes/languages/english/shopping_cart.php
+++ includes/languages/english/shopping_cart.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * @package languageDefines
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: shopping_cart.php 3183 2006-03-14 07:58:59Z birdbrain $
  */
+
+define('TEXT_INFORMATION', 'You may proceed with your purchase by clicking the Checkout button below. Shipping and Taxes and Discounts will be handled on subsequent pages.');
 
 define('NAVBAR_TITLE', 'The Shopping Cart');
 define('HEADING_TITLE', 'Your Shopping Cart Contents');
@@ -26,6 +28,5 @@ define('TEXT_TOTAL_ITEMS', 'Total Items: ');
 define('TEXT_TOTAL_WEIGHT', '&nbsp;&nbsp;Weight: ');
 define('TEXT_TOTAL_AMOUNT', '&nbsp;&nbsp;Amount: ');
 
-define('TEXT_VISITORS_CART', '<a href="javascript:session_win();">[help (?)]</a>');
+define('TEXT_VISITORS_CART', '<a class="help" href="javascript:session_win();"><i class="icon-help-circled"></i></a>');
 define('TEXT_OPTION_DIVIDER', '&nbsp;-&nbsp;');
-?>
