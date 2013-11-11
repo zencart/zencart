2257dad1a48e38989717429edb60b0f33ceb81f9
ajeh <ajeh@zen-cart.com>
2013-11-11 13:51:00 -0500

    CHANGE-661 Increase size of Discount Coupon name

--- zc_install/sql/install/mysql_zencart.sql
+++ zc_install/sql/install/mysql_zencart.sql
@@ -513,7 +513,7 @@ DROP TABLE IF EXISTS coupons_description;
 CREATE TABLE coupons_description (
   coupon_id int(11) NOT NULL default '0',
   language_id int(11) NOT NULL default '0',
-  coupon_name varchar(32) NOT NULL default '',
+  coupon_name varchar(64) NOT NULL default '',
   coupon_description text,
   PRIMARY KEY (coupon_id,language_id)
 ) ENGINE=MyISAM;
