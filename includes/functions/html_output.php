b57d6a0172c3ce2e55a761bc879bb6cfda2d2fa6
DrByte <drbyte@zen-cart.com>
2014-03-13 00:02:45 -0400

    CHANGE-568 - (part 2) Add storeowner-definable session timeout limit

--- includes/functions/html_output.php
+++ includes/functions/html_output.php
@@ -4,7 +4,7 @@
  * HTML-generating functions used throughout the core
  *
  * @package functions
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: html_output.php 19355 2011-08-21 21:12:09Z drbyte $
