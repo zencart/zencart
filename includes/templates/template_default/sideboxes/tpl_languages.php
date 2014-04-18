e161842b093f8ce23660e21c9e7cc58d820d08e6
DrByte <drbyte@zen-cart.com>
2014-04-17 22:39:43 -0400

    ISSUE-84 - Fixed sidebox references to $GET[main_page] to $current_page_base instead, to accommodate implementation of Request Class

--- includes/templates/template_default/sideboxes/tpl_languages.php
+++ includes/templates/template_default/sideboxes/tpl_languages.php
@@ -3,7 +3,7 @@
  * Side Box Template
  *
  * @package templateSystem
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_languages.php 2982 2006-02-07 07:56:41Z birdbrain $
@@ -13,7 +13,7 @@
 
   $lng_cnt = 0;
   while (list($key, $value) = each($lng->catalog_languages)) {
-    $content .= '<a href="' . zen_href_link($_GET['main_page'], zen_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . zen_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a>&nbsp;&nbsp;';
+    $content .= '<a href="' . zen_href_link($current_page_base, zen_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . zen_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a>&nbsp;&nbsp;';
     $lng_cnt ++;
     if ($lng_cnt >= MAX_LANGUAGE_FLAGS_COLUMNS) {
       $lng_cnt = 0;
@@ -21,4 +21,3 @@
     }
   }
 $content .= '</div>';
-?>
