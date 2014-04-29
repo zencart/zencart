d0421c5a1921dbc56e24edcc7e1399c98f962c76
Ian Wilson <ian@zen-cart.com>
2014-04-29 13:52:11 +0100

    CI testing - remove for now

--- testFramework/webtests/zcCommonTestResources.php
+++ testFramework/webtests/zcCommonTestResources.php
@@ -27,39 +27,39 @@ class zcCommonTestResources extends Sauce\Sausage\WebDriverTestCase
   private $dbActive;
   private $dbLink;
   private $VATcreated = FALSE;
-  public static $browsers = array(
-          // run FF15 on Windows 8 on Sauce
-          array(
-                  'browserName' => 'firefox',
-                  'desiredCapabilities' => array(
-                          'version' => '15',
-                          'platform' => 'Windows 2012',
-                  )
-          ),
-          // run Chrome on Linux on Sauce
-          array(
-                  'browserName' => 'chrome',
-                  'desiredCapabilities' => array(
-                          'platform' => 'Linux'
-                  )
-          ),
-          // run Mobile Safari on iOS
-          //array(
-          //'browserName' => '',
-          //'desiredCapabilities' => array(
-          //'app' => 'safari',
-          //'device' => 'iPhone Simulator',
-          //'version' => '6.1',
-          //'platform' => 'Mac 10.8',
-          //)
-          //)//,
-          // run Chrome locally
-          //array(
-          //'browserName' => 'chrome',
-          //'local' => true,
-          //'sessionStrategy' => 'shared'
-          //)
-          );
+//   public static $browsers = array(
+//           // run FF15 on Windows 8 on Sauce
+//           array(
+//                   'browserName' => 'firefox',
+//                   'desiredCapabilities' => array(
+//                           'version' => '15',
+//                           'platform' => 'Windows 2012',
+//                   )
+//           ),
+//           // run Chrome on Linux on Sauce
+//           array(
+//                   'browserName' => 'chrome',
+//                   'desiredCapabilities' => array(
+//                           'platform' => 'Linux'
+//                   )
+//           ),
+//           // run Mobile Safari on iOS
+//           //array(
+//           //'browserName' => '',
+//           //'desiredCapabilities' => array(
+//           //'app' => 'safari',
+//           //'device' => 'iPhone Simulator',
+//           //'version' => '6.1',
+//           //'platform' => 'Mac 10.8',
+//           //)
+//           //)//,
+//           // run Chrome locally
+//           //array(
+//           //'browserName' => 'chrome',
+//           //'local' => true,
+//           //'sessionStrategy' => 'shared'
+//           //)
+//           );
 
   public function setUp()
   {
