f2ed660df9192222f317e99b3690e390d5218ee0
lhungil <--global>
2014-05-22 12:01:38 -0500

    Cleaned up use of tabs / spaces.

--- testFramework/unittests/support/zcTestCase.php
+++ testFramework/unittests/support/zcTestCase.php
@@ -22,37 +22,38 @@ class zcTestCase extends PHPUnit_Framework_TestCase
 
   public function setUp()
   {
-  	if(!defined('IS_ADMIN_FLAG')) define('IS_ADMIN_FLAG', false);
+    if(!defined('IS_ADMIN_FLAG')) define('IS_ADMIN_FLAG', false);
 
-  	// Define some pre-requisites
-  	if(!defined('TESTCWD')) define('TESTCWD', realpath(dirname(__FILE__) . '/../') . '/');
-  	if(!defined('DIR_FS_CATALOG')) define('DIR_FS_CATALOG', realpath(dirname(__FILE__) . '/../../../') . '/');
-  	if(!defined('DIR_FS_INCLUDES')) define('DIR_FS_INCLUDES', DIR_FS_CATALOG . 'includes/');
-  	if(!defined('CWD')) define('CWD', DIR_FS_INCLUDES . '../');
+    // Define some pre-requisites
+    if(!defined('TESTCWD')) define('TESTCWD', realpath(dirname(__FILE__) . '/../') . '/');
+    if(!defined('DIR_FS_CATALOG')) define('DIR_FS_CATALOG', realpath(dirname(__FILE__) . '/../../../') . '/');
+    if(!defined('DIR_FS_INCLUDES')) define('DIR_FS_INCLUDES', DIR_FS_CATALOG . 'includes/');
+    if(!defined('CWD')) define('CWD', DIR_FS_INCLUDES . '../');
 
-  	if (strpos(@ini_get('include_path'), '.') === false)
-  	{
-  	  @ini_set('include_path', '.' . PATH_SEPARATOR . @ini_get('include_path'));
-  	}
+    if (strpos(@ini_get('include_path'), '.') === false)
+    {
+      @ini_set('include_path', '.' . PATH_SEPARATOR . @ini_get('include_path'));
+    }
 
-  	if (file_exists(TESTCWD . 'localTestSetup.php'))
-  	  require_once TESTCWD . 'localTestSetup.php';
+    if (file_exists(TESTCWD . 'localTestSetup.php'))
+      require_once TESTCWD . 'localTestSetup.php';
 
-  	// Configure some additional paths if not already configured
-  	if(!defined('DIR_FS_ADMIN')) define('DIR_FS_ADMIN', DIR_FS_CATALOG . 'admin/');
-  	if(!defined('DIR_WS_CATALOG')) define('DIR_WS_CATALOG', '/');
-  	if(!defined('DIR_WS_HTTPS_CATALOG')) define('DIR_WS_HTTPS_CATALOG', '/ssl/');
+    // Configure some additional paths if not already configured
+    if(!defined('DIR_WS_ADMIN')) define('DIR_WS_ADMIN', '/admin/');
+    if(!defined('DIR_FS_ADMIN')) define('DIR_FS_ADMIN', DIR_FS_CATALOG . 'admin/');
+    if(!defined('DIR_WS_CATALOG')) define('DIR_WS_CATALOG', '/');
+    if(!defined('DIR_WS_HTTPS_CATALOG')) define('DIR_WS_HTTPS_CATALOG', '/ssl/');
 
-  	// Configure the rest of the paths if needed
-  	require_once(DIR_FS_INCLUDES . 'defined_paths.php');
-  	require_once('zcCatalogTestCase.php');
-  	require_once('zcAdminTestCase.php');
+    // Configure the rest of the paths if needed
+    require_once(DIR_FS_INCLUDES . 'defined_paths.php');
+    require_once('zcCatalogTestCase.php');
+    require_once('zcAdminTestCase.php');
 
-  	// Load required common files
-  	require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'class.base.php');
-  	require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'class.notifier.php');
+    // Load required common files
+    require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'class.base.php');
+    require_once(DIR_FS_CATALOG . DIR_WS_CLASSES . 'class.notifier.php');
 
-  	if(IS_ADMIN_FLAG)
+    if(IS_ADMIN_FLAG)
     {
       require_once(DIR_FS_ADMIN . DIR_WS_FUNCTIONS . 'general.php');
     }
