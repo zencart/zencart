2bc797ecbc9b41585cb4137b51b8b76e2bad9db6
DrByte <drbyte@zen-cart.com>
2014-06-29 21:53:58 -0400

    CHANGE-672 - Unset large variables when finished processing

--- admin/includes/init_includes/init_db_config_read.php
+++ admin/includes/init_includes/init_db_config_read.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: init_db_config_read.php 3001 2006-02-09 21:45:06Z wilt $
