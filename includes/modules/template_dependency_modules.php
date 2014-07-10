0bdbaf472a62998f242f79b526c3d263e8304e9e
DrByte <drbyte@zen-cart.com>
2014-07-10 01:29:07 -0400

    Revert "template_dependency module wasn't fully falling back to defaults if override missing"
    This reverts commit 27665cfcf434d0223a1cd9b843c2234d4251a6c3.

--- includes/modules/template_dependency_modules.php
+++ includes/modules/template_dependency_modules.php
@@ -3,9 +3,9 @@
  * Load common dependencies required by templates
  *
  * @package initSystem
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: Author: DrByte  Jul 9 2014 New in v1.6.0 $
+ * @version GIT: $Id: Author: DrByte  Fri Jul 6 11:57:44 2012 -0400 Modified in v1.5.1 $
  */
 // must be called appropriately
 if (!defined('IS_ADMIN_FLAG')) {
@@ -19,10 +19,5 @@ include(DIR_WS_TEMPLATE . 'template_info.php');
 
 // dynamically discover and prepare proper markup for relevant stylesheets and javascripts
 if (NULL == $css_js_handler || $css_js_handler == '') $css_js_handler = 'tpl_css_js_generator.php';
-$val = DIR_WS_MODULES . zen_get_module_directory($css_js_handler);
-if (file_exists($val)) {
-  require($val);
-} else {
-  $css_js_handler = 'tpl_css_js_generator';
-  require(DIR_WS_MODULES . zen_get_module_directory($css_js_handler));
-}
+require(DIR_WS_MODULES . zen_get_module_directory($css_js_handler));
+
