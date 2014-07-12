933a2b6d5ff87b95b7ceea0a22f6962342e17458
DrByte <drbyte@zen-cart.com>
2014-07-11 22:21:49 -0400

    Clean up redundant and orphaned constants for TEXT_CLOSE_WINDOW ref: http://www.zen-cart.com/showthread.php?212344-TEXT_CLOSE_WINDOW-in-info_shopping_cart-php-of-No-Effect

--- includes/templates/template_default/popup_image/tpl_main_page.php
+++ includes/templates/template_default/popup_image/tpl_main_page.php
@@ -3,7 +3,7 @@
  * Override Template for common/tpl_main_page.php
  *
  * @package templateSystem
- * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_main_page.php 2993 2006-02-08 07:14:52Z birdbrain $
@@ -15,7 +15,7 @@ $zco_notifier->notify('NOTIFY_TPL_MAIN_PAGE_BEFORE_BODY', $body_id, $template_di
 <body id="<?php echo $body_id; ?>"<?php if ($bodyClasses) echo ' class="' . $bodyClasses . '"';?>>
 
 <div>
-<a class="btn close-window" href="javascript:window.close()"><?php echo zen_image($products_image_path, $products_name . ' ' . TEXT_CLOSE_WINDOW); ?></a>
+<a class="btn close-window" href="javascript:window.close()"><?php echo zen_image($products_image_path, $products_name . ' ' . TEXT_CLOSE_IMAGE_WINDOW); ?></a>
 </div>
 
 <?php
