35e2b0fc91418ca51cbabe298b34da427b417e79
George Cooksey <texdc3@gmail.com>
2014-07-14 22:45:49 -0500

    Conform query style to existing standards

--- admin/includes/functions/general.php
+++ admin/includes/functions/general.php
@@ -2571,10 +2571,11 @@ function zen_copy_products_attributes($products_id_from, $products_id_to) {
       return false;
     }
 
-    $query = 'select pad.products_attributes_id from ' . TABLE_PRODUCTS_ATTRIBUTES . ' pa '
-           . 'inner join ' . TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD . ' pad '
-           . 'on pad.products_attributes_id = pa.products_attributes_id '
-           . 'where pa.products_id = ' . (int) $products_id;
+    $query = "select pad.products_attributes_id
+              from " . TABLE_PRODUCTS_ATTRIBUTES . " pa
+              inner join " . TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD . " pad
+              on pad.products_attributes_id = pa.products_attributes_id
+              where pa.products_id = " . (int) $products_id;
 
     global $db;
     return ($db->Execute($query)->RecordCount() > 0);
