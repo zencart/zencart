4ec050ca6c67733b1e78e0ec90a4f5d51a65b13d
zcwilt <ian@zen-cart.com>
2014-08-21 18:39:57 +0100

    CHANGE-714 fix authorize.net to update fields correctly

--- includes/templates/template_default/templates/tpl_ajax_checkout_confirmation_default.php
+++ includes/templates/template_default/templates/tpl_ajax_checkout_confirmation_default.php
@@ -6,7 +6,7 @@
  * Displays final checkout details, cart, payment and shipping info details.
  *
  * @package templateSystem
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: Ian Wilson   New in v1.5.4 $
