cb4754f4317e09d0a3e6632de52eae5a68ec625b
ajeh <ajeh@zen-cart.com>
2014-09-05 11:40:10 -0400

    Some servers returning references to an invalid index value if the SQL query returned is empty

--- includes/functions/functions_lookups.php
+++ includes/functions/functions_lookups.php
@@ -237,12 +237,7 @@
     }
 
     $attributes = $db->Execute($attributes_query);
-
-    if ($attributes->recordCount() > 0 && $attributes->fields['products_attributes_id'] > 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return !($attributes->EOF);
   }
 
 /*
