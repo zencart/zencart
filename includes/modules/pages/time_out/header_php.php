6d8ac52051dd21fe0dbd87612c8f4755ee28e001
DrByte <drbyte@zen-cart.com>
2014-09-12 13:56:26 -0400

    Another related misspelling, albeit this is more for semantics than a typo

--- includes/modules/pages/time_out/header_php.php
+++ includes/modules/pages/time_out/header_php.php
@@ -3,7 +3,7 @@
  * Time out page
  *
  * @package page
- * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: header_php.php 6350 2007-05-20 21:00:41Z drbyte $
@@ -40,9 +40,9 @@ if (isset($_GET['action']) && ($_GET['action'] == 'process')) {
         $error = true;
       $messageStack->add('login', TEXT_LOGIN_ERROR);
       } else {
-        $newPassword = $check_customer->fields['customers_password'];
+        $dbPassword = $check_customer->fields['customers_password'];
         // Check that password is good
-        if (!zen_validate_password($password, $newPassword)) {
+        if (!zen_validate_password($password, $dbPassword)) {
           $error = true;
           $messageStack->add('login', TEXT_LOGIN_ERROR);
         } else {
