623609b9a4bed37e1374742276cffd7c6a5b2a2b
ajeh <ajeh@zen-cart.com>
2014-11-11 16:58:15 -0500

    missing equal in IF

--- admin/options_name_manager.php
+++ admin/options_name_manager.php
@@ -280,7 +280,7 @@
 
             $all_update_products->MoveNext();
           }
-          if ($updated='true') {
+          if ($updated == 'true') {
             $messageStack->add_session(SUCCESS_PRODUCTS_OPTIONS_VALUES, 'success');
           } else {
             $messageStack->add_session(ERROR_PRODUCTS_OPTIONS_VALUES, 'error');
