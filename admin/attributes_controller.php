64bd8f5d1c5d09600b76851cda2acca11307cd97
ajeh <ajeh@zen-cart.com>
2014-11-20 18:57:36 -0500

    Generates debug errors on some servers Missing $pInfo = new stdClass();

--- admin/attributes_controller.php
+++ admin/attributes_controller.php
@@ -921,6 +921,7 @@ if ($action == 'attributes_preview') {
 // preview shot of attributes
 if ($action == 'attributes_preview') {
   $_GET['products_id'] = $products_filter;
+  $pInfo = new stdClass();
   $pInfo->products_id = $products_filter;
 
   include(DIR_WS_INCLUDES . 'attributes_preview.php');
