8a2c48c1d2d60e4144e946033651edfe55620cd6
DrByte <drbyte@zen-cart.com>
2014-12-16 17:39:15 -0500

    docs and header dates

--- ajax.php
+++ ajax.php
@@ -3,7 +3,7 @@
  * ajax front controller
  *
  * @package templateSystem
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: Ian Wilson   New in v1.5.4 $
