92889ba51bfa22045a631b7eceffd4c16cd89d41
DrByte <drbyte@zen-cart.com>
2015-01-05 17:53:03 -0500

    Fix class/id markup for looped items in template files
    Ref: http://www.zen-cart.com/showthread.php?215429-tpl_account_history_info_default-php-id-instead-of-class-in-attributes-list

--- includes/templates/template_default/templates/tpl_account_history_info_default.php
+++ includes/templates/template_default/templates/tpl_account_history_info_default.php
@@ -6,7 +6,7 @@
  * Displays information related to a single specific order
  *
  * @package templateSystem
- * @copyright Copyright 2003-2011 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_account_history_info_default.php 19103 2011-07-13 18:10:46Z wilt $
@@ -39,7 +39,7 @@
         <td class="accountProductDisplay"><?php echo  $order->products[$i]['name'];
 
     if ( (isset($order->products[$i]['attributes'])) && (sizeof($order->products[$i]['attributes']) > 0) ) {
-      echo '<ul id="orderAttribsList">';
+      echo '<ul class="orderAttribsList">';
       for ($j=0, $n2=sizeof($order->products[$i]['attributes']); $j<$n2; $j++) {
         echo '<li>' . $order->products[$i]['attributes'][$j]['option'] . TEXT_OPTION_DIVIDER . nl2br(zen_output_string_protected($order->products[$i]['attributes'][$j]['value'])) . '</li>';
       }
