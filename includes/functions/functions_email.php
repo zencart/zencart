8845842747aa9e974e99f68fa24cc37af5936951
ajeh <ajeh@zen-cart.com>
2015-01-06 14:22:03 -0500

    Header fix

--- includes/functions/functions_email.php
+++ includes/functions/functions_email.php
@@ -5,7 +5,7 @@
  * Hooks into phpMailer class for actual email encoding and sending
  *
  * @package functions
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: ajeh  Modified in v1.6.0 $
