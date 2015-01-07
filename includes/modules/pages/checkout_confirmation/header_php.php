8bbfd482fdcdf58c1d94264b4e3b518bfa84b91c
ajeh <ajeh@zen-cart.com>
2015-01-07 14:34:22 -0500

    Header fix

--- includes/modules/pages/checkout_confirmation/header_php.php
+++ includes/modules/pages/checkout_confirmation/header_php.php
@@ -3,7 +3,7 @@
  * checkout_confirmation header_php.php
  *
  * @package page
- * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: header_php.php 16435 2010-05-28 09:34:32Z drbyte $
--- includes/modules/pages/checkout_shipping/header_php.php
+++ includes/modules/pages/checkout_shipping/header_php.php
@@ -3,7 +3,7 @@
  * Checkout Shipping Page
  *
  * @package page
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: header_php.php 18697 2011-05-04 14:35:20Z wilt $
