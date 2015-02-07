452ec322ea2402aa4e76ca5f50ec08312494f90c
DrByte <drbyte@zen-cart.com>
2015-02-07 12:01:36 -0500

    Fix detection of whether "cheapest shipping selector" should be triggered
    Detection should be based on the 'id' element, not the 'shipping' element alone

--- includes/modules/pages/checkout_shipping/header_php.php
+++ includes/modules/pages/checkout_shipping/header_php.php
@@ -209,8 +209,7 @@ if (isset($_SESSION['cart']->cartID)) {
 // if the modules status was changed when none were available, to save on implementing
 // a javascript force-selection method, also automatically select the cheapest shipping
 // method if more than one module is now enabled
-  if ( !isset($_SESSION['shipping']) && (zen_count_shipping_modules() > 1) )  $_SESSION['shipping'] = $shipping_modules->cheapest();
-
+  if ((!isset($_SESSION['shipping']) || !isset($_SESSION['shipping']['id']) || $_SESSION['shipping']['id'] == '') && zen_count_shipping_modules() > 1)  $_SESSION['shipping'] = $shipping_modules->cheapest();
 
   // Should address-edit button be offered?
   $displayAddressEdit = (MAX_ADDRESS_BOOK_ENTRIES >= 2);
