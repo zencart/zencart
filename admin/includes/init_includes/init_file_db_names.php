10551bf796f6e7f27932bca38a0e100ba0f5c037
DrByte <drbyte@zen-cart.com>
2015-02-22 16:26:10 -0500

    Merge #303 Update proxy/load-balancer detection to accommodate X-Forwarded-Port headers
    While all the existing proxy/load-balancer detections will probably "catch" any proxy forwarded SSL headers, X-Forwarded-Port is one additional low-priority one, now added to the list.

--- admin/includes/init_includes/init_file_db_names.php
+++ admin/includes/init_includes/init_file_db_names.php
@@ -1,16 +1,18 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: Author: DrByte  Wed Jan 23 23:39:37 2013 -0500 Modified in v1.5.2 $
+ * @version $Id:  Modified in v1.5.5 $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
 /**
- * set the type of request (secure or not)
+ * Detect the type of request (secure or not)
+ * Currently only used as a helper when generating protocol-matched URLs for forms, templates, etc.
+ * This is not used as a validation tool at all in Zen Cart core code.
  */
 $request_type = (((isset($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1'))) ||
                  (isset($_SERVER['HTTP_X_FORWARDED_BY']) && strpos(strtoupper($_SERVER['HTTP_X_FORWARDED_BY']), 'SSL') !== false) ||
@@ -20,6 +22,7 @@ $request_type = (((isset($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) ==
                  (isset($_SERVER['HTTP_X_FORWARDED_SSL']) && ($_SERVER['HTTP_X_FORWARDED_SSL'] == '1' || strtolower($_SERVER['HTTP_X_FORWARDED_SSL']) == 'on')) ||
                  (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && (strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'ssl' || strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) == 'https')) ||
                  (isset($_SERVER['HTTP_SSLSESSIONID']) && $_SERVER['HTTP_SSLSESSIONID'] != '') ||
+                 (isset($_SERVER['HTTP_X_FORWARDED_PORT']) && $_SERVER['HTTP_X_FORWARDED_PORT'] == '443') ||
                  (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443')) ? 'SSL' : 'NONSSL';
 
 // set php_self in the local scope
