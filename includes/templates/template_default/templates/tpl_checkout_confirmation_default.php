6acf305b57424c973bf83d3b4fee4709b3e3a3a5
Scott C Wilson <scottcwilson@gmail.com>
2015-03-03 10:26:15 -0500

    Update copyright

--- includes/templates/template_default/templates/tpl_checkout_confirmation_default.php
+++ includes/templates/template_default/templates/tpl_checkout_confirmation_default.php
@@ -6,7 +6,7 @@
  * Displays final checkout details, cart, payment and shipping info details.
  *
  * @package templateSystem
- * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_checkout_confirmation_default.php 6247 2007-04-21 21:34:47Z wilt $
