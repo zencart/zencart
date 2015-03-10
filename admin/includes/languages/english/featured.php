b0c35cfe5d0d535ea6d5b5bbe9a70522d4821e30
ajeh <ajeh@zen-cart.com>
2015-03-10 13:57:27 -0400

    Fix #342 headers

--- admin/includes/languages/english/featured.php
+++ admin/includes/languages/english/featured.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  *  $Id: featured.php ajeh  Modified in v1.6.0 $
