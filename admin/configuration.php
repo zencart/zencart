fbce0de227063c711fdafcc73446fe5c844ff31c
Scott C Wilson <scottcwilson@gmail.com>
2015-03-11 14:54:27 -0400

    Adds error checking to Min Values->Admin Usernames

--- admin/configuration.php
+++ admin/configuration.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Jun 30 2014 Modified in v1.5.4 $
@@ -21,7 +21,9 @@
           $messageStack->add_session(ERROR_ADMIN_DEMO, 'caution');
           zen_redirect(zen_href_link(FILENAME_CONFIGURATION, 'gID=' . $_GET['gID'] . '&cID=' . (int)$cID));
         }
+        
         $configuration_value = zen_db_prepare_input($_POST['configuration_value']);
+        require("includes/configuration_checks.php"); 
 
         $db->Execute("update " . TABLE_CONFIGURATION . "
                       set configuration_value = '" . zen_db_input($configuration_value) . "',
---
+++ admin/includes/configuration_checks.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2015 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version 
+ */
+
+  $gID=(int) zen_db_prepare_input($_GET['gID']);
+  $cID=(int) zen_db_prepare_input($_GET['cID']);
+
+  if ($gID == 2 && $cID == 59) { 
+    // Admin Usernames.  Must be >= 4
+    if ($configuration_value < 4) { 
+          $_GET['action']= '';
+          $messageStack->add_session(TEXT_ADMIN_USERNAME_LENGTH, 'caution');
+          zen_redirect(zen_href_link(FILENAME_CONFIGURATION, 'gID=' . $_GET['gID'] . '&cID=' . (int)$cID));
+    }
+  }
