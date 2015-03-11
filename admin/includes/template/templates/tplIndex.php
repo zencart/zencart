37902a18027c0d1abfdcc8e07680783aa219c305
Scott C Wilson <scottcwilson@gmail.com>
2015-03-11 14:31:49 -0400

    Update dates

--- admin/includes/template/templates/tplIndex.php
+++ admin/includes/template/templates/tplIndex.php
@@ -3,7 +3,7 @@
  * Admin Home Template
  *
  * @package templateSystem
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: $
  */
