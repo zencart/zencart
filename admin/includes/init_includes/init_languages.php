8502038093fb5dfe0610dd0dea59088fbd179079
Scott C Wilson <scottcwilson@gmail.com>
2015-04-21 18:07:13 -0400

    Fixes #67 Multiple languages create debug logs

--- admin/includes/init_includes/init_languages.php
+++ admin/includes/init_includes/init_languages.php
@@ -58,7 +58,9 @@ if ($current_page != '' && file_exists(DIR_WS_LANGUAGES . $_SESSION ['language']
 
 // include additional files:
 require (DIR_WS_LANGUAGES . $_SESSION ['language'] . '/' . FILENAME_EMAIL_EXTRAS);
-include (DIR_WS_LANGUAGES . $_SESSION ['language'] . '/widgets_default.php');
+if (file_exists(DIR_WS_LANGUAGES . $_SESSION ['language'] . '/widgets_default.php')) { 
+  include (DIR_WS_LANGUAGES . $_SESSION ['language'] . '/widgets_default.php');
+}
 include (zen_get_file_directory(DIR_FS_CATALOG_LANGUAGES . $_SESSION ['language'] . '/', FILENAME_OTHER_IMAGES_NAMES, 'false'));
 
 if ($za_dir = @dir(DIR_WS_LANGUAGES . $_SESSION ['language'] . '/extra_definitions')) {
