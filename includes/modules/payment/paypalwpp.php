72443de1931711bca531f9e4f0a69c8670e0ec7b
Diego <diego@sv2.com.br>
2015-05-20 08:52:36 -0300

    better check for float quantity. ie 0.25

--- includes/modules/payment/paypalwpp.php
+++ includes/modules/payment/paypalwpp.php
@@ -1252,7 +1252,7 @@ class paypalwpp extends base {
       } // endif attribute-info
 
       // PayPal can't handle fractional-quantity values, so convert it to qty 1 here
-      if ($order->products[$i]['qty'] > 1 && ($order->products[$i]['qty'] != (int)$order->products[$i]['qty'] || $flag_treat_as_partial)) {
+      if (is_float($order->products[$i]['qty'])) {
         $optionsLI["L_NAME$k"] = '('.$order->products[$i]['qty'].' x ) ' . $optionsLI["L_NAME$k"];
         // zen_add_tax already handles whether DISPLAY_PRICES_WITH_TAX is set
         $optionsLI["L_AMT$k"] = zen_round(zen_round(zen_add_tax($order->products[$i]['final_price'], $order->products[$i]['tax']), $decimals) * $order->products[$i]['qty'], $decimals);
