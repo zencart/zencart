dd8ef7d3ff381b8df3c04db423f374516d4eec6f
DrByte <drbyte@zen-cart.com>
2015-05-27 11:11:04 -0400

    Fix iterator syntax errors Fix some syntax errors in #426 and #427 and #424 and implement the iterator fully in these small files

--- admin/includes/init_includes/init_db_config_read.php
+++ admin/includes/init_includes/init_db_config_read.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: init_db_config_read.php 3001 2006-02-09 21:45:06Z wilt $
@@ -10,7 +10,7 @@ if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
 // Determine the DATABASE patch level
-  $project_db_info= $db->Execute("select * from " . TABLE_PROJECT_VERSION . " WHERE project_version_key = 'Zen-Cart Database' ");
+  $project_db_info = $db->Execute("select * from " . TABLE_PROJECT_VERSION . " WHERE project_version_key = 'Zen-Cart Database' ");
   define('PROJECT_DB_VERSION_MAJOR',$project_db_info->fields['project_version_major']);
   define('PROJECT_DB_VERSION_MINOR',$project_db_info->fields['project_version_minor']);
   define('PROJECT_DB_VERSION_PATCH1',$project_db_info->fields['project_version_patch1']);
@@ -21,17 +21,14 @@ if (!defined('IS_ADMIN_FLAG')) {
 // set application wide parameters
   $configuration = $db->Execute('select configuration_key as cfgKey, configuration_value as cfgValue
                                  from ' . TABLE_CONFIGURATION);
-  while (!$configuration->EOF) {
-    define($configuration->fields['cfgKey'], $configuration->fields['cfgValue']);
-    $configuration->MoveNext();
+  foreach ($configuration as $row) {
+    define($row['cfgKey'], $row['cfgValue']);
   }
 
 // set product type layout paramaters
   $configuration = $db->Execute('select configuration_key as cfgKey, configuration_value as cfgValue
                           from ' . TABLE_PRODUCT_TYPE_LAYOUT);
-
-  while (!$configuration->EOF) {
-    define($configuration->fields['cfgKey'], $configuration->fields['cfgValue']);
-    $configuration->MoveNext();
+  foreach ($configuration as $row) {
+    define($row['cfgKey'], $row['cfgValue']);
   }
 unset($configuration);
