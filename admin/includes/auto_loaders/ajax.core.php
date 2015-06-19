61a9d4f0c0096697d85114f3199a9bb95c15b7b9
zcwilt <ian@zen-cart.com>
2015-06-19 18:25:44 +0100

    fix header doc block

--- admin/includes/auto_loaders/ajax.core.php
+++ admin/includes/auto_loaders/ajax.core.php
@@ -3,7 +3,7 @@
  * @package admin
  * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id:
+ * @version  $Id: New in v1.6.0 $
  */
 if (!defined('USE_PCONNECT')) define('USE_PCONNECT', 'false');
 /**
--- admin/includes/template/partials/tplWidgetEditForm.php
+++ admin/includes/template/partials/tplWidgetEditForm.php
@@ -5,7 +5,7 @@
  * @package templateSystem
  * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: $
+ * @version  $Id: New in v1.6.0 $
  */
 
 use ZenCart\DashboardWidget\WidgetManager;
