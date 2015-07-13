440e4710c098766841f0e8458d9580e541419fe4
Scott C Wilson <scottcwilson@gmail.com>
2015-07-13 05:38:29 -0400

    Fix string error

--- admin/includes/init_includes/init_languages.php
+++ admin/includes/init_includes/init_languages.php
@@ -31,7 +31,7 @@ $template_query = $db->Execute("select template_dir from " . TABLE_TEMPLATE_SELE
 $template_dir = $template_query->fields ['template_dir'];
 
 // include the language translations
-zen_pull_language_file(locale.php); 
+zen_pull_language_file('locale.php'); 
 $ajax = FALSE;
 require (DIR_WS_LANGUAGES . $_SESSION ['language'] . '.php');
 if (! empty($_SERVER ['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER ['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
