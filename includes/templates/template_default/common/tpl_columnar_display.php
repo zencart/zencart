7c788475b75a3123c46c4402030c1458121f44da
DrByte <drbyte@zen-cart.com>
2015-07-18 18:37:53 -0400

    Small tidying of tpl_columnar_display.php to output HTML directly instead of echoed by PHP

--- includes/templates/template_default/common/tpl_columnar_display.php
+++ includes/templates/template_default/common/tpl_columnar_display.php
@@ -5,10 +5,10 @@
  * This file is used for generating tabular output where needed, based on the supplied array of table-cell contents.
  *
  * @package templateSystem
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: tpl_columnar_display.php 3157 2006-03-10 23:24:22Z drbyte $
+ * @version $Id: tpl_columnar_display.php drbyte  Modified in v1.6.0 $
  */
 
 ?>
@@ -30,9 +30,10 @@ if (is_array($list_box_contents) > 0 ) {
     for($col=0;$col<sizeof($list_box_contents[$row]);$col++) {
       $r_params = "";
       if (isset($list_box_contents[$row][$col]['params'])) $r_params .= ' ' . (string)$list_box_contents[$row][$col]['params'];
-     if (isset($list_box_contents[$row][$col]['text'])) {
+      if (isset($list_box_contents[$row][$col]['text'])) {
 ?>
-    <?php echo '<div' . $r_params . '>' . $list_box_contents[$row][$col]['text'] .  '</div>' . "\n"; ?>
+    <div<?php echo $r_params;?>><?php echo $list_box_contents[$row][$col]['text']; ?></div>
+
 <?php
       }
     }
