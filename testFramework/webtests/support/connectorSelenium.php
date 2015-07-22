d648e1fba865a157b4ae6110ebcf5222a05122a2
zcwilt <ian@zen-cart.com>
2015-07-22 18:08:46 +0100

    Update to web testing framework
    This is a rewrite of the web testing framework.
    It supports using selenium server and suace labs connector
    
    Not all legacy web tests have been converted. these will be done later. However the most fragile stuff
    e.g. coupons/gv/group discounts have been done.

---
+++ testFramework/webtests/support/connectorSelenium.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package tests
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: $
+ */
+
+/**
+ * Class baseSeleniumTestClass
+ */
+class baseSeleniumTestClass extends PHPUnit_Extensions_Selenium2TestCase
+{
+    use \CompatibilityTestCase;
+
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('http://zen.local');
+    }
+    public function setUpPage()
+    {
+        $this->timeouts()->implicitWait(15000);
+    }
+}
