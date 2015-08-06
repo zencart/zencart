d07a6f7fc08db80940123704eda701d75274b568
zcwilt <ian@zen-cart.com>
2015-08-06 19:11:20 +0100

    Fixes #474 loworder fee isssue
    Also added some we tests to cover.

--- testFramework/webtests/tests/loworderfeeTest.php
+++ testFramework/webtests/tests/loworderfeeTest.php
@@ -15,8 +15,8 @@ class loworderfeeTest extends CommonTestResources
     public function testLowOrderFeeSetupGV()
     {
         $this->switchItemShippingTax('off');
-        if ($this->getCouponBalanceCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL) < 200) {
-            $this->purchaseGiftVoucherQueueOn(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD, 2);
+        if ($this->getCouponBalanceCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL) < 300) {
+            $this->purchaseGiftVoucherQueueOn(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD, 3);
         }
     }
 
@@ -70,6 +70,144 @@ class loworderfeeTest extends CommonTestResources
         $this->byName('cot_gv')->value('45.29');
         $this->byCss('input[type="image"]')->click();
         $this->assertTextPresent('Please select a payment method for your order');
+        $this->switchLowOrderFee('off');
+    }
+
+    public function testLowOrderFeeWithGVAlmostFullShippingTax()
+    {
+        $this->switchLowOrderFee('on');
+        $this->switchItemShippingTax('on');
+        $this->loginStandardCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD);
+        $this->url('http://' . BASE_URL . 'index.php?main_page=shopping_cart&action=empty_cart');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=product_info&cPath=1_9&products_id=3&action=buy_now');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=checkout_shipping');
+        $this->byId('ship-item-item')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->byName('cot_gv')->clear();
+        $this->byName('cot_gv')->value('45.76');
+        $this->byCss('input[type="image"]')->click();
+        $this->assertTextPresent('Please select a payment method for your order');
+        $this->switchItemShippingTax('off');
+        $this->switchLowOrderFee('off');
+    }
+
+    public function testLowOrderFeeWithGVFullShippingTax()
+    {
+        $this->switchLowOrderFee('on');
+        $this->switchItemShippingTax('on');
+        $this->loginStandardCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD);
+        $this->url('http://' . BASE_URL . 'index.php?main_page=shopping_cart&action=empty_cart');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=product_info&cPath=1_9&products_id=3&action=buy_now');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=checkout_shipping');
+        $this->byId('ship-item-item')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->byName('cot_gv')->clear();
+        $this->byName('cot_gv')->value('50.76');
+        $this->byCss('input[type="image"]')->click();
+        $this->assertTextPresent('Confirmation');
+        $this->assertTextPresent('39.99'); //net price
+        $this->assertTextPresent('$2.50'); //shippimg
+        $this->assertTextPresent('$3.27'); //tax
+        $this->assertTextPresent('$5.00'); //loworder fee
+        $this->assertTextPresent('-$50.76'); //gift certificates
+        $this->assertTextPresent('$0.00'); //total
+        $this->byId('btn_submit')->click();
+        $this->switchItemShippingTax('off');
+        $this->switchLowOrderFee('off');
+    }
+
+    public function testLowOrderFeeWithGVFullShippingTaxInclusive()
+    {
+        $this->switchToTaxInclusive();
+        $this->switchLowOrderFee('on');
+        $this->switchItemShippingTax('on');
+        $this->loginStandardCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD);
+        $this->url('http://' . BASE_URL . 'index.php?main_page=shopping_cart&action=empty_cart');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=product_info&cPath=1_9&products_id=3&action=buy_now');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=checkout_shipping');
+        $this->byId('ship-item-item')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->byName('cot_gv')->clear();
+        $this->byName('cot_gv')->value('50.76');
+        $this->byCss('input[type="image"]')->click();
+        $this->assertTextPresent('Confirmation');
+        $this->assertTextPresent('42.79'); //net price
+        $this->assertTextPresent('$2.98'); //shippimg
+        $this->assertTextPresent('$3.27'); //tax
+        $this->assertTextPresent('$5.00'); //loworder fee
+        $this->assertTextPresent('-$50.76'); //gift certificates
+        $this->assertTextPresent('$0.00'); //total
+        $this->byId('btn_submit')->click();
+        $this->switchItemShippingTax('off');
+        $this->switchLowOrderFee('off');
+        $this->switchToTaxNonInclusive();
+    }
+
+    public function testLowOrderFeeWithGroupDiscount()
+    {
+        $this->switchLowOrderFee('on');
+        $this->setCustomerGroupDiscount(WEBTEST_DEFAULT_CUSTOMER_EMAIL, 1);
+        $this->loginStandardCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD);
+        $this->url('http://' . BASE_URL . 'index.php?main_page=shopping_cart&action=empty_cart');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=product_info&cPath=1_9&products_id=3&action=buy_now');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=checkout_shipping');
+        $this->byId('ship-item-item')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->byId('pmt-cod')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->assertTextPresent('Confirmation');
+        $this->assertTextPresent('39.99'); //net price
+        $this->assertTextPresent('$2.50'); //shippimg
+        $this->assertTextPresent('-$4.00'); //group discount
+        $this->assertTextPresent('$2.52'); //tax
+        $this->assertTextPresent('$5.00'); //loworder fee
+        $this->assertTextPresent('46.01'); //total
+        $this->byId('btn_submit')->click();
+        $this->setCustomerGroupDiscount(WEBTEST_DEFAULT_CUSTOMER_EMAIL, 0);
+        $this->switchLowOrderFee('off');
+    }
+
+    public function testLowOrderFeeWithGroupDiscountGVFull()
+    {
+        $this->switchLowOrderFee('on');
+        $this->setCustomerGroupDiscount(WEBTEST_DEFAULT_CUSTOMER_EMAIL, 1);
+        $this->loginStandardCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD);
+        $this->url('http://' . BASE_URL . 'index.php?main_page=shopping_cart&action=empty_cart');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=product_info&cPath=1_9&products_id=3&action=buy_now');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=checkout_shipping');
+        $this->byId('ship-item-item')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->byName('cot_gv')->clear();
+        $this->byName('cot_gv')->value('45.29');
+        $this->byCss('input[type="image"]')->click();
+        $this->assertTextPresent('Please select a payment method for your order');
+        $this->setCustomerGroupDiscount(WEBTEST_DEFAULT_CUSTOMER_EMAIL, 0);
+        $this->switchLowOrderFee('off');
+    }
+
+    public function testLowOrderFeeWithCouponGVFull()
+    {
+        $this->switchLowOrderFee('on');
+        $this->loginStandardCustomer(WEBTEST_DEFAULT_CUSTOMER_EMAIL, WEBTEST_DEFAULT_CUSTOMER_PASSWORD);
+        $this->createCoupon('test10percent');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=shopping_cart&action=empty_cart');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=product_info&cPath=1_9&products_id=3&action=buy_now');
+        $this->url('http://' . BASE_URL . 'index.php?main_page=checkout_shipping');
+        $this->byId('ship-item-item')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->byName('cot_gv')->clear();
+        $this->byName('cot_gv')->value('46.01');
+        $this->byName('dc_redeem_code')->value('test10percent');
+        $this->byId('pmt-cod')->click();
+        $this->byCss('input[type="image"]')->click();
+        $this->assertTextPresent('Confirmation');
+        $this->assertTextPresent('39.99'); //net price
+        $this->assertTextPresent('$2.50'); //shippimg
+        $this->assertTextPresent('-$4.00'); //coupon discount
+        $this->assertTextPresent('$2.52'); //tax
+        $this->assertTextPresent('$5.00'); //loworder fee
+        $this->assertTextPresent('-$46.01'); //gv
+        $this->byId('btn_submit')->click();
         $this->switchLowOrderFee('off');
     }
 }
