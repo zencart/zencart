2ec6449c04a807b7fe332f93f1102fdc9609518f
DrByte <drbyte@zen-cart.com>
2015-09-01 14:32:39 -0400

    Fix UTF8 sequence error in ajax call
    Ref: https://www.zen-cart.com/showthread.php?216691-PHP-Warning-Invalid-UTF-8-sequence-in-argument-in-ajax-php-on-line-24

--- ajax.php
+++ ajax.php
@@ -2,11 +2,11 @@
 /**
  * ajax front controller
  *
- * @package templateSystem
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @package core
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: Author: Ian Wilson   New in v1.5.4 $
+ * @version GIT: $Id: Author: Ian Wilson   Modified in v1.6.0 $
  */
 require ('includes/application_top.php');
 $language_page_directory = DIR_WS_LANGUAGES.$_SESSION['language'].'/';
@@ -21,6 +21,7 @@ if (isset ($_GET['act'])&&isset ($_GET['method'])) {
           $class,
           $_GET['method']
       ));
+      $result = htmlentities((string)$result, ENT_QUOTES, 'utf-8', FALSE);
       echo json_encode ($result);exit();
     } else {
       echo 'method error';
