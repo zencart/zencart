63f6558acadc12c31c648fdd98efb421a3510a8b
DrByte <drbyte@zen-cart.com>
2015-09-15 13:30:34 -0400

    Merge pull request #552 from drbyte/authnet-cctype-fix Fix missing cc_type field introduced in v154

--- includes/modules/payment/authorizenet_aim.php
+++ includes/modules/payment/authorizenet_aim.php
@@ -288,7 +288,7 @@ class authorizenet_aim extends base {
     return $process_button_string;
   }
   function process_button_ajax() {
-    $processButton = array('ccFields'=>array('cc_number'=>'authorizenet_aim_cc_number', 'cc_owner'=>'authorizenet_aim_cc_owner', 'cc_cvv'=>'authorizenet_aim_cc_cvv', 'cc_expires'=>array('name'=>'concatExpiresFields', 'args'=>"['authorizenet_aim_cc_expires_month','authorizenet_aim_cc_expires_year']"), 'cc_expires_month'=>'authorizenet_aim_cc_expires_month', 'cc_expires_year'=>'authorizenet_aim_cc_expires_year'), 'extraFields'=>array(zen_session_name()=>zen_session_id()));
+    $processButton = array('ccFields'=>array('cc_number'=>'authorizenet_aim_cc_number', 'cc_owner'=>'authorizenet_aim_cc_owner', 'cc_cvv'=>'authorizenet_aim_cc_cvv', 'cc_expires'=>array('name'=>'concatExpiresFields', 'args'=>"['authorizenet_aim_cc_expires_month','authorizenet_aim_cc_expires_year']"), 'cc_expires_month'=>'authorizenet_aim_cc_expires_month', 'cc_expires_year'=>'authorizenet_aim_cc_expires_year', 'cc_type' => $this->cc_card_type), 'extraFields'=>array(zen_session_name()=>zen_session_id()));
         return $processButton;
   }
   /**
