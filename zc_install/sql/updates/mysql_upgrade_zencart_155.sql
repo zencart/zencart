02d619523659121dff9c2d0a0cb41a98cb9dde32
DrByte <drbyte@zen-cart.com>
2015-10-07 16:48:36 -0400

    Version stamping

---
+++ zc_install/sql/updates/mysql_upgrade_zencart_155.sql
@@ -0,0 +1,65 @@
+#
+# * This SQL script upgrades the core Zen Cart database structure from v1.5.4 to v1.5.5
+# *
+# * @package Installer
+# * @access private
+# * @copyright Copyright 2003-2015 Zen Cart Development Team
+# * @copyright Portions Copyright 2003 osCommerce
+# * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+# * @version GIT: $Id: Author: DrByte  Thu Sept 10 2015  New in v1.5.5 $
+#
+
+############ IMPORTANT INSTRUCTIONS ###############
+#
+# * Zen Cart uses the zc_install/index.php program to do database upgrades
+# * This SQL script is intended to be used by running zc_install
+# * It is *not* recommended to simply run these statements manually via any other means
+# * ie: not via phpMyAdmin or via the Install SQL Patch tool in Zen Cart admin
+# * The zc_install program catches possible problems and also handles table-prefixes automatically
+# *
+# * To use the zc_install program to do your database upgrade:
+# * a. Upload the NEWEST zc_install folder to your server
+# * b. Surf to zc_install/index.php via your browser
+# * c. On the System Inspection page, scroll to the bottom and click on Database Upgrade
+# *    NOTE: do NOT click on the "Install" button, because that will erase your database.
+# * d. On the Database Upgrade screen, you will be presented with a list of checkboxes for
+# *    various Zen Cart versions, with the recommended upgrades already pre-selected.
+# * e. Verify the checkboxes, then scroll down and enter your Zen Cart Admin username
+# *    and password, and then click on the Upgrade button.
+# * f. If any errors occur, you will be notified.  Some warnings can be ignored.
+# * g. When done, you will be taken to the Finished page.
+#
+#####################################################
+
+# Set store to Down-For-Maintenance mode.  Must reset manually via admin after upgrade is done.
+#UPDATE configuration set configuration_value = 'true' where configuration_key = 'DOWN_FOR_MAINTENANCE';
+
+# Clear out active customer sessions
+TRUNCATE TABLE whos_online;
+TRUNCATE TABLE db_cache;
+
+UPDATE countries set countries_name = 'Åland Islands' where countries_iso_code_3 = 'ALA';
+UPDATE countries set countries_name = 'Réunion' where countries_iso_code_3 = 'REU';
+UPDATE countries set countries_name = "Côte d'Ivoire" where countries_iso_code_3 = 'CIV';
+UPDATE countries set countries_name = 'Bonaire, Sint Eustatius and Saba', countries_iso_code_2 = 'BQ', countries_iso_code_3 = 'BES' WHERE countries_iso_code_3 = 'ANT';
+INSERT INTO countries (countries_id, countries_name, countries_iso_code_2, countries_iso_code_3, address_format_id) VALUES (247,'Curaçao','CW','CUW','1');
+INSERT INTO countries (countries_id, countries_name, countries_iso_code_2, countries_iso_code_3, address_format_id) VALUES (248,'Sint Maarten (Dutch part)','SX','SXM','1');
+
+UPDATE configuration SET configuration_title='Credit Card Enable Status - Debit', configuration_key = 'CC_ENABLED_DEBIT', configuration_value ='0', configuration_description='Accept Debit Cards 0= off 1= on<br>NOTE: This is not deeply integrated at this time, and this setting may be redundant if your payment modules do not yet specifically have code to honour this switch.', date_added=now() WHERE configuration_key='CC_ENABLED_SWITCH';
+
+#############
+
+#### VERSION UPDATE STATEMENTS
+## THE FOLLOWING 2 SECTIONS SHOULD BE THE "LAST" ITEMS IN THE FILE, so that if the upgrade fails prematurely, the version info is not updated.
+##The following updates the version HISTORY to store the prior version info (Essentially "moves" the prior version info from the "project_version" to "project_version_history" table
+#NEXT_X_ROWS_AS_ONE_COMMAND:3
+INSERT INTO project_version_history (project_version_key, project_version_major, project_version_minor, project_version_patch, project_version_date_applied, project_version_comment)
+SELECT project_version_key, project_version_major, project_version_minor, project_version_patch1 as project_version_patch, project_version_date_applied, project_version_comment
+FROM project_version;
+
+## Now set to new version
+UPDATE project_version SET project_version_major='1', project_version_minor='5.5', project_version_patch1='', project_version_patch1_source='', project_version_patch2='', project_version_patch2_source='', project_version_comment='Version Update 1.5.4->1.5.5', project_version_date_applied=now() WHERE project_version_key = 'Zen-Cart Main';
+UPDATE project_version SET project_version_major='1', project_version_minor='5.5', project_version_patch1='', project_version_patch1_source='', project_version_patch2='', project_version_patch2_source='', project_version_comment='Version Update 1.5.4->1.5.5', project_version_date_applied=now() WHERE project_version_key = 'Zen-Cart Database';
+
+#####  END OF UPGRADE SCRIPT
+
