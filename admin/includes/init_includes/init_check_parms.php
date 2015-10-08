dfe362d155f9fcc5d8b608c0d7ca1f4b14a788e6
Scott C Wilson <scottcwilson@gmail.com>
2015-10-08 06:41:26 -0400

    Copyright update

--- admin/includes/init_includes/init_check_parms.php
+++ admin/includes/init_includes/init_check_parms.php
@@ -2,7 +2,6 @@
 /**
  * @package admin
  * @copyright Copyright 2003-2015 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  */
 if (!defined('IS_ADMIN_FLAG')) {
