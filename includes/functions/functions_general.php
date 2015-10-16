4170329fbe5d139428c70e480fdbc3b9324eac3e
Chris Brown <drbyte@zen-cart.com>
2015-10-16 12:01:57 -0400

    remove old date

--- includes/functions/functions_general.php
+++ includes/functions/functions_general.php
@@ -7,7 +7,7 @@
  * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: Author: Ian Wilson  Wed Feb 19 15:57:35 2014 +0000 Modified in v1.6.0 $
+ * @version GIT: $Id: Author: Ian Wilson  Modified in v1.6.0 $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
