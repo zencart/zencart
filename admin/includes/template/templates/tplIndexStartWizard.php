b18992f6e1509bee2bb60cbf9770a1341e61aa5e
Michael Barnes <admin@dashingcatstudios.com>
2015-10-16 21:56:27 -0500

    Styling improvements for "Setup Wizard" dialog

--- admin/includes/template/templates/tplIndexStartWizard.php
+++ admin/includes/template/templates/tplIndexStartWizard.php
@@ -20,7 +20,7 @@
 </style>
 <div class="content-fluid">
   <div class="row">
-    <div class="small-12 columns small-centered">
+    <div class="small-7 columns small-centered">
   <h1><?php echo TEXT_HEADING_SETUP_WIZARD; ?></h1>
 <?php
   if (!zen_is_superuser())
@@ -36,61 +36,91 @@
    <input type="hidden" name="action" value="setupWizard">
    <fieldset>
    <legend><?php echo TEXT_FORM_LEGEND_REQUIRED_SETUP; ?></legend>
-  <div class="row">
-  <div class="small-3 columns">
-  <label class="inline" for="store_name"><?php echo TEXT_FORM_LABEL_STORE_NAME; ?></label>
+   <div class="panel callout radius">
+   <div class="row">
+    <div class="small-8 small-centered columns">
+      <div class="row collapse prefix-radius">
+        <div class="small-3 columns">
+          <label><span class="prefix"><?php echo TEXT_FORM_LABEL_STORE_NAME; ?></span>
+        </div>
+        <div class="small-9 columns">
+          <input type="text" name="store_name" value="<?php echo zen_output_string_protected($tplVars['storeName']); ?>" autofocus="autofocus" tabindex="1" placeholder="<?php echo TEXT_EXAMPLE_STORENAME; ?>">
+        </div></label>
+      </div>
+    </div>
   </div>
-  <div class="small-9 columns">
-  <input type="text" id="store_name" name="store_name" value="<?php echo zen_output_string_protected($tplVars['storeName']); ?>" autofocus="autofocus" tabindex="1" placeholder="<?php echo TEXT_EXAMPLE_STORENAME; ?>">
+  
+  <div class="row">
+    <div class="small-8 small-centered columns">
+      <div class="row collapse prefix-radius">
+        <div class="small-3 columns">
+          <label><span class="prefix"><?php echo TEXT_FORM_LABEL_STORE_OWNER; ?></span>
+        </div>
+        <div class="small-9 columns">
+          <input type="text" name="store_owner" value="<?php echo zen_output_string_protected($tplVars['storeOwner']); ?>" tabindex="2" placeholder="<?php echo TEXT_EXAMPLE_STOREOWNER; ?>">
+        </div></label>
+	  </div>
+    </div>
   </div>
-   </div>
 
   <div class="row">
-  <div class="small-3 columns">
-   <label class="inline" for="store_owner"><?php echo TEXT_FORM_LABEL_STORE_OWNER; ?></label>
-   </div>
-  <div class="small-9 columns">
-    <input type="text" id="store_owner" name="store_owner" value="<?php echo zen_output_string_protected($tplVars['storeOwner']); ?>" tabindex="2" placeholder="<?php echo TEXT_EXAMPLE_STOREOWNER; ?>">
-   </div>
-   </div>
-
+    <div class="small-8 small-centered columns">
+      <div class="row collapse prefix-radius">
+        <div class="small-3 columns">
+          <label><span class="prefix"><?php echo TEXT_FORM_LABEL_STORE_OWNER_EMAIL; ?></span>
+        </div>
+        <div class="small-9 columns">
+          <input type="text" name="store_owner_email" value="<?php echo zen_output_string_protected($tplVars['storeOwnerEmail']); ?>" tabindex="3" placeholder="webmaster@example.com">
+        </div></label>
+	  </div>
+    </div>
+  </div>
+  
   <div class="row">
-  <div class="small-3 columns">
-   <label class="inline" for="store_owner_email"><?php echo TEXT_FORM_LABEL_STORE_OWNER_EMAIL; ?></label>
-   </div>
-  <div class="small-9 columns">
-   <input type="text" id="store_owner_email" name="store_owner_email" value="<?php echo zen_output_string_protected($tplVars['storeOwnerEmail']); ?>" tabindex="3" placeholder="webmaster@example.com">
-   </div>
-   </div>
-
+    <div class="small-8 small-centered columns">
+      <div class="row collapse prefix-radius">
+        <div class="small-3 columns">
+          <label><span class="prefix"><?php echo TEXT_FORM_LABEL_STORE_COUNTRY; ?></span>
+        </div>
+        <div class="small-9 columns">
+          <?php echo $tplVars['countryString']; ?>
+        </div></label>
+	  </div>
+    </div>
+  </div>
+  
   <div class="row">
-  <div class="small-3 columns">
-   <label class="inline" for="store_country"><?php echo TEXT_FORM_LABEL_STORE_COUNTRY; ?></label>
-   </div>
-  <div class="small-9 columns">
-   <?php echo $tplVars['countryString']; ?>
-   </div>
-   </div>
-
+    <div class="small-8 small-centered columns">
+      <div class="row collapse prefix-radius">
+        <div class="small-3 columns">
+          <label><span class="prefix"><?php echo TEXT_FORM_LABEL_STORE_ZONE; ?></span>
+        </div>
+        <div class="small-9 columns">
+          <?php echo $tplVars['zoneString']; ?>
+        </div></label>
+	  </div>
+    </div>
+  </div>
+  
   <div class="row">
-  <div class="small-3 columns">
-   <label class="inline" for="store_zone"><?php echo TEXT_FORM_LABEL_STORE_ZONE; ?></label>
-   </div>
-  <div class="small-9 columns" id="store_zone_container">
-   <?php echo $tplVars['zoneString']; ?>
-   </div>
-   </div>
-
+    <div class="small-8 small-centered columns">
+      <div class="row collapse prefix-radius">
+        <div class="small-3 columns">
+          <label><span class="prefix"><?php echo TEXT_FORM_LABEL_STORE_ADDRESS; ?></span>
+        </div>
+        <div class="small-9 columns">
+          <textarea rows="4" cols="" name="store_address" tabindex="6" placeholder="<?php echo TEXT_EXAMPLE_STOREADDRESS; ?>"><?php echo $tplVars['storeAddress']; ?></textarea>
+        </div></label>
+	  </div>
+    </div>
+  </div>
+ 
   <div class="row">
-  <div class="small-3 columns">
-   <label class="inline" for="store_address"><?php echo TEXT_FORM_LABEL_STORE_ADDRESS; ?></label>
-   </div>
-  <div class="small-9 columns">
-   <textarea rows="" cols=""  id="store_address" name="store_address" tabindex="6" placeholder="<?php echo TEXT_EXAMPLE_STOREADDRESS; ?>"><?php echo $tplVars['storeAddress']; ?></textarea>
-   </div>
-   </div>
-
-   <input type="submit" class="radius button" id="btnsubmit" name="btnsubmit" value="<?php echo TEXT_SUBMIT; ?>" tabindex="10">
+    <div class="small-8 small-centered columns">
+      <input type="submit" class="radius button" id="btnsubmit" name="btnsubmit" value="<?php echo TEXT_SUBMIT; ?>" tabindex="10">
+	</div>
+  </div>
+  </div>
    </fieldset>
    </form>
 <script>
