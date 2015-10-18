06dd961a11943a7bf3644ab3e0a7b045bc313205
DrByte <drbyte@zen-cart.com>
2015-10-17 22:01:06 -0400

    Tweaks to input sanitizations

--- includes/templates/template_default/templates/tpl_address_book_process_default.php
+++ includes/templates/template_default/templates/tpl_address_book_process_default.php
@@ -50,7 +50,7 @@
 <?php
     if (isset($_GET['edit']) && is_numeric($_GET['edit'])) {
 ?>
-<div class="buttonRow forward"><?php echo zen_draw_hidden_field('action', 'update') . zen_draw_hidden_field('edit', $_GET['edit']) . zen_image_submit(BUTTON_IMAGE_UPDATE, BUTTON_UPDATE_ALT); ?></div>
+<div class="buttonRow forward"><?php echo zen_draw_hidden_field('action', 'update') . zen_draw_hidden_field('edit', (int)$_GET['edit']) . zen_image_submit(BUTTON_IMAGE_UPDATE, BUTTON_UPDATE_ALT); ?></div>
 <div class="buttonRow back"><?php echo '<a href="' . zen_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?></div>
     
 <?php
