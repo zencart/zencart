0ad105bcdf23bebb414701b04b7cfc0214d6f3e3
DrByte <drbyte@zen-cart.com>
2015-10-18 03:20:05 -0400

    Merge ISSUE-94 - Consolidate currencies class

--- admin/includes/classes/currencies.php
+++ admin/includes/classes/currencies.php
@@ -1,63 +1,8 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2011 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: currencies.php 19054 2011-07-08 07:02:41Z drbyte $
  */
 
-// Class to handle currencies
-// TABLES: currencies
-  class currencies {
-    var $currencies;
-
-// class constructor
-    function __construct() {
-      global $db;
-      $this->currencies = array();
-      $currencies = $db->Execute("select code, title, symbol_left, symbol_right, decimal_point,
-                                         thousands_point, decimal_places, value
-                                  from " . TABLE_CURRENCIES);
-
-      while (!$currencies->EOF) {
-	      $this->currencies[$currencies->fields['code']] = array('title' => $currencies->fields['title'],
-                                                       'symbol_left' => $currencies->fields['symbol_left'],
-                                                       'symbol_right' => $currencies->fields['symbol_right'],
-                                                       'decimal_point' => $currencies->fields['decimal_point'],
-                                                       'thousands_point' => $currencies->fields['thousands_point'],
-                                                       'decimal_places' => (int)$currencies->fields['decimal_places'],
-                                                       'value' => $currencies->fields['value']);
-        $currencies->MoveNext();
-      }
-    }
-
-// class methods
-    function format($number, $calculate_currency_value = true, $currency_type = DEFAULT_CURRENCY, $currency_value = '') {
-      if ($calculate_currency_value) {
-        $rate = ($currency_value) ? $currency_value : $this->currencies[$currency_type]['value'];
-        $format_string = $this->currencies[$currency_type]['symbol_left'] . number_format($number * $rate, $this->currencies[$currency_type]['decimal_places'], $this->currencies[$currency_type]['decimal_point'], $this->currencies[$currency_type]['thousands_point']) . $this->currencies[$currency_type]['symbol_right'];
-// if the selected currency is in the european euro-conversion and the default currency is euro,
-// the currency will displayed in the national currency and euro currency
-        if ( (DEFAULT_CURRENCY == 'EUR') && ($currency_type == 'DEM' || $currency_type == 'BEF' || $currency_type == 'LUF' || $currency_type == 'ESP' || $currency_type == 'FRF' || $currency_type == 'IEP' || $currency_type == 'ITL' || $currency_type == 'NLG' || $currency_type == 'ATS' || $currency_type == 'PTE' || $currency_type == 'FIM' || $currency_type == 'GRD') ) {
-          $format_string .= ' <small>[' . $this->format($number, true, 'EUR') . ']</small>';
-        }
-      } else {
-        $format_string = $this->currencies[$currency_type]['symbol_left'] . number_format($number, $this->currencies[$currency_type]['decimal_places'], $this->currencies[$currency_type]['decimal_point'], $this->currencies[$currency_type]['thousands_point']) . $this->currencies[$currency_type]['symbol_right'];
-      }
-
-      return $format_string;
-    }
-
-    function get_value($code) {
-      return $this->currencies[$code]['value'];
-    }
-
-    function get_decimal_places($code) {
-      return $this->currencies[$code]['decimal_places'];
-    }
-
-    function display_price($products_price, $products_tax, $quantity = 1) {
-      return $this->format(zen_add_tax($products_price, $products_tax) * $quantity);
-    }
-  }
+include DIR_FS_CATALOG . DIR_WS_CLASSES . 'currencies.php'; 
