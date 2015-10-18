15924803c3765ef6b3776fe6788550851b07e16e
DrByte <drbyte@zen-cart.com>
2015-10-18 01:36:14 -0400

    Signature parity in html_output

--- admin/includes/functions/html_output.php
+++ admin/includes/functions/html_output.php
@@ -294,10 +294,10 @@
 
 ////
 // Output a form hidden field
-  function zen_draw_hidden_field($name, $value = '', $parameters = '') {
+  function zen_draw_hidden_field($name, $value = '~*~*#', $parameters = '') {
     $field = '<input type="hidden" name="' . zen_output_string($name) . '"';
 
-    if (zen_not_null($value)) {
+    if (zen_not_null($value) && $value != '~*~*#') {
       $field .= ' value="' . zen_output_string($value) . '"';
     } elseif (isset($GLOBALS[$name]) && is_string($GLOBALS[$name])) {
       $field .= ' value="' . zen_output_string(stripslashes($GLOBALS[$name])) . '"';
