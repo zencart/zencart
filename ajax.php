45ea0381c1ab9ef2080ed8579bcae9dd4f64fb11
zcwilt <ian@zen-cart.com>
2015-11-26 10:01:58 +0000

    fix for lfi

--- ajax.php
+++ ajax.php
@@ -13,8 +13,9 @@ $language_page_directory = DIR_WS_LANGUAGES.$_SESSION['language'].'/';
 if (isset ($_GET['act'])&&isset ($_GET['method'])) {
   $className = 'zc'.ucfirst ($_GET['act']);
   $classFile = $className.'.php';
-  if (file_exists (DIR_FS_CATALOG.DIR_WS_CLASSES.'ajax/'.$classFile)) {
-    require (DIR_FS_CATALOG.DIR_WS_CLASSES.'ajax/'.$classFile);
+  $basePath = DIR_FS_CATALOG.DIR_WS_CLASSES;
+  if (file_exists (basename(realpath($basePath. 'ajax/'  .$classFile)))) {
+    require basename(realpath($basePath .'ajax/'  . $classFile));
     $class = new $className ();
     if (method_exists ($class, $_GET['method'])) {
       $result = call_user_func (array(
