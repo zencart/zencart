ae7a4785eea5274a2d27825e809b42a4adaaeda4
DrByte <drbyte@zen-cart.com>
2015-12-07 16:39:51 -0500

    Fix also_purchased FULL GROUP BY compatibility

--- includes/classes/db/mysql/define_queries.php
+++ includes/classes/db/mysql/define_queries.php
@@ -20,7 +20,7 @@ DEFINE('SQL_SHOW_PRODUCT_INFO_LISTING_BELOW',"select configuration_key, configur
 DEFINE('SQL_BANNER_CHECK_QUERY', "select count(*) as count from " . TABLE_BANNERS_HISTORY . "                where banners_id = '%s' and date_format(banners_history_date, '%%Y%%m%%d') = date_format(now(), '%%Y%%m%%d')");
 DEFINE('SQL_BANNER_CHECK_UPDATE', "update " . TABLE_BANNERS_HISTORY . " set banners_shown = banners_shown +1 where banners_id = '%s' and date_format(banners_history_date, '%%Y%%m%%d') = date_format(now(), '%%Y%%m%%d')");
 DEFINE('SQL_BANNER_UPDATE_CLICK_COUNT', "update " . TABLE_BANNERS_HISTORY . " set banners_clicked = banners_clicked + 1 where banners_id = '%s' and date_format(banners_history_date, '%%Y%%m%%d') = date_format(now(), '%%Y%%m%%d')");
-DEFINE('SQL_ALSO_PURCHASED', "SELECT p.products_id, p.products_image
+DEFINE('SQL_ALSO_PURCHASED', "SELECT p.products_id, p.products_image, o.date_purchased
                      FROM " . TABLE_ORDERS_PRODUCTS . " opa, " . TABLE_ORDERS_PRODUCTS . " opb, "
                             . TABLE_ORDERS . " o, " . TABLE_PRODUCTS . " p
                      WHERE opa.products_id = '%s'
@@ -29,7 +29,7 @@ DEFINE('SQL_ALSO_PURCHASED', "SELECT p.products_id, p.products_image
                      AND opb.products_id = p.products_id
                      AND opb.orders_id = o.orders_id
                      AND p.products_status = 1
-                     GROUP BY p.products_id, p.products_image
-                     ORDER BY o.date_purchased desc
+                     GROUP BY p.products_id, p.products_image, o.date_purchased desc
+                     ORDER BY o.date_purchased desc, p.products_id
                      LIMIT 50");
 DEFINE('SQL_SHOW_SHOPPING_CART_EMPTY',"select configuration_key, configuration_value from " . TABLE_CONFIGURATION . " where configuration_key RLIKE 'SHOW_SHOPPING_CART_EMPTY' and configuration_value > 0 order by configuration_value");
