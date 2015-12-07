2dfa2c0470a39de1f784157e68c76d0581c43486
DrByte <drbyte@zen-cart.com>
2015-12-07 14:58:57 -0500

    More fixes for PHP7
    These are related to use of $$foo['element'] which need to add the {braces} for specificity

--- includes/modules/payment/linkpoint_api.php
+++ includes/modules/payment/linkpoint_api.php
@@ -323,7 +323,7 @@ class linkpoint_api {
         if ($order_totals[$i]['code'] == 'ot_total')    $myorder["chargetotal"] = round($order_totals[$i]['value'],2);
       } else {
         global ${$order_totals[$i]['code']};
-        if (substr($order_totals[$i]['text'], 0, 1) == '-' || (isset($$order_totals[$i]['code']->credit_class) && $$order_totals[$i]['code']->credit_class == true)) {
+        if (substr($order_totals[$i]['text'], 0, 1) == '-' || (isset(${$order_totals[$i]['code']}->credit_class) && ${$order_totals[$i]['code']}->credit_class == true)) {
           $creditsApplied += round($order_totals[$i]['value'],2);
         } else {
           $surcharges += round($order_totals[$i]['value'],2);
