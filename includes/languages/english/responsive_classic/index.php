13840324ed0037f15f89613211495d0eaadf237b
DrByte <drbyte@zen-cart.com>
2015-12-13 16:46:52 -0500

    Merge language file customizations

--- includes/languages/english/responsive_classic/index.php
+++ includes/languages/english/responsive_classic/index.php
@@ -1,2 +1,58 @@
 <?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version GIT: $Id: picaflor-azul Modified in v1.5.5 $
+ */
+
+define('TEXT_MAIN','This is the main define statement for the page for english when no template defined file exists. It is located in: <strong>/includes/languages/english/index.php</strong>');
+
+// Showcase vs Store
+if (STORE_STATUS == '0') {
+  define('TEXT_GREETING_GUEST', 'Welcome <span class="greetUser">Guest!</span> Would you like to <a href="%s">log yourself in</a>?');
+} else {
+  define('TEXT_GREETING_GUEST', 'Welcome, please enjoy our online showcase.');
+}
+
+define('TEXT_GREETING_PERSONAL', 'Hello <span class="greetUser">%s</span>! Would you like to see our <a href="%s">newest additions</a>?');
+
+define('TEXT_INFORMATION', 'Define your main Index page copy here.');
+
+//moved to english
+//define('TABLE_HEADING_FEATURED_PRODUCTS','Featured Products');
+
+//define('TABLE_HEADING_NEW_PRODUCTS', 'New Products For %s');
+//define('TABLE_HEADING_UPCOMING_PRODUCTS', 'Upcoming Products');
+//define('TABLE_HEADING_DATE_EXPECTED', 'Date Expected');
+
+if ( ($category_depth == 'products') || (zen_check_url_get_terms()) ) {
+  // This section deals with product-listing page contents
+  define('HEADING_TITLE', 'Available Products');
   define('TABLE_HEADING_IMAGE', '');
+  define('TABLE_HEADING_MODEL', 'Model');
+  define('TABLE_HEADING_PRODUCTS', 'Product Name');
+  define('TABLE_HEADING_MANUFACTURER', 'Manufacturer');
+  define('TABLE_HEADING_QUANTITY', 'Quantity');
+  define('TABLE_HEADING_PRICE', 'Price');
+  define('TABLE_HEADING_WEIGHT', 'Weight');
+  define('TABLE_HEADING_BUY_NOW', 'Buy Now');
+  define('TEXT_NO_PRODUCTS', 'There are no products to list in this category.');
+  define('TEXT_NO_PRODUCTS2', 'There is no product available from this manufacturer.');
+  define('TEXT_NUMBER_OF_PRODUCTS', 'Number of Products: ');
+  define('TEXT_SHOW', 'Filter Results by:');
+  define('TEXT_BUY', 'Buy 1 \'');
+  define('TEXT_NOW', '\' now');
+  define('TEXT_ALL_CATEGORIES', 'All Categories');
+  define('TEXT_ALL_MANUFACTURERS', 'All Manufacturers');
+} elseif ($category_depth == 'top') {
+  // This section deals with the "home" page at the top level with no options/products selected
+  /*Replace this text with the headline you would like for your shop. For example: 'Welcome to My SHOP!'*/
+  define('HEADING_TITLE', 'Congratulations! You have successfully installed your Zen Cart&reg; E-Commerce Solution.');
+} elseif ($category_depth == 'nested') {
+  // This section deals with displaying a subcategory
+  /*Replace this line with the headline you would like for your shop. For example: 'Welcome to My SHOP!'*/
+  define('HEADING_TITLE', 'Congratulations! You have successfully installed your Zen Cart&reg; E-Commerce Solution.'); 
+}
+?>
