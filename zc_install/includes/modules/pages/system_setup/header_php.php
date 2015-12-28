5ace6ef66bd52f7afe6b9a91f8d74fe6ee6e7c78
zcwilt <ian@zen-cart.com>
2015-12-28 19:34:40 +0000

    set enable_ssl_catalog based on wheter installer is run in ssl

--- zc_install/includes/modules/pages/system_setup/header_php.php
+++ zc_install/includes/modules/pages/system_setup/header_php.php
@@ -1,9 +1,14 @@
 <?php
 /**
  * @package Installer
- * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id:
  */
 list($adminDir, $documentRoot, $adminServer, $catalogHttpServer, $catalogHttpUrl, $catalogHttpsServer, $catalogHttpsUrl, $dir_ws_http_catalog, $dir_ws_https_catalog) = getDetectedURIs();
 $db_type = 'mysql';
+$enableSslCatalog = '';
+if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    || $_SERVER['SERVER_PORT'] == 443) {
+    $enableSslCatalog = 'checked = checked';
+}
