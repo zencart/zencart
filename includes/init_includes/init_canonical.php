dd2ee04fe7ffd966aa6be56b21c5f5fee5cb22c4
DrByte <drbyte@zen-cart.com>
2015-12-30 11:49:27 -0500

    Remove beta stamp

--- includes/init_includes/init_canonical.php
+++ includes/init_includes/init_canonical.php
@@ -6,7 +6,7 @@
  * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: Author: DrByte  Modified in v1.5.5-beta $
+ * @version GIT: $Id: Author: DrByte  Modified in v1.5.5 $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
