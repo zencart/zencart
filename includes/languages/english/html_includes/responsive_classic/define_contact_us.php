03b728b9b421a4924f58d4510f7840f7f559a760
DrByte <drbyte@zen-cart.com>
2016-01-01 15:56:24 -0500

    Add additional default define_pages

---
+++ includes/languages/english/html_includes/responsive_classic/define_contact_us.php
@@ -0,0 +1,2 @@
+<p><strong>Contact Us Sample Text ...</strong></p>
+<p>We haven't updated this custom text yet. Please use the Contact Us form to let us know!</p>
---
+++ includes/languages/english/html_includes/responsive_classic/define_page_not_found.php
@@ -0,0 +1,2 @@
+<p><strong>Custom 404 Error Page Sample Text ...</strong></p>
+<p>We haven't updated this page yet. Please use the Contact Us form to let us know!</p>
