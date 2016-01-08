60075170333aa4d19546ae91be7523aa12c85110
DrByte <drbyte@zen-cart.com>
2016-01-07 21:39:28 -0500

    Too many nested </div> and </form> tags in these altered templates

--- includes/templates/responsive_classic/templates/tpl_modules_product_listing.php
+++ includes/templates/responsive_classic/templates/tpl_modules_product_listing.php
@@ -14,12 +14,12 @@
 
 <div id="productListing" class="group">
 
-
-
-<?php if ( ($listing_split->number_of_rows > 0) && ( (PREV_NEXT_BAR_LOCATION == '1') || (PREV_NEXT_BAR_LOCATION == '3') ) ) {
+<?php
+  $openGroupWrapperDiv = false;
+  if ( ($listing_split->number_of_rows > 0) && ( (PREV_NEXT_BAR_LOCATION == '1') || (PREV_NEXT_BAR_LOCATION == '3') ) ) {
+    $openGroupWrapperDiv = true;
+    echo '<div class="prod-list-wrap group">';
 ?>
-<div class="prod-list-wrap group">
-
 <div id="productsListingListingTopLinks" class="back navSplitPagesLinks"><?php echo TEXT_RESULT_PAGE . ' ' . $listing_split->display_links(($isMobile ? MAX_DISPLAY_PAGE_LINKS_MOBILE : MAX_DISPLAY_PAGE_LINKS), zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $isMobile); ?></div>
 <div id="productsListingTopNumber" class="navSplitPagesResult back"><?php echo $listing_split->display_count(TEXT_DISPLAY_NUMBER_OF_PRODUCTS); ?></div>
 <?php
@@ -31,7 +31,7 @@
     if ($show_top_submit_button == true) {
 ?>
 <?php
-	if (PREV_NEXT_BAR_LOCATION == '2' && $listing->RecordCount()) {
+  if (PREV_NEXT_BAR_LOCATION == '2' && $listing->RecordCount()) {
       echo '<div class="prod-list-wrap group">';
     }
 ?>
@@ -44,23 +44,15 @@
     }
 ?>
 
-
 <?php
     } // show top submit
 ?>
 
-
-
 <?php
-
-    if ($show_top_submit_button == '2' && PREV_NEXT_BAR_LOCATION == '2' or $show_top_submit_button == '0' && PREV_NEXT_BAR_LOCATION == '2') {
-
-  }
-    else {
+    if ($openGroupWrapperDiv) {
       echo '</div>';
     }
 ?>
-
 
 
 <?php
@@ -71,23 +63,18 @@
 ?>
 
 <?php
-    if ($show_bottom_submit_button == false && PREV_NEXT_BAR_LOCATION == '1') {
-
-    }
-elseif ($listing->RecordCount()) {
-      echo '<div class="prod-list-wrap group">';
-    }
+  if ($show_bottom_submit_button == true && PREV_NEXT_BAR_LOCATION != '1' && $listing->RecordCount()) {
+    echo '<div class="prod-list-wrap group">';
+  }
 ?>
 
 <?php if ( ($listing_split->number_of_rows > 0) && ((PREV_NEXT_BAR_LOCATION == '2') || (PREV_NEXT_BAR_LOCATION == '3')) ) {
 ?>
 <div id="productsListingListingBottomLinks"  class="navSplitPagesLinks back"><?php echo TEXT_RESULT_PAGE . ' ' . $listing_split->display_links(($isMobile ? MAX_DISPLAY_PAGE_LINKS_MOBILE : MAX_DISPLAY_PAGE_LINKS), zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $isMobile); ?></div>
 <div id="productsListingBottomNumber" class="navSplitPagesResult back"><?php echo $listing_split->display_count(TEXT_DISPLAY_NUMBER_OF_PRODUCTS); ?></div>
-
 <?php
   }
 ?>
-
 
 <?php
 // only show when there is something to submit and enabled
@@ -95,35 +82,30 @@ elseif ($listing->RecordCount()) {
 ?>
 
 <?php
-	if (PREV_NEXT_BAR_LOCATION == '1') {
-      echo '<div class="prod-list-wrap group button-bottom">';
-    }
+  if (PREV_NEXT_BAR_LOCATION == '1') {
+    echo '<div class="prod-list-wrap group button-bottom">';
+  }
 ?>
 <div class="forward button-top"><?php echo zen_image_submit(BUTTON_IMAGE_ADD_PRODUCTS_TO_CART, BUTTON_ADD_PRODUCTS_TO_CART_ALT, 'id="submit2" name="submit1"'); ?></div>
 <?php
-	if (PREV_NEXT_BAR_LOCATION == '1') {
-      echo '</div>';
-    }
+  if (PREV_NEXT_BAR_LOCATION == '1') {
+    echo '</div>';
+  }
 ?>
 
 <?php
     } // show_bottom_submit_button
 ?>
+<?php
+  if ($show_bottom_submit_button == true && PREV_NEXT_BAR_LOCATION != '1' && $listing->RecordCount()) {
+    echo '</div>';
+  }
+?>
 </div>
 
 <?php
-// if ($show_top_submit_button == true or $show_bottom_submit_button == true or (PRODUCT_LISTING_MULTIPLE_ADD_TO_CART != 0 and $show_submit == true and $listing_split->number_of_rows > 0)) {
-  if ($show_top_submit_button == true or $show_bottom_submit_button == true) {
-?>
-
-<?php } ?>
-
-<?php
-    if ($show_bottom_submit_button == false && PREV_NEXT_BAR_LOCATION == '1') {
-
-    }
-elseif ($listing->RecordCount()) {
-      echo '</div>';
-    }
+  if ($how_many > 0 && PRODUCT_LISTING_MULTIPLE_ADD_TO_CART != 0 and $show_submit == true and $listing_split->number_of_rows > 0) {
 ?>
 </form>
+<?php } ?>
+
