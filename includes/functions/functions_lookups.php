580ea5f885e88b2e866a7b81583e94dbac9d1c49
mc12345678 <mc12345678@users.noreply.github.com>
2016-02-02 16:23:08 -0500

    Update functions_lookups.php
    Modified function name to zen_option_name_base_expects_no_values from zen_option_name_no_values

--- includes/functions/functions_lookups.php
+++ includes/functions/functions_lookups.php
@@ -240,7 +240,7 @@
 /*
  *  Check if option name is not expected to have an option value (ie. text field, or File upload field)
  */
-  function zen_option_name_no_values($option_name_id) {
+  function zen_option_name_base_expects_no_values($option_name_id) {
     global $db, $zco_notifier;
 
     $option_name_no_value = true;
@@ -255,7 +255,7 @@
         $sql2 .= ':option_id:,';
         $sql2 = $db->bindVars($sql2, ':option_id:', $option_id, 'integer');
       }
-      $sql2 = substr($sql2, 0, -1); // Need to remove the final comma off of the above.
+      $sql2 = rtrim($sql2, ','); // Need to remove the final comma off of the above.
       $sql2 = ')';
     } else {
       $sql2 = ' = :option_id:';
@@ -269,7 +269,7 @@
     foreach($sql_result as $opt_type) {
 
       $test_var = true; // Set to false in observer if the name is not supposed to have a value associated
-      $zco_notifier->notify('FUNCTIONS_LOOKUPS_OPTION_NAME_NO_VALUES_OPT_TYPE', array($test_var), $test_var);
+      $zco_notifier->notify('FUNCTIONS_LOOKUPS_OPTION_NAME_NO_VALUES_OPT_TYPE', $opt_type, $test_var);
 
       if ($test_var && $opt_type['products_options_type'] != PRODUCTS_OPTIONS_TYPE_TEXT && $opt_type['products_options_type'] != PRODUCTS_OPTIONS_TYPE_FILE) {
         $option_name_no_value = false;
