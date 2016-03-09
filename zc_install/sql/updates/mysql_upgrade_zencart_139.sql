bb5bb289b430e77286b89317560e150d8f20e769
zcwilt <ian@zen-cart.com>
2016-03-09 19:48:13 +0000

    fix sql file headers

--- zc_install/sql/updates/mysql_upgrade_zencart_139.sql
+++ zc_install/sql/updates/mysql_upgrade_zencart_139.sql
@@ -3,7 +3,7 @@
 # *
 # * @package Installer
 # * @access private
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+# * @copyright Copyright 2003-2016 Zen Cart Development Team
 # * @copyright Portions Copyright 2003 osCommerce
 # * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
 # * @version $Id: Author: zcwilt  Wed Sep 23 20:04:38 2015 +0100 New in v1.5.5 $
--- zc_install/sql/updates/mysql_upgrade_zencart_155.sql
+++ zc_install/sql/updates/mysql_upgrade_zencart_155.sql
@@ -3,7 +3,7 @@
 # *
 # * @package Installer
 # * @access private
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+# * @copyright Copyright 2003-2016 Zen Cart Development Team
 # * @copyright Portions Copyright 2003 osCommerce
 # * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
 # * @version $Id: Author: DrByte  Tue Jan 12 12:52:48 2016 -0500 New in v1.5.5 $
