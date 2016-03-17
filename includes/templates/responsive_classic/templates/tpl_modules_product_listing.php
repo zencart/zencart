3ee4397b041a1fc11ff1edbea5989d51ab8f9f9a
DrByte <drbyte@zen-cart.com>
2016-03-17 12:19:27 -0400

    Tidying of code

--- includes/templates/responsive_classic/templates/tpl_modules_product_listing.php
+++ includes/templates/responsive_classic/templates/tpl_modules_product_listing.php
@@ -6,7 +6,7 @@
  * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: picaflor-azul Sat Jan 9 13:13:41 2016 -0500 New in v1.5.5 $
+ * @version $Id: picaflor-azul Thu Mar 17 12:13:18 2016 -0500  Modified in v1.5.5 $
  */
  include(DIR_WS_MODULES . zen_get_module_directory(FILENAME_PRODUCT_LISTING));
 ?>
@@ -15,12 +15,10 @@
 <div id="productListing" class="group">
 
 <?php
-  $openGroupWrapperDiv = false;
-  if ( ($listing_split->number_of_rows > 0) && ( (PREV_NEXT_BAR_LOCATION == '1') || (PREV_NEXT_BAR_LOCATION == '3') ) ) {
-    $openGroupWrapperDiv = true;
-    echo '<div class="prod-list-wrap group">';
+  if ($listing_split->number_of_rows > 0 && (PREV_NEXT_BAR_LOCATION == '1' || PREV_NEXT_BAR_LOCATION == '3') ) {
 ?>
-<div id="productsListingListingTopLinks" class="back navSplitPagesLinks"><?php echo TEXT_RESULT_PAGE . $listing_split->display_links($max_display_page_links, zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $paginateAsUL); ?></div>
+<div class="prod-list-wrap group">
+  <div id="productsListingListingTopLinks" class="navSplitPagesLinks back"><?php echo TEXT_RESULT_PAGE . $listing_split->display_links($max_display_page_links, zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $paginateAsUL); ?></div>
   <div id="productsListingTopNumber" class="navSplitPagesResult back<?php echo $listing_split->number_of_pages == 1 ? ' navSplitEmpty3rdColumn' : ''; ?>"><?php echo $listing_split->display_count(TEXT_DISPLAY_NUMBER_OF_PRODUCTS); ?></div>
 <?php
 }
@@ -30,29 +28,21 @@
 // only show when there is something to submit and enabled
     if ($show_top_submit_button == true) {
 ?>
-<?php
-  if (PREV_NEXT_BAR_LOCATION == '2' && $listing->RecordCount()) {
-      echo '<div class="prod-list-wrap group">';
-    }
-?>
-
-<div class="forward button-top"><?php echo zen_image_submit(BUTTON_IMAGE_ADD_PRODUCTS_TO_CART, BUTTON_ADD_PRODUCTS_TO_CART_ALT, 'id="submit1" name="submit1"'); ?></div>
-
-<?php
- if  (PREV_NEXT_BAR_LOCATION == '2') {
-      echo '</div>';
-    }
-?>
+<?php if (PREV_NEXT_BAR_LOCATION == '2' && $listing_split->number_of_rows) { ?>
+  <div class="prod-list-wrap group">
+<?php } ?>
+    <div class="forward button-top"><?php echo zen_image_submit(BUTTON_IMAGE_ADD_PRODUCTS_TO_CART, BUTTON_ADD_PRODUCTS_TO_CART_ALT, 'id="submit1" name="submit1"'); ?></div>
+<?php if (PREV_NEXT_BAR_LOCATION == '2' && $listing_split->number_of_rows) { ?>
+  </div>
+<?php } ?>
 
 <?php
     } // show top submit
 ?>
 
-<?php
-    if ($openGroupWrapperDiv) {
-      echo '</div>';
-    }
-?>
+<?php if ($listing_split->number_of_rows > 0 && (PREV_NEXT_BAR_LOCATION == '1' || PREV_NEXT_BAR_LOCATION == '3') ) { ?>
+</div>
+<?php } ?>
 
 
 <?php
@@ -62,15 +52,9 @@
   require($template->get_template_dir('tpl_tabular_display.php',DIR_WS_TEMPLATE, $current_page_base,'common'). '/tpl_tabular_display.php');
 ?>
 
-<?php
-  if ($show_bottom_submit_button == true && PREV_NEXT_BAR_LOCATION != '1' && $listing->RecordCount()) {
-    echo '<div class="prod-list-wrap group">';
-  }
-?>
-
-<?php if ( ($listing_split->number_of_rows > 0) && ((PREV_NEXT_BAR_LOCATION == '2') || (PREV_NEXT_BAR_LOCATION == '3')) ) {
-?>
-<div id="productsListingListingBottomLinks"  class="navSplitPagesLinks back"><?php echo TEXT_RESULT_PAGE . $listing_split->display_links($max_display_page_links, zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $paginateAsUL); ?></div>
+<?php if ($listing_split->number_of_rows && (PREV_NEXT_BAR_LOCATION == '2' || PREV_NEXT_BAR_LOCATION == '3') ) { ?>
+<div class="prod-list-wrap group">
+  <div id="productsListingListingBottomLinks"  class="navSplitPagesLinks back"><?php echo TEXT_RESULT_PAGE . $listing_split->display_links($max_display_page_links, zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $paginateAsUL); ?></div>
   <div id="productsListingBottomNumber" class="navSplitPagesResult back<?php echo $listing_split->number_of_pages == 1 ? ' navSplitEmpty3rdColumn' : ''; ?>"><?php echo $listing_split->display_count(TEXT_DISPLAY_NUMBER_OF_PRODUCTS); ?></div>
 <?php
   }
@@ -81,31 +65,23 @@
     if ($show_bottom_submit_button == true) {
 ?>
 
-<?php
-  if (PREV_NEXT_BAR_LOCATION == '1') {
-    echo '<div class="prod-list-wrap group button-bottom">';
-  }
-?>
-<div class="forward button-top"><?php echo zen_image_submit(BUTTON_IMAGE_ADD_PRODUCTS_TO_CART, BUTTON_ADD_PRODUCTS_TO_CART_ALT, 'id="submit2" name="submit1"'); ?></div>
-<?php
-  if (PREV_NEXT_BAR_LOCATION == '1') {
-    echo '</div>';
-  }
-?>
+<?php if (PREV_NEXT_BAR_LOCATION == '1') { ?>
+  <div class="prod-list-wrap group button-bottom">
+<?php } ?>
+    <div class="forward button-top"><?php echo zen_image_submit(BUTTON_IMAGE_ADD_PRODUCTS_TO_CART, BUTTON_ADD_PRODUCTS_TO_CART_ALT, 'id="submit2" name="submit1"'); ?></div>
+<?php if (PREV_NEXT_BAR_LOCATION == '1') { ?>
+  </div>
+<?php } ?>
 
 <?php
     } // show_bottom_submit_button
 ?>
-<?php
-  if ($show_bottom_submit_button == true && PREV_NEXT_BAR_LOCATION != '1' && $listing->RecordCount()) {
-    echo '</div>';
-  }
-?>
+<?php if ($listing_split->number_of_rows && (PREV_NEXT_BAR_LOCATION == '2' || PREV_NEXT_BAR_LOCATION == '3') ) { ?>
 </div>
-
-<?php
-  if ($how_many > 0 && PRODUCT_LISTING_MULTIPLE_ADD_TO_CART != 0 and $show_submit == true and $listing_split->number_of_rows > 0) {
-?>
-</form>
 <?php } ?>
 
+</div>
+
+<?php if ($how_many > 0 && PRODUCT_LISTING_MULTIPLE_ADD_TO_CART != 0 and $show_submit == true and $listing_split->number_of_rows > 0) { ?>
+</form>
+<?php } ?>
