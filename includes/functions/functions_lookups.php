abe3576c219b905e50f6128f0daa0c2201d39b0d
mc12345678 <mc12345678@users.noreply.github.com>
2016-04-25 10:46:16 -0400

    Remove unnecessary IDE placed carriage return
    Same as the previous commit, the carriage return that was added by the IDE has been removed from the four locations that it moved text to the next line while changing $prefix to $suffix and vice versa.

--- includes/functions/functions_lookups.php
+++ includes/functions/functions_lookups.php
@@ -676,8 +676,7 @@
 /*
  * Look up SHOW_XXX_INFO switch for product ID and product type
  */
-    function zen_get_show_product_switch_name($lookup, $field, $prefix= 'SHOW_', $suffix= '_INFO', $field_prefix= '_', 
-                                              $field_suffix='') {
+    function zen_get_show_product_switch_name($lookup, $field, $prefix= 'SHOW_', $suffix= '_INFO', $field_prefix= '_', $field_suffix='') {
       global $db;
 
       $sql = "select products_type from " . TABLE_PRODUCTS . " where products_id='" . (int)$lookup . "'";
@@ -687,8 +686,7 @@
       $show_key = $db->Execute($sql);
 
 
-      $zv_key = strtoupper($prefix . $show_key->fields['type_handler'] . $suffix . $field_prefix . $field . 
-          $field_suffix);
+      $zv_key = strtoupper($prefix . $show_key->fields['type_handler'] . $suffix . $field_prefix . $field . $field_suffix);
 
       return $zv_key;
     }
@@ -696,8 +694,7 @@
 /*
  * Look up SHOW_XXX_INFO switch for product ID and product type
  */
-    function zen_get_show_product_switch($lookup, $field, $prefix= 'SHOW_', $suffix= '_INFO', $field_prefix= '_', 
-                                         $field_suffix='') {
+    function zen_get_show_product_switch($lookup, $field, $prefix= 'SHOW_', $suffix= '_INFO', $field_prefix= '_', $field_suffix='') {
       global $db;
 
       $sql = "select products_type from " . TABLE_PRODUCTS . " where products_id='" . $lookup . "'";
@@ -707,8 +704,7 @@
       $show_key = $db->Execute($sql);
 
 
-      $zv_key = strtoupper($prefix . $show_key->fields['type_handler'] . $suffix . $field_prefix . $field . 
-          $field_suffix);
+      $zv_key = strtoupper($prefix . $show_key->fields['type_handler'] . $suffix . $field_prefix . $field . $field_suffix);
 
       $sql = "select configuration_key, configuration_value from " . TABLE_PRODUCT_TYPE_LAYOUT . " where configuration_key='" . $zv_key . "'";
       $zv_key_value = $db->Execute($sql);
