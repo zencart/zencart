111e222ee3c7b811588a83a9d79c9e8482986943
DrByte <drbyte@zen-cart.com>
2016-05-05 11:38:43 -0400

    date

--- includes/modules/payment/authorizenet_aim.php
+++ includes/modules/payment/authorizenet_aim.php
@@ -6,7 +6,7 @@
  * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Author: DrByte  Wed Mar 16 16:12:21 2016 -0500 Modified in v1.5.5 $
+ * @version $Id: Author: DrByte  May 5 2016  Modified in v1.5.5a $
  */
 /**
  * Authorize.net Payment Module (AIM version)
