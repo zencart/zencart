4759fc88e255c6e9675062f6b331f877ef1bc257
DrByte <drbyte@zen-cart.com>
2016-06-16 14:00:52 -0400

    Fix for PHP7 and dashboard widget signature

--- includes/library/zencart/DashboardWidget/src/BannerStatistics.php
+++ includes/library/zencart/DashboardWidget/src/BannerStatistics.php
@@ -3,7 +3,7 @@
  * BannerStatistics Dashboard Widget
  *
  * @package   ZenCart\Admin\DashboardWidget
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @license   http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version   GIT: $Id: $
  */
--- includes/library/zencart/DashboardWidget/src/Logs.php
+++ includes/library/zencart/DashboardWidget/src/Logs.php
@@ -3,7 +3,7 @@
  * Logs Dashboard Widget
  *
  * @package classes
- * @copyright Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: $
  */
