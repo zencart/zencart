eecbc5bac77031bceae7eaf93cfbd10a6d45b135
pRose <prose@rossroberts.com>
2016-06-24 12:18:46 -0700

    fixes data issues with removal of product notifcations/newsletter

--- zc_install/sql/install/mysql_zencart.sql
+++ zc_install/sql/install/mysql_zencart.sql
@@ -1122,26 +1122,6 @@ CREATE TABLE music_genre (
 # --------------------------------------------------------
 
 #
-# Table structure for table 'newsletters'
-#
-
-DROP TABLE IF EXISTS newsletters;
-CREATE TABLE newsletters (
-  newsletters_id int(11) NOT NULL auto_increment,
-  title varchar(255) NOT NULL default '',
-  content text NOT NULL,
-  content_html text NOT NULL,
-  module varchar(255) NOT NULL default '',
-  date_added datetime NOT NULL default '0001-01-01 00:00:00',
-  date_sent datetime default NULL,
-  status int(1) default NULL,
-  locked int(1) default '0',
-  PRIMARY KEY  (newsletters_id)
-) ENGINE=MyISAM;
-
-# --------------------------------------------------------
-
-#
 # Table structure for table 'orders'
 #
 
@@ -2303,7 +2283,6 @@ VALUES ('configMyStore', 'BOX_CONFIGURATION_MY_STORE', 'FILENAME_CONFIGURATION',
        ('layoutController', 'BOX_TOOLS_LAYOUT_CONTROLLER', 'FILENAME_LAYOUT_CONTROLLER', '', 'tools', 'Y', 2),
        ('banners', 'BOX_TOOLS_BANNER_MANAGER', 'FILENAME_BANNER_MANAGER', '', 'tools', 'Y', 3),
        ('mail', 'BOX_TOOLS_MAIL', 'FILENAME_MAIL', '', 'tools', 'Y', 4),
-       ('newsletters', 'BOX_TOOLS_NEWSLETTER_MANAGER', 'FILENAME_NEWSLETTERS', '', 'tools', 'Y', 5),
        ('server', 'BOX_TOOLS_SERVER_INFO', 'FILENAME_SERVER_INFO', '', 'tools', 'Y', 6),
        ('whosOnline', 'BOX_TOOLS_WHOS_ONLINE', 'FILENAME_WHOS_ONLINE', '', 'tools', 'Y', 7),
        ('storeManager', 'BOX_TOOLS_STORE_MANAGER', 'FILENAME_STORE_MANAGER', '', 'tools', 'Y', 9),
