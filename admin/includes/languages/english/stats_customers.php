60e5ab67aa5c13b4e494bc6e4e90123d7cc83ee9
Scott C Wilson <scottcwilson@gmail.com>
2016-06-25 06:37:47 -0400

    Remove unused pre-LEAD report Also note in language file where to find new code

--- admin/includes/languages/english/stats_customers.php
+++ admin/includes/languages/english/stats_customers.php
@@ -1,28 +1,17 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
-// |                                                                      |
-// | http://www.zen-cart.com/index.php                                    |
-// |                                                                      |
-// | Portions Copyright (c) 2003 osCommerce                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-//  $Id: stats_customers.php 1105 2005-04-04 22:05:35Z birdbrain $
-//
+/**
+ * @package admin
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version GIT: $Id:  Modified in v1.6.0 $
+ */
+
+// NOTE: As of v1.6.0, this has been converted into a LEAD page.  See:
+// includes/library/zencart/listingBox/src/boxes/ReportStatsCustomers.php
 
 define('HEADING_TITLE', 'Best Customer Orders-Total');
 
 define('TABLE_HEADING_NUMBER', 'ID#');
 define('TABLE_HEADING_CUSTOMERS', 'Customers');
 define('TABLE_HEADING_TOTAL_PURCHASED', 'Total Purchased');
-?>
