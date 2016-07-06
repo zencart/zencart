897769a9d8588225556ca5cdd7d06eb0130a8e52
Scott C Wilson <scottcwilson@gmail.com>
2016-07-06 03:03:35 -0400

    Find Duplicate Models Find Duplicate Models mod is a new report with edit capabilities to assist in building the documentation for LEAD pages.

---
+++ admin/includes/extra_datafiles/dup_models.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2006 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: $ 
+ */
+define('FILENAME_FINDDUPMODELS', 'dup_models');
+define('BOX_REPORTS_FINDDUPMODELS', 'Find Duplicate Models');
+?>
