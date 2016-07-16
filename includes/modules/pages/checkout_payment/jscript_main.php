306bc74bf5537d943b15a2c0478d9d80dc9dccd7
Chris Brown <drbyte@zen-cart.com>
2016-07-16 17:21:27 -0400

    Fix incorrect selector in checkout_payment form-submit jQuery (#1098)
    Fix posted by  @lat9  ref https://www.zen-cart.com/showthread.php?220658

--- includes/modules/pages/checkout_payment/jscript_main.php
+++ includes/modules/pages/checkout_payment/jscript_main.php
@@ -3,8 +3,7 @@
  * jscript_main
  *
  * @package page
- * @copyright Copyright 2003-2015 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: Ian Wilson   New in v1.6.0 $
  */
@@ -13,11 +12,11 @@
 <script>
 $(document).ready(function(){
   $('form[name="checkout_payment"]').submit(function() {
-      $('.paymentSubmit').attr('disabled', true);
+      $('#paymentSubmit').attr('disabled', true);
 <?php if ($flagOnSubmit) { ?>
       formPassed = check_form();
       if (formPassed == false) {
-          $('.paymentSubmit').attr('disabled', false);
+          $('#paymentSubmit').attr('disabled', false);
       }
       return formPassed;
 <?php } ?>
