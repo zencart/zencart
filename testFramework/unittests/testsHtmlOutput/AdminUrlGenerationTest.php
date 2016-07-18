a8d68a6c3e4f2990ac6917a81a6bb074a9d09e8e
Dayo Akanji <dakanji@user.noreply.github.com>
2016-07-18 22:36:34 +0300

    Updated as per review comments

--- testFramework/unittests/testsHtmlOutput/AdminUrlGenerationTest.php
+++ testFramework/unittests/testsHtmlOutput/AdminUrlGenerationTest.php
@@ -30,15 +30,16 @@ class testAdminUrlGeneration extends zcTestCase
         if (!defined('SESSION_USE_FQDN')) {
             define('SESSION_USE_FQDN', 'True');
         }
-
+        // Do not fail due to E_USER_DEPRECATED warning in zen_href_link()
+		$this->iniSet('error_reporting', -1 & ~E_USER_DEPRECATED);
     }
 
     public function testUrlFunctionsExist()
     {
-        $this->assertTrue(function_exists('zen_catalog_href_link'), 'zen_catalog_href_link() did not exist');
-        $reflect = new ReflectionFunction('zen_catalog_href_link');
-        $this->assertEquals(6, $reflect->getNumberOfParameters());
-        $params = array('page', 'parameters', 'connection', 'search_engine_safe', 'static', 'use_dir_ws_catalog');
+        $this->assertTrue(function_exists('zen_href_link'), 'zen_href_link() did not exist');
+        $reflect = new ReflectionFunction('zen_href_link');
+        $this->assertEquals(4, $reflect->getNumberOfParameters());
+        $params = array('page', 'parameters', 'connection', 'add_session_id');
         foreach ($reflect->getParameters() as $param) {
             $this->assertTrue(in_array($param->getName(), $params));
         }
@@ -57,6 +58,20 @@ class testAdminUrlGeneration extends zcTestCase
      */
     public function testAdminPage()
     {
+        $this->assertURLGenerated(
+            zen_href_link(),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        
+        // Tests for zen_admin_href_link
         $this->assertURLGenerated(
             zen_admin_href_link(),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN
@@ -77,11 +92,11 @@ class testAdminUrlGeneration extends zcTestCase
     public function testAdminPageSsl()
     {
         $this->assertURLGenerated(
-            zen_admin_href_link(FILENAME_DEFAULT),
+            zen_href_link(FILENAME_DEFAULT, null, 'SSL'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN
         );
         $this->assertURLGenerated(
-            zen_admin_href_link(FILENAME_DEFAULT, 'test=test'),
+            zen_href_link(FILENAME_DEFAULT, 'test=test', 'SSL'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
         );
     }
@@ -94,11 +109,21 @@ class testAdminUrlGeneration extends zcTestCase
         $GLOBALS['session_started'] = true;
         define('SID', 'zenadminid=1234567890');
         $this->assertURLGenerated(
-            zen_admin_href_link(FILENAME_DEFAULT),
+            zen_href_link(FILENAME_DEFAULT),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . '?zenadminid=1234567890'
         );
         $this->assertURLGenerated(
-            zen_admin_href_link(FILENAME_DEFAULT, 'test=test'),
+            zen_href_link(FILENAME_DEFAULT, 'test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zenadminid=1234567890'
+        );
+        
+        // Tests for zen_admin_href_link
+        $this->assertURLGenerated(
+             zen_admin_href_link(FILENAME_DEFAULT),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . '?zenadminid=1234567890'
+        );
+        $this->assertURLGenerated(
+             zen_admin_href_link(FILENAME_DEFAULT, 'test=test'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zenadminid=1234567890'
         );
     }
@@ -108,6 +133,32 @@ class testAdminUrlGeneration extends zcTestCase
      */
     public function testAutoCorrectLeadingQuerySeparator()
     {
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '&test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '&&test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '?test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '??test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '?&test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '&?test=test'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        
+        // Tests for zen_admin_href_link
         $this->assertURLGenerated(
             zen_admin_href_link(FILENAME_DEFAULT, '&test=test'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
@@ -140,11 +191,37 @@ class testAdminUrlGeneration extends zcTestCase
     public function testAutoCorrectTrailingQuerySeparator()
     {
         $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test?'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test??'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test?&'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&?'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
+        );
+        
+        // Tests for zen_admin_href_link
+        $this->assertURLGenerated(
             zen_admin_href_link(FILENAME_DEFAULT, 'test=test&'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
         );
         $this->assertURLGenerated(
-            zen_admin_href_link(FILENAME_DEFAULT, 'test=test&&'),
+           zen_admin_href_link(FILENAME_DEFAULT, 'test=test&&'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test'
         );
         $this->assertURLGenerated(
@@ -170,6 +247,46 @@ class testAdminUrlGeneration extends zcTestCase
      */
     public function testAutoCorrectMultipleAmpersandsInQuery()
     {
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '&&test=test&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '&&&test=test&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, '&&&&test=test&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&zen-cart=the-art-of-e-commerce&&'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&zen-cart=the-art-of-e-commerce&&&'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&zen-cart=the-art-of-e-commerce&&&&'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        
+        // Tests for zen_admin_href_link
         $this->assertURLGenerated(
             zen_admin_href_link(FILENAME_DEFAULT, 'test=test&&zen-cart=the-art-of-e-commerce'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
@@ -216,6 +333,24 @@ class testAdminUrlGeneration extends zcTestCase
     public function testAutoCorrectAmpersandEntitiesInQuery()
     {
         $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;&amp;&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;&amp;&amp;&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        
+        // Tests for zen_admin_href_link
+        $this->assertURLGenerated(
             zen_admin_href_link(FILENAME_DEFAULT, 'test=test&amp;zen-cart=the-art-of-e-commerce'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
         );
@@ -238,6 +373,40 @@ class testAdminUrlGeneration extends zcTestCase
      */
     public function testAutoCorrectMixedAmpersandAndAmbersandEntitiesInQuery()
     {
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;&&&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&&&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&amp;&&amp;zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_DEFAULT, 'test=test&&amp;&zen-cart=the-art-of-e-commerce'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
+        );
+        
+        // Tests for zen_admin_href_link
         $this->assertURLGenerated(
             zen_admin_href_link(FILENAME_DEFAULT, 'test=test&amp;&zen-cart=the-art-of-e-commerce'),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_DEFAULT . '&amp;test=test&amp;zen-cart=the-art-of-e-commerce'
@@ -277,6 +446,32 @@ class testAdminUrlGeneration extends zcTestCase
      */
     public function testConfigurationURLs()
     {
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_CONFIGURATION),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_CONFIGURATION
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_CONFIGURATION, 'gID=1'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_CONFIGURATION . '&amp;gID=1'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_CONFIGURATION, 'gID=1&cID=1'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_CONFIGURATION . '&amp;gID=1&amp;cID=1'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_CONFIGURATION, 'gID=1&cID=1&action=edit'),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_CONFIGURATION . '&amp;gID=1&amp;cID=1&amp;action=edit'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_CONFIGURATION, array('gID' => '1', 'cID' => '1', 'action' => 'edit')),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_CONFIGURATION . '&amp;gID=1&amp;cID=1&amp;action=edit'
+        );
+        $this->assertURLGenerated(
+            zen_href_link(FILENAME_CONFIGURATION, array('gID' => '1', 'cID' => '1', 'action' => 'save')),
+            ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_CONFIGURATION . '&amp;gID=1&amp;cID=1&amp;action=save'
+        );
+        
+        // Tests for zen_admin_href_link
         $this->assertURLGenerated(
             zen_admin_href_link(FILENAME_CONFIGURATION),
             ADMIN_HTTP_SERVER . DIR_WS_ADMIN . 'index.php?cmd=' . FILENAME_CONFIGURATION
