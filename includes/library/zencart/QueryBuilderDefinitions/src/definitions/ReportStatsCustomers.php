054d046841a72067bfaa5b07683866debd858ef0
Scott C Wilson <scottcwilson@gmail.com>
2016-07-27 15:35:26 -0400

    Remove commented out line. (#1156)

--- includes/library/zencart/QueryBuilderDefinitions/src/definitions/ReportStatsCustomers.php
+++ includes/library/zencart/QueryBuilderDefinitions/src/definitions/ReportStatsCustomers.php
@@ -2,7 +2,7 @@
 /**
  * Class Index
  *
- * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @copyright Copyright 2003-2016 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id:$
  */
@@ -95,7 +95,6 @@ class ReportStatsCustomers extends AbstractLeadDefinition
                         'common' => array(
                             'title' => TABLE_HEADING_CUSTOMERS,
                             'align' => 'right',
-                            //                            'type' => 'text',
                             'size' => '30'
                         )
                     )
