a8267dd868867d2df8af93c030a7e359149fc5d5
zcwilt <ian@zen-cart.com>
2016-09-08 11:44:51 +0100

    rename class to remove ambiguity

--- includes/library/zencart/Paginator/src/AbstractAdapter.php
+++ includes/library/zencart/Paginator/src/AbstractAdapter.php
@@ -2,7 +2,7 @@
 /**
  * Class AbstractAdapter
  *
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: $
  */
