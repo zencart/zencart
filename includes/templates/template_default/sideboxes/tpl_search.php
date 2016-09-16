b21e65dbd62ac3cfcaff81bd73d61b77d0a2beff
Chris Brown <drbyte@zen-cart.com>
2016-09-16 13:42:57 -0400

    Fix "non well formed numeric value" in tpl_search.php (#1292)

--- includes/templates/template_default/sideboxes/tpl_search.php
+++ includes/templates/template_default/sideboxes/tpl_search.php
@@ -15,10 +15,10 @@
   $content .= zen_draw_hidden_field('search_in_description', '1') . zen_hide_session_id();
 
   if (strtolower(IMAGE_USE_CSS_BUTTONS) == 'yes') {
-    $content .= zen_draw_input_field('keyword', '', 'size="18" maxlength="100" style="width: ' . ($column_width-30) . 'px" placeholder="' . SEARCH_DEFAULT_TEXT . '"') . '<br />' . zen_image_submit (BUTTON_IMAGE_SEARCH,HEADER_SEARCH_BUTTON);
+    $content .= zen_draw_input_field('keyword', '', 'size="18" maxlength="100" style="width: ' . ((int)$column_width-30) . 'px" placeholder="' . SEARCH_DEFAULT_TEXT . '"') . '<br />' . zen_image_submit (BUTTON_IMAGE_SEARCH,HEADER_SEARCH_BUTTON);
     $content .= '<br /><a href="' . zen_href_link(FILENAME_ADVANCED_SEARCH) . '">' . BOX_SEARCH_ADVANCED_SEARCH . '</a>';
   } else {
-    $content .= zen_draw_input_field('keyword', '', 'size="18" maxlength="100" style="width: ' . ($column_width-30) . 'px" placeholder="' . SEARCH_DEFAULT_TEXT . '" onfocus="if (this.value == \'' . SEARCH_DEFAULT_TEXT . '\') this.value = \'\';" onblur="if (this.value == \'\') this.value = \'' . SEARCH_DEFAULT_TEXT . '\';"') . '<br /><input type="submit" value="' . HEADER_SEARCH_BUTTON . '" style="width: 50px" />';
+    $content .= zen_draw_input_field('keyword', '', 'size="18" maxlength="100" style="width: ' . ((int)$column_width-30) . 'px" placeholder="' . SEARCH_DEFAULT_TEXT . '" onfocus="if (this.value == \'' . SEARCH_DEFAULT_TEXT . '\') this.value = \'\';" onblur="if (this.value == \'\') this.value = \'' . SEARCH_DEFAULT_TEXT . '\';"') . '<br /><input type="submit" value="' . HEADER_SEARCH_BUTTON . '" style="width: 50px" />';
     $content .= '<br /><a href="' . zen_href_link(FILENAME_ADVANCED_SEARCH) . '">' . BOX_SEARCH_ADVANCED_SEARCH . '</a>';
   }
 
