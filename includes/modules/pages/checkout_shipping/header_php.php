13d8a8e4e62600dad9b3866552932a0cf5b52716
Chris Brown <drbyte@zen-cart.com>
2016-10-17 15:20:57 -0400

    Fix: checkout_shipping: No method chosen after cart goes from virtual to mixed (#1316)
    Thanks @lat9
    
    Ref https://www.zen-cart.com/showthread.php?220727-checkout_shipping-No-method-chosen-after-cart-goes-from-virtual-to-mixed

--- includes/modules/pages/checkout_shipping/header_php.php
+++ includes/modules/pages/checkout_shipping/header_php.php
@@ -199,7 +199,7 @@ if (isset($_SESSION['cart']->cartID)) {
       }
     }
     $checkval = $_SESSION['shipping']['id'];
-    if (!in_array($checkval, $checklist) && $_SESSION['shipping']['id'] != 'free_free') {
+    if (!in_array($checkval, $checklist)) {
       $messageStack->add('checkout_shipping', ERROR_PLEASE_RESELECT_SHIPPING_METHOD, 'error');
     }
   }
