9a7bc2386128913ba228a22c0bbfd83e30fe194c
Chris Brown <drbyte@zen-cart.com>
2016-10-18 14:10:01 -0400

    Rearrange function declarations in ajax handler for compatibility

--- ajax.php
+++ ajax.php
@@ -8,17 +8,45 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: Author: zcwilt  Thu Dec 31 19:12:00 2015 +0000 Modified in v1.5.5 $
  */
-
 // Abort if the request was not an AJAX call
 if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
     http_response_code(400); // "Bad Request"
     exit();
 }
-
 require('includes/application_top.php');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 header("Access-Control-Allow-Headers: X-Requested-With");
+
+
+// --- support functions ------------------
+if (!function_exists('utf8_encode_recurse')) {
+    function utf8_encode_recurse($mixed_value)
+    {
+        if (strtolower(CHARSET) == 'utf-8') {
+            return $mixed_value;
+        } elseif (!is_array($mixed_value)) {
+            return utf8_encode((string)$mixed_value);
+        } else {
+            $result = array();
+            foreach ($mixed_value as $key => $value) {
+                $result[$key] = utf8_encode($value);
+            }
+            return $result;
+        }
+    }
+}
+
+function ajaxAbort($status = 400, $msg = null)
+{
+    http_response_code($status); // 400 = "Bad Request"
+    if ($msg) echo $msg;
+    require('includes/application_bottom.php');
+    exit();
+}
+// --- support functions ------------------
+
+
 
 if (!isset($_GET['act']) || !isset($_GET['method'])) {
     ajaxAbort();
@@ -45,31 +73,3 @@ $result = call_user_func(array($class, $_GET['method']));
 $result = utf8_encode_recurse($result);
 echo json_encode($result);
 require('includes/application_bottom.php');
-
-
-
-// --- Additional support functions ------------------
-if (!function_exists('utf8_encode_recurse')) {
-    function utf8_encode_recurse($mixed_value)
-    {
-        if (strtolower(CHARSET) == 'utf-8') {
-            return $mixed_value;
-        } elseif (!is_array($mixed_value)) {
-            return utf8_encode((string)$mixed_value);
-        } else {
-            $result = array();
-            foreach ($mixed_value as $key => $value) {
-                $result[$key] = utf8_encode($value);
-            }
-            return $result;
-        }
-    }
-}
-
-function ajaxAbort($status = 400, $msg = null)
-{
-    http_response_code($status); // 400 = "Bad Request"
-    if ($msg) echo $msg;
-    require('includes/application_bottom.php');
-    exit();
-}
