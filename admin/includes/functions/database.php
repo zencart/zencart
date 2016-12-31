7adbc61c93ee09af3cfb5ff201ee18989eadd481
Chris Brown <drbyte@zen-cart.com>
2016-12-31 13:55:35 -0500

    Revert "Backport rest of #898 for consistency in NULL between admin/catalog"
    This reverts commit 3cb885db6a7c61431d6d5dbf10bcbae8bf6cc99d.

--- admin/includes/functions/database.php
+++ admin/includes/functions/database.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Copyright 2003-2014 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version GIT: $Id: Author: Ian Wilson  Modified in v1.5.5 $
+ * @version GIT: $Id: Author: Ian Wilson  Modified in v1.5.4 $
  */
 
 
@@ -23,7 +23,7 @@
           case 'now()':
             $query .= 'now(), ';
             break;
-          case 'NULL':
+          case 'null':
             $query .= 'null, ';
             break;
           default:
@@ -39,7 +39,7 @@
           case 'now()':
             $query .= $columns . ' = now(), ';
             break;
-          case 'NULL':
+          case 'null':
             $query .= $columns .= ' = null, ';
             break;
           default:
