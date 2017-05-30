292e139f3517756b830ef271bf2967693fb90e29
Chris Brown <drbyte@zen-cart.com>
2017-05-30 13:30:36 -0400

    Fix bug: when not logged in, tax zone defaults were providing a string value instead of a zero.
    Resolves: `PHP Warning:  A non-numeric value encountered in /includes/functions/functions_taxes.php on line 172`

--- includes/functions/functions_taxes.php
+++ includes/functions/functions_taxes.php
@@ -3,10 +3,10 @@
  * functions_taxes
  *
  * @package functions
- * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @copyright Copyright 2003-2017 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: functions_taxes.php 16190 2010-05-03 20:18:57Z wilt $
+ * @version $Id: functions_taxes.php 2017-05-30 $
  */
 
 ////
