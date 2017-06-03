4d5afa680d9e3736700f1e9818454cf5edf5e255
Chris Brown <drbyte@zen-cart.com>
2017-06-03 19:45:31 -0400

    Use jQuery noConflict mode

--- includes/modules/pages/checkout_payment/jscript_payeezy.php
+++ includes/modules/pages/checkout_payment/jscript_payeezy.php
@@ -133,7 +133,7 @@ var Payeezy = function() {
 }();
 
 var responseHandler = function(status, response) {
-    var $form = $('form[name="checkout_payment"]');
+    var $form = jQuery('form[name="checkout_payment"]');
 
     // alert('Status = ' + status);
 
@@ -160,18 +160,18 @@ var responseHandler = function(status, response) {
         // alert('success');
         console.log(response);
         var result = response.token.value;
-        $('#payeezyjszc_fdtoken').val(result);
+        jQuery('#payeezyjszc_fdtoken').val(result);
 
-        // alert('FDToken:' + $('#payeezyjszc_fdtoken').val());
+        // alert('FDToken:' + jQuery('#payeezyjszc_fdtoken').val());
 
         $form.unbind();
         $form.off();
 
-        var cc_num = $('#payeezyjszc_cc-number').val();
+        var cc_num = jQuery('#payeezyjszc_cc-number').val();
         var lastFour = cc_num.substr(cc_num.length - 4)
         var firstFour = cc_num.substr(0, 4)
         var new_cc_num = firstFour + '-XXXX-XXXX-' + lastFour;
-        $('#payeezyjszc_cc-number').val(new_cc_num);
+        jQuery('#payeezyjszc_cc-number').val(new_cc_num);
 
         // delay for DOM update
         setTimeout($form.submit(), 800);
