160a657df99553b2d6c36a217adc2c6c9ed77132
Zen4All <info@zen4all.nl>
2017-06-27 21:01:11 +0200

    remove obsolete file

--- admin/includes/extra_datafiles/dup_models.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package admin
- * @copyright Copyright 2006 Zen Cart Development Team
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: $ 
- */
-define('FILENAME_FINDDUPMODELS', 'dup_models');
