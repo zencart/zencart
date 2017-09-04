44301ec70d8d16981b110b47e449eae942b0e1bc
Chris Brown <drbyte@zen-cart.com>
2017-09-04 12:44:18 +0100

    Update jQuery/Bootstrap dist components

--- admin/includes/javascript_loader.php
+++ admin/includes/javascript_loader.php
@@ -9,10 +9,10 @@
  * @version $Id: Author: DrByte  Aug 2017  New in v1.5.6 $
  */
 ?>
-<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script> -->
-<!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
+<!--<script type="text/javascript" src="//code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>-->
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
 
-<script src="includes/javascript/jquery-1.12.1.min.js"></script>
+<script src="includes/javascript/jquery-1.12.4.min.js"></script>
 <script src="includes/javascript/bootstrap.min.js"></script>
 
 <?php if (file_exists(DIR_WS_INCLUDES . 'keepalive_module.php')) require(DIR_WS_INCLUDES . 'keepalive_module.php'); ?>
