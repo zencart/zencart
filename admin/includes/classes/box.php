db98ba48257a9044357fc5d2d2be6fb3ba6efec4
Zen4All <info@zen4all.nl>
2017-11-09 12:37:01 +0100

    update to make use of divs
    This updates the info side box to make use of divs instead of tables.

--- admin/includes/classes/box.php
+++ admin/includes/classes/box.php
@@ -31,28 +31,12 @@ if (!defined('IS_ADMIN_FLAG')) {
     }
 
     function infoBox($heading, $contents) {
-      $this->table_row_parameters = 'class="infoBoxHeading"';
-      $this->table_data_parameters = 'class="infoBoxHeading"';
+      $this->table_row_parameters = 'infoBoxHeading';
+      $this->table_data_parameters = 'infoBoxHeading';
       $this->heading = $this->tableBlock($heading);
 
       $this->table_row_parameters = '';
-      $this->table_data_parameters = 'class="infoBoxContent"';
-      $this->contents = $this->tableBlock($contents);
-
-      return $this->heading . $this->contents;
-    }
-
-    function menuBox($heading, $contents) {
-      $this->table_data_parameters = 'class="menuBoxHeading"';
-      if (isset($heading[0]['link'])) {
-        $this->table_data_parameters .= ' onmouseover="this.style.cursor=\'hand\'" onclick="document.location.href=\'' . $heading[0]['link'] . '\'"';
-        $heading[0]['text'] = '&nbsp;<a href="' . $heading[0]['link'] . '" class="menuBoxHeadingLink">' . $heading[0]['text'] . '</a>&nbsp;';
-      } else {
-        $heading[0]['text'] = '&nbsp;' . $heading[0]['text'] . '&nbsp;';
-      }
-      $this->heading = $this->tableBlock($heading);
-
-      $this->table_data_parameters = 'class="menuBoxContent"';
+      $this->table_data_parameters = 'infoBoxContent';
       $this->contents = $this->tableBlock($contents);
 
       return $this->heading . $this->contents;
