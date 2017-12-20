678c03480b680feaec2588ae221b69d155f65411
Chris Brown <drbyte@zen-cart.com>
2017-12-19 21:27:25 -0500

    Fix EZ-Pages internal-URL over-sanitization

--- admin/includes/init_includes/init_sanitize.php
+++ admin/includes/init_includes/init_sanitize.php
@@ -206,7 +206,7 @@ $group = array('pages_title', 'page_params', 'music_genre_name', 'artists_name',
                'group_name', 'geo_zone_name', 'geo_zone_description',
                'tax_class_description', 'tax_class_title', 'tax_description', 'entry_company', 'customers_firstname',
                'customers_lastname', 'entry_street_address', 'entry_suburb', 'entry_city', 'entry_state', 'customers_referral',
-               'symbol_left', 'symbol_right', 'products_model');
+               'symbol_left', 'symbol_right', 'products_model', 'alt_url');
 $sanitizer->addSimpleSanitization('WORDS_AND_SYMBOLS_REGEX', $group);
 
 $group = array('metatags_title', 'metatags_keywords', 'metatags_description');
