ef87d10b7a696ea255f9b43bd88016f60769c4e6
Zen4All <info@zen4all.nl>
2018-10-02 19:29:10 +0200

    Re-Style Invoice/packingslip for consistency (#1800)
    - Remove some layers of table structure
    - remove unnecessary order_check query, as this data is already in $order
    - add container class, to make the width smaller on wide screens, better resembling a paper sheet

--- admin/packingslip.php
+++ admin/packingslip.php
@@ -3,209 +3,204 @@
  * @package admin
  * @copyright Copyright 2003-2018 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
- * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: packingslip.php  drbyte  Modified in v1.5.6 $
-*/
+ * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: packingslip.php Zen4All  Modified in v1.5.6 $
+ */
+require('includes/application_top.php');
 
-  require('includes/application_top.php');
+require(DIR_WS_CLASSES . 'currencies.php');
+$currencies = new currencies();
 
-  require(DIR_WS_CLASSES . 'currencies.php');
-  $currencies = new currencies();
+$oID = zen_db_prepare_input($_GET['oID']);
 
-  $oID = zen_db_prepare_input($_GET['oID']);
-  $orders = $db->Execute("select orders_id
-                          from " . TABLE_ORDERS . "
-                          where orders_id = '" . (int)$oID . "'");
+include DIR_FS_CATALOG . DIR_WS_CLASSES . 'order.php';
+$order = new order($oID);
 
-  include DIR_FS_CATALOG . DIR_WS_CLASSES . 'order.php';
-  $order = new order($oID);
+// prepare order-status pulldown list
+$orders_statuses = array();
+$orders_status_array = array();
+$orders_status = $db->Execute("SELECT orders_status_id, orders_status_name
+                               FROM " . TABLE_ORDERS_STATUS . "
+                               WHERE language_id = " . (int)$_SESSION['languages_id']);
+foreach ($orders_status as $order_status) {
+  $orders_statuses[] = array(
+    'id' => $order_status['orders_status_id'],
+    'text' => $order_status['orders_status_name'] . ' [' . $order_status['orders_status_id'] . ']');
+  $orders_status_array[$order_status['orders_status_id']] = $order_status['orders_status_name'];
+}
 
-  // prepare order-status pulldown list
-  $orders_statuses = array();
-  $orders_status_array = array();
-  $orders_status = $db->Execute("select orders_status_id, orders_status_name
-                                 from " . TABLE_ORDERS_STATUS . "
-                                 where language_id = '" . (int)$_SESSION['languages_id'] . "'");
-  while (!$orders_status->EOF) {
-    $orders_statuses[] = array('id' => $orders_status->fields['orders_status_id'],
-                               'text' => $orders_status->fields['orders_status_name'] . ' [' . $orders_status->fields['orders_status_id'] . ']');
-    $orders_status_array[$orders_status->fields['orders_status_id']] = $orders_status->fields['orders_status_name'];
-    $orders_status->MoveNext();
-  }
+$show_customer = false;
+if ($order->billing['name'] != $order->delivery['name']) {
+  $show_customer = true;
+}
+if ($order->billing['street_address'] != $order->delivery['street_address']) {
+  $show_customer = true;
+}
 ?>
-<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!doctype html>
 <html <?php echo HTML_PARAMS; ?>>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
-<title><?php echo TITLE; ?></title>
-<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
-<script language="javascript" src="includes/menu.js"></script>
-</head>
-<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
-<!-- body_text //-->
-<table border="0" width="100%" cellspacing="0" cellpadding="2">
-  <tr>
-    <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
-      <tr>
-        <td class="pageHeading"><?php echo nl2br(STORE_NAME_ADDRESS); ?></td>
-        <td class="pageHeading" align="right"><?php echo zen_image(DIR_WS_IMAGES . HEADER_LOGO_IMAGE, HEADER_ALT_TEXT); ?></td>
-      </tr>
-    </table></td>
-  </tr>
-  <tr>
-    <td><table width="100%" border="0" cellspacing="0" cellpadding="2">
-      <tr>
-        <td colspan="2"><?php echo zen_draw_separator(); ?></td>
-      </tr>
+  <head>
+    <meta charset="<?php echo CHARSET; ?>">
+    <title><?php echo TITLE; ?></title>
+    <link rel="stylesheet" href="includes/stylesheet.css">
+  </head>
+  <body>
+    <div class="container">
+      <!-- body_text //-->
+      <table class="table">
+        <tr>
+          <td class="pageHeading"><?php echo nl2br(STORE_NAME_ADDRESS); ?></td>
+          <td class="pageHeading" align="right"><?php echo zen_image(DIR_WS_IMAGES . HEADER_LOGO_IMAGE, HEADER_ALT_TEXT); ?></td>
+        </tr>
+      </table>
+      <div><?php echo zen_draw_separator(); ?></div>
+      <table class="table">
+          <?php
+          if ($show_customer == true) {
+            ?>
+          <tr>
+            <td class="main" colspan="2"><b><?php echo ENTRY_CUSTOMER; ?></b></td>
+          </tr>
+          <tr>
+            <td class="main" colspan="2"><?php echo zen_address_format($order->customer['format_id'], $order->customer, 1, '', '<br>'); ?></td>
+          </tr>
+        <?php } ?>
+        <tr>
+          <td style="border: none">
+            <table>
+              <tr>
+                <td class="main"><b><?php echo ENTRY_SOLD_TO; ?></b></td>
+              </tr>
+              <tr>
+                <td class="main"><?php echo zen_address_format($order->billing['format_id'], $order->billing, 1, '', '<br>'); ?></td>
+              </tr>
+              <tr>
+                <td><?php echo zen_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
+              </tr>
+              <tr>
+                <td class="main">
+                    <?php echo ENTRY_TELEPHONE_NUMBER . ' ' . $order->customer['telephone']; ?>
+                </td>
+              </tr>
+              <tr>
+                <td class="main"><?php echo '<a href="mailto:' . $order->customer['email_address'] . '">' . $order->customer['email_address'] . '</a>'; ?></td>
+              </tr>
+            </table>
+          </td>
+          <td style="border: none">
+            <table>
+              <tr>
+                <td class="main"><b><?php echo ENTRY_SHIP_TO; ?></b></td>
+              </tr>
+              <tr>
+                <td class="main"><?php echo zen_address_format($order->delivery['format_id'], $order->delivery, 1, '', '<br>'); ?></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <td class="main"><strong><?php echo ENTRY_ORDER_ID; ?></strong></td>
+          <td class="main"><?php echo $oID; ?></td>
+        </tr>
+        <tr>
+          <td class="main"><strong><?php echo ENTRY_DATE_PURCHASED; ?></strong></td>
+          <td class="main"><?php echo zen_date_long($order->info['date_purchased']); ?></td>
+        </tr>
+        <tr>
+          <td class="main"><strong><?php echo ENTRY_PAYMENT_METHOD; ?></strong></td>
+          <td class="main"><?php echo $order->info['payment_method']; ?></td>
+        </tr>
+      </table>
+      <div><?php echo zen_draw_separator('pixel_trans.gif', '', '10'); ?></div>
+      <table class="table table-striped">
+        <thead>
+          <tr class="dataTableHeadingRow">
+            <th class="dataTableHeadingContent" colspan="2"><?php echo TABLE_HEADING_PRODUCTS; ?></th>
+            <th class="dataTableHeadingContent"><?php echo TABLE_HEADING_PRODUCTS_MODEL; ?></th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php
+            for ($i = 0, $n = sizeof($order->products); $i < $n; $i++) {
+              ?>
+            <tr class="dataTableRow">
+              <td class="dataTableContent text-right"><?php echo $order->products[$i]['qty']; ?>&nbsp;x</td>
+              <td class="dataTableContent"><?php echo $order->products[$i]['name']; ?>
+                  <?php
+                  if (isset($order->products[$i]['attributes']) && (sizeof($order->products[$i]['attributes']) > 0)) {
+                    ?>
+                  <ul>
+                      <?php
+                      for ($j = 0, $k = sizeof($order->products[$i]['attributes']); $j < $k; $j++) {
+                        ?>
+                      <li>
+                        <small><i>
+                                <?php echo $order->products[$i]['attributes'][$j]['option'] . ': ' . nl2br(zen_output_string_protected($order->products[$i]['attributes'][$j]['value'])); ?>
+                          </i></small>
+                      </li>
+                      <?php
+                    }
+                    ?>
+                  </ul>
+                  <?php
+                }
+                ?>
+              </td>
+              <td class="dataTableContent"><?php echo $order->products[$i]['model']; ?></td>
+            </tr>
+            <?php
+          }
+          ?>
+        </tbody>
+      </table>
+      <?php if (ORDER_COMMENTS_PACKING_SLIP > 0) { ?>
+        <table class="table table-condensed" style="width:25%;">
+          <thead>
+            <tr>
+              <th class="text-center"><strong><?php echo TABLE_HEADING_DATE_ADDED; ?></strong></th>
+              <th class="text-center"><strong><?php echo TABLE_HEADING_STATUS; ?></strong></th>
+              <th class="text-center"><strong><?php echo TABLE_HEADING_COMMENTS; ?></strong></th>
+            </tr>
+          </thead>
+          <tbody>
+              <?php
+              $orders_history = $db->Execute("SELECT orders_status_id, date_added, customer_notified, comments
+                                            FROM " . TABLE_ORDERS_STATUS_HISTORY . "
+                                            WHERE orders_id = " . zen_db_input($oID) . "
+                                            AND customer_notified >= 0
+                                            ORDER BY date_added");
 
-<?php
-      $order_check = $db->Execute("select cc_cvv, customers_name, customers_company, customers_street_address,
-                                    customers_suburb, customers_city, customers_postcode,
-                                    customers_state, customers_country, customers_telephone,
-                                    customers_email_address, customers_address_format_id, delivery_name,
-                                    delivery_company, delivery_street_address, delivery_suburb,
-                                    delivery_city, delivery_postcode, delivery_state, delivery_country,
-                                    delivery_address_format_id, billing_name, billing_company,
-                                    billing_street_address, billing_suburb, billing_city, billing_postcode,
-                                    billing_state, billing_country, billing_address_format_id,
-                                    payment_method, cc_type, cc_owner, cc_number, cc_expires, currency,
-                                    currency_value, date_purchased, orders_status, last_modified
-                             from " . TABLE_ORDERS . "
-                             where orders_id = '" . (int)$oID . "'");
-  $show_customer = 'false';
-  if ($order_check->fields['billing_name'] != $order_check->fields['delivery_name']) {
-    $show_customer = 'true';
-  }
-  if ($order_check->fields['billing_street_address'] != $order_check->fields['delivery_street_address']) {
-    $show_customer = 'true';
-  }
-  if ($show_customer == 'true') {
-?>
-      <tr>
-        <td class="main"><b><?php echo ENTRY_CUSTOMER; ?></b></td>
-      </tr>
-      <tr>
-        <td class="main"><?php echo zen_address_format($order->customer['format_id'], $order->customer, 1, '', '<br>'); ?></td>
-      </tr>
-<?php } ?>
-      <tr>
-        <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="2">
-         <tr>
-            <td class="main"><b><?php echo ENTRY_SOLD_TO; ?></b></td>
-          </tr>
-          <tr>
-            <td class="main"><?php echo zen_address_format($order->billing['format_id'], $order->billing, 1, '', '<br>'); ?></td>
-          </tr>
-          <tr>
-            <td><?php echo zen_draw_separator('pixel_trans.gif', '1', '5'); ?></td>
-          </tr>
-          <tr>
-            <td class="main"><?php echo $order->customer['telephone']; ?></td>
-          </tr>
-          <tr>
-            <td class="main"><?php echo '<a href="mailto:' . $order->customer['email_address'] . '">' . $order->customer['email_address'] . '</a>'; ?></td>
-          </tr>
-        </table></td>
-        <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="2">
-          <tr>
-            <td class="main"><b><?php echo ENTRY_SHIP_TO; ?></b></td>
-          </tr>
-          <tr>
-            <td class="main"><?php echo zen_address_format($order->delivery['format_id'], $order->delivery, 1, '', '<br>'); ?></td>
-          </tr>
-        </table></td>
-      </tr>
-    </table></td>
-  </tr>
-  <tr>
-    <td><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-  </tr>
-  <tr>
-    <td class="main"><b><?php echo ENTRY_ORDER_ID . $oID; ?></b></td>
-  </tr>
-  <tr>
-    <td><table border="0" cellspacing="0" cellpadding="2">
-      <tr>
-        <td class="main"><strong><?php echo ENTRY_DATE_PURCHASED; ?></strong></td>
-        <td class="main"><?php echo zen_date_long($order->info['date_purchased']); ?></td>
-      </tr>
-      <tr>
-        <td class="main"><b><?php echo ENTRY_PAYMENT_METHOD; ?></b></td>
-        <td class="main"><?php echo $order->info['payment_method']; ?></td>
-      </tr>
-    </table></td>
-  </tr>
-  <tr>
-    <td><?php echo zen_draw_separator('pixel_trans.gif', '1', '10'); ?></td>
-  </tr>
-  <tr>
-    <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
-      <tr class="dataTableHeadingRow">
-        <td class="dataTableHeadingContent" colspan="2"><?php echo TABLE_HEADING_PRODUCTS; ?></td>
-        <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_PRODUCTS_MODEL; ?></td>
-      </tr>
-<?php
-    for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
-      echo '      <tr class="dataTableRow">' . "\n" .
-           '        <td class="dataTableContent" valign="top" align="right">' . $order->products[$i]['qty'] . '&nbsp;x</td>' . "\n" .
-           '        <td class="dataTableContent" valign="top">' . $order->products[$i]['name'];
+              if ($orders_history->RecordCount() > 0) {
+                $count_comments = 0;
+                foreach ($orders_history as $order_history) {
+                  $count_comments++;
+                  ?>
+                <tr>
+                  <td class="text-center"><?php echo zen_datetime_short($order_history['date_added']); ?></td>
+                  <td><?php echo $orders_status_array[$order_history['orders_status_id']]; ?></td>
+                  <td><?php echo ($order_history['comments'] == '' ? TEXT_NONE : nl2br(zen_db_output($order_history['comments']))); ?>&nbsp;</td>
+                </tr>
+                <?php
+                if (ORDER_COMMENTS_PACKING_SLIP == 1 && $count_comments >= 1) {
+                  break;
+                }
+              }
+            } else {
+              ?>
+              <tr>
+                <td colspan="3"><?php echo TEXT_NO_ORDER_HISTORY; ?></td>
+              </tr>
+              <?php
+            }
+            ?>
+          </tbody>
+        </table>
+      <?php } // order comments ?>
+    </div>
 
-      if (isset($order->products[$i]['attributes']) && (sizeof($order->products[$i]['attributes']) > 0)) {
-        for ($j=0, $k=sizeof($order->products[$i]['attributes']); $j<$k; $j++) {
-          echo '<br><nobr><small>&nbsp;<i> - ' . $order->products[$i]['attributes'][$j]['option'] . ': ' . nl2br(zen_output_string_protected($order->products[$i]['attributes'][$j]['value']));
-          echo '</i></small></nobr>';
-        }
-      }
-
-      echo '        </td>' . "\n" .
-           '        <td class="dataTableContent" valign="top">' . $order->products[$i]['model'] . '</td>' . "\n" .
-           '      </tr>' . "\n";
-    }
-?>
-    </table></td>
-  </tr>
-
-<?php if (ORDER_COMMENTS_PACKING_SLIP > 0) { ?>
-      <tr>
-        <td class="main"><table border="0" cellspacing="0" cellpadding="5">
-          <tr>
-            <td class="smallText" align="center"><strong><?php echo TABLE_HEADING_DATE_ADDED; ?></strong></td>
-            <td class="smallText" align="center"><strong><?php echo TABLE_HEADING_STATUS; ?></strong></td>
-            <td class="smallText" align="center"><strong><?php echo TABLE_HEADING_COMMENTS; ?></strong></td>
-          </tr>
-<?php
-    $orders_history = $db->Execute("select orders_status_id, date_added, customer_notified, comments
-                                    from " . TABLE_ORDERS_STATUS_HISTORY . "
-                                    where orders_id = '" . zen_db_input($oID) . "' and customer_notified >= 0
-                                    order by date_added");
-
-    if ($orders_history->RecordCount() > 0) {
-      $count_comments=0;
-      while (!$orders_history->EOF) {
-        $count_comments++;
-        echo '          <tr>' . "\n" .
-             '            <td class="smallText" align="center" valign="top">' . zen_datetime_short($orders_history->fields['date_added']) . '</td>' . "\n";
-        echo '            <td class="smallText" valign="top">' . $orders_status_array[$orders_history->fields['orders_status_id']] . '</td>' . "\n";
-        echo '            <td class="smallText" valign="top">' . ($orders_history->fields['comments'] == '' ? TEXT_NONE : nl2br(zen_db_output($orders_history->fields['comments']))) . '&nbsp;</td>' . "\n" .
-             '          </tr>' . "\n";
-        $orders_history->MoveNext();
-        if (ORDER_COMMENTS_PACKING_SLIP == 1 && $count_comments >= 1) {
-          break;
-        }
-      }
-    } else {
-        echo '          <tr>' . "\n" .
-             '            <td class="smallText" colspan="5">' . TEXT_NO_ORDER_HISTORY . '</td>' . "\n" .
-             '          </tr>' . "\n";
-    }
-?>
-        </table></td>
-      </tr>
-<?php } // order comments ?>
-
-</table>
-<!-- body_text_eof //-->
-<br>
-</body>
+    <!-- body_text_eof //-->
+  </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
