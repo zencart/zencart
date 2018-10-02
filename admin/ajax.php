344c6f8e51045f60a67153d56f2aeab578c9dc6f
Chris Brown <drbyte@zen-cart.com>
2018-10-02 13:30:57 -0400

    Update ajax.php

--- admin/ajax.php
+++ admin/ajax.php
@@ -5,9 +5,8 @@
  * NOTE: "Assumes" that the admin directory is a direct subdirectory off the store's file-system!
  *
  * @package core
- * @copyright Copyright 2003-2017 Zen Cart Development Team
- * @copyright Portions Copyright 2003 osCommerce
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id:  Aug 2017 Modified in v1.5.6 $
+ * @version $Id:  Sept 2018 Modified in v1.5.6 $
  */
 require '../ajax.php';
