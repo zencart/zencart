8c1a7d5f325bc00d7199055aa3524438e0682f81
Scott C Wilson <scottcwilson@gmail.com>
2018-12-11 11:05:40 -0500

    Align with 1.6.0; use "Admin" as prefix

--- admin/includes/languages/english.php
+++ admin/includes/languages/english.php
@@ -52,9 +52,9 @@ function zen_date_raw($date, $reverse = false) {
 //   require(DIR_FS_CATALOG_LANGUAGES . $_SESSION['language'] . '/' . $template_dir_select . 'meta_tags.php');
 
 // used for prefix to browser tabs in admin pages
-define('TEXT_ADMIN_TAB_PREFIX', 'Webstore ');
+define('TEXT_ADMIN_TAB_PREFIX', 'Admin ');
 // if you have multiple stores and want the Store Name to be part of the admin title (ie: for browser tabs), swap this line with the one above
-//define('TEXT_ADMIN_TAB_PREFIX', 'Webstore ' . STORE_NAME);
+//define('TEXT_ADMIN_TAB_PREFIX', 'Admin ' . STORE_NAME);
 
 // meta tags
 define('ICON_METATAGS_ON', 'Meta Tags Defined');
