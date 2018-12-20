b3b872ad63502b99d09c2417c7a069df9f913727
Scott C Wilson <scottcwilson@gmail.com>
2018-12-20 10:28:05 -0500

    prevent ezpages link display from erroring in PHP 7.2+

--- includes/templates/responsive_classic/templates/tpl_ezpages_bar_footer.php
+++ includes/templates/responsive_classic/templates/tpl_ezpages_bar_footer.php
@@ -16,7 +16,7 @@
  */
   include(DIR_WS_MODULES . zen_get_module_directory('ezpages_bar_footer.php'));
 ?>
-<?php if (sizeof($var_linksList) >= 1) { ?>
+<?php if (!empty($var_linksList) && sizeof($var_linksList) >= 1) { ?>
 <?php for ($i=1, $n=sizeof($var_linksList); $i<=$n; $i++) {  ?>
   <li><a href="<?php echo $var_linksList[$i]['link']; ?>"><?php echo $var_linksList[$i]['name']; ?></a></li>
 <?php } // end FOR loop ?>
