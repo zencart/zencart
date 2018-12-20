11ecf0ada348c8bb3608cf220bc6552edc673d2f
Scott C Wilson <scottcwilson@gmail.com>
2018-12-20 10:44:20 -0500

    tightening up

--- includes/templates/responsive_classic/templates/tpl_ezpages_bar_header.php
+++ includes/templates/responsive_classic/templates/tpl_ezpages_bar_header.php
@@ -16,7 +16,7 @@
    */
   include(DIR_WS_MODULES . zen_get_module_directory('ezpages_bar_header.php'));
 ?>
-<?php if (!empty($var_linksList) && sizeof($var_linksList) >= 1) { ?>
+<?php if (!empty($var_linksList)) { ?>
 <div id="navEZPagesTop">
   <ul>
 <?php for ($i=1, $n=sizeof($var_linksList); $i<=$n; $i++) {  ?>
--- includes/templates/template_default/templates/tpl_ezpages_bar_header.php
+++ includes/templates/template_default/templates/tpl_ezpages_bar_header.php
@@ -16,7 +16,7 @@
    */
   include(DIR_WS_MODULES . zen_get_module_directory('ezpages_bar_header.php'));
 ?>
-<?php if (!empty($var_linksList) && sizeof($var_linksList) >= 1) { ?>
+<?php if (!empty($var_linksList)) { ?>
 <div id="navEZPagesTop">
 <?php for ($i=1, $n=sizeof($var_linksList); $i<=$n; $i++) {  ?>
   <a href="<?php echo $var_linksList[$i]['link']; ?>"><?php echo $var_linksList[$i]['name']; ?></a><?php echo ($i < $n ? EZPAGES_SEPARATOR_HEADER : '') . "\n"; ?>
