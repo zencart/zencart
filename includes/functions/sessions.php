b89e30ad0d0e78c60b8d4bb2aa3a4a5e968efb3d
DrByte <drbyte@zen-cart.com>
2018-12-22 16:30:12 -0500

    Tidy session code
    Resolves #1958

--- includes/functions/sessions.php
+++ includes/functions/sessions.php
@@ -4,14 +4,11 @@
  * Session functions
  *
  * @package functions
- * @copyright Copyright 2003-2016 Zen Cart Development Team
+ * @copyright Copyright 2003-2018 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: $
  */
-if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
-}
   if (IS_ADMIN_FLAG === true) {
     if (PADSS_ADMIN_SESSION_TIMEOUT_ENFORCED != 0 && SESSION_TIMEOUT_ADMIN > 900) {
       $SESS_LIFE = 900;
@@ -49,12 +46,12 @@ if (!defined('IS_ADMIN_FLAG')) {
       return $value->fields['value'];
     }
 
-    return ("");
+    return '';
   }
 
   function _sess_write($key, $val) {
     global $db;
-    if (!is_object($db)) return;
+    if (!is_object($db)) return false;
     $val = base64_encode($val);
 
     global $SESS_LIFE;
@@ -112,31 +109,30 @@ if (!defined('IS_ADMIN_FLAG')) {
   }
 
   function zen_session_id($sessid = '') {
-    if (!empty($sessid)) {
-      $tempSessid = $sessid;
-      if (preg_replace('/[a-zA-Z0-9,-]/', '', $tempSessid) != '')
-      {
-        $sessid = md5(uniqid(rand(), true));
-      }
-      return session_id($sessid);
-    } else {
-      return session_id();
+    if (empty($sessid)) {
+        return session_id();
     }
+    $tempSessid = $sessid;
+    if (preg_replace('/[a-zA-Z0-9,-]/', '', $tempSessid) != '')
+    {
+        $sessid = md5(uniqid(rand(), true));
+    }
+    return session_id($sessid);
   }
 
   function zen_session_name($name = '') {
-    if (!empty($name)) {
-      $tempName = $name;
-      if (preg_replace('/[a-zA-Z0-9,-]/', '', $tempName) == '') return session_name($name);
-      return FALSE;
-    } else {
-      return session_name();
+    if (empty($name)) {
+        return session_name();
     }
+    $tempName = $name;
+    if (preg_replace('/[a-zA-Z0-9,-]/', '', $tempName) == '') {
+        return session_name($name);
+    }
+    return false;
   }
 
   function zen_session_write_close() {
     session_write_close();
-    return;
   }
 
   function zen_session_destroy() {
@@ -146,22 +142,18 @@ if (!defined('IS_ADMIN_FLAG')) {
   function zen_session_save_path($path = '') {
     if (!empty($path)) {
       return session_save_path($path);
-    } else {
-      return session_save_path();
     }
+    return session_save_path();
   }
 
   function zen_session_recreate() {
-    global $http_domain, $https_domain, $current_domain;
-      if ($http_domain == $https_domain) {
-      $saveSession = $_SESSION;
+    global $http_domain, $https_domain;
+    if ($http_domain == $https_domain) {
+//      $saveSession = $_SESSION;
       $oldSessID = session_id();
       session_regenerate_id();
       $newSessID = session_id();
-      session_id($oldSessID);
-      session_id($newSessID);
-      session_set_save_handler('_sess_open', '_sess_close', '_sess_read', '_sess_write', '_sess_destroy', '_sess_gc');
-      $_SESSION = $saveSession;
+//      $_SESSION = $saveSession;
       if (IS_ADMIN_FLAG !== true) {
         whos_online_session_recreate($oldSessID, $newSessID);
       }
