ce93f8724e0dcfaa45396aaa9ddf9f4855edea4d
zcwilt <ian@zen-cart.com>
2019-01-05 21:53:59 +0000

    namespacing broken for some new controllers.

--- app/Controllers/admin/OrdersStatus.php
+++ app/Controllers/admin/OrdersStatus.php
@@ -4,7 +4,9 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id:  New in v1.6.0 $
  */
-namespace App\Controllers;
+namespace App\Controllers\admin;
+
+use App\Controllers\AbstractLeadController;
 
 /**
  * Class OrdersStatus
