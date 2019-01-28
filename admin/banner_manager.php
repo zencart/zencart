daf4106ddd259fa5afa855b2415be046c0221099
Zen4All <info@zen4all.nl>
2019-01-28 13:42:21 +0100

    Update banner_manager.php

--- admin/banner_manager.php
+++ admin/banner_manager.php
@@ -466,7 +466,7 @@ if (zen_not_null($action)) {
                     $banners_shown = ($info->fields['banners_shown'] != '') ? $info->fields['banners_shown'] : '0';
                     $banners_clicked = ($info->fields['banners_clicked'] != '') ? $info->fields['banners_clicked'] : '0';
 
-                    if (isset($bInfo) && is_object($bInfo) && ($banner['banners_id'] == $bInfo->banners_id)) {
+                    if ((isset($bInfo) && is_object($bInfo) && ($banner['banners_id'] == $bInfo->banners_id)) {
                       ?>
                     <tr id="defaultSelected" class="dataTableRowSelected" onclick="document.location.href = '<?php echo zen_href_link(FILENAME_BANNER_MANAGER, 'page=' . $_GET['page'] . '&bID=' . $bInfo->banners_id . '&action=new'); ?>'" role="button">
                         <?php
