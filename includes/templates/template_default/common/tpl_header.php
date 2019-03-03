7a71ae275e6054667d236761c0ab1206e0163d52
lat9 <lat9@vinosdefrutastropicales.com>
2019-03-03 14:48:57 -0500

    #2177: Use new functions for templates' headers

--- includes/templates/template_default/common/tpl_header.php
+++ includes/templates/template_default/common/tpl_header.php
@@ -43,7 +43,9 @@ if (!isset($flag_disable_header) || !$flag_disable_header) {
 <div id="navMain">
     <ul class="back">
     <li><?php echo '<a href="' . HTTP_SERVER . DIR_WS_CATALOG . '">'; ?><?php echo HEADER_TITLE_CATALOG; ?></a></li>
-<?php if ($_SESSION['customer_id']) { ?>
+<?php
+    if (zen_is_logged_in() && !zen_in_guest_checkout()) {
+?>
     <li><a href="<?php echo zen_href_link(FILENAME_LOGOFF, '', 'SSL'); ?>"><?php echo HEADER_TITLE_LOGOFF; ?></a></li>
     <li><a href="<?php echo zen_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>"><?php echo HEADER_TITLE_MY_ACCOUNT; ?></a></li>
 <?php
