7ed60c78f9c4123688a55e99c91ce77ff09bae40
Chris Brown <drbyte@zen-cart.com>
2019-05-27 14:02:23 -0400

    Move coupon-remove define to ot_coupon
    Updates e82e90a28ca278c5023147488ad06eca33f70cca
    Ref #2343
    Fixes #2360

--- includes/languages/english/modules/order_total/ot_coupon.php
+++ includes/languages/english/modules/order_total/ot_coupon.php
@@ -11,7 +11,6 @@
   define('MODULE_ORDER_TOTAL_COUPON_HEADER', TEXT_GV_NAMES . '/Discount Coupon');
   define('MODULE_ORDER_TOTAL_COUPON_DESCRIPTION', 'Discount Coupon');
   define('MODULE_ORDER_TOTAL_COUPON_TEXT_ENTER_CODE', TEXT_GV_REDEEM);
-  define('MODULE_ORDER_TOTAL_COUPON_HEADER', TEXT_GV_NAMES . '/Discount Coupon');
   define('SHIPPING_NOT_INCLUDED', ' [Shipping not included]');
   define('TAX_NOT_INCLUDED', ' [Tax not included]');
   define('IMAGE_REDEEM_VOUCHER', 'Redeem Voucher');
@@ -20,3 +19,4 @@
   define('MODULE_ORDER_TOTAL_COUPON_REMOVE_INSTRUCTIONS', '<p>To remove a Discount Coupon from this order replace the coupon code with: ' . TEXT_COMMAND_TO_DELETE_CURRENT_COUPON_FROM_ORDER . '</p>');
   define('TEXT_REMOVE_REDEEM_COUPON', 'Discount Coupon Removed by Request!');
   define('MODULE_ORDER_TOTAL_COUPON_INCLUDE_ERROR', ' Setting Include tax = true, should only happen when recalculate = None');
+  define('TEXT_COMMAND_TO_DELETE_CURRENT_COUPON_FROM_ORDER', 'REMOVE');
