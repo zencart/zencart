8b37d78cf4fba3a71593dbd73b8f68dadfaae84b
Scott C Wilson <scottcwilson@gmail.com>
2019-05-30 11:31:25 -0400

    Remove TABLE_HEADING_QUANTITY override This value is defined in english.php so the override won't work

--- includes/languages/english/advanced_search.php
+++ includes/languages/english/advanced_search.php
@@ -35,7 +35,6 @@ define('ENTRY_DATE_RANGE', 'Search by Date Added');
   define('TABLE_HEADING_MODEL', 'Model');
   define('TABLE_HEADING_PRODUCTS', 'Product Name');
   define('TABLE_HEADING_MANUFACTURER', 'Manufacturer');
-  define('TABLE_HEADING_QUANTITY', 'Quantity');
   define('TABLE_HEADING_PRICE', 'Price');
   define('TABLE_HEADING_WEIGHT', 'Weight');
   define('TABLE_HEADING_BUY_NOW', 'Buy Now');
