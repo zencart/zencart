1672946c12b4206c6616abc0eeefb0b9a46a68bd
mc12345678 <mc12345678@users.noreply.github.com>
2019-06-19 16:28:39 -0400

    Add language file with initial language define
    This file needs to be included/incorporated to the originating class.
    Is a potential source of error messages for val_function errors that can support multiple admin languages.

---
+++ admin/includes/languages/english/configuration_validation.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: mc12345678 Wed Jun 19 20:23:50 2019 +0000 New in v1.5.6c $
+ **/
+ 
+ define('TEXT_EMAIL_ADDRESS_VALIDATE', 'The text entered does not resolve to an acceptable email address. (I.e. Name &lt;email@domain&gt; or &lt;email@domain&gt; or email@domain or any combination of these separated by a comma.');
