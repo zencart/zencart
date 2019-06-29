a5e66c8028ae1eae76b0f3a4449f80937e237e53
mc12345678 <mc12345678@users.noreply.github.com>
2019-06-29 16:55:02 -0400

    Correct TEXT_EMAIL_ADDRESS_VALIDATE statement
    Was missing a closing parenthesis when error was displayed.

--- admin/includes/languages/english/configuration_validation.php
+++ admin/includes/languages/english/configuration_validation.php
@@ -7,4 +7,4 @@
  * @version $Id: mc12345678 Wed Jun 19 20:23:50 2019 +0000 New in v1.5.6c $
  **/
  
- define('TEXT_EMAIL_ADDRESS_VALIDATE', 'The text entered does not resolve to an acceptable email address. (I.e. Name &lt;email@domain&gt; or &lt;email@domain&gt; or email@domain or any combination of these separated by a comma.');
+ define('TEXT_EMAIL_ADDRESS_VALIDATE', 'The text entered does not resolve to an acceptable email address. (I.e. Name &lt;email@domain&gt; or &lt;email@domain&gt; or email@domain or any combination of these separated by a comma.)');
