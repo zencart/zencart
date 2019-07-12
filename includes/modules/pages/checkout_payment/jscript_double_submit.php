91652bd7a505eddafb5ca851343ec66b0a2ed71b
Scott C Wilson <scottcwilson@gmail.com>
2019-07-12 08:00:24 -0400

    Add condition and doc for submitonce check on payment page

--- includes/modules/pages/checkout_payment/jscript_double_submit.php
+++ includes/modules/pages/checkout_payment/jscript_double_submit.php
@@ -1,2 +1,6 @@
 <?php 
-  require(DIR_WS_MODULES . '/pages/checkout_confirmation/jscript_double_submit.php'); 
+  // For payment modules using AJAX for security, confirmation page JS is 
+  // not loaded.  So load the relevant file here. 
+  if (PADSS_AJAX_CHECKOUT == '1') { 
+    require(DIR_WS_MODULES . '/pages/checkout_confirmation/jscript_double_submit.php'); 
+  }
