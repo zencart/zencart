3a77ecfb7eabb617a4968694f42febb9ccae6989
Scott C Wilson <scottcwilson@gmail.com>
2019-07-22 09:48:30 -0400

    Updates from code inspection

--- includes/languages/english/ask_a_question.php
+++ includes/languages/english/ask_a_question.php
@@ -1,24 +1,10 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2003 The zen-cart developers                           |
-// |                                                                      |
-// | http://www.zen-cart.com/index.php                                    |
-// |                                                                      |
-// | Portions Copyright (c) 2003 osCommerce                               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-//
-
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version GIT: $Id: Author: DrByte  Tue Jan 15 15:50:09 2013 -0500 New in v1.5.2 $
+ */
 define('NAVBAR_TITLE', 'Ask a Question');
 
 define('HEADING_TITLE', 'Ask a Question About ');
--- includes/languages/english/extra_definitions/ask_a_question_defines.php
+++ includes/languages/english/extra_definitions/ask_a_question_defines.php
@@ -1,7 +1,11 @@
 <?php
-
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2013 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version GIT: $Id: Author: DrByte  Tue Jan 15 15:50:09 2013 -0500 New in v1.5.2 $
+ * @TODO consider merge into core files 
+ */
 define('ASK_A_QUESTION', 'Ask a question about this product');
 define('BUTTON_IMAGE_ASK_A_QUESTION','button_ask_a_question.gif');
 define('BUTTON_ASK_A_QUESTION_ALT', 'Ask a Question');
-
-?>
