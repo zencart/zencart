zencart/zencart

commit 897fae96fc1b48aa45d35fc5af7fffabca268453

Add download link to order details page for files attached to orders (#2726)
Add download link to order details page

admin/orders.php
@@ -77,6 +77,73 @@ if (!empty($oID) && !empty($action)) {
 
 if (zen_not_null($action) && $order_exists == true) {
   switch ($action) {
+    case 'download': 
+ 
+      $fileName = basename($_GET['filename']); 
+      $file_extension = strtolower(substr(strrchr($fileName, '.'), 1));
+      switch ($file_extension) {
+        case 'csv':
+          $content = 'text/csv';
+          break;
+        case 'zip':
+          $content = 'application/zip';
+          break;
+        case 'jpg':
+          $content = 'image/jpeg';
+          break;
+        case 'jpeg':
+          $content = 'image/jpeg';
+          break;
+        case 'gif':
+          $content = 'image/gif';
+          break;
+        case 'png':
+          $content = 'image/png';
+          break;
+        case 'eps':
+          $content = 'application/postscript';
+          break;
+        case 'cdr':
+          $content = 'application/cdr';
+          break;  
+        case 'ai':
+          $content = 'application/postscript';
+          break;
+        case 'pdf':
+          $content = 'application/pdf';
+          break;
+        case 'tif':
+          $content = 'image/tiff';
+          break;
+        case 'tiff':
+          $content = 'image/tiff';
+          break;
+        case 'bmp':
+          $content = 'image/bmp';
+          break;
+        case 'xls':
+          $content = 'application/vnd.ms-excel';
+          break;
+        case 'numbers':
+          $content = 'application/vnd.ms-excel';
+          break;
+        default:
+          $messageStack->add_session(sprintf(TEXT_EXTENSION_NOT_UNDERSTOOD, $file_extension), 'error');
+          zen_redirect(zen_href_link(FILENAME_ORDERS, zen_get_all_get_params(array('download', 'action'),'action=edit'), 'NONSSL'));
+      }
+      $fs_path = DIR_FS_CATALOG_IMAGES . 'uploads/' . $fileName; 
+      if (!file_exists($fs_path)) {
+        $messageStack->add_session(TEXT_FILE_NOT_FOUND, 'error');
+        zen_redirect(zen_href_link(FILENAME_ORDERS, zen_get_all_get_params(array('download', 'action'),'action=edit')); 
+      }
+      header('Content-type: ' . $content);
+      header('Content-Disposition: attachment; filename="' . $fileName . '"');
+      header('Content-Transfer-Encoding: binary');
+      header('Cache-Control: no-cache, must-revalidate');
+      header("Expires: Mon, 22 Jan 2002 00:00:00 GMT");
+      readfile($fs_path);
+      exit(); 
+
     case 'edit':
       // reset single download to on
       if (!empty($_GET['download_reset_on'])) {
@@ -611,6 +678,10 @@ if (zen_not_null($action) && $order_exists == true) {
                     if (isset($order->products[$i]['attributes']) && (sizeof($order->products[$i]['attributes']) > 0)) {
                       for ($j = 0, $k = sizeof($order->products[$i]['attributes']); $j < $k; $j++) {
                         echo '<br><span style="white-space:nowrap;"><small>&nbsp;<i> - ' . $order->products[$i]['attributes'][$j]['option'] . ': ' . nl2br(zen_output_string_protected($order->products[$i]['attributes'][$j]['value']));
+                        if (zen_is_option_file($order->products[$i]['attributes'][$j]['option_id'])) { 
+                          $upload_name = zen_get_uploaded_file($order->products[$i]['attributes'][$j]['value']); 
+                          echo ' ' . '<a href="' . zen_href_link(FILENAME_ORDERS, 'action=download&oID=' . $oID . '&filename=' .  $upload_name) . '">' . TEXT_DOWNLOAD . '</a>' . ' ';
+                        }
                         if ($order->products[$i]['attributes'][$j]['price'] != '0') {
                           echo ' (' . $order->products[$i]['attributes'][$j]['prefix'] . $currencies->format($order->products[$i]['attributes'][$j]['price'] * $order->products[$i]['qty'], true, $order->info['currency'], $order->info['currency_value']) . ')';
                         }

admin/includes/languages/english/orders.php
@@ -93,6 +93,7 @@ define('WARNING_ORDER_NOT_UPDATED', 'Warning: Nothing to change. The order was n
 define('ENTRY_ORDER_ID','Invoice No. ');
 define('TEXT_INFO_ATTRIBUTE_FREE', '&nbsp;-&nbsp;<span class="alert">FREE</span>');
 
+define('TEXT_DOWNLOAD','Download'); 
 define('TEXT_DOWNLOAD_TITLE', 'Order Download Status');
 define('TEXT_DOWNLOAD_STATUS', 'Status');
 define('TEXT_DOWNLOAD_FILENAME', 'Filename');
@@ -103,6 +104,8 @@ define('TEXT_DOWNLOAD_AVAILABLE', 'Available');
 define('TEXT_DOWNLOAD_EXPIRED', 'Expired');
 define('TEXT_DOWNLOAD_MISSING', 'Not on Server');
 
+define('TEXT_EXTENSION_NOT_UNDERSTOOD', 'File extension %s not supported'); 
+define('TEXT_FILE_NOT_FOUND', 'File not found'); 
 define('IMAGE_ICON_STATUS_CURRENT', 'Status - Available');
 define('IMAGE_ICON_STATUS_EXPIRED', 'Status - Expired');
 define('IMAGE_ICON_STATUS_MISSING', 'Status - Missing');

admin/includes/functions/general.php
@@ -3672,3 +3672,22 @@ function get_logs_data($maxToList = 'count') {
       return -1;
     }
   }
+
+  function zen_is_option_file($option_id) {
+    global $db;
+    $query = $db->Execute("SELECT products_options_type FROM " . TABLE_PRODUCTS_OPTIONS . " WHERE products_options_id = " . (int)$option_id); 
+    $option_type = $query->fields['products_options_type']; 
+    $query = $db->Execute("SELECT products_options_types_name FROM " . TABLE_PRODUCTS_OPTIONS_TYPES . " WHERE products_options_types_id = " . (int)$option_type); 
+    if ($query->fields['products_options_types_name'] == 'File') return true; 
+    return false; 
+  }
+
+  function zen_get_uploaded_file($filename) {
+    global $db; 
+    $parts = explode(". ", $filename, 2);
+    $filenum = $parts[0];
+    $filename = $parts[1]; 
+    $file_parts = explode(".", $filename, 2);
+    $filetype = $file_parts[sizeof($file_parts) - 1]; 
+    return $filenum . "." . $filetype; 
+  }

commit 4b742623a0fb79d16b120b23b7441b297dccc936

Add notifier for admin Orders search-override
Also corrects the PHP error introduced in PR #2726.

admin/orders.php
@@ -134,7 +134,7 @@ if (zen_not_null($action) && $order_exists == true) {
       $fs_path = DIR_FS_CATALOG_IMAGES . 'uploads/' . $fileName; 
       if (!file_exists($fs_path)) {
         $messageStack->add_session(TEXT_FILE_NOT_FOUND, 'error');
-        zen_redirect(zen_href_link(FILENAME_ORDERS, zen_get_all_get_params(array('download', 'action'),'action=edit')); 
+        zen_redirect(zen_href_link(FILENAME_ORDERS, zen_get_all_get_params(array('download', 'action'),'action=edit'))); 
       }
       header('Content-type: ' . $content);
       header('Content-Disposition: attachment; filename="' . $fileName . '"');
@@ -999,7 +999,8 @@ if (zen_not_null($action) && $order_exists == true) {
                   $search_distinct = ' ';
                   $new_table = '';
                   $new_fields = '';
-                  if (isset($_GET['search_orders_products']) && zen_not_null($_GET['search_orders_products'])) {
+                  $keywords = '';
+                  if (!empty($_GET['search_orders_products'])) {
                     $search_distinct = ' distinct ';
                     $new_table = " left join " . TABLE_ORDERS_PRODUCTS . " op on (op.orders_id = o.orders_id) ";
                     $keywords = zen_db_input(zen_db_prepare_input($_GET['search_orders_products']));
@@ -1008,18 +1009,15 @@ if (zen_not_null($action) && $order_exists == true) {
                       $keywords = TRIM(substr($_GET['search_orders_products'], 3));
                       $search = " and op.products_id ='" . (int)$keywords . "'";
                     }
-                  } else {
-
+                  } elseif (!empty($_GET['search'])) {
 // create search filter
-                    $search = '';
-                    if (isset($_GET['search']) && zen_not_null($_GET['search'])) {
-                      $search_distinct = ' ';
                       $keywords = zen_db_input(zen_db_prepare_input($_GET['search']));
                       $search = " and (o.customers_city like '%" . $keywords . "%' or o.customers_postcode like '%" . $keywords . "%' or o.date_purchased like '%" . $keywords . "%' or o.billing_name like '%" . $keywords . "%' or o.billing_company like '%" . $keywords . "%' or o.billing_street_address like '%" . $keywords . "%' or o.delivery_city like '%" . $keywords . "%' or o.delivery_postcode like '%" . $keywords . "%' or o.delivery_name like '%" . $keywords . "%' or o.delivery_company like '%" . $keywords . "%' or o.delivery_street_address like '%" . $keywords . "%' or o.billing_city like '%" . $keywords . "%' or o.billing_postcode like '%" . $keywords . "%' or o.customers_email_address like '%" . $keywords . "%' or o.customers_name like '%" . $keywords . "%' or o.customers_company like '%" . $keywords . "%' or o.customers_street_address  like '%" . $keywords . "%' or o.customers_telephone like '%" . $keywords . "%' or o.ip_address  like '%" . $keywords . "%')";
-                      $new_table = '';
-                    }
-                  } // eof: search orders or orders_products
+                  } 
                   $new_fields .= ", o.customers_company, o.customers_email_address, o.customers_street_address, o.delivery_company, o.delivery_name, o.delivery_street_address, o.billing_company, o.billing_name, o.billing_street_address, o.payment_module_code, o.shipping_module_code, o.ip_address ";
+                  
+                  $order_by = " ORDER BY o.orders_id DESC";
+                  $zco_notifier->notify('NOTIFY_ADMIN_ORDERS_SEARCH_PARMS', $keywords, $search, $search_distinct, $new_fields, $new_table, $order_by);
 
                   $orders_query_raw = "select " . $search_distinct . " o.orders_id, o.customers_id, o.customers_name, o.payment_method, o.shipping_method, o.date_purchased, o.last_modified, o.currency, o.currency_value, s.orders_status_name, ot.text as order_total" .
                       $new_fields . "
@@ -1039,7 +1037,7 @@ if (zen_not_null($action) && $order_exists == true) {
                     $orders_query_raw .= (trim($search) != '') ? preg_replace('/ *AND /i', ' WHERE ', $search) : '';
                   }
 
-                  $orders_query_raw .= " order by o.orders_id DESC";
+                  $orders_query_raw .= $order_by;
 
 // Split Page
 // reset page when page is unknown