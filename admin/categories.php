203f57a0b4b51840512086222136a4784fe4e80d
torvista <4391026+torvista@users.noreply.github.com>
2019-12-05 12:25:22 +0100

    --> PHP Notice: Undefined index: metatags_title in admin1\categories.php on line 497.
    language flag icon ALT was referencing metatags title instead of language name.

--- admin/categories.php
+++ admin/categories.php
@@ -487,14 +487,14 @@ if (is_dir(DIR_FS_CATALOG_IMAGES)) {
           <?php echo TEXT_EDIT_CATEGORIES_META_TAGS_INTRO; ?> - <strong><?php echo $cInfo->categories_id ?> <?php echo $cInfo->categories_name; ?></strong>
         </div>
         <div class="form-group">
-            <?php echo zen_draw_label(TEXT_EDIT_CATEGORIES_META_TAGS_TITLE, 'metatags_title[' . $languages[$i]['id'] . ']', 'class="col-sm-3 control-label"'); ?>
+            <?php echo zen_draw_label(TEXT_EDIT_CATEGORIES_META_TAGS_TITLE, '', 'class="col-sm-3 control-label"'); ?>
           <div class="col-sm-9 col-md-6">
               <?php
               for ($i = 0, $n = sizeof($languages); $i < $n; $i++) {
                 ?>
               <div class="input-group">
                 <span class="input-group-addon">
-                    <?php echo zen_image(DIR_WS_CATALOG_LANGUAGES . $languages[$i]['directory'] . '/images/' . $languages[$i]['image'], $languages[$i]['metatags_title']); ?>
+                    <?php echo zen_image(DIR_WS_CATALOG_LANGUAGES . $languages[$i]['directory'] . '/images/' . $languages[$i]['image'], $languages[$i]['name']); ?>
                 </span>
                 <?php echo zen_draw_input_field('metatags_title[' . $languages[$i]['id'] . ']', htmlspecialchars(zen_get_category_metatags_title($cInfo->categories_id, $languages[$i]['id']), ENT_COMPAT, CHARSET, TRUE), zen_set_field_length(TABLE_METATAGS_CATEGORIES_DESCRIPTION, 'metatags_title') . ' class="form-control"');
                 ?>
