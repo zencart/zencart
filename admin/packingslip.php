c7d3dd24852c49372baaf60c1ad3f9c87fd29966
Chris Brown <drbyte@zen-cart.com>
2020-01-12 20:15:37 -0500

    Update packingslip.php

--- admin/packingslip.php
+++ admin/packingslip.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Zen4All Tue Oct 2 19:29:10 2018 +0200 Modified in v1.5.6 $
+ * @version $Id: Zen4All Tue Oct 2 19:29:10 2018 +0200 Modified in v1.5.7 $
  */
 require('includes/application_top.php');
 
@@ -126,8 +126,11 @@ if ($order->billing['street_address'] != $order->delivery['street_address']) {
             for ($i = 0, $n = sizeof($order->products); $i < $n; $i++) {
               ?>
             <tr class="dataTableRow">
-              <td class="dataTableContent text-right"><?php echo $order->products[$i]['qty']; ?>&nbsp;x</td>
-              <td class="dataTableContent"><?php echo $order->products[$i]['name']; ?>
+              <td class="dataTableContent text-right">
+                <?php echo $order->products[$i]['qty']; ?>&nbsp;x
+              </td>
+              <td class="dataTableContent">
+                <?php echo $order->products[$i]['name']; ?>
                   <?php
                   if (isset($order->products[$i]['attributes']) && (sizeof($order->products[$i]['attributes']) > 0)) {
                     ?>
@@ -146,7 +149,9 @@ if ($order->billing['street_address'] != $order->delivery['street_address']) {
                 }
                 ?>
               </td>
-              <td class="dataTableContent"><?php echo $order->products[$i]['model']; ?></td>
+              <td class="dataTableContent">
+                <?php echo $order->products[$i]['model']; ?>
+              </td>
             </tr>
             <?php
           }
