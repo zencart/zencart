480b2358ecc2a5852ad7dd1925062a3cca894fe4
Chris Brown <drbyte@zen-cart.com>
2020-01-20 12:49:53 -0500

    Update and rename security_patch_get_20191109.php to patch_clean_ampersands.php
    Renamed because it's not a security issue.
    Refactored for simplicity.

---
+++ includes/extra_configures/patch_clean_ampersands.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Patch GET 20191111
+ * 
+ * @package initSystem
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: patch_clean_ampersands.php New in v1.5.7 $
+ */
+/**
+ * Suitable for versions of Zen Cart prior to v1.5.7
+ *
+ * Non-sanitization/access - $_GET
+ *
+ * Please Note : This file should be placed in includes/extra_configures and will automatically load.
+ *  
+ */
+
+if (!isset($_GET)) {
+    return;
+}
+if (!is_array($_GET)) {
+    return;
+}
+foreach ($_GET as $key => $value) {
+    if ($key === 'amp;') continue;
+    if (strpos($key, 'amp;') !== 0) {
+        continue;
+    }
+    $newtext = substr($key, 4);
+    if (isset($_GET[$newtext])) continue;
+
+    $_GET[$newtext] = $_GET['amp;' . $newtext];
+    unset($_GET['amp;' . $newtext]);
+}
+
