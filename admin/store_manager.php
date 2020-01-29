98846c9bb163ac19ccebd8081ec9f3433a6c7508
Chris Brown <drbyte@zen-cart.com>
2020-01-29 09:01:49 -0500

    Update store_manager.php

--- admin/store_manager.php
+++ admin/store_manager.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2017 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: Author: DrByte  Aug 2017 Modified in v1.5.6 $
