f77e7d2f626b3afbb40a5acfca9d65b9cc659533
Chris Brown <drbyte@zen-cart.com>
2020-01-29 19:52:29 -0500

    Update header_php.php

--- includes/modules/pages/advanced_search_result/header_php.php
+++ includes/modules/pages/advanced_search_result/header_php.php
@@ -3,7 +3,7 @@
  * Header code file for the Advanced Search Results page
  *
  * @package page
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: mc12345678 2019 Apr 30 Modified in v1.5.6b $
