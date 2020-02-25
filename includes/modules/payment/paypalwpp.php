bc0f030dade065d5ecf9de301e0a1cbf40ba8ae4
Chris Brown <drbyte@zen-cart.com>
2020-02-24 22:38:18 -0500

    Update paypalwpp.php

--- includes/modules/payment/paypalwpp.php
+++ includes/modules/payment/paypalwpp.php
@@ -3,7 +3,7 @@
  * paypalwpp.php payment module class for PayPal Express Checkout payment method
  *
  * @package paymentMethod
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: Scott C Wilson 2019 Jun 19 Modified in v1.5.6c $
