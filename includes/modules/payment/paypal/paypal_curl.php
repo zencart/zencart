3e5b6ce9e5a8cabbb1b88238f3d794650f87e137
Chris Brown <drbyte@zen-cart.com>
2020-03-14 11:28:44 -0400

    Update paypal_curl.php

--- includes/modules/payment/paypal/paypal_curl.php
+++ includes/modules/payment/paypal/paypal_curl.php
@@ -3,9 +3,9 @@
  * paypal_curl.php communications class for PayPal Express Checkout / Website Payments Pro / Payflow Pro payment methods
  *
  * @package paymentMethod
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott C Wilson 2019 Mar 31 Modified in v1.5.6b $
+ * @version $Id: Modified in v1.5.7 $
  */
 
 /**
