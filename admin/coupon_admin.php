eab3c1b4152e049220dfbd894ede3cd6f13c8d64
Chris Brown <drbyte@zen-cart.com>
2020-03-14 12:21:58 -0400

    Update coupon_admin.php

--- admin/coupon_admin.php
+++ admin/coupon_admin.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: lat9 2019 May 18 Modified in v1.5.6b $
+ * @version $Id:  Modified in v1.5.7 $
  */
   require('includes/application_top.php');
   require(DIR_WS_CLASSES . 'currencies.php');
