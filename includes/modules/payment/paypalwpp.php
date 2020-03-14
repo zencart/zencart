6f7638272dca78b9d80d1ad2a42aefe69b11e0d2
Chris Brown <drbyte@zen-cart.com>
2020-03-14 11:32:12 -0400

    Update paypalwpp.php

--- includes/modules/payment/paypalwpp.php
+++ includes/modules/payment/paypalwpp.php
@@ -6,7 +6,7 @@
  * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott C Wilson 2019 Jun 19 Modified in v1.5.6c $
+ * @version $Id:  Modified in v1.5.7 $
  */
 /**
  * load the communications layer code
