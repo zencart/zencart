a3c78d47d55e47e2f5d98d621d254dad5e57b11d
Chris Brown <drbyte@zen-cart.com>
2020-03-14 12:15:29 -0400

    Update functions_general.php

--- includes/functions/functions_general.php
+++ includes/functions/functions_general.php
@@ -4,14 +4,14 @@
  * General functions used throughout Zen Cart
  *
  * @package functions
- * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Drbyte Mon Nov 12 15:55:25 2018 -0500 Modified in v1.5.6 $
+ * @version $Id:  Modified in v1.5.7 $
  */
 /**
- * Stop from parsing any further PHP code
-*/
+ * Stop execution completely
+ */
   function zen_exit() {
    session_write_close();
    exit();
