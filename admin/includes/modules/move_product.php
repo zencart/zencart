6b1ea58ad17c01ac81a9daf419d0bf186cdbf93e
Steve <4391026+torvista@users.noreply.github.com>
2020-03-15 09:59:34 +0100

    bugfix: notice generated when product was only in one category, $product_master_category_string was not set TEXT_MOVE_PRODUCTS_INTRO was used incorrectly with sprintf

--- admin/includes/modules/move_product.php
+++ admin/includes/modules/move_product.php
@@ -18,10 +18,10 @@ if (!isset($category_path)) $category_path = '';
         $category_path .= $product_categories[$i][$j]['text'];
         if ($j+1 < $k)  $category_path .= '&nbsp;&gt;&nbsp;';
     }
-    if (sizeof($product_categories) > 1 && zen_get_parent_category_id($pInfo->products_id) == $product_categories[$i][sizeof($product_categories[$i]) - 1]['id']) {
+    if (sizeof($product_categories) > 0 && zen_get_parent_category_id($pInfo->products_id) == $product_categories[$i][sizeof($product_categories[$i]) - 1]['id']) {
         $product_master_category_string = $category_path;
     }
-    if (sizeof($product_categories) > 1 && $current_category_id == $product_categories[$i][sizeof($product_categories[$i]) - 1]['id']) {
+    if (sizeof($product_categories) > 0 && $current_category_id == $product_categories[$i][sizeof($product_categories[$i]) - 1]['id']) {
         $product_current_category_string = $category_path;
     }
 }
@@ -30,7 +30,7 @@ $heading = array();
 $heading[] = array('text' => '<h4>' . TEXT_INFO_HEADING_MOVE_PRODUCT . '</h4>');
 $contents = array('form' => zen_draw_form('products', FILENAME_CATEGORY_PRODUCT_LISTING, 'action=move_product_confirm&cPath=' . $cPath . (isset($_GET['page']) ? '&page=' . $_GET['page'] : ''), 'post', 'class="form-horizontal"') . zen_draw_hidden_field('products_id', $pInfo->products_id));
 $contents[] = array('text' => '<h3>' . 'ID#' . $pInfo->products_id . ': ' . $pInfo->products_name . '</h3>');
-$contents[] = array('text' => sprintf(TEXT_MOVE_PRODUCTS_INTRO, $pInfo->products_id));
+$contents[] = array('text' => TEXT_MOVE_PRODUCTS_INTRO);
 $contents[] = array('text' => zen_draw_label(sprintf(TEXT_MOVE_PRODUCT, $pInfo->products_id, $pInfo->products_name, $product_current_category_string), 'move_to_category_id', 'style="font-weight:normal;font-size:larger;"') . zen_draw_pull_down_menu('move_to_category_id', zen_get_category_tree(), $current_category_id, 'id="move_to_category_id" class="form-control"'));
 $contents[] = array('align' => 'center', 'text' => '<button type="submit" class="btn btn-primary">' . IMAGE_MOVE . '</button> <a href="' . zen_href_link(FILENAME_CATEGORY_PRODUCT_LISTING, 'cPath=' . $cPath . '&pID=' . $pInfo->products_id . (isset($_GET['page']) ? '&page=' . $_GET['page'] : '')) . '" class="btn btn-default" role="button">' . IMAGE_CANCEL . '</a>');
 $contents[] = array('text' => TEXT_INFO_CURRENT_CATEGORIES);
