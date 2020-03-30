eb4190b10aea79f44178317a12299cc12fdbc638
Steve <4391026+torvista@users.noreply.github.com>
2020-03-30 21:11:45 +0200

    add type cast

--- admin/includes/modules/product/collect_info.php
+++ admin/includes/modules/product/collect_info.php
@@ -120,7 +120,7 @@ if (zen_get_categories_status($current_category_id) == 0 && $pInfo->products_sta
   <div class="form-group">
       <?php
 // show when product is linked
-      if (isset($_GET['pID']) && zen_get_product_is_linked($_GET['pID']) == 'true' && $_GET['pID'] > 0) {
+      if (isset($_GET['pID']) && zen_get_product_is_linked($_GET['pID']) == 'true' && (int)$_GET['pID'] > 0) {
         ?>
         <?php echo zen_draw_label(TEXT_MASTER_CATEGORIES_ID, 'master_category', 'class="col-sm-3 control-label"'); ?>
       <div class="col-sm-9 col-md-6">
