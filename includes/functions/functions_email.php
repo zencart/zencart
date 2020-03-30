8219c0d87ccb94a5805bcd45aaace4ff275afaf5
Chris Brown <drbyte@zen-cart.com>
2020-03-30 14:53:26 -0400

    Update functions_email.php

--- includes/functions/functions_email.php
+++ includes/functions/functions_email.php
@@ -5,10 +5,10 @@
  * Hooks into phpMailer class for actual email encoding and sending
  *
  * @package functions
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2019 May 25 Modified in v1.5.6b $
+ * @version $Id:  Modified in v1.5.7 $
  */
 
 use PHPMailer\PHPMailer\PHPMailer;
