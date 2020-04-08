7cec82c55812a83b1302c026398e6a95ead1de6f
Chris Brown <drbyte@zen-cart.com>
2020-04-08 14:26:00 -0400

    Update define_pages_editor.php
    Fixes #3191

--- admin/includes/languages/english/define_pages_editor.php
+++ admin/includes/languages/english/define_pages_editor.php
@@ -30,4 +30,4 @@ define('ERROR_FILE_NOT_WRITEABLE', 'Error: I can not write to this file. Please 
 define('TEXT_INFO_SELECT_FILE', 'Select a file to edit ...');
 define('TEXT_INFO_EDITING', 'Editing file:');
 
-define('TEXT_INFO_CAUTION','Note: you should always edit the files located in your current template override directory, Example: /languages/' . $_SESSION['language'] . '/html_includes/' . $template_dir . '<br />Be sure to make backups after changing your files.');
+define('TEXT_INFO_CAUTION','Note: you should always edit the files located in your current template override directory, Example: /languages/' . $_SESSION['language'] . '/html_includes/YOUR_TEMPLATE_DIRECTORY<br>Be sure to make backups after changing your files.');
