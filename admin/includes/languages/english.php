6d9ea772bfc79ae685648114184b6751f281416d
Chris Brown <drbyte@zen-cart.com>
2020-04-08 15:07:45 -0400

    Update english.php

--- admin/includes/languages/english.php
+++ admin/includes/languages/english.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package admin
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: torvista 2019 Aug 08 Modified in v1.5.7 $
+ * @version $Id:  Modified in v1.5.7 $
  */
 if (!defined('IS_ADMIN_FLAG'))
 {
