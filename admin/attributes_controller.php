4f290bafdfa80248db47bf05faec5aafcb6ea979
Scott C Wilson <scottcwilson@gmail.com>
2020-04-15 18:27:41 -0400

    Don't write to TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD unless it's a file

--- admin/attributes_controller.php
+++ admin/attributes_controller.php
@@ -337,12 +337,6 @@ if (zen_not_null($action)) {
             $products_attributes_maxdays = (int)$_POST['products_attributes_maxdays'];
             $products_attributes_maxcount = (int)$_POST['products_attributes_maxcount'];
 
-            // check to see if it's a file missing a name
-            if ($options_id === 1) {
-              if (empty($products_attributes_filename)) {
-                $products_attributes_filename = 'missing_file';
-              }
-            }
             if (!empty($products_attributes_filename)) {
               $db->Execute("INSERT INTO " . TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD . " (products_attributes_id, products_attributes_filename, products_attributes_maxdays, products_attributes_maxcount)
                             VALUES (" . (int)$products_attributes_id . ",
