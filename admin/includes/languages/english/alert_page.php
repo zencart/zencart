181bbe2731b8077ee3e8bd962069543a1e0c0b11
Chris Brown <drbyte@zen-cart.com>
2020-04-18 14:51:15 -0400

    Update alert_page.php

--- admin/includes/languages/english/alert_page.php
+++ admin/includes/languages/english/alert_page.php
@@ -11,4 +11,4 @@ define('ALERT_PART1', 'Warning: You cannot access the admin area until you have'
 define('ALERT_RENAME_ADMIN', 'renamed the admin folder.');
 define('ALERT_REMOVE_ZCINSTALL', 'deleted the zc_install folder.<br>(Use your FTP program or your hosting control panel.)');
 define('ADMIN_RENAME_FAQ_NOTE', 'Help for renaming the admin folder can be found here');
-define('ALERT_PART2', 'Then, to access your admin area, type the admin URL into your browser, ie: <strong>http://www.your_site.com/YourAdminFolder/</strong> ');
+define('ALERT_PART2', 'Then, to access your admin area, type the admin URL into your browser, ie: <u>http://www.your_site.com/YourAdminFolder/</u> ');
