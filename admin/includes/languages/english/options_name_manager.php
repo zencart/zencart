aa3af9de3be020b5ff4e025a9a529dc0842e0ef0
Steve <4391026+torvista@users.noreply.github.com>
2020-04-20 22:47:34 +0200

    line by line 3

--- admin/includes/languages/english/options_name_manager.php
+++ admin/includes/languages/english/options_name_manager.php
@@ -103,7 +103,7 @@ define('TEXT_SELECT_OPTION_FROM', 'Copy from Option Name: ');
 define('TEXT_SELECT_OPTION_TO', 'Copy to Option Name: ');
 
 define('SUCCESS_OPTION_VALUE_COPIED', 'Option Value "%6$s" ID#%5$u copied from Option Name "%2$s" ID#%1$u to Option Name "%4$s" ID#%3$u.');
-define('ERROR_OPTION_VALUES_NONE', 'Error - Copy from Option Name has 0 Values Defined. Nothing was copied! ');
+define('SUCCESS_OPTION_VALUES_COPIED', '%5$u Option Value(s) copied from Option Name "%2$s" ID#%1$u to Option Name "%4$s" ID#%3$u.');
 define('TEXT_WARNING_BACKUP', 'Warning: Always make proper backups of your database before making global changes');
 
 define('TEXT_OPTION_ATTRIBUTE_IMAGES_PER_ROW', 'Attribute Images per Row: ');
