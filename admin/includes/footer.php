923dfb49bebce2a2de9b890dc1e3d2ba47234af8
Chris Brown <drbyte@zen-cart.com>
2020-05-04 12:48:49 -0400

    Update to https links

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -18,9 +18,9 @@
 ?>
 <footer>
   <div id="footer">
-    <a href="http://www.zen-cart.com" target="_blank"><img src="images/small_zen_logo.gif" alt="Zen Cart:: the art of e-commerce"></a><br />
+    <a href="https://www.zen-cart.com" target="_blank"><img src="images/small_zen_logo.gif" alt="Zen Cart:: the art of e-commerce"></a><br />
     <br />
-    E-Commerce Engine Copyright &copy; 2003-<?php echo date('Y'); ?> <a href="http://www.zen-cart.com" target="_blank">Zen Cart&reg;</a><br />
+    E-Commerce Engine Copyright &copy; 2003-<?php echo date('Y'); ?> <a href="https://www.zen-cart.com" target="_blank">Zen Cart&reg;</a><br />
     <?php echo '<a href="' . zen_href_link(FILENAME_SERVER_INFO) . '">' . $current_sinfo . '</a>'; ?>
   </div>
 </footer>
