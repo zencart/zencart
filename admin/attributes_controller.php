97491088fda8a437cba8bb995835252b9dd362f8
Steve <4391026+torvista@users.noreply.github.com>
2020-05-05 20:11:03 +0200

    add space between attributes

--- admin/attributes_controller.php
+++ admin/attributes_controller.php
@@ -1995,8 +1995,8 @@ function zen_js_option_values_list($selectedName, $fieldName)
                       <h4><?php echo TABLE_HEADING_DOWNLOAD; ?></h4>
                       <div class="row">
                         <div class="col-sm-3 col-lg-2">
-                          <?php echo zen_draw_label(TABLE_TEXT_FILENAME, 'products_attributes_filename', 'class="control-label"'); ?>
-                          <?php echo zen_draw_input_field('products_attributes_filename', $products_attributes_filename, zen_set_field_length(TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD, 'products_attributes_filename', 35) . 'class="form-control"'); ?>
+                          <?php echo zen_draw_label(TABLE_TEXT_FILENAME, 'products_attributes_filename', 'class="control-label"') . "\n"; ?>
+                          <?php echo zen_draw_input_field('products_attributes_filename', $products_attributes_filename, zen_set_field_length(TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD, 'products_attributes_filename', 35) . ' class="form-control"'); ?>
                         </div>
                         <div class="col-sm-3 col-lg-2">
                           <?php echo zen_draw_label(TABLE_TEXT_MAX_DAYS, 'products_attributes_maxdays', 'class="control-label"'); ?>
