b9084788b65e4bfab68083de088815969f4e5a15
Chris Brown <drbyte@zen-cart.com>
2020-05-07 12:12:08 -0400

    Prevent notice if tax_groups not set

--- includes/templates/template_default/templates/tpl_account_history_info_default.php
+++ includes/templates/template_default/templates/tpl_account_history_info_default.php
@@ -26,7 +26,7 @@
         <th scope="col" id="myAccountQuantity"><?php echo HEADING_QUANTITY; ?></th>
         <th scope="col" id="myAccountProducts"><?php echo HEADING_PRODUCTS; ?></th>
 <?php
-  if (sizeof($order->info['tax_groups']) > 1) {
+  if (isset($order->info['tax_groups']) && count($order->info['tax_groups']) > 1) {
 ?>
         <th scope="col" id="myAccountTax"><?php echo HEADING_TAX; ?></th>
 <?php
@@ -51,7 +51,7 @@
 ?>
         </td>
 <?php
-    if (sizeof($order->info['tax_groups']) > 1) {
+    if (isset($order->info['tax_groups']) && count($order->info['tax_groups']) > 1) {
 ?>
         <td class="accountTaxDisplay"><?php echo zen_display_tax_value($order->products[$i]['tax']) . '%' ?></td>
 <?php
