84ab217a5db3854d6a4150e6cfbcd4838a741492
Zen4All <info@zen4all.nl>
2020-05-12 09:42:31 +0200

    Update admin side to jQuery 3.5.1
    3.5.1fixes the bug reported earlier about non working order info buttons

--- admin/includes/ckeditor.php
+++ admin/includes/ckeditor.php
@@ -17,7 +17,7 @@ foreach ($var as $key)
   $jsLanguageLookupArray .= "        lang[" . $key['id'] . "] = '" . $key['code'] . "';\n";
 }
 ?>
-<script>window.jQuery || document.write('<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"><\/script>');</script>
+<script>window.jQuery || document.write('<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"><\/script>');</script>
 <script>window.jQuery || document.write('<script src="includes/javascript/jquery.min.js"><\/script>');</script>
 <script src="https://cdn.ckeditor.com/4.14.0/standard-all/ckeditor.js" title="CKEditorCDN"></script>
 
