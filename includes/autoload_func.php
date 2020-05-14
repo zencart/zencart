e0c3c911b86a233a71dabb9deb6f099b888e05c7
Chris Brown <drbyte@zen-cart.com>
2020-05-14 14:13:59 -0400

    Update autoload_func.php

--- includes/autoload_func.php
+++ includes/autoload_func.php
@@ -7,10 +7,10 @@
  * see {@link http://www.zen-cart.com/wiki/index.php/Developers_API_Tutorials#InitSystem} for more details.
  *
  * @package initSystem
- * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: mc12345678 2019 Jan 20 Modified in v1.5.6b $
+ * @version $Id:  Modified in v1.5.7 $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
