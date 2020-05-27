0add03395fbcc5c67e27ab8a86a18560b39db92c
Steve <4391026+torvista@users.noreply.github.com>
2020-05-27 22:59:24 +0200

    remove deprecated tag, br / to br

--- admin/includes/languages/english/layout_controller.php
+++ admin/includes/languages/english/layout_controller.php
@@ -9,11 +9,11 @@
 define('HEADING_TITLE', 'Column Boxes');
 
 define('TABLE_HEADING_LAYOUT_BOX_NAME', 'Box File Name');
-define('TABLE_HEADING_LAYOUT_BOX_STATUS', 'LEFT/RIGHT COLUMN<br />Status');
-define('TABLE_HEADING_LAYOUT_BOX_STATUS_SINGLE', 'SINGLE COLUMN<br />Status');
-define('TABLE_HEADING_LAYOUT_BOX_LOCATION', 'LEFT or RIGHT<br />COLUMN');
-define('TABLE_HEADING_LAYOUT_BOX_SORT_ORDER', 'LEFT/RIGHT COLUMN<br />Sort Order');
-define('TABLE_HEADING_LAYOUT_BOX_SORT_ORDER_SINGLE', 'SINGLE COLUMN<br />Sort Order');
+define('TABLE_HEADING_LAYOUT_BOX_STATUS', 'LEFT/RIGHT COLUMN<br>Status');
+define('TABLE_HEADING_LAYOUT_BOX_STATUS_SINGLE', 'SINGLE COLUMN<br>Status');
+define('TABLE_HEADING_LAYOUT_BOX_LOCATION', 'LEFT or RIGHT<br>COLUMN');
+define('TABLE_HEADING_LAYOUT_BOX_SORT_ORDER', 'LEFT/RIGHT COLUMN<br>Sort Order');
+define('TABLE_HEADING_LAYOUT_BOX_SORT_ORDER_SINGLE', 'SINGLE COLUMN<br>Sort Order');
 define('TABLE_HEADING_ACTION', 'Action');
 
 define('TEXT_INFO_EDIT_INTRO', 'Please make any necessary changes');
@@ -29,7 +29,7 @@ define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete this box?');
 define('TEXT_INFO_HEADING_EDIT_BOX', 'Edit Box');
 define('TEXT_INFO_HEADING_DELETE_BOX', 'Delete Box');
 define('TEXT_INFO_DELETE_MISSING_LAYOUT_BOX','Delete missing box from Template listing: ');
-define('TEXT_INFO_DELETE_MISSING_LAYOUT_BOX_NOTE','NOTE: This does not remove files and you can re-add the box at anytime by adding it to the correct directory.<br /><br /><strong>Delete box name: </strong>');
+define('TEXT_INFO_DELETE_MISSING_LAYOUT_BOX_NOTE','NOTE: This does not remove files and you can re-add the box at anytime by adding it to the correct directory.<br><br><strong>Delete box name: </strong>');
 define('TEXT_INFO_RESET_TEMPLATE_SORT_ORDER','Reset All Box Sort Order to match DEFAULT Sort Order for Template: ');
 define('TEXT_INFO_RESET_TEMPLATE_SORT_ORDER_NOTE','This does not remove any of the boxes. It will only reset the current sort order');
 define('TEXT_INFO_BOX_DETAILS','Box Details: ');
@@ -48,7 +48,7 @@ define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
 
 // file exists
 define('TEXT_GOOD_BOX',' ');
-define('TEXT_BAD_BOX','<font color="ff0000"><b>MISSING</b></font><br />');
+define('TEXT_BAD_BOX','<span style="color:#ff0000;"><b>MISSING</b></span><br>');
 
 
 // Success message
