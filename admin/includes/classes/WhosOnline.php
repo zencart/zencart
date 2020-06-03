fa2ba193cb7f0cf69487877ad342cfe027bed22e
Chris Brown <drbyte@zen-cart.com>
2020-06-02 20:11:02 -0400

    Update WhosOnline.php

--- admin/includes/classes/WhosOnline.php
+++ admin/includes/classes/WhosOnline.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id:  New in v1.5.7 $
+ */
 
 class WhosOnline extends base
 {
