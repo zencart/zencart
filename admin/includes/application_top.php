3418a4ac80249b3a5140d1931c53e3adbb422fa1
zcwilt <ian@zen-cart.com>
2020-06-12 16:02:27 +0100

    further version stamping

--- admin/includes/application_top.php
+++ admin/includes/application_top.php
@@ -3,7 +3,7 @@
  * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: mc12345678 2019 Oct 09 Modified in v1.5.7 $
+ * @version $Id: Zcwilt 2020 Jun 08 Modified in v1.5.7 $
  */
 /**
  * File contains just application_top code
--- admin/includes/languages/english/users.php
+++ admin/includes/languages/english/users.php
@@ -3,7 +3,7 @@
  * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Steve 2020 Apr 12 Modified in v1.5.7 $
+ * @version $Id: DrByte 2020 Jun 07 Modified in v1.5.7 $
  */
 
 if (!defined('HEADING_TITLE')) { //this file included by admin_account.php
