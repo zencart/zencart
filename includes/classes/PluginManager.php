2ba24bd77003101e76105b7fe6c647936fed4c5a
Chris Brown <drbyte@zen-cart.com>
2020-06-16 14:13:17 -0400

    Add preliminary version-query method

--- includes/classes/PluginManager.php
+++ includes/classes/PluginManager.php
@@ -62,6 +62,78 @@ class PluginManager
         return $versionList;
     }
 
+    public function getPluginsAfterCheckingForNewVersionsOnline()
+    {
+        $plugins = $this->getPluginsFromDb();
+
+        // new array for reverse-lookup after getting results back
+        $pluginsById = [];
+
+        $ids_csv = '';
+        foreach($plugins as $plugin) {
+            $pluginsById[$plugin['zc_contrib_id']] = $plugin;
+            $ids_csv .= (int)trim($plugin['zc_contrib_id']) . ',';
+        }
+
+        $results = $this->getLatestPluginVersionsOnline($ids_csv);
+
+        // if no results or invalid format, abort
+        // @TODO - is this the right return type? or should we return the unaltered $plugins array?
+        if (empty($results)) return false;
+
+        // make sure $results is the actual array we want to iterate over, and not a sub-array
+        if (is_array($results) && !isset($results[0]['id']) && isset($results[0][0]['id'])) {
+            $results = $results[0];
+        }
+
+        if (!isset($results[0]['id'])) {
+            return false; // @TODO or return original $plugins array?
+        }
+
+        $present_zc_version = 'v' . preg_replace('/[^0-9.]/', '', zen_get_zcversion());
+
+        foreach($results as $result) {
+            $unique_key = $pluginsById[$result['id']]['unique_key'];
+
+            if (version_compare($pluginsById[$result['id']]['version'], $result['latest_plugin_version'], '<')) {
+                $plugins[$unique_key]['new_online_version_exists'] = true;
+                $plugins[$unique_key]['latest_plugin_version'] = $result['latest_plugin_version'];
+                $plugins[$unique_key]['zcversions'] = $result['zcversions'];
+
+                if (in_array($present_zc_version, $result['zcversions'], $strict=false)) {
+                    $plugins[$unique_key]['new_plugin_exists_for_this_zc_version'] = true;
+                }
+            }
+        }
+
+        return $plugins;
+    }
+
+    protected function getLatestPluginVersionsOnline($plugin_ids_csv = 0)
+    {
+        if (empty(trim($plugin_ids_csv, ','))) return false;
+
+        $versionServer = new \VersionServer();
+        $data = json_decode($versionServer->getPluginVersion($plugin_ids_csv), true);
+
+        if (null === $data || isset($data['error'])) {
+            if (LOG_PLUGIN_VERSIONCHECK_FAILURES) error_log('CURL error checking plugin versions (in batch): ' . print_r(!empty($data)? $data : 'null', true));
+            return false;
+        }
+
+        if (!is_array($data)) {
+            try {
+                $data = json_decode($data, true);
+            } catch (\Exception $exception) {
+                if (LOG_PLUGIN_VERSIONCHECK_FAILURES) error_log('CURL error checking plugin versions (in batch): ' . print_r(!empty($data) ? $data : 'null', true));
+                return false;
+            }
+        }
+
+        return $data;
+    }
+
+
     protected function getPluginVersions($uniqueKey)
     {
         $sql = "SELECT version FROM " . TABLE_PLUGIN_CONTROL_VERSIONS . " WHERE unique_key = :uniqueKey:";
