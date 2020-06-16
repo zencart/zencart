54b5adf9e9ff2ce77d669e1b6f7118220c9fa8d8
Chris Brown <drbyte@zen-cart.com>
2020-06-16 13:15:58 -0400

    Restructure manifest file

--- zc_plugins/DisplayLogs/v3.0.0/manifest.php
+++ zc_plugins/DisplayLogs/v3.0.0/manifest.php
@@ -1,15 +1,13 @@
 <?php
 
 return [
-
-    'pluginType' => 'free',
-    'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
+    'pluginVersion' => 'v3.0.0',
     'pluginName' => 'Display logs',
     'pluginDescription' => 'Display and manage Zen Cart log files.',
-    'pluginVersion' => 'v3.0.0',
-    'pluginId' => 1583,
-    'managed' => true,
+    'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
+    'pluginId' => 1583, // ID from Zen Cart forum
     'zcVersions' => ['v157'],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => '', // url
     'pluginGroups' => [],
-    'changelog' => [],
 ];
