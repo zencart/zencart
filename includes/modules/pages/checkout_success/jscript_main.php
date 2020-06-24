9340ba7f6e883d0fa558cc09b8ebf9322ccb96fa
Scott C Wilson <scottcwilson@gmail.com>
2020-06-24 11:20:37 -0400

    Link on checkout success page does not work

---
+++ includes/modules/pages/checkout_success/jscript_main.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * jscript_main
+ *
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 2019 Jul 11 Modified in v1.5.7 $
+ */
+?>
+<script type="text/javascript">
+/*
+function popupWindow(url) {
+  window.open(url,'popupWindow','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,copyhistory=no,width=450,height=320,screenX=150,screenY=150,top=150,left=150')
+}
+*/
+
+function couponpopupWindow(url) {
+  window.open(url,'couponpopupWindow','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,copyhistory=no,width=450,height=320,screenX=150,screenY=150,top=150,left=150')
+}
+
+</script>
