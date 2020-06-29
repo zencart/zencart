2183b15f2079cda0126a64ff6db2aa2e505f51e1
Steve <4391026+torvista@users.noreply.github.com>
2020-06-29 19:24:53 +0200

    add missing constant definitions

--- admin/includes/languages/english/categories.php
+++ admin/includes/languages/english/categories.php
@@ -19,7 +19,9 @@ define('TEXT_CATEGORIES_IMAGE_MANUAL', 'Use an existing image file in the above 
 define('TEXT_EDIT_SORT_ORDER', 'Sort Order:');
 define('TEXT_RESTRICT_PRODUCT_TYPE', 'Restrict to Product Type');
 define('TEXT_CATEGORY_HAS_RESTRICTIONS', 'This Category has been restricted to these Product Types');
-
+define('MESSAGE_IMAGE_REMOVED_CATEGORY', 'Image removed from category #%1$u "%2$s."');
+define('MESSAGE_IMAGE_ADDED_MANUAL', 'Image was manually added to category #%1$u "%2$s."');
+define('ERROR_IMAGE_MANUAL_NOT_FOUND', 'The manually added image "%s" was not found.');
 // Metatags
 define('TEXT_INFO_HEADING_EDIT_CATEGORY_META_TAGS', 'Categories Meta Tags Definitions');
 define('TEXT_EDIT_CATEGORIES_META_TAGS_INTRO', 'Define Custom Meta Tags for ID#%1$u - "%2$s"');
