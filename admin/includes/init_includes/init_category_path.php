b001a088c4802baeae41cb5b86d5f6b7e30211ef
Chris Brown <drbyte@zen-cart.com>
2020-07-07 16:04:56 -0400

    PSR2 reformat

--- admin/includes/init_includes/init_category_path.php
+++ admin/includes/init_includes/init_category_path.php
@@ -7,31 +7,32 @@
  * @version $Id: DrByte 2019 Jan 04 Modified in v1.5.6a $
  */
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
-// calculate category path
-  if (isset($_POST['cPath'])) {
-    $cPath = $_POST['cPath'];
-  } elseif (isset($_GET['cPath'])) {
-    $cPath = $_GET['cPath'];
-  } else {
-    $cPath = '';
-  }
 
-  if (zen_not_null($cPath)) {
+// calculate category path
+if (isset($_POST['cPath'])) {
+    $cPath = $_POST['cPath'];
+} elseif (isset($_GET['cPath'])) {
+    $cPath = $_GET['cPath'];
+} else {
+    $cPath = '';
+}
+
+if (zen_not_null($cPath)) {
     $cPath_array = zen_parse_category_path($cPath);
     $cPath = implode('_', $cPath_array);
-    $current_category_id = $cPath_array[(sizeof($cPath_array)-1)];
-  } else {
-    $cPath_array = array();
+    $current_category_id = $cPath_array[(count($cPath_array) - 1)];
+} else {
+    $cPath_array = [];
     $current_category_id = 0;
-  }
+}
 
 // default open navigation box
-  if (!isset($_SESSION['selected_box'])) {
+if (!isset($_SESSION['selected_box'])) {
     $_SESSION['selected_box'] = 'configuration';
-  }
+}
 
-  if (isset($_GET['selected_box'])) {
+if (isset($_GET['selected_box'])) {
     $_SESSION['selected_box'] = $_GET['selected_box'];
-  }
+}
--- includes/init_includes/init_category_path.php
+++ includes/init_includes/init_category_path.php
@@ -10,31 +10,36 @@
  * @version $Id: init_category_path.php 17018 2010-07-27 07:25:41Z drbyte $
  */
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
+
 $show_welcome = false;
 if (isset($_GET['cPath'])) {
-  $cPath = $_GET['cPath'];
+    $cPath = $_GET['cPath'];
 } elseif (isset($_GET['products_id']) && !zen_check_url_get_terms()) {
-  $cPath = zen_get_product_path($_GET['products_id']);
+    $cPath = zen_get_product_path($_GET['products_id']);
 } else {
-  if ($current_page == 'index' && SHOW_CATEGORIES_ALWAYS == '1' && !zen_check_url_get_terms()) {
-    $show_welcome = true;
-    $cPath = (defined('CATEGORIES_START_MAIN') ? CATEGORIES_START_MAIN : '');
-  } else {
-    $show_welcome = false;
-    $cPath = '';
-  }
+    if ($current_page == 'index' && SHOW_CATEGORIES_ALWAYS == '1' && !zen_check_url_get_terms()) {
+        $show_welcome = true;
+        $cPath = (defined('CATEGORIES_START_MAIN') ? CATEGORIES_START_MAIN : '');
+    } else {
+        $show_welcome = false;
+        $cPath = '';
+    }
 }
 if (zen_not_null($cPath)) {
-  $cPath_array = zen_parse_category_path($cPath);
-  $cPath = implode('_', $cPath_array);
-  $current_category_id = $cPath_array[(sizeof($cPath_array)-1)];
+    $cPath_array = zen_parse_category_path($cPath);
+    $cPath = implode('_', $cPath_array);
+    $current_category_id = $cPath_array[(count($cPath_array) - 1)];
 } else {
-  $current_category_id = 0;
-  $cPath_array = array();
+    $current_category_id = 0;
+    $cPath_array = [];
 }
 
 // determine whether the current page is the home page or a product listing
 //$this_is_home_page = ($current_page=='index' && ((int)$cPath == 0 || $show_welcome == true));
-$this_is_home_page = ($current_page=='index' && (!isset($_GET['cPath']) || $_GET['cPath'] == '') && (!isset($_GET['manufacturers_id']) || $_GET['manufacturers_id'] == '') && (!isset($_GET['typefilter']) || $_GET['typefilter'] == '') );
+$this_is_home_page = ($current_page == 'index'
+    && (!isset($_GET['cPath']) || $_GET['cPath'] == '')
+    && (!isset($_GET['manufacturers_id']) || $_GET['manufacturers_id'] == '')
+    && (!isset($_GET['typefilter']) || $_GET['typefilter'] == '')
+);
