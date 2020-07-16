549eca568477f14adc07c5302ba214e07ff569d5
Scott C Wilson <scottcwilson@gmail.com>
2020-07-16 07:32:24 -0400

    test data for plugin manager

---
+++ not_for_release/zc_plugins/ModList/1.4.0/Installer/uninstall.sql
@@ -0,0 +1,2 @@
+DELETE FROM admin_pages WHERE page_key = 'mod_list'; 
+DELETE FROM admin_pages_to_profiles WHERE page_key = 'mod_list'; 
---
+++ not_for_release/zc_plugins/ModList/1.4.0/admin/includes/languages/english/extra_definitions/mod_list.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Mod List by That Software Guy 
+ * @package admin
+ * @copyright That Software Guy, Inc., 2015
+ * @copyright Portions Copyright 2003-2014 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+*/ 
+define('BOX_TOOLS_MOD_LIST', 'Mod List');
+define('FILENAME_MOD_LIST', 'mod_list'); 
