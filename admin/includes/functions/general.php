5cbacae3ec9d7949bc160a0e5e45898596b219ae
Chris Brown <drbyte@zen-cart.com>
2020-08-08 16:17:40 -0400

    Remove duplicate coupon functions already relocated to functions_gvcoupons

--- admin/includes/functions/general.php
+++ admin/includes/functions/general.php
@@ -1341,64 +1341,6 @@ while (!$chk_sale_categories_all->EOF) {
   }
 
 
-/**
- * Create a Coupon Code. Returns blank if cannot generate a unique code using the passed criteria.
- * @param string $salt - this is an optional string to help seed the random code with greater entropy
- * @param int $length - this is the desired length of the generated code
- * @param string $prefix - include a prefix string if you want to force the generated code to start with a specific string
- * @return string (new coupon code) (will be blank if the function failed)
- */
-  function zen_create_coupon_code($salt="secret", $length=SECURITY_CODE_LENGTH, $prefix = '') {
-    global $db;
-    $length = (int)$length;
-    static $max_db_length;
-    if (!isset($max_db_length)) $max_db_length = zen_field_length(TABLE_COUPONS, 'coupon_code');  // schema is normally max 32 chars for this field
-    if ($length > $max_db_length) $length = $max_db_length;
-    if (strlen($prefix) > $max_db_length) return ''; // if prefix is already too long for the db, we can't generate a new code
-    if (strlen($prefix) + (int)$length > $max_db_length) $length = $max_db_length - strlen($prefix);
-    if ($length < 4) return ''; // if the recalculated length (esp in respect to prefixes) is less than 4 (for very basic entropy) then abort
-    $ccid = md5(uniqid("",$salt));
-    $ccid .= md5(uniqid("",$salt));
-    $ccid .= md5(uniqid("",$salt));
-    $ccid .= md5(uniqid("",$salt));
-    srand((double)microtime()*1000000); // seed the random number generator
-    $good_result = 0;
-    $id1 = '';
-    while ($good_result == 0) {
-      $random_start = @rand(0, (128-$length));
-      $id1=substr($ccid, $random_start, $length);
-      $sql = "select coupon_code
-              from " . TABLE_COUPONS . "
-              where coupon_code = :couponcode";
-      $sql = $db->bindVars($sql, ':couponcode', $prefix . $id1, 'string');
-      $result = $db->Execute($sql);
-      if ($result->RecordCount() < 1 ) $good_result = 1;
-    }
-    return ($good_result == 1) ? $prefix . $id1 : ''; // blank means couldn't generate a unique code (typically because the max length was encountered before being able to generate unique)
-  }
-
-/**
- * Update the Customers GV account
- */
-  function zen_gv_account_update($customer_id, $gv_id) {
-    global $db;
-    $customer_gv = $db->Execute("select amount
-                                 from " . TABLE_COUPON_GV_CUSTOMER . "
-                                 where customer_id = " . (int)$customer_id);
-
-    $coupon_gv = $db->Execute("select coupon_amount
-                               from " . TABLE_COUPONS . "
-                               where coupon_id = " . (int)$gv_id);
-
-    if ($customer_gv->RecordCount() > 0) {
-      $new_gv_amount = $customer_gv->fields['amount'] + $coupon_gv->fields['coupon_amount'];
-      $gv_query = $db->Execute("update " . TABLE_COUPON_GV_CUSTOMER . "
-                                set amount = '" . $new_gv_amount . "'
-                                where customer_id = " . (int)$customer_id);
-    } else {
-      $db->Execute("insert into " . TABLE_COUPON_GV_CUSTOMER . " (customer_id, amount) values (" . (int)$customer_id . ", '" . $coupon_gv->fields['coupon_amount'] . "')");
-    }
-  }
 
 /**
  * Validate Option Name and Option Type Match
@@ -2744,41 +2686,6 @@ function get_logs_data($maxToList = 'count') {
     return @chmod($filepath, 0444);
   }
 
-
-/**
- * is coupon valid for specials and sales
- * @param int $product_id
- * @param int $coupon_id
- * @return bool
- */
-  function is_coupon_valid_for_sales($product_id, $coupon_id) {
-    global $db;
-    $sql = "SELECT coupon_id, coupon_is_valid_for_sales
-            FROM " . TABLE_COUPONS . "
-            WHERE coupon_id = " . (int)$coupon_id . "
-            LIMIT 1";
-
-    $result = $db->Execute($sql);
-    if ($result->EOF) {
-        return false;
-    }
-
-    // check whether coupon has been flagged for valid with sales
-    if ($result->fields['coupon_is_valid_for_sales']) {
-      return true;
-    }
-
-    // check for any special on $product_id
-    $chk_product_on_sale = zen_get_products_special_price($product_id, true);
-    if (!$chk_product_on_sale) {
-      // check for any sale on $product_id
-      $chk_product_on_sale = zen_get_products_special_price($product_id, false);
-    }
-    if ($chk_product_on_sale) {
-      return false;
-    }
-    return true; // is not on special or sale
-  }
 
 /**
  * Convert value to a float -- mainly used for sanitizing and returning non-empty strings or nulls
--- includes/functions/functions_gvcoupons.php
+++ includes/functions/functions_gvcoupons.php
@@ -15,7 +15,8 @@
  * @param int $customer_id
  * @param int $gv_id
  */
-  function zen_gv_account_update(int $customer_id, int $gv_id) {
+function zen_gv_account_update(int $customer_id, int $gv_id)
+{
     global $db;
     $sql = "SELECT amount
             FROM " . TABLE_COUPON_GV_CUSTOMER . "
@@ -32,17 +33,17 @@
     if ($coupon_gv->EOF) return;
 
     if ($customer_gv->RecordCount() > 0) {
-      $new_gv_amount = $customer_gv->fields['amount'] + $coupon_gv->fields['coupon_amount'];
-      $sql = "UPDATE " . TABLE_COUPON_GV_CUSTOMER . "
+        $new_gv_amount = $customer_gv->fields['amount'] + $coupon_gv->fields['coupon_amount'];
+        $sql = "UPDATE " . TABLE_COUPON_GV_CUSTOMER . "
               SET amount = '" . $db->prepare_input($new_gv_amount) . "' WHERE customer_id = " . (int)$customer_id;
-      $db->Execute($sql);
+        $db->Execute($sql);
 
     } else {
-      $sql = "INSERT INTO " . TABLE_COUPON_GV_CUSTOMER . " (customer_id, amount)
-              VALUES (" . (int)$customer_id . ", '" . $db->prepare_input($coupon_gv->fields['coupon_amount']) . "')";
-      $db->Execute($sql);
+        $sql = "INSERT INTO " . TABLE_COUPON_GV_CUSTOMER . " (customer_id, amount)
+                VALUES (" . (int)$customer_id . ", '" . $db->prepare_input($coupon_gv->fields['coupon_amount']) . "')";
+        $db->Execute($sql);
     }
-  }
+}
 
 /**
  * Return GV balance for customer
@@ -52,18 +53,18 @@
  * @param int $customer_id
  * @return mixed|string
  */
-function zen_user_has_gv_account(int $customer_id) {
-  global $db;
-  if (zen_is_logged_in() && !zen_in_guest_checkout()) {
-    $gv_result = $db->Execute("select amount from " . TABLE_COUPON_GV_CUSTOMER . " where customer_id = '" . (int)$customer_id . "'");
-    if ($gv_result->RecordCount() > 0) {
-      if ($gv_result->fields['amount'] > 0) {
-        return $gv_result->fields['amount'];
-      }
+function zen_user_has_gv_account(int $customer_id)
+{
+    global $db;
+    if (zen_is_logged_in() && !zen_in_guest_checkout()) {
+        $gv_result = $db->Execute("select amount from " . TABLE_COUPON_GV_CUSTOMER . " where customer_id = '" . (int)$customer_id . "'");
+        if ($gv_result->RecordCount() > 0) {
+            if ($gv_result->fields['amount'] > 0) {
+                return $gv_result->fields['amount'];
+            }
+        }
     }
-  }
-  return '0.00';
-
+    return '0.00';
 }
 
 /**
@@ -73,7 +74,8 @@ function zen_user_has_gv_account(int $customer_id) {
  * @param string $prefix - include a prefix string if you want to force the generated code to start with a specific string
  * @return string (new coupon code) (will be blank if the function failed)
  */
-  function zen_create_coupon_code(string $salt="secret", int $length=SECURITY_CODE_LENGTH, string $prefix = '') {
+function zen_create_coupon_code(string $salt = "secret", int $length = SECURITY_CODE_LENGTH, string $prefix = '')
+{
     global $db;
     $length = (int)$length;
     static $max_db_length;
@@ -82,25 +84,25 @@ function zen_user_has_gv_account(int $customer_id) {
     if (strlen($prefix) > $max_db_length) return ''; // if prefix is already too long for the db, we can't generate a new code
     if (strlen($prefix) + (int)$length > $max_db_length) $length = $max_db_length - strlen($prefix);
     if ($length < 4) return ''; // if the recalculated length (esp in respect to prefixes) is less than 4 (for very basic entropy) then abort
-    $ccid = md5(uniqid("",$salt));
-    $ccid .= md5(uniqid("",$salt));
-    $ccid .= md5(uniqid("",$salt));
-    $ccid .= md5(uniqid("",$salt));
-    srand((double)microtime()*1000000); // seed the random number generator
+    $ccid = md5(uniqid("", $salt));
+    $ccid .= md5(uniqid("", $salt));
+    $ccid .= md5(uniqid("", $salt));
+    $ccid .= md5(uniqid("", $salt));
+    srand((double)microtime() * 1000000); // seed the random number generator
     $good_result = 0;
     $id1 = '';
     while ($good_result == 0) {
-      $random_start = @rand(0, (128-$length));
-      $id1=substr($ccid, $random_start, $length);
-      $sql = "SELECT coupon_code
-              FROM " . TABLE_COUPONS . "
-              WHERE coupon_code = :couponcode";
-      $sql = $db->bindVars($sql, ':couponcode', $prefix . $id1, 'string');
-      $result = $db->Execute($sql);
-      if ($result->RecordCount() < 1 ) $good_result = 1;
+        $random_start = @rand(0, (128 - $length));
+        $id1 = substr($ccid, $random_start, $length);
+        $sql = "SELECT coupon_code
+                FROM " . TABLE_COUPONS . "
+                WHERE coupon_code = :couponcode";
+        $sql = $db->bindVars($sql, ':couponcode', $prefix . $id1, 'string');
+        $result = $db->Execute($sql);
+        if ($result->RecordCount() < 1) $good_result = 1;
     }
     return ($good_result == 1) ? $prefix . $id1 : ''; // blank means couldn't generate a unique code (typically because the max length was encountered before being able to generate unique)
-  }
+}
 
 
 /**
@@ -109,7 +111,8 @@ function zen_user_has_gv_account(int $customer_id) {
  * @param int $coupon_id
  * @return bool
  */
-  function is_coupon_valid_for_sales(int $product_id, int $coupon_id) {
+function is_coupon_valid_for_sales(int $product_id, int $coupon_id)
+{
     global $db;
     $sql = "SELECT coupon_id, coupon_is_valid_for_sales
             FROM " . TABLE_COUPONS . "
@@ -117,22 +120,27 @@ function zen_user_has_gv_account(int $customer_id) {
 
     $result = $db->Execute($sql);
 
+    if ($result->EOF) {
+        return false;
+    }
+
     // check whether coupon has been flagged for not valid with sales
     if (!empty($result->fields['coupon_is_valid_for_sales'])) {
-      return true;
+        return true;
     }
 
     // check for any special on $product_id
     $chk_product_on_sale = zen_get_products_special_price($product_id, true);
     if (!$chk_product_on_sale) {
-      // check for any sale on $product_id
-      $chk_product_on_sale = zen_get_products_special_price($product_id, false);
+        // check for any sale on $product_id
+        $chk_product_on_sale = zen_get_products_special_price($product_id, false);
     }
     if ($chk_product_on_sale) {
-      return false;
+        return false;
     }
     return true; // is on special or sale
-  }
+}
+
 
 /**
  * Check whether the product is valid for the specified coupon, according to model/category/product restrictions assigned to the coupon
@@ -162,13 +170,13 @@ function is_product_valid(int $product_id, int $coupon_id): bool
     if ($coupons->RecordCount() == 0) return true;
     if ($coupons->RecordCount() == 1) {
 // If product is restricted(deny) and is same as tested product deny
-        if (($coupons->fields['product_id'] != 0) && $coupons->fields['product_id'] == (int)$product_id && $coupons->fields['coupon_restrict']=='Y') return false;
+        if (($coupons->fields['product_id'] != 0) && $coupons->fields['product_id'] == (int)$product_id && $coupons->fields['coupon_restrict'] == 'Y') return false;
 // If product is not restricted(allow) and is not same as tested product deny
-        if (($coupons->fields['product_id'] != 0) && $coupons->fields['product_id'] != (int)$product_id && $coupons->fields['coupon_restrict']=='N') return false;
+        if (($coupons->fields['product_id'] != 0) && $coupons->fields['product_id'] != (int)$product_id && $coupons->fields['coupon_restrict'] == 'N') return false;
 // if category is restricted(deny) and product in category deny
-        if (($coupons->fields['category_id'] !=0) && (zen_product_in_category($product_id, $coupons->fields['category_id'])) && ($coupons->fields['coupon_restrict']=='Y')) return false;
+        if (($coupons->fields['category_id'] != 0) && (zen_product_in_category($product_id, $coupons->fields['category_id'])) && ($coupons->fields['coupon_restrict'] == 'Y')) return false;
 // if category is not restricted(allow) and product not in category deny
-        if (($coupons->fields['category_id'] !=0) && (!zen_product_in_category($product_id, $coupons->fields['category_id'])) && ($coupons->fields['coupon_restrict']=='N')) return false;
+        if (($coupons->fields['category_id'] != 0) && (!zen_product_in_category($product_id, $coupons->fields['category_id'])) && ($coupons->fields['coupon_restrict'] == 'N')) return false;
         return true;
     }
     $allow_for_category = validate_for_category($product_id, $coupon_id);
@@ -232,7 +240,8 @@ function validate_for_category(int $product_id, int $coupon_id)
  * @param int $coupon_id
  * @return bool|string
  */
-function validate_for_product(int $product_id, int $coupon_id) {
+function validate_for_product(int $product_id, int $coupon_id)
+{
     global $db;
     $sql = "SELECT * FROM " . TABLE_COUPON_RESTRICT . "
             WHERE product_id = " . (int)$product_id . "
