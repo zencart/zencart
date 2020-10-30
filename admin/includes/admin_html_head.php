bbabda254603cf519a235a157f537971a3e68663
Chris Brown <drbyte@zen-cart.com>
2020-10-30 09:48:12 -0400

    Prevent filename clashes when using admin_html_head
    `$file` is used in many pages for localized values
    `$value` is a safer choice, and consistent with other uses in this same script

--- admin/includes/admin_html_head.php
+++ admin/includes/admin_html_head.php
@@ -13,26 +13,26 @@ if (!defined('IS_ADMIN_FLAG')) {
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo TITLE; ?></title>
-<?php if (file_exists($file = 'includes/css/bootstrap.min.css')) { ?>
-    <link rel="stylesheet" href="<?php echo $file; ?>">
+<?php if (file_exists($value = 'includes/css/bootstrap.min.css')) { ?>
+    <link rel="stylesheet" href="<?php echo $value; ?>">
 <?php } else { ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 <?php } ?>
-<?php if (file_exists($file = 'includes/css/font-awesome.min.css')) { ?>
-    <link rel="stylesheet" href="<?php echo $file; ?>">
+<?php if (file_exists($value = 'includes/css/font-awesome.min.css')) { ?>
+    <link rel="stylesheet" href="<?php echo $value; ?>">
 <?php } else { ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <?php } ?>
-<?php if (file_exists($file = 'includes/css/jquery-ui.css')) { ?>
-    <link rel="stylesheet" href="<?php echo $file; ?>">
+<?php if (file_exists($value = 'includes/css/jquery-ui.css')) { ?>
+    <link rel="stylesheet" href="<?php echo $value; ?>">
 <?php } else { ?>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <?php } ?>
     <link rel="stylesheet" href="includes/css/jAlert.css">
     <link rel="stylesheet" href="includes/css/menu.css">
     <link rel="stylesheet" href="includes/css/stylesheet.css">
-<?php if (file_exists($file = 'includes/css/' . basename($PHP_SELF, '.php') . '.css')) { ?>
-    <link rel="stylesheet" href="<?php echo $file; ?>">
+<?php if (file_exists($value = 'includes/css/' . basename($PHP_SELF, '.php') . '.css')) { ?>
+    <link rel="stylesheet" href="<?php echo $value; ?>">
 <?php
 }
 foreach ($installedPlugins as $plugin) {
