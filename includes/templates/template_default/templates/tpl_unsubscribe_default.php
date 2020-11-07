3f37e3191220103dea9b19e3a164832aa019aeed
Chris Brown <drbyte@zen-cart.com>
2020-11-07 15:58:18 -0500

    Update tpl_unsubscribe_default.php

--- includes/templates/template_default/templates/tpl_unsubscribe_default.php
+++ includes/templates/template_default/templates/tpl_unsubscribe_default.php
@@ -16,17 +16,17 @@
 
 <?php echo ($unsubscribe_address=='') ? UNSUBSCRIBE_TEXT_NO_ADDRESS_GIVEN : UNSUBSCRIBE_TEXT_INFORMATION; ?>
 
-<div class="buttonRow forward"><?php echo '<a href="' . zen_href_link(FILENAME_UNSUBSCRIBE, 'addr=' . $unsubscribe_address . '&action=unsubscribe', 'NONSSL') . '">' . zen_image_button(BUTTON_IMAGE_UNSUBSCRIBE, BUTTON_UNSUBSCRIBE) . '</a>'; ?></div>
+<div class="buttonRow forward"><?php echo '<a href="' . zen_href_link(FILENAME_UNSUBSCRIBE, 'addr=' . $unsubscribe_address . '&action=unsubscribe', 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_UNSUBSCRIBE, BUTTON_UNSUBSCRIBE) . '</a>'; ?></div>
 
 <?php } elseif (isset($_GET['action']) && ($_GET['action'] == 'unsubscribe')) { ?>
 <h1 id="unsubDefaultHeading"><?php echo HEADING_TITLE; ?></h1>
 
 <?php echo $status_display; ?>
 
-<div class="buttonRow forward"><?php echo '<a href="' . zen_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '">' . zen_image_button(BUTTON_IMAGE_CONTINUE_SHOPPING, BUTTON_CONTINUE_SHOPPING_ALT) . '</a>'; ?></div>
+<div class="buttonRow forward"><?php echo '<a href="' . zen_href_link(FILENAME_DEFAULT, '', 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_CONTINUE_SHOPPING, BUTTON_CONTINUE_SHOPPING_ALT) . '</a>'; ?></div>
 
 <?php } else {
-        zen_redirect(zen_href_link(FILENAME_DEFAULT,'','NONSSL'));
+        zen_redirect(zen_href_link(FILENAME_DEFAULT,'','SSL'));
    }
 ?>
 </div>
