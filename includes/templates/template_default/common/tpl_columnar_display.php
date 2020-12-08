4c64595af8a453f7e027a3dc539ccc2a560e1331
Chris Brown <drbyte@zen-cart.com>
2020-12-07 21:40:42 -0500

    Simplify array handling using foreach

--- includes/templates/template_default/common/tpl_columnar_display.php
+++ includes/templates/template_default/common/tpl_columnar_display.php
@@ -4,11 +4,10 @@
  *
  * This file is used for generating tabular output where needed, based on the supplied array of table-cell contents.
  *
- * @package templateSystem
- * @copyright Copyright 2003-2018 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Drbyte Sun Jan 7 21:28:50 2018 -0500 Modified in v1.5.6 $
+ * @version $Id:  Modified in v1.5.8 $
  */
 
 $zco_notifier->notify('NOTIFY_TPL_COLUMNAR_DISPLAY_START', $current_page_base, $list_box_contents, $title);
@@ -23,18 +22,18 @@ $zco_notifier->notify('NOTIFY_TPL_COLUMNAR_DISPLAY_START', $current_page_base, $
  ?>
 <?php
 if (is_array($list_box_contents)) {
- for($row=0, $n=sizeof($list_box_contents); $row<$n; $row++) {
-    $params = "";
+    foreach ($list_box_contents as $row => $cols) {
+        $params = '';
     //if (isset($list_box_contents[$row]['params'])) $params .= ' ' . $list_box_contents[$row]['params'];
 ?>
 
 <?php
-    for($col=0, $j=sizeof($list_box_contents[$row]); $col<$j; $col++) {
+    foreach ($cols as $col) {
       $r_params = "";
-      if (isset($list_box_contents[$row][$col]['params'])) $r_params .= ' ' . (string)$list_box_contents[$row][$col]['params'];
-     if (isset($list_box_contents[$row][$col]['text'])) {
+      if (isset($col['params'])) $r_params .= ' ' . (string)$col['params'];
+      if (isset($col['text'])) {
 ?>
-    <?php echo '<div' . $r_params . '>' . $list_box_contents[$row][$col]['text'] .  '</div>' . "\n"; ?>
+    <?php echo '<div' . $r_params . '>' . $col['text'] .  '</div>' . "\n"; ?>
 <?php
       }
     }
