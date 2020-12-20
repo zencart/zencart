4e6f4dc0126a6bba1019fe904bf24e8548d19083
Chris Brown <drbyte@zen-cart.com>
2020-12-20 12:12:18 -0500

    Fix formatting

--- includes/modules/pages/about_us/header_php.php
+++ includes/modules/pages/about_us/header_php.php
@@ -8,7 +8,7 @@
  */
 
 
-require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
+require DIR_WS_MODULES . zen_get_module_directory('require_languages.php');
 $breadcrumb->add(NAVBAR_TITLE);
 
 $define_page = zen_get_file_directory(DIR_WS_LANGUAGES . $_SESSION['language'] . '/html_includes/', FILENAME_DEFINE_ABOUT_US, 'false');
