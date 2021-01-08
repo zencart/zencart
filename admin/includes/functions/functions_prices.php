65dc43059c9fd0ca51f2f0851c4d1838b6faf535
Chris Brown <drbyte@zen-cart.com>
2021-01-08 17:51:45 -0500

    Merge #4166 to admin
    Copy changes to admin copy of same function

--- admin/includes/functions/functions_prices.php
+++ admin/includes/functions/functions_prices.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2020 Dec 29 Modified in v1.5.7c $
+ * @version $Id: DrByte 2021 Jan 08  Modified in v1.5.7c $
  */
 ////
 //get specials price or sale price
@@ -473,13 +473,12 @@ if (false) {
 
     $discount_type_id = zen_get_products_sale_discount_type($product_id);
 
+    $special_price_discount = 0;
     if ($new_products_price != 0) {
       $special_price_discount = ($new_special_price != 0 ? ($new_special_price/$new_products_price) : 1);
-    } else {
-      $special_price_discount = '';
     }
 
-    $sale_price_discount = '';
+    $sale_price_discount = 0;
     if ($new_products_price != 0) {
       $sale_price_discount = ($new_sale_price != 0 ? ($new_sale_price/$new_products_price) : 1);
     }
