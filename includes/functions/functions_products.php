ab6c290b40f995418ad3b1f8f2225e8dbae9fa19
Chris Brown <drbyte@zen-cart.com>
2021-01-13 12:05:19 -0500

    Change order of GV check to prevent notice
    Fixes #4173

--- includes/functions/functions_products.php
+++ includes/functions/functions_products.php
@@ -776,7 +776,7 @@ function zen_get_products_allow_add_to_cart($product_id)
 
 
     // If product is encoded as GV but GV feature is turned off, disallow add-to-cart
-    if (preg_match('/^GIFT/', addslashes($product_query_results->fields['products_model'])) && $allow_add_to_cart) {
+    if ($allow_add_to_cart && preg_match('/^GIFT/', addslashes($product_query_results->fields['products_model']))) {
         if (MODULE_ORDER_TOTAL_GV_STATUS !== 'true') {
             $allow_add_to_cart = false;
         }
