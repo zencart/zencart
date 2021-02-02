524dd355ac3cd7ca3493efc539f32cfcf97801d4
Chris Brown <drbyte@zen-cart.com>
2021-02-02 13:17:50 -0500

    Update header_php.php

--- includes/modules/pages/account_edit/header_php.php
+++ includes/modules/pages/account_edit/header_php.php
@@ -2,10 +2,10 @@
 /**
  * Header code file for the customer's Account-Edit page
  *
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Scott C Wilson 2020 Mar 18 Modified in v1.5.7 $
+ * @version $Id:  Modified in v1.5.8 $
  */
 // This should be first line of the script:
 $zco_notifier->notify('NOTIFY_HEADER_START_ACCOUNT_EDIT');
