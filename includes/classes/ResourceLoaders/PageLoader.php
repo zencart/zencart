6b3f6ac35d45cb426d5851eb6581c9ab3e5d37e9
Chris Brown <drbyte@zen-cart.com>
2021-02-24 17:48:26 -0500

    header date

--- includes/classes/ResourceLoaders/PageLoader.php
+++ includes/classes/ResourceLoaders/PageLoader.php
@@ -1,9 +1,9 @@
 <?php
 /**
  *
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: Zcwilt 2019 Aug 23 New in v1.5.7 $
+ * @version $Id: Modified in v1.5.8 $
  */
 
 namespace Zencart\PageLoader;
