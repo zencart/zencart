29cd1ab5c7deb5116fdece4332629d15bcf380ac
Chris Brown <drbyte@zen-cart.com>
2021-03-03 20:37:52 -0500

    Header

--- includes/classes/db/mysql/query_factory.php
+++ includes/classes/db/mysql/query_factory.php
@@ -3,7 +3,7 @@
  * MySQL query_factory class.
  * Class used for database abstraction to MySQL via mysqli
  *
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @copyright Portions adapted from http://www.data-diggers.com/
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
