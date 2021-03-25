c415e854a651e8b7363ed6848990508f624d963a
Steve <4391026+torvista@users.noreply.github.com>
2021-03-25 11:38:32 +0100

    use backwards-compatible charset declaration

--- includes/templates/template_default/common/html_header.php
+++ includes/templates/template_default/common/html_header.php
@@ -30,7 +30,7 @@ require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php echo HTML_PARAMS; ?>>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>" />
+<meta charset="<?php echo CHARSET; ?>" />
 <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com">
 <link rel="dns-prefetch" href="https://code.jquery.com">
 <title><?php echo META_TAG_TITLE; ?></title>
