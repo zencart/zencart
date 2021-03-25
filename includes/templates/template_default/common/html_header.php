1a5801240094611695fe883a958380ee93e16157
Steve <4391026+torvista@users.noreply.github.com>
2021-03-25 11:31:14 +0100

    make charset declaration first in head

--- includes/templates/template_default/common/html_header.php
+++ includes/templates/template_default/common/html_header.php
@@ -30,10 +30,9 @@ require(DIR_WS_MODULES . zen_get_module_directory('meta_tags.php'));
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php echo HTML_PARAMS; ?>>
 <head>
-    <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com">
-    <link rel="dns-prefetch" href="https://code.jquery.com">
-
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>" />
+<link rel="dns-prefetch" href="https://cdnjs.cloudflare.com">
+<link rel="dns-prefetch" href="https://code.jquery.com">
 <title><?php echo META_TAG_TITLE; ?></title>
 <meta name="keywords" content="<?php echo META_TAG_KEYWORDS; ?>" />
 <meta name="description" content="<?php echo META_TAG_DESCRIPTION; ?>" />
