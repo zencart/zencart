69ac6e1098578e93b901421a8bfa619e7d3c387a
Chris Brown <drbyte@zen-cart.com>
2021-05-07 18:14:11 -0400

    Dates

--- includes/classes/traits/NotifierManager.php
+++ includes/classes/traits/NotifierManager.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2020 Jun 22 Modified in v1.5.7 $
+ * @version $Id: DrByte  Modified in v1.5.8 $
  */
 
 namespace Zencart\Traits;
