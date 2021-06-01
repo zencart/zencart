4cd612968d0283f10a35690c220b4d9cd62b8221
Chris Brown <drbyte@zen-cart.com>
2021-06-01 17:29:34 -0400

    header

--- admin/orders.php
+++ admin/orders.php
@@ -3,7 +3,7 @@
  * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: mc12345678 2021 Feb 22 Modified in v1.5.7c $
+ * @version $Id:  Modified in v1.5.8 $
  */
 require('includes/application_top.php');
 
