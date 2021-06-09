947ad240ff1df628ae01404a0d6ad6d93cab036d
Steve <4391026+torvista@users.noreply.github.com>
2021-06-09 21:59:59 +0200

    correct label for entry_suburb

--- admin/customers.php
+++ admin/customers.php
@@ -450,7 +450,7 @@ if (!empty($action)) {
           if (ACCOUNT_SUBURB == 'true') {
             ?>
             <div class="form-group">
-              <?php echo zen_draw_label(ENTRY_SUBURB, 'suburb', 'class="col-sm-3 control-label"'); ?>
+              <?php echo zen_draw_label(ENTRY_SUBURB, 'entry_suburb', 'class="col-sm-3 control-label"'); ?>
               <div class="col-sm-9 col-md-6">
                 <?php echo zen_draw_input_field('entry_suburb', htmlspecialchars($cInfo->suburb, ENT_COMPAT, CHARSET, TRUE), zen_set_field_length(TABLE_ADDRESS_BOOK, 'entry_suburb', 50) . ' class="form-control" id="entry_suburb"'); ?>
               </div>
