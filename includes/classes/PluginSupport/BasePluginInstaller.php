48ba3b2c247aca814f2a47ff7a6a025cd86e1396
Chris Brown <drbyte@zen-cart.com>
2021-06-24 10:47:23 -0400

    Fix typo: langauages -> languages

--- includes/classes/PluginSupport/BasePluginInstaller.php
+++ includes/classes/PluginSupport/BasePluginInstaller.php
@@ -78,7 +78,7 @@ class BasePluginInstaller
     protected function loadInstallerLanguageFile($file)
     {
         $lng = $_SESSION['language'];
-        $filename = $this->pluginDir . '/installer/langauages/' . $lng . '/' . $file;
+        $filename = $this->pluginDir . '/installer/languages/' . $lng . '/' . $file;
         if (file_exists($filename)) {
             require_once($filename);
         }
