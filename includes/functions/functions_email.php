08ffe3dff2500fb733adef8b4dfcd836a073c9f1
Chris Brown <drbyte@zen-cart.com>
2021-06-24 20:18:35 -0400

    header

--- includes/functions/functions_email.php
+++ includes/functions/functions_email.php
@@ -4,10 +4,10 @@
  * Processes all outbound email from Zen Cart
  * Hooks into phpMailer class for actual email encoding and sending
  *
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2020 Apr 26 Modified in v1.5.7 $
+ * @version $Id: DrByte  Modified in v1.5.8 $
  */
 
 use PHPMailer\PHPMailer\PHPMailer;
