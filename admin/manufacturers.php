cf1e828eada542458d980932fb8b1dc544ffc2d8
Chris Brown <drbyte@zen-cart.com>
2021-07-23 16:56:28 -0400

    date

--- admin/manufacturers.php
+++ admin/manufacturers.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id:  Modified in v1.5.8 $
