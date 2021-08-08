2e889a78863d8842efea03b2eea8e46abea464cb
mc12345678 <mc12345678@users.noreply.github.com>
2021-08-08 11:17:54 -0400

    Prevent debug log when hiding cart combine
    Addresses the issue described at:
    https://www.zen-cart.com/showthread.php?228272-Help-with-Error-Log-(PHP-Notice-Undefined-variable-zc_check_basket_before)
    
    This takes the shortest/simplest action to correct the issue by use of logic
    instead of additional code.  Any other change simply adds more code to file
    but may be desired to ensure updaters take at least one action which would
    prevent this issue in a common otherwise unmodified store.
    
    This is applicable to V1.5.7 and previous version as well, but likely
    is not made known unless using strict PHP operation as it does not appear
    to have been revealed until a high enough version of php was used and
    someone reported it.

--- includes/modules/pages/login/header_php.php
+++ includes/modules/pages/login/header_php.php
@@ -106,7 +106,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'process') {
 
         // check current cart contents count if required
         $zc_check_basket_after = $_SESSION['cart']->count_contents();
-        if (($zc_check_basket_before != $zc_check_basket_after) && $_SESSION['cart']->count_contents() > 0 && SHOW_SHOPPING_CART_COMBINED > 0) {
+        if (SHOW_SHOPPING_CART_COMBINED > 0 && ($zc_check_basket_before != $zc_check_basket_after) && $_SESSION['cart']->count_contents() > 0) {
           if (SHOW_SHOPPING_CART_COMBINED == 2) {
             // warning only do not send to cart
             $messageStack->add_session('header', WARNING_SHOPPING_CART_COMBINED, 'caution');
