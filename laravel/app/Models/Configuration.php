6acd7142dca05d5ab15fe669d97e8dfd6410a0ca
Chris Brown <drbyte@zen-cart.com>
2021-08-31 19:35:17 -0400

    date

--- laravel/app/Models/Configuration.php
+++ laravel/app/Models/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright 2003-2020 Zen Cart Development Team
+ * @copyright Copyright 2003-2021 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version GIT: $Id: $
  */
