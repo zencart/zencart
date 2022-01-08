6ffecc91ae3a79e3be41cc64995cabcb6d643770
Scott C Wilson <scottcwilson@gmail.com>
2022-01-08 13:57:09 -0500

    Allow (new style) COD defs to be loaded without error

--- includes/languages/english/modules/payment/lang.cod.php
+++ includes/languages/english/modules/payment/lang.cod.php
@@ -1,8 +1,8 @@
 <?php
 
-$defines = [
+$define = [
     'MODULE_PAYMENT_COD_TEXT_TITLE' => 'Cash on Delivery',
     'MODULE_PAYMENT_COD_TEXT_DESCRIPTION' => 'Cash on Delivery',
 ];
 
-return $defines;
+return $define;
