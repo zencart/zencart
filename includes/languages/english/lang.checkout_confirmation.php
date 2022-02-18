62d2803175bcf075fff70c4b39335b1c01a471e7
torvista <4391026+torvista@users.noreply.github.com>
2022-02-18 20:43:21 +0100

    Rationalise HEADING_ORDER_COMMENTS

--- includes/languages/english/lang.checkout_confirmation.php
+++ includes/languages/english/lang.checkout_confirmation.php
@@ -4,7 +4,6 @@ $define = [
     'NAVBAR_TITLE_2' => 'Order Confirmation',
     'HEADING_TITLE' => 'Step 3 of 3 - Order Confirmation',
     'HEADING_PRODUCTS' => 'Shopping Cart Contents',
-    'HEADING_ORDER_COMMENTS' => 'Special Instructions or Order Comments',
     'NO_COMMENTS_TEXT' => 'None',
     'TITLE_CONTINUE_CHECKOUT_PROCEDURE' => '<strong>Final Step</strong>',
     'TEXT_CONTINUE_CHECKOUT_PROCEDURE' => '- continue to confirm your order. Thank you!',
