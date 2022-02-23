e9eb92db9a2b83be6519d8c867474d22aa0658c2
torvista <4391026+torvista@users.noreply.github.com>
2022-02-23 18:19:56 +0100

    new file for robots pages to skip

--- includes/languages/english/lang.meta_tags.php
+++ includes/languages/english/lang.meta_tags.php
@@ -11,7 +11,6 @@ $define = [
     'SECONDARY_SECTION' => ' - ',
     'TERTIARY_SECTION' => ', ',
     'METATAGS_DIVIDER' => ' ',
-    'ROBOTS_PAGES_TO_SKIP' => 'login,logoff,create_account,account,account_edit,account_history,account_history_info,account_newsletters,account_notifications,account_password,address_book,search,search_result,checkout_success,checkout_process,checkout_shipping,checkout_payment,checkout_confirmation,cookie_usage,create_account_success,contact_us,download,download_timeout,customers_authorization,down_for_maintenance,password_forgotten,time_out,unsubscribe,info_shopping_cart,gv_faq,gv_redeem,gv_send,popup_image,popup_image_additional,product_reviews_write,ssl_check,shopping_cart,no_account,order_status',
 ];
 
 return $define;
