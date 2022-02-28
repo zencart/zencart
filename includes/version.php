40c161c8f5c0d58ac9e39d2dec479780d522e47e
Ian Wilson <ian@zen-cart.com>
2022-02-28 09:14:29 +0000

    fix version file

--- includes/version.php
+++ includes/version.php
@@ -15,7 +15,7 @@
 
 define('PROJECT_VERSION_NAME', 'Zen Cart');
 define('PROJECT_VERSION_MAJOR', '1');
-define('PROJECT_VERSION_MINOR', '5.7c');
+define('PROJECT_VERSION_MINOR', '5.7d');
 define('PROJECT_VERSION_PATCH1', '');
 define('PROJECT_VERSION_PATCH2', '');
 define('PROJECT_VERSION_PATCH1_SOURCE', '');
