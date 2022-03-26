12086932b47260b9cdc1ea9e26aff23232c2730a
torvista <4391026+torvista@users.noreply.github.com>
2022-03-26 16:11:50 +0100

    strict comparisons

--- includes/modules/pages/down_for_maintenance/header_php.php
+++ includes/modules/pages/down_for_maintenance/header_php.php
@@ -11,10 +11,10 @@
 require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
 $breadcrumb->add(NAVBAR_TITLE);
 
-if (DOWN_FOR_MAINTENANCE_COLUMN_RIGHT_OFF == 'true') $flag_disable_right = true;
-if (DOWN_FOR_MAINTENANCE_COLUMN_LEFT_OFF == 'true') $flag_disable_left = true;
-if (DOWN_FOR_MAINTENANCE_FOOTER_OFF == 'true') $flag_disable_footer = true;
-if (DOWN_FOR_MAINTENANCE_HEADER_OFF == 'true') $flag_disable_header = true;
+if (DOWN_FOR_MAINTENANCE_COLUMN_RIGHT_OFF === 'true') $flag_disable_right = true;
+if (DOWN_FOR_MAINTENANCE_COLUMN_LEFT_OFF === 'true') $flag_disable_left = true;
+if (DOWN_FOR_MAINTENANCE_FOOTER_OFF === 'true') $flag_disable_footer = true;
+if (DOWN_FOR_MAINTENANCE_HEADER_OFF === 'true') $flag_disable_header = true;
 
   $sql = "SELECT last_modified from " . TABLE_CONFIGURATION . "
           WHERE configuration_key = 'DOWN_FOR_MAINTENANCE'";
