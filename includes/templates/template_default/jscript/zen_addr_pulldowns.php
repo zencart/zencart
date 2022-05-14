68b022415312fd0a6b751896a1781529095da3a7
lat9 <cindy@vinosdefrutastropicales.com>
2022-05-14 08:17:42 -0400

    - Adding the previously-missing jscript inclusion on the 'shopping_cart' page (for the shipping-estimator). - Correcting unwanted display of the #stateLabel when it's an empty element.

--- includes/templates/template_default/jscript/zen_addr_pulldowns.php
+++ includes/templates/template_default/jscript/zen_addr_pulldowns.php
@@ -11,47 +11,46 @@
  * @version $Id: Zen4All 2019 Jun 03 Modified in v1.5.7 $
  */
 // -----
-// If the site does NOT require a 'State' entry in an address or has NOT enabled states to
-// be displayed as dropdowns, nothing to be done here!
+// If the site does NOT require a 'State' entry in an address, nothing to be done here!
 //
-if (ACCOUNT_STATE !== 'true' || ACCOUNT_STATE_DRAW_INITIAL_DROPDOWN !== 'true') {
+if (ACCOUNT_STATE !== 'true') {
     return;
 }
 
 // -----
-// If the current site has at least one country enabled that uses zones, a JSON-encoded array of
-// countries-to-zones will be created for use by the jQuery section.
+// If the current site is using the state dropdowns, create the array that identifies the various
+// zones for the currently-active countries.  When state dropdowns aren't being used, the
+// empty $c2z array will 'instruct' the jQuery section to presume that no countries have
+// associated zones.
 //
-$countries = $db->Execute(
-    "SELECT DISTINCT zone_country_id
-       FROM " . TABLE_ZONES . "
-            INNER JOIN " . TABLE_COUNTRIES . "
-                ON countries_id = zone_country_id
-               AND status = 1
-   ORDER BY zone_country_id"
-);
-
 $c2z = [];
-foreach ($countries as $next_country) {
-    $current_country_id = $next_country['zone_country_id'];
-    $c2z[$current_country_id] = [];
-
-    $states = $db->Execute(
-        "SELECT zone_name, zone_id
+if (ACCOUNT_STATE_DRAW_INITIAL_DROPDOWN === 'true') {
+    // -----
+    // If the current site has at least one country enabled that uses zones, a JSON-encoded array of
+    // countries-to-zones will be created for use by the jQuery section.
+    //
+    $countries = $db->Execute(
+        "SELECT DISTINCT zone_country_id
            FROM " . TABLE_ZONES . "
-          WHERE zone_country_id = $current_country_id
-       ORDER BY zone_name"
+                INNER JOIN " . TABLE_COUNTRIES . "
+                    ON countries_id = zone_country_id
+                   AND status = 1
+       ORDER BY zone_country_id"
     );
-    foreach ($states as $next_state) {
-        $c2z[$current_country_id][$next_state['zone_id']] = $next_state['zone_name'];
-    }
-}
+    foreach ($countries as $next_country) {
+        $current_country_id = $next_country['zone_country_id'];
+        $c2z[$current_country_id] = [];
 
-// -----
-// If none of the currently-enabled countries use zones, nothing to do here.
-//
-if (count($c2z) === 0) {
-    return;
+        $states = $db->Execute(
+            "SELECT zone_name, zone_id
+               FROM " . TABLE_ZONES . "
+              WHERE zone_country_id = $current_country_id
+           ORDER BY zone_name"
+        );
+        foreach ($states as $next_state) {
+            $c2z[$current_country_id][$next_state['zone_id']] = $next_state['zone_name'];
+        }
+    }
 }
 ?>
 <script>
@@ -62,20 +61,25 @@ jQuery(document).ready(function() {
 // -----
 // Notes:
 //
-// 1. When the site is set to display states as dropdowns, it's **assumed** that the '#stateLabel' label's
-// text has been set to an empty string, thus unneeded.  Since this processing is **only** invoked when
-// dropdown states are enabled, start by hiding that label.  The '#stBreak' <br> is also never needed/wanted
-// since it will 'throw' the state-field input underneath the state/zone label.
+// 1. The '#stBreak' <br> is also never needed/wanted since it will 'throw' the state-field input underneath
+// the state/zone label.
 //
-// 2. Initialize the display for the dropdown vs. hand-entry of the state fields.  If the initially-selected
-// country doesn't have zones, the dropdown will contain only 1 element ('Type a choice below ...').
+// 2. If the '#stateLabel' label is empty, hide it!  It will be when the site uses dropdown states and on
+// the 'shipping_estimator' page.
 //
-// 3. There can be unwanted whitespace, e.g. an &nbsp; prior to the (optional) <span class="alert"> following
+// 3. Initialize the display for the dropdown vs. hand-entry of the state fields.  If the initially-selected
+// country doesn't have zones, the dropdown will contain only 1 element ('Type a choice below ...').  In that
+// case, the dropdown and associated elements will be hidden and the hand-input 'state' field will be shown.
+//
+// 4. There can be unwanted whitespace, e.g. an &nbsp; prior to the (optional) <span class="alert"> following
 // the 'stateZone' dropdown.  In that case, when the <span> is hidden for unzoned countries, the state input
 // field is slightly offset from the other input fields.
 //
 ?>
-    jQuery('#stateLabel, #stBreak').hide();
+    jQuery('#stBreak').hide();
+    if (jQuery('#stateLabel').text().length == 0) {
+        jQuery('#stateLabel').hide();
+    }
     if (jQuery('#stateZone > option').length > 1) {
         jQuery('#state').hide();
         jQuery('#stateZone').show();
@@ -100,7 +104,7 @@ jQuery(document).ready(function() {
         var countryHasZones = false;
         var countryZones = '<option selected="selected" value="0">' + pleaseSelect + '</option>';
         var selected_country = jQuery('#country option:selected').val();
-        jQuery.each(jQuery.parseJSON(country_zones), function(country_id, country_zones) {
+        jQuery.each(JSON.parse(country_zones), function(country_id, country_zones) {
             if (selected_country == country_id) {
                 countryHasZones = true;
                 jQuery.each(country_zones, function(zone_id, zone_name) {
