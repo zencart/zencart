e8c2b3de2aed9354af2b0df28129a99c1077c0ce
lat9 <cindy@vinosdefrutastropicales.com>
2022-06-12 07:29:14 -0400

    v158: `account_history_info`, add variable for compatability with pre-existing templates
    Fixes #4868

--- includes/modules/pages/account_history_info/header_php.php
+++ includes/modules/pages/account_history_info/header_php.php
@@ -19,19 +19,19 @@ if (empty($_GET['order_id']) || !is_numeric($_GET['order_id'])) {
   zen_redirect(zen_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL'));
 }
 
-require(DIR_WS_CLASSES . 'order.php');
+require DIR_WS_CLASSES . 'order.php';
 $order = new order($_GET['order_id']);
+$statusArray = $order->statuses;    //- For compatability with pre-existing templates
 
 $customer = new Customer;
 if (!$customer->isSameAsLoggedIn($order->info['customer_id'])) {
      zen_redirect(zen_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL'));
 }
 
-require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
+require DIR_WS_MODULES . zen_get_module_directory('require_languages.php');
 $breadcrumb->add(NAVBAR_TITLE_1, zen_href_link(FILENAME_ACCOUNT, '', 'SSL'));
 $breadcrumb->add(NAVBAR_TITLE_2, zen_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL'));
 $breadcrumb->add(sprintf(NAVBAR_TITLE_3, $_GET['order_id']));
-
 
 // This should be last line of the script:
 $zco_notifier->notify('NOTIFY_HEADER_END_ACCOUNT_HISTORY_INFO');
