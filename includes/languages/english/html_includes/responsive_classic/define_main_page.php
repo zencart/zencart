e83a8d6e0a252a17ed881efe2be715776ccec24f
John <dbltoe@users.noreply.github.com>
2022-06-25 09:57:36 -0500

    Update define_main_page.php

--- includes/languages/english/html_includes/responsive_classic/define_main_page.php
+++ includes/languages/english/html_includes/responsive_classic/define_main_page.php
@@ -1,8 +1,8 @@
 <a href="https://docs.zen-cart.com/user/" target="_blank" rel="noopener"><img src="images/large/zencart-docs.jpg" alt="Zen Cart Documentation" title="View the online Zen Cart documentation!" class="home-image"></a>
 <p class="biggerText">The template package uses PHP Mobile Detect to serve up the optimized layout based on device.  
-    If you are on a Desktop, you can also view the site in the <a class="red" href="index.php?main_page=index&amp;layoutType=tablet" name="Tablet Layout View">Tablet Layout</a>.  
-    Clicking on <a class="red" href="index.php?main_page=index&amp;layoutType=mobile" name="Mobile Layout View">Mobile layout</a> shows how the site will look on a mobile device. 
-    Click on <a class="red" href="index.php?main_page=index&amp;layoutType=default" name="Desktop Layout View">Desktop view</a> to switch back to the Desktop view.</p>
+    If you are on a Desktop, you can also view the site in the <a class="red" href="index.php?main_page=index&amp;layoutType=tablet" title="Tablet Layout View">Tablet Layout</a>.  
+    Clicking on <a class="red" href="index.php?main_page=index&amp;layoutType=mobile" title="Mobile Layout View">Mobile layout</a> shows how the site will look on a mobile device. 
+    Click on <a class="red" href="index.php?main_page=index&amp;layoutType=default" title="Desktop Layout View">Desktop view</a> to switch back to the Desktop view.</p>
     
 <p>This text is located in the file at: <code> /languages/english/html_includes/YOUR_TEMPLATE/define_main_page.php</code></p>
 <p>You can quickly edit this content (and remove this image) via Admin->Tools->Define Pages Editor, and select define_main_page from the pulldown.</p>
