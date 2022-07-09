25e9253e45c3517214e2f38639c5d253a4b9ecd0
brittainmark <gitbrit@inner-light.co.uk>
2022-07-09 18:16:51 +0100

    emailed_to length in coupon_email_track table
    Increase length to varchar(96) to be consistant with other zen cart
    email address fields.

--- zc_install/sql/install/mysql_zencart.sql
+++ zc_install/sql/install/mysql_zencart.sql
@@ -431,7 +431,7 @@ CREATE TABLE coupon_email_track (
   customer_id_sent int(11) NOT NULL default '0',
   sent_firstname varchar(32) default NULL,
   sent_lastname varchar(32) default NULL,
-  emailed_to varchar(32) default NULL,
+  emailed_to varchar(96) default NULL,
   date_sent datetime NOT NULL default '0001-01-01 00:00:00',
   PRIMARY KEY  (unique_id),
   KEY idx_coupon_id_zen (coupon_id)
