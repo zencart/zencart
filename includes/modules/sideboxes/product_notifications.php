e3da86b679e97b49a0ff3293d4960c3bf88bfa1a
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-21 11:27:29 -0400

    v158: product_notifications sidebox, use zen_is_logged_in and zen_in_guest_checkout ...
    ... includes refactoring.

--- includes/modules/sideboxes/product_notifications.php
+++ includes/modules/sideboxes/product_notifications.php
@@ -9,49 +9,51 @@
  */
 
 // test if box should show
-  $show_product_notifications = false;
+$show_product_notifications = false;
 
-  if (isset($_GET['products_id']) and zen_products_id_valid($_GET['products_id'])) {
-    if (isset($_SESSION['customer_id'])) {
-      $check_query = "select count(*) as count
-                      from " . TABLE_CUSTOMERS_INFO . "
-                      where customers_info_id = '" . (int)$_SESSION['customer_id'] . "'
-                      and global_product_notifications = '1'";
+if (isset($_GET['products_id']) && zen_products_id_valid($_GET['products_id'])) {
+    if (zen_is_logged_in() && !zen_in_guest_checkout()) {
+        $check_query =
+            "SELECT customers_info_id
+               FROM " . TABLE_CUSTOMERS_INFO . "
+              WHERE customers_info_id = " . (int)$_SESSION['customer_id'] . "
+                AND global_product_notifications = 1
+              LIMIT 1";
+        $check = $db->Execute($check_query);
 
-      $check = $db->Execute($check_query);
-
-      if ($check->fields['count'] <= 0) {
-        $show_product_notifications= true;
-      }
+        if (!$check->EOF) {
+            $show_product_notifications = true;
+        }
     } else {
-      $show_product_notifications= true;
+        $show_product_notifications = true;
     }
-  }
-
-if ($show_product_notifications == true) {
-  if (isset($_GET['products_id'])) {
-    if (isset($_SESSION['customer_id'])) {
-      $check_query = "select count(*) as count
-                      from " . TABLE_PRODUCTS_NOTIFICATIONS . "
-                      where products_id = '" . (int)$_GET['products_id'] . "'
-                      and customers_id = '" . (int)$_SESSION['customer_id'] . "'";
-
-      $check = $db->Execute($check_query);
-
-      $notification_exists = (($check->fields['count'] > 0) ? true : false);
-    } else {
-      $notification_exists = false;
-    }
-
-    if ($notification_exists == true) {
-      require($template->get_template_dir('tpl_yes_notifications.php',DIR_WS_TEMPLATE, $current_page_base,'sideboxes'). '/tpl_yes_notifications.php');
-    } else {
-      require($template->get_template_dir('tpl_no_notifications.php',DIR_WS_TEMPLATE, $current_page_base,'sideboxes'). '/tpl_no_notifications.php');
-    }
-    $title =  BOX_HEADING_NOTIFICATIONS;
-    $box_id = 'productnotifications';
-    $title_link = false;
-    require($template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base,'common') . '/' . $column_box_default);
-  }
 }
-?>
+
+if ($show_product_notifications === true) {
+    if (isset($_GET['products_id'])) {
+        $notification_exists = false;
+        if (zen_is_logged_in() && !zen_in_guest_checkout()) {
+           $check_query = 
+                "SELECT customers_id
+                   FROM " . TABLE_PRODUCTS_NOTIFICATIONS . "
+                  WHERE products_id = " . (int)$_GET['products_id'] . "
+                    AND customers_id = " . (int)$_SESSION['customer_id'] . "
+                  LIMIT 1";
+            $check = $db->Execute($check_query);
+
+            $notification_exists = !$check->EOF;
+        }
+
+        if ($notification_exists === true) {
+            require $template->get_template_dir('tpl_yes_notifications.php', DIR_WS_TEMPLATE, $current_page_base, 'sideboxes') . '/tpl_yes_notifications.php';
+        } else {
+            require $template->get_template_dir('tpl_no_notifications.php', DIR_WS_TEMPLATE, $current_page_base, 'sideboxes') . '/tpl_no_notifications.php';
+        }
+
+        $title =  BOX_HEADING_NOTIFICATIONS;
+        $box_id = 'productnotifications';
+        $title_link = false;
+        require $template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base, 'common') . '/' . $column_box_default;
+    }
+}
+
