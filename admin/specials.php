8de8b3ff8ba9a9547fcef8ceb915a0c8f0aad072
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-27 12:46:59 -0400

    v158: Adding ENT_COMPAT to various functions since default changed in PHP 8.1

--- admin/specials.php
+++ admin/specials.php
@@ -622,7 +622,10 @@ if (!empty($action)) {
                             }
                             $contents[] = ['text' => TEXT_INFO_LAST_MODIFIED . ' ' . zen_date_short($sInfo->specials_last_modified)];
                             $contents[] = ['text' => TEXT_INFO_DATE_ADDED . ' ' . zen_date_short($sInfo->specials_date_added)];
-                            $contents[] = ['align' => 'text-center', 'text' => zen_info_image($sInfo->products_image, htmlspecialchars($sInfo->products_name), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT)];
+                            $contents[] = [
+                                'align' => 'text-center',
+                                'text' => zen_info_image($sInfo->products_image, htmlspecialchars($sInfo->products_name, ENT_COMPAT, CHARSET, true), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT)
+                            ];
                             $contents[] = [
                                 'align' => 'text-center',
                                 'text' => '<a href="' . zen_href_link(FILENAME_PRODUCT, 'action=new_product' . '&cPath=' . zen_get_product_path($sInfo->products_id) . '&pID=' . $sInfo->products_id . '&product_type=' . zen_get_products_type($sInfo->products_id)) . '" class="btn btn-primary" role="button">' . IMAGE_EDIT_PRODUCT . '</a>'
