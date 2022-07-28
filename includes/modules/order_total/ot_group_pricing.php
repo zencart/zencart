870f618f59aedb570ee9ff4d5ee1287b5d3cacee
lat9 <cindy@vinosdefrutastropicales.com>
2022-07-28 14:22:16 -0400

    Add missing brace

--- includes/modules/order_total/ot_group_pricing.php
+++ includes/modules/order_total/ot_group_pricing.php
@@ -80,7 +80,7 @@ class ot_group_pricing {
   function calculate_deductions($order_total) {
     global $db, $order;
     $od_amount = array();
-    if ($order_total == 0 || !zen_is_logged_in() || zen_in_guest_checkout())
+    if ($order_total == 0 || !zen_is_logged_in() || zen_in_guest_checkout()) {
         return $od_amount;
     }
     $orderTotal = $this->get_order_total();
