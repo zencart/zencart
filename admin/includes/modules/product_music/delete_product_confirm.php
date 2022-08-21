1eeb42ebde100bd2cbf69e5febbe9c04991655d7
brittainmark <gitbrit@inner-light.co.uk>
2022-08-21 17:28:25 +0100

    Missing Music links
    Fix #5147 #5151
    Add link to default handler
    
    Either merge this change of delete copy_product.php, copy_product_confirm.php, update_product.php and /delete_product_confirm.php from admin/includes/modules/product_music

--- admin/includes/modules/product_music/delete_product_confirm.php
+++ admin/includes/modules/product_music/delete_product_confirm.php
@@ -5,3 +5,7 @@
  * See /admin/includes/classes/observers/auto.ProductMusicObserver.php for
  * the updated handling.
  */
+// -----
+// Use the default type's handling.
+//
+require DIR_WS_MODULES . 'delete_product_confirm.php';
