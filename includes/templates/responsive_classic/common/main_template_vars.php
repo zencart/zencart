2ff38a7f1bcc1f3900d90d6cb6e45da704d23529
dbltoe <dbltoe@gmail.com>
2022-09-13 19:21:48 -0500

    Saved this file to the wrong location

--- includes/templates/responsive_classic/common/main_template_vars.php
+++ includes/templates/responsive_classic/common/main_template_vars.php
@@ -1,49 +1,61 @@
 <?php
 /**
- * Common Template main_template_vars handler
- *
- * Normally a page will automatically load its own template based on the page name.
- * so that a page called some_page will load tpl_some_page_default.php from the template directory.
- *
- * However sometimes a page may need to choose the template it displays based on a set of criteria.
- * Placing a file in the includes/modules/pages/some_page/ directory called main_template_vars.php
- * allows you to override this page and choose the template that loads.
+ * Specials
  *
  * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2020 Dec 25 Modified in v1.5.8-alpha $
+ * @version $Id: DrByte 2021 Apr 26 Modified in v1.5.8-alpha $
  */
 
-if (!defined('IS_ADMIN_FLAG')) {
-    die('Illegal Access');
+if (MAX_DISPLAY_SPECIAL_PRODUCTS > 0 ) {
+  $specials_query_raw = "SELECT p.products_id, p.products_image, pd.products_name,
+                          p.master_categories_id
+                         FROM (" . TABLE_PRODUCTS . " p
+                         LEFT JOIN " . TABLE_SPECIALS . " s on p.products_id = s.products_id
+                         LEFT JOIN " . TABLE_PRODUCTS_DESCRIPTION . " pd on p.products_id = pd.products_id )
+                         WHERE p.products_id = s.products_id and p.products_id = pd.products_id and p.products_status = '1'
+                         AND s.status = 1
+                         AND pd.language_id = :languagesID
+                         ORDER BY s.specials_date_added DESC";
+
+  $specials_query_raw = $db->bindVars($specials_query_raw, ':languagesID', $_SESSION['languages_id'], 'integer');
+
+  $count_key = '*';
+  $zco_notifier->notify('NOTIFY_SPECIALS_MAIN_TEMPLATE_VARS_SQL_STRING', array(), $specials_query_raw, $count_key);
+
+  $specials_split = new splitPageResults($specials_query_raw, MAX_DISPLAY_SPECIAL_PRODUCTS, $count_key);
+  $specials = $db->Execute($specials_split->sql_query);
+  $row = 0;
+  $col = 0;
+  $list_box_contents = array();
+  $title = '';
+
+  $num_products_count = $specials->RecordCount();
+  if ($num_products_count) {
+    if ($num_products_count < SHOW_PRODUCT_INFO_COLUMNS_SPECIALS_PRODUCTS || SHOW_PRODUCT_INFO_COLUMNS_SPECIALS_PRODUCTS==0 ) {
+      $col_width = floor(100/$num_products_count);
+    } else {
+      $col_width = floor(100/SHOW_PRODUCT_INFO_COLUMNS_SPECIALS_PRODUCTS);
+    }
+
+    $list_box_contents = array();
+    while (!$specials->EOF) {
+
+      $products_price = zen_get_products_display_price($specials->fields['products_id']);
+      $specials->fields['products_name'] = zen_get_products_name($specials->fields['products_id']);
+      $list_box_contents[$row][$col] = array('params' => 'class="specialsListBoxContents"' . ' ' . 'style="width:' . $col_width . '%;"',
+                                             'text' => '<a href="' . zen_href_link(zen_get_info_page($specials->fields['products_id']), 'cPath=' . zen_get_generated_category_path_rev($specials->fields['master_categories_id']) . '&products_id=' . $specials->fields['products_id']) . '" title="' . "Product ID " . $specials->fields['products_id'] . '">' . (($specials->fields['products_image'] == '' and PRODUCTS_IMAGE_NO_IMAGE_STATUS == 0) ? '' : zen_image(DIR_WS_IMAGES . $specials->fields['products_image'], $specials->fields['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a>') . '<br><a href="' . zen_href_link(zen_get_info_page($specials->fields['products_id']), 'cPath=' . zen_get_generated_category_path_rev($specials->fields['master_categories_id']) . '&products_id=' . $specials->fields['products_id']) . '">' . $specials->fields['products_name'] . '</a><br>' . $products_price);
+      $col ++;
+      if ($col > (SHOW_PRODUCT_INFO_COLUMNS_SPECIALS_PRODUCTS - 1)) {
+        $col = 0;
+        $row ++;
+      }
+      $specials->MoveNext();
+    }
+
+    $zco_notifier->notify('NOTIFY_SPECIALS_MAIN_TEMPLATE_VARS_END', array(), $list_box_contents);
+
+    require($template->get_template_dir('tpl_specials_default.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_specials_default.php');
+  }
 }
-
-  $zco_notifier->notify('NOTIFY_MAIN_TEMPLATE_VARS_START', $template_dir);
-
-/**
- * set some variables used by templates
- */
-  if (!isset($layoutType)) $layoutType = 'legacy';
-  if (!isset($max_display_page_links)) $max_display_page_links = ($layoutType == 'mobile' ? MAX_DISPLAY_PAGE_LINKS_MOBILE : MAX_DISPLAY_PAGE_LINKS);
-  if (!isset($paginateAsUL)) $paginateAsUL = $layoutType == 'mobile' || (isset($isMobile) && $isMobile) || (isset($isTablet) && $isTablet);
-  if (!isset($flag_disable_left)) {
-    $flag_disable_left = false;
-  }
-  if (!isset($flag_disable_right)) {
-    $flag_disable_right = false;
-  }
-
-  if (!class_exists('Mobile_Detect')) {
-    include_once(DIR_WS_CLASSES . 'Mobile_Detect.php');
-  }
-  if (!isset($detect)) $detect = new Mobile_Detect;
-  if (!isset($_SESSION['layoutType'])) $_SESSION['layoutType'] = 'legacy';
-
-  $display_as_mobile = ($detect->isMobile() || $detect->isTablet() || $_SESSION['layoutType'] == 'mobile' || $_SESSION['layoutType'] == 'tablet');
-
-/**
- * load page-specific main_template_vars if present, or jump directly to template file
- */
-  $body_code = $pageLoader->getBodyCode();
-
-  $zco_notifier->notify('NOTIFY_MAIN_TEMPLATE_VARS_END', $template_dir, $body_code);
