96ce3fd116076a6436241b74ac75e3930279308a
John <dbltoe@users.noreply.github.com>
2022-09-13 19:18:34 -0500

    Update tpl_modules_products_featured_listing.php

--- includes/templates/responsive_classic/templates/tpl_modules_products_featured_listing.php
+++ includes/templates/responsive_classic/templates/tpl_modules_products_featured_listing.php
@@ -8,7 +8,7 @@
  * @copyright Copyright 2003-2022 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: dbltoe 2022 Sep 12 Modified in v1.5.8-alpha2 $
+ * @version $Id: John Thompson 2022 Jul 30 Modified in v1.5.8-alpha2 $
  */
 ?>
 <div id="product-listing">
