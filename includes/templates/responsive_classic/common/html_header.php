5b5b4d8abf6eed04d4eb9f77f5e45edf64aff581
Chris Brown <drbyte@zen-cart.com>
2022-10-05 22:58:01 -0400

    Upgrade jQuery from 3.5.1 to 3.6.1

--- includes/templates/responsive_classic/common/html_header.php
+++ includes/templates/responsive_classic/common/html_header.php
@@ -137,7 +137,7 @@ if (count($lng->catalog_languages) > 1) {
 
 /** CDN for jQuery core **/
 ?>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <?php if (file_exists(DIR_WS_TEMPLATE . "jscript/jquery.min.js")) { ?>
 <script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo $template->get_template_dir('.js',DIR_WS_TEMPLATE, $current_page_base,'jscript'); ?>/jquery.min.js"%3E%3C/script%3E'));</script>
 <?php } ?>
