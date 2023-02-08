ab2cd06cc09ea77b95c85ecee348855b49f011e0
lat9 <lat9@vinosdefrutastropicales.com>
2023-02-08 18:50:43 -0500

    #5523: Start with refactoring

--- admin/includes/init_includes/init_html_editor.php
+++ admin/includes/init_includes/init_html_editor.php
@@ -4,10 +4,12 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: DrByte 2020 Jul 10 Modified in v1.5.8-alpha $
  */
-if (!defined('DIR_WS_EDITORS')) define('DIR_WS_EDITORS', 'editors/');
+zen_define_default('DIR_WS_EDITORS', 'editors/');
+
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
+
 /**
  * List of potential editors apps
  *
@@ -19,29 +21,51 @@ if (!defined('IS_ADMIN_FLAG')) {
  *
  * NOTE: THERE SHOULD BE NO NEED TO EDIT ANYTHING BELOW THIS LINE:
  */
-  $editors_list['NONE'] = array('desc' => EDITOR_NONE, 'handler' => '', 'special_needs' => ''); // plain text
-  $editors_list['CKEDITOR']  = array('desc' => EDITOR_CKEDITOR,  'handler' => 'ckeditor.php',  'special_needs' => '');
-  if (is_dir(DIR_FS_CATALOG . DIR_WS_EDITORS . 'tiny_mce')) $editors_list['TINYMCE']   = array('desc' => EDITOR_TINYMCE,   'handler' => 'tinymce.php',   'special_needs' => '');
+$editors_list = [
+    'NONE' => [                 // Plain-text editor
+        'desc' => EDITOR_NONE,
+        'handler' => '',
+        'special_needs' => '',
+    ],
+    'CKEDITOR' => [
+        'desc' => EDITOR_CKEDITOR,
+        'handler' => 'ckeditor.php',
+        'special_needs' => '',
+    ],
+];
+if (is_dir(DIR_FS_CATALOG . DIR_WS_EDITORS . 'tiny_mce')) {
+    $editors_list['TINYMCE'] = [
+        'desc' => EDITOR_TINYMCE,
+        'handler' => 'tinymce.php',
+        'special_needs' => '',
+    ];
+}
 
 /**
  * Prepare pulldown menu for use in various pages where editor selections should be offered
  */
-  $editors_pulldown = array();
-  $i = 0;
-  foreach($editors_list as $key=>$value) {
-	  $i++;
-    $editors_pulldown[] = array('id' => $i, 'text' => $value['desc'], 'key' => $key);
-  }
+$editors_pulldown = [];
+$i = 0;
+foreach ($editors_list as $key => $value) {
+    $i++;
+    $editors_pulldown[] = [
+        'id' => $i,
+        'text' => $value['desc'],
+        'key' => $key
+    ];
+}
+
 /**
  * Session default is set if no preference has been chosen during this login session
  */
-  if (!isset($_SESSION['html_editor_preference_status'])) {
+if (!isset($_SESSION['html_editor_preference_status'])) {
     $_SESSION['html_editor_preference_status'] = HTML_EDITOR_PREFERENCE;
-  }
+}
+
 /**
  * If a new preference has been selected via a pulldown menu, set the details:
  */
-  $new_editor_choice = (isset($_GET['action']) && $_GET['action'] == 'set_editor' && isset($_GET['reset_editor'])) ? $_GET['reset_editor'] : -1;
+$new_editor_choice = (isset($_GET['action']) && $_GET['action'] === 'set_editor' && isset($_GET['reset_editor'])) ? (int)$_GET['reset_editor'] : -1;
 
 /**
  * Set a few variables for use in admin pages
@@ -51,29 +75,34 @@ if (!defined('IS_ADMIN_FLAG')) {
  * $editor_handler = the path to the handler file containing the logic required for <HEAD> insertion to activate editor features
  *
  */
-  foreach($editors_pulldown as $key=>$value) {
-    if ($new_editor_choice == $value['id']) $_SESSION['html_editor_preference_status'] = $value['key'];
-    if ($_SESSION['html_editor_preference_status'] == $value['key']) $current_editor_key = $value['id'];
-  }
-  $editor_handler = DIR_WS_INCLUDES . $editors_list[$_SESSION['html_editor_preference_status']]['handler'];
-  $editor_handler = ($editor_handler == DIR_WS_INCLUDES) ? '' : $editor_handler;
-  /* if handler not found, reset to NONE */
-  if ($editor_handler != '' && !file_exists($editor_handler)) {
+foreach ($editors_pulldown as $key => $value) {
+    if ($new_editor_choice === $value['id']) {
+        $_SESSION['html_editor_preference_status'] = $value['key'];
+    }
+    if ($_SESSION['html_editor_preference_status'] == $value['key']) {
+        $current_editor_key = $value['id'];
+    }
+}
+
+$editor_handler = DIR_WS_INCLUDES . $editors_list[$_SESSION['html_editor_preference_status']]['handler'];
+$editor_handler = ($editor_handler === DIR_WS_INCLUDES) ? '' : $editor_handler;
+/* if handler not found, reset to NONE */
+if ($editor_handler != '' && !file_exists($editor_handler)) {
     $editor_handler = '';
     $_SESSION['html_editor_preference_status'] = 'NONE';
     $current_editor_key = 0;
-  }
+}
 
 /**
  * Debug code:
  */
 if (false) {
-  echo '<br><pre>'; print_r($_GET); echo '</pre>';
-  echo '<br>new_editor_choice = ' . $new_editor_choice;
-  echo '<br>current_editor_key = ' . $current_editor_key;
-  echo '<br>$_SESSION[html_editor_preference_status] = ' . $_SESSION['html_editor_preference_status'];
-  echo '<br>editor_handler = ' . $editor_handler;
-  echo '<br><pre>'; print_r($editors_list); echo '</pre>';
-  echo '<br><pre>'; print_r($editors_pulldown); echo '</pre>';
-  //die('debug end');
+    echo '<br><pre>'; print_r($_GET); echo '</pre>';
+    echo '<br>new_editor_choice = ' . $new_editor_choice;
+    echo '<br>current_editor_key = ' . $current_editor_key;
+    echo '<br>$_SESSION[html_editor_preference_status] = ' . $_SESSION['html_editor_preference_status'];
+    echo '<br>editor_handler = ' . $editor_handler;
+    echo '<br><pre>'; print_r($editors_list); echo '</pre>';
+    echo '<br><pre>'; print_r($editors_pulldown); echo '</pre>';
+    //die('debug end');
 }
