5af0e41ac80fc0f4d852fae156eb5c4e8f186ec2
lat9 <lat9@vinosdefrutastropicales.com>
2023-02-12 07:40:46 -0500

    Best-sellers list, now refactor for readability

--- includes/modules/sideboxes/best_sellers.php
+++ includes/modules/sideboxes/best_sellers.php
@@ -7,53 +7,54 @@
 * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
 * @version $Id: Scott C Wilson 2022 Jul 27 Modified in v1.5.8-alpha2 $
 */
-$limit = (trim(MAX_DISPLAY_BESTSELLERS) == "") ? "" : " LIMIT " . (int)MAX_DISPLAY_BESTSELLERS;
 if (isset($current_category_id) && ($current_category_id > 0)) {
-  $best_sellers_query = "SELECT distinct p.products_id, pd.*, p.*
-                         FROM " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, "
-                                . TABLE_PRODUCTS_TO_CATEGORIES . " p2c, " . TABLE_CATEGORIES . " c
-                         WHERE p.products_status = '1'
-                         AND p.products_ordered > 0
-                         AND p.products_id = pd.products_id
-                         AND pd.language_id = '" . (int)$_SESSION['languages_id'] . "'
-                         AND p.products_id = p2c.products_id
-                         AND p2c.categories_id = c.categories_id
-                         AND '" . (int)$current_category_id . "' IN (c.categories_id, c.parent_id)
-                         ORDER BY p.products_ordered desc, pd.products_name";
+    $best_sellers_query =
+        "SELECT DISTINCT p.products_id, pd.*, p.*
+           FROM " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, "
+                    . TABLE_PRODUCTS_TO_CATEGORIES . " p2c, " . TABLE_CATEGORIES . " c
+          WHERE p.products_status = 1
+            AND p.products_ordered > 0
+            AND p.products_id = pd.products_id
+            AND pd.language_id = " . (int)$_SESSION['languages_id'] . "
+            AND p.products_id = p2c.products_id
+            AND p2c.categories_id = c.categories_id
+            AND " . (int)$current_category_id . " IN (c.categories_id, c.parent_id)
+          ORDER BY p.products_ordered desc, pd.products_name";
 
-  $best_sellers_query .= $limit;
-  $best_sellers = $db->Execute($best_sellers_query);
 } else {
-  $best_sellers_query = "SELECT DISTINCT p.products_id, pd.*, p.*
-                         FROM " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd
-                         WHERE p.products_status = '1'
-                         AND p.products_ordered > 0
-                         AND p.products_id = pd.products_id
-                         AND pd.language_id = '" . (int)$_SESSION['languages_id'] . "'
-                         ORDER BY p.products_ordered desc, pd.products_name";
-
-  $best_sellers_query .= $limit;
-  $best_sellers = $db->Execute($best_sellers_query);
+    $best_sellers_query =
+        "SELECT DISTINCT p.products_id, pd.*, p.*
+           FROM " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd
+          WHERE p.products_status = 1
+            AND p.products_ordered > 0
+            AND p.products_id = pd.products_id
+            AND pd.language_id = " . (int)$_SESSION['languages_id'] . "
+          ORDER BY p.products_ordered desc, pd.products_name";
 }
-if ($best_sellers->RecordCount() >= MIN_DISPLAY_BESTSELLERS) {
-  $title =  BOX_HEADING_BESTSELLERS;
-  $box_id =  'bestsellers';
-  $rows = 0;
-  $bestsellers_list = [];
-  while (!$best_sellers->EOF) {
-    $rows++;
-    $bestsellers_list[$rows]['id'] = $best_sellers->fields['products_id'];
-    $bestsellers_list[$rows]['name']  = $best_sellers->fields['products_name'];
-    $bestsellers_list[$rows]['image'] = zen_image(DIR_WS_IMAGES . $best_sellers->fields['products_image'], $best_sellers->fields['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);
-    $bestsellers_list[$rows]['href'] = zen_href_link(zen_get_info_page($best_sellers->fields["products_id"]), 'cPath=' . zen_get_generated_category_path_rev($best_sellers->fields["master_categories_id"]) . '&products_id=' . $best_sellers->fields["products_id"]);
-    $bestsellers_list[$rows]['price'] = zen_get_products_display_price((int)$best_sellers->fields['products_id']);
-    $bestsellers_list[$rows]['model']  = $best_sellers->fields['products_model'];
-    $bestsellers_list[$rows]['description']  = $best_sellers->fields['products_description'];
-    $best_sellers->MoveNext();
-  }
 
-  $title_link = false;
-  require($template->get_template_dir('tpl_best_sellers.php',DIR_WS_TEMPLATE, $current_page_base,'sideboxes'). '/tpl_best_sellers.php');
-  $title =  BOX_HEADING_BESTSELLERS;
-  require($template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base,'common') . '/' . $column_box_default);
+$limit = (trim(MAX_DISPLAY_BESTSELLERS) === '') ? '' : (' LIMIT ' . (int)MAX_DISPLAY_BESTSELLERS);
+$best_sellers_query .= $limit;
+$best_sellers = $db->Execute($best_sellers_query);
+if ($best_sellers->RecordCount() >= MIN_DISPLAY_BESTSELLERS) {
+    $bestsellers_list = [];
+    foreach ($best_sellers as $bestseller) {
+        $best_products_id = $bestseller['products_id'];
+        $bestsellers_list[] = [
+            'id' => $best_products_id,
+            'name'  => $bestseller['products_name'],
+            'image' => zen_image(DIR_WS_IMAGES . $bestseller['products_image'], $bestseller['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT),
+            'href' => zen_href_link(zen_get_info_page($best_products_id), 'cPath=' . zen_get_generated_category_path_rev($bestseller['master_categories_id']) . '&products_id=' . $best_products_id),
+            'price' => zen_get_products_display_price((int)$bestseller['products_id']),
+            'model'  => $bestseller['products_model'],
+            'description'  => $bestseller['products_description'],
+        ];
+    }
+
+    $title =  BOX_HEADING_BESTSELLERS;
+    $box_id =  'bestsellers';
+    $title_link = false;
+    require $template->get_template_dir('tpl_best_sellers.php', DIR_WS_TEMPLATE, $current_page_base, 'sideboxes') . '/tpl_best_sellers.php';
+
+    $title =  BOX_HEADING_BESTSELLERS;
+    require $template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base, 'common') . '/' . $column_box_default;
 }
