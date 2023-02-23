18e99e5c66a656f9894c6db9bffd43a8700f9219
Steve <4391026+torvista@users.noreply.github.com>
2023-02-23 11:42:36 +0100

    move strong tag from lang. to  templates

--- includes/languages/english/lang.checkout_confirmation.php
+++ includes/languages/english/lang.checkout_confirmation.php
@@ -5,7 +5,7 @@ $define = [
     'HEADING_TITLE' => 'Step 3 of 3 - Order Confirmation',
     'HEADING_PRODUCTS' => 'Shopping Cart Contents',
     'NO_COMMENTS_TEXT' => 'None',
-    'TITLE_CONTINUE_CHECKOUT_PROCEDURE' => '<strong>Final Step</strong>',
+    'TITLE_CONTINUE_CHECKOUT_PROCEDURE' => 'Final Step',
     'TEXT_CONTINUE_CHECKOUT_PROCEDURE' => '- continue to complete your order. Thank you!',
 ];
 
--- includes/templates/responsive_classic/templates/tpl_checkout_shipping_address_default.php
+++ includes/templates/responsive_classic/templates/tpl_checkout_shipping_address_default.php
@@ -58,7 +58,7 @@
 
 ?>
 
-<div class="buttonRow back"><?php echo TITLE_CONTINUE_CHECKOUT_PROCEDURE . '<br>' . TEXT_CONTINUE_CHECKOUT_PROCEDURE; ?></div>
+<div class="buttonRow back"><?php echo '<strong>' . TITLE_CONTINUE_CHECKOUT_PROCEDURE . '</strong>' . '<br>' . TEXT_CONTINUE_CHECKOUT_PROCEDURE; ?></div>
 
 <div class="buttonRow back clearBoth"><?php echo zen_back_link() . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>'; ?></div>
 </div>
