810920e4fb7331956934979e98d24ab21b315905
lat9 <cindy@vinosdefrutastropicales.com>
2023-02-28 09:01:55 -0500

    Brands page and sidebox restrictions (#5632)
    * #5629: Start with basic refactoring of sidebox
    
    * #5629: Brands page/sidebox, limit by PRODUCTS_MANUFACTURERS_STATUS

--- includes/modules/sideboxes/brands.php
+++ includes/modules/sideboxes/brands.php
@@ -8,22 +8,36 @@
  */
 
 // test if brands sidebox should show
-if ($current_page_base === 'brands') {
+if ($current_page_base === FILENAME_BRANDS) {
     return;
 }
-if ($current_page_base === 'index' && !empty($_GET['manufacturers_id'])) {
+if ($current_page_base === FILENAME_DEFAULT && !empty($_GET['manufacturers_id'])) {
     return;
 }
 
-// retrieve with featured items first
-$sql = "SELECT manufacturers_name, manufacturers_image, manufacturers_id, featured, (featured=1) as weighted
-        FROM " . TABLE_MANUFACTURERS . " m
-        ORDER BY weighted DESC, manufacturers_name";
+if (PRODUCTS_MANUFACTURERS_STATUS === '1') {
+    // retrieve with featured manufacturers first
+    $sql =
+        "SELECT DISTINCT m.manufacturers_name, m.manufacturers_image, m.manufacturers_id, m.featured, (m.featured=1) AS weighted
+           FROM " . TABLE_MANUFACTURERS . " m
+                LEFT JOIN " . TABLE_PRODUCTS . " p
+                    ON m.manufacturers_id = p.manufacturers_id
+          WHERE p.products_status = 1
+          ORDER BY weighted DESC, manufacturers_name";
+} else {
+    // retrieve with featured manufacturers first
+    $sql =
+        "SELECT m.manufacturers_name, m.manufacturers_image, m.manufacturers_id, m.featured, (m.featured=1) AS weighted
+           FROM " . TABLE_MANUFACTURERS . " m
+           ORDER BY weighted DESC, manufacturers_name";
+}
 $results = $db->Execute($sql);
 
-if ($results->RecordCount() == 0) return;
+if ($results->EOF) {
+    return;
+}
 
-$brands_array = array();
+$brands_array = [];
 foreach ($results as $result) {
     $brands_array[] = [
         'id' => $result['manufacturers_id'],
@@ -33,7 +47,7 @@ foreach ($results as $result) {
     ];
 }
 
-require($template->get_template_dir('tpl_brands.php', DIR_WS_TEMPLATE, $current_page_base, 'sideboxes') . '/tpl_brands.php');
+require $template->get_template_dir('tpl_brands.php', DIR_WS_TEMPLATE, $current_page_base, 'sideboxes') . '/tpl_brands.php';
 $title = BOX_HEADING_BRANDS;
 $title_link = FILENAME_BRANDS;
-require($template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base, 'common') . '/' . $column_box_default);
+require $template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base, 'common') . '/' . $column_box_default;
