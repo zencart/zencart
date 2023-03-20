8c3d0117da592abbb058a44155a6e76e9dcc7c08
mc12345678 <mc12345678@users.noreply.github.com>
2023-03-19 22:53:10 -0400

    Refactor spacing of code
    Visual formatting only. No change in operation.

--- admin/includes/classes/observers/auto.search_box.php
+++ admin/includes/classes/observers/auto.search_box.php
@@ -15,15 +15,15 @@ class zcObserverSearchBox extends base
 
     public function notify_build_keyword_search(&$class, $eventID, $unused, &$fields, &$string)
     {
-                if (!empty($_REQUEST['restrictIDs']) && $_REQUEST['restrictIDs'] === 'on') {
-                    $removeElements = [
-                        'pd.products_name',
-                        'p.products_model',
-                        'pd.products_description',
-                        'cd.categories_name',
-                        'cd.categories_description',
-                    ];
-                    $fields = array_diff($fields, $removeElements);
+        if (!empty($_REQUEST['restrictIDs']) && $_REQUEST['restrictIDs'] === 'on') {
+            $removeElements = [
+                'pd.products_name',
+                'p.products_model',
+                'pd.products_description',
+                'cd.categories_name',
+                'cd.categories_description',
+            ];
+            $fields = array_diff($fields, $removeElements);
         }
     }
 }
