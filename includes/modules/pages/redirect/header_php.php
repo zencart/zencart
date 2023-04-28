162f4e4a7e45c7a54de2a78a2f60c12181671c19
lat9 <lat9@vinosdefrutastropicales.com>
2023-04-28 11:31:40 -0400

    #5719: Correct default language handling ...
    ... and:
    - Don't redirect if the associated URL is an empty string.
    - Check for default-language only if the session's current language is other than the default.
    - Add notifications for banner- and manufacturer-redirects.
    - Pass default language's ID to default notification

--- includes/modules/pages/redirect/header_php.php
+++ includes/modules/pages/redirect/header_php.php
@@ -12,6 +12,19 @@ if (!defined('IS_ADMIN_FLAG')) {
 }
 
 $_GET['action'] = $_GET['action'] ?? '';
+
+// -----
+// Grab the currently-configured **default** language's id value.  Used
+// to determine if an additional query is needed when a language-specific URL
+// isn't found for the session's current language.
+//
+$configured_languages = new language();
+$default_language_id = (int)$configured_languages->language['id'];
+unset($configured_languages);
+
+// -----
+// Determine whick redirect is to be performed ...
+//
 switch ($_GET['action']) {
     case 'product':
         if (!empty($_GET['products_id'])) {
@@ -19,16 +32,16 @@ switch ($_GET['action']) {
             $sql = $db->bindVars($sql, ':productId:', $_GET['products_id'], 'integer');
             $sql = $db->bindVars($sql, ':languageId:', $_SESSION['languages_id'], 'integer');
             $result = $db->Execute($sql);
-            if (!$result->EOF) {
+            if (!$result->EOF && $result->fields['products_url'] !== '') {
                 $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_PRODUCT', [], $_GET['products_id'], $_SESSION['languages_id']);
                 zen_redirect(fixup_url($result->fields['products_url']));
-            } else {
+            } elseif ($default_language_id !== $_SESSION['languages_id']) {
                 $sql = 'SELECT products_url FROM ' . TABLE_PRODUCTS_DESCRIPTION . ' WHERE products_id = :productId: AND language_id = :languageId: LIMIT 1';
                 $sql = $db->bindVars($sql, ':productId:', $_GET['products_id'], 'integer');
-                $sql = $db->bindVars($sql, ':languageId:', DEFAULT_LANGUAGE, 'integer');
+               $sql = $db->bindVars($sql, ':languageId:', $default_language_id, 'integer');
                 $result = $db->Execute($sql);
-                if (!$result->EOF) {
-                    $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_PRODUCT', [], $_GET['products_id'], $_SESSION['languages_id']);
+                if (!$result->EOF && $result->fields['products_url'] !== '') {
+                    $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_PRODUCT', [], $_GET['products_id'], $default_language_id);
                     zen_redirect(fixup_url($result->fields['products_url']));
                 }
             }
@@ -41,18 +54,18 @@ switch ($_GET['action']) {
             $sql = $db->bindVars($sql, ':artistId:', $_GET['artists_id'], 'integer');
             $sql = $db->bindVars($sql, ':languageId:', $_SESSION['languages_id'], 'integer');
             $result = $db->Execute($sql);
-            if (!$result->EOF) {
+            if (!$result->EOF && $result->fields['artists_url'] !== '') {
                 $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_MUSIC_ARTIST', [], $_GET['artists_id'], $_SESSION['languages_id']);
                 zen_update_music_artist_clicked($_GET['artists_id'], $_SESSION['languages_id']);
                 zen_redirect(fixup_url($result->fields['artists_url']));
-            } else {
+            } elseif ($default_language_id !== $_SESSION['languages_id']) {
                 $sql = 'SELECT artists_url FROM ' . TABLE_RECORD_ARTISTS_INFO . ' WHERE artists_id = :artistId: AND languages_id = :languageId: LIMIT 1';
                 $sql = $db->bindVars($sql, ':artistId:', $_GET['artists_id'], 'integer');
-                $sql = $db->bindVars($sql, ':languageId:', DEFAULT_LANGUAGE, 'integer');
+                $sql = $db->bindVars($sql, ':languageId:', $default_language_id, 'integer');
                 $result = $db->Execute($sql);
-                if (!$result->EOF) {
-                    $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_MUSIC_ARTIST', [], $_GET['artists_id'], $_SESSION['languages_id']);
-                    zen_update_music_artist_clicked($_GET['artists_id'], DEFAULT_LANGUAGE);
+                if (!$result->EOF && $result->fields['artists_url'] !== '') {
+                    $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_MUSIC_ARTIST', [], $_GET['artists_id'], $default_language_id);
+                    zen_update_music_artist_clicked($_GET['artists_id'], $default_language_id);
                     zen_redirect(fixup_url($result->fields['artists_url']));
                 }
             }
@@ -65,18 +78,18 @@ switch ($_GET['action']) {
             $sql = $db->bindVars($sql, ':rcId:', $_GET['record_company_id'], 'integer');
             $sql = $db->bindVars($sql, ':languageId:', $_SESSION['languages_id'], 'integer');
             $result = $db->Execute($sql);
-            if (!$result->EOF) {
+            if (!$result->EOF && $result->fields['record_company_url'] !== '') {
                 $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_RECORD_COMPANY', [], $_GET['record_company_id'], $_SESSION['languages_id']);
                 zen_update_record_company_clicked($_GET['record_company_id'], $_SESSION['languages_id']);
                 zen_redirect(fixup_url($result->fields['record_company_url']));
-            } else {
+            } elseif ($default_language_id !== $_SESSION['languages_id']) {
                 $sql = 'SELECT record_company_url FROM ' . TABLE_RECORD_COMPANY_INFO . ' WHERE record_company_id = :rcId: AND languages_id = :languageId: LIMIT 1';
                 $sql = $db->bindVars($sql, ':rcId:', $_GET['record_company_id'], 'integer');
-                $sql = $db->bindVars($sql, ':languageId:', DEFAULT_LANGUAGE, 'integer');
+                $sql = $db->bindVars($sql, ':languageId:', $default_language_id, 'integer');
                 $result = $db->Execute($sql);
-                if (!$result->EOF) {
-                    $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_RECORD_COMPANY', [], $_GET['record_company_id'], $_SESSION['languages_id']);
-                    zen_update_record_company_clicked($_GET['record_company_id'], DEFAULT_LANGUAGE);
+                if (!$result->EOF && $result->fields['record_company_url'] !== '') {
+                    $zco_notifier->notify('NOTIFY_BEFORE_REDIRECT_ACTION_RECORD_COMPANY', [], $_GET['record_company_id'], $default_language_id);
+                    zen_update_record_company_clicked($_GET['record_company_id'], $default_language_id);
                     zen_redirect(fixup_url($result->fields['record_company_url']));
                 }
             }
@@ -84,30 +97,65 @@ switch ($_GET['action']) {
         break;
 
     case 'banner':
-        $banner_query = 'SELECT banners_url FROM ' . TABLE_BANNERS . ' WHERE banners_id = :bannersID LIMIT 1';
-        $banner_query = $db->bindVars($banner_query, ':bannersID', $_GET['goto'], 'integer');
-        $banner = $db->Execute($banner_query);
-        if (!$banner->EOF) {
-            zen_update_banner_click_count($_GET['goto']);
-            zen_redirect($banner->fields['banners_url']);
+        if (!empty($_GET['goto'])) {
+            $banner_query = 'SELECT banners_url FROM ' . TABLE_BANNERS . ' WHERE banners_id = :bannersID LIMIT 1';
+            $banner_query = $db->bindVars($banner_query, ':bannersID', $_GET['goto'], 'integer');
+            $banner = $db->Execute($banner_query);
+            if (!$banner->EOF && $banner->fields['banners_url'] !== '') {
+                $zco_notifier->notify(
+                    'NOTIFY_BEFORE_REDIRECT_ACTION_BANNER',
+                    [
+                        'banners_id' => (int)$_GET['goto'],
+                        'banners_url' => $banner->fields['banners_url'],
+                    ]
+                );
+                zen_update_banner_click_count($_GET['goto']);
+                zen_redirect($banner->fields['banners_url']);
+            }
         }
         break;
 
     case 'manufacturer':
         if (!empty($_GET['manufacturers_id'])) {
-            $sql =
-                'SELECT manufacturers_url
-                   FROM ' . TABLE_MANUFACTURERS_INFO . '
-                  WHERE manufacturers_id = :manufacturersID
-                    AND languages_id = :languagesID
-                  LIMIT 1';
+            $sql = 'SELECT manufacturers_url FROM ' . TABLE_MANUFACTURERS_INFO . ' WHERE manufacturers_id = :manufacturersID AND languages_id = :languagesID LIMIT 1';
             $sql = $db->bindVars($sql, ':manufacturersID', $_GET['manufacturers_id'], 'integer');
             $sql = $db->bindVars($sql, ':languagesID', $_SESSION['languages_id'], 'integer');
             $manufacturer = $db->Execute($sql);
+            if (!$manufacturer->EOF && $manufacturer->fields['manufacturers_url'] !== '') {
+                $zco_notifier->notify(
+                    'NOTIFY_BEFORE_REDIRECT_ACTION_MANUFACTURER',
+                    [
+                        'manufacturers_id' => $_GET['manufacturers_id'],
+                        'manufacturers_url' => $manufacturer->fields['manufacturers_url'],
+                        'language_id' => $_SESSION['languages_id'],
+                    ]
+                );
+                $sql =
+                    'UPDATE ' . TABLE_MANUFACTURERS_INFO . '
+                        SET url_clicked = url_clicked+1, date_last_click = now()
+                      WHERE manufacturers_id = :manufacturersID
+                        AND languages_id = :languagesID
+                      LIMIT 1';
 
-            if (!$manufacturer->EOF) {
-                // url exists in selected language
-                if (!empty($manufacturer->fields['manufacturers_url'])) {
+                $sql = $db->bindVars($sql, ':manufacturersID', $_GET['manufacturers_id'], 'integer');
+                $sql = $db->bindVars($sql, ':languagesID', $_SESSION['languages_id'], 'integer');
+                $db->Execute($sql);
+                zen_redirect(fixup_url($manufacturer->fields['manufacturers_url']));
+            } elseif ($default_language_id !== $_SESSION['languages_id']) {
+                // no url exists for the selected language, lets use the default language then
+                $sql = 'SELECT manufacturers_url FROM ' . TABLE_MANUFACTURERS_INFO . ' WHERE manufacturers_id = :manufacturersID AND languages_id = :languagesID LIMIT 1';
+                $sql = $db->bindVars($sql, ':manufacturersID', $_GET['manufacturers_id'], 'integer');
+                $sql = $db->bindVars($sql, ':languagesID', $default_language_id, 'integer');
+                $manufacturer = $db->Execute($sql);
+                if (!$manufacturer->EOF  && $manufacturer->fields['manufacturers_url'] !== '') {
+                    $zco_notifier->notify(
+                        'NOTIFY_BEFORE_REDIRECT_ACTION_MANUFACTURER',
+                        [
+                            'manufacturers_id' => $_GET['manufacturers_id'],
+                            'manufacturers_url' => $manufacturer->fields['manufacturers_url'],
+                            'language_id' => $default_language_id,
+                        ]
+                    );
                     $sql =
                         'UPDATE ' . TABLE_MANUFACTURERS_INFO . '
                             SET url_clicked = url_clicked+1, date_last_click = now()
@@ -116,43 +164,16 @@ switch ($_GET['action']) {
                           LIMIT 1';
 
                     $sql = $db->bindVars($sql, ':manufacturersID', $_GET['manufacturers_id'], 'integer');
-                    $sql = $db->bindVars($sql, ':languagesID', $_SESSION['languages_id'], 'integer');
+                    $sql = $db->bindVars($sql, ':languagesID', $default_language_id, 'integer');
                     $db->Execute($sql);
                     zen_redirect(fixup_url($manufacturer->fields['manufacturers_url']));
-                }
-            } else {
-                // no url exists for the selected language, lets use the default language then
-                $sql =
-                    'SELECT mi.languages_id, mi.manufacturers_url
-                       FROM ' . TABLE_MANUFACTURERS_INFO . ' mi, ' . TABLE_LANGUAGES . " l
-                      WHERE mi.manufacturers_id = :manufacturersID
-                        AND mi.languages_id = l.languages_id
-                        AND l.code = '" . DEFAULT_LANGUAGE . "'
-                      LIMIT 1";
-
-                $sql = $db->bindVars($sql, ':manufacturersID', $_GET['manufacturers_id'], 'integer');
-                $manufacturer = $db->Execute($sql);
-
-                if (!$manufacturer->EOF) {
-                    if (!empty($manufacturer->fields['manufacturers_url'])) {
-                        $sql =
-                            'UPDATE ' . TABLE_MANUFACTURERS_INFO . '
-                                SET url_clicked = url_clicked+1, date_last_click = now()
-                              WHERE manufacturers_id = :manufacturersID
-                                AND languages_id = :languagesID
-                              LIMIT 1';
-
-                        $sql = $db->bindVars($sql, ':manufacturersID', $_GET['manufacturers_id'], 'integer');
-                        $sql = $db->bindVars($sql, ':languagesID', $_SESSION['languages_id'], 'integer');
-                        $db->Execute($sql);
-                        zen_redirect(fixup_url($manufacturer->fields['manufacturers_url']));
-                    }
                 }
             }
         }
         break;
+
     default:
-        $zco_notifier->notify('NOTIFY_REDIRECT_DEFAULT_ACTION');
+        $zco_notifier->notify('NOTIFY_REDIRECT_DEFAULT_ACTION', $default_language_id);
         break;
 }
 zen_redirect(zen_href_link(FILENAME_DEFAULT));
