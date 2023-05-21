aa9ec9379e60f712fd4846bc039a478b56da8a93
simon1066 <simon@1066traffic.com>
2023-05-21 10:19:39 +0100

    Remove duplicated line Fixes #5732 Line 26 in \includes\modules\pages\reviews\header_php.php duplicated in line 27

--- includes/modules/pages/reviews/header_php.php
+++ includes/modules/pages/reviews/header_php.php
@@ -24,7 +24,6 @@ $reviews_query_raw = "SELECT r.reviews_id, rd.reviews_text, r.reviews_rating, r.
                       ORDER BY r.reviews_id DESC";
 
 $reviews_query_raw = $db->bindVars($reviews_query_raw, ':languageID', $_SESSION['languages_id'], 'integer');
-$reviews_query_raw = $db->bindVars($reviews_query_raw, ':languageID', $_SESSION['languages_id'], 'integer');
 $reviews_split = new splitPageResults($reviews_query_raw, MAX_DISPLAY_NEW_REVIEWS);
 
 $breadcrumb->add(NAVBAR_TITLE);
