c3870955c5e0b4b9f1145aa7d567207315bf89a1
brittainmark <gitbrit@inner-light.co.uk>
2023-06-12 19:20:09 +0100

    Add Call For Price To Ask A Question
    This is a solution for call for price. A better solution may be to create a separate document for call for price. If that is the considered opinion Am happy to do that instead.
    
    Update define_ask_a_question.php
    
    format

--- includes/languages/english/html_includes/define_ask_a_question.php
+++ includes/languages/english/html_includes/define_ask_a_question.php
@@ -1,1 +1,12 @@
+<?php 
+if ($call_for_price === false) {
+    //Put Ask Questuion HTML here
+?>
 <p>Got a product question?  We're happy to help!</p>
+<?php
+} else {
+    //put Call For Price HTML here
+?>
+<p>Call for price -  We will get back to you!</p>
+<?php
+}
