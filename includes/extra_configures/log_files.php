c9baee8b22de36fbab4e42af2a4e6d8d5a04a293
Scott C Wilson <scottcwilson@gmail.com>
2023-06-17 21:03:38 -0400

    Remove UPS and Fedex logs in Store Manager purge (#5751)

---
+++ includes/extra_configures/log_files.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Scott C Wilson 
+ */
+
+/**
+ * @param array $log_filename_prefix_patterns Used by /admin/store_manager.php for purging debug logs
+ *
+ * Future use: could also be used by DisplayLogs plugin
+ */
+$log_filename_prefix_patterns = [
+     'myDEBUG-',
+     'upsoauth-',
+     'fedexrest-',
+     'Bambora_Debug_',
+     'Square_',
+     'SquareWebPay_',
+     'AIM_Debug_',
+     'SIM_Debug_',
+     'FirstData_Debug_',
+     'Linkpoint_Debug_',
+     'Paypal',
+     'paypal',
+     'ipn_',
+     'zcInstall',
+     'SHIP_',
+     'PAYMENT_',
+     'usps_',
+     '.*debug',
+];
