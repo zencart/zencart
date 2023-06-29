a4fb06d017b660664e50b0484bf61f5a2880c851
Steve <4391026+torvista@users.noreply.github.com>
2023-06-29 19:15:31 +0200

    replace hard-coded warning missing shipping info

--- includes/languages/english/lang.account_history_info.php
+++ includes/languages/english/lang.account_history_info.php
@@ -5,6 +5,7 @@ $define = [
     'NAVBAR_TITLE_2' => 'History',
     'NAVBAR_TITLE_3' => 'Order #%s',
     'HEADING_TITLE' => 'Order Information',
+    'TEXT_MISSING_SHIPPING_INFO' => 'WARNING: missing shipping details',
 ];
 
 return $define;
