eac99e2bcf999a4559e653117a6fed1167207524
Steve <4391026+torvista@users.noreply.github.com>
2023-07-02 21:04:30 +0200

    correct typo (#5840)

--- includes/languages/english/html_includes/define_ask_a_question.php
+++ includes/languages/english/html_includes/define_ask_a_question.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 if ($call_for_price === false) {
-    //Put Ask Questuion HTML here
+    //put Ask A Question HTML below
 ?>
 <p>Got a product question?  We're happy to help!</p>
 <?php
 } else {
-    //put Call For Price HTML here
+    //put Call For Price HTML below
 ?>
 <p>Call for price -  We will get back to you!</p>
 <?php
