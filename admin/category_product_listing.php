c668a0beb8c7b8da95af58296cbcc2db061360d7
Steve <4391026+torvista@users.noreply.github.com>
2023-07-28 05:10:59 +0200

    correct category sort on first subcategory (#5899)

--- admin/category_product_listing.php
+++ admin/category_product_listing.php
@@ -33,7 +33,7 @@ if (isset($_GET['page'])) {
 if (isset($_GET['product_type'])) {
   $_GET['product_type'] = (int)$_GET['product_type'];
 }
-if (isset($_GET['cID'])) {
+if (!empty($_GET['cID'])) {
   $_GET['cID'] = (int)$_GET['cID'];
   if (!$sniffer->rowExists(TABLE_CATEGORIES, 'categories_id', $_GET['cID'])) {
     zen_redirect(zen_href_link(FILENAME_CATEGORY_PRODUCT_LISTING));
