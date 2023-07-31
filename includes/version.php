7c16bd30d5d589ce3aab2c7cf34ddcd342891a84
Chris Brown <drbyte@zen-cart.com>
2023-07-31 15:47:57 -0400

    DB version-stamping (alpha)

--- includes/version.php
+++ includes/version.php
@@ -15,7 +15,7 @@
 
 define('PROJECT_VERSION_NAME', 'Zen Cart');
 define('PROJECT_VERSION_MAJOR', '2');
-define('PROJECT_VERSION_MINOR', '0.0');
+define('PROJECT_VERSION_MINOR', '0.0-alpha');
 define('PROJECT_VERSION_PATCH1', '');
 define('PROJECT_VERSION_PATCH2', '');
 define('PROJECT_VERSION_PATCH1_SOURCE', '');
@@ -24,5 +24,5 @@ define('NEW_VERSION_CHECKUP_URL','https://ping.zen-cart.com/version_id.txt');
 define('PROJECT_VERSIONSERVER_URL', 'https://ping.zen-cart.com/zcversioncheck');
 define('PLUGIN_VERSIONSERVER_URL', 'https://ping.zen-cart.com/plugincheck');
 define('PROJECT_NOTIFICATIONSERVER_URL', 'https://ping.zen-cart.com/api/notifications');
-define('EXPECTED_DATABASE_VERSION_MAJOR', '1');
+define('EXPECTED_DATABASE_VERSION_MAJOR', '2');
 define('EXPECTED_DATABASE_VERSION_MINOR', '0.0');
