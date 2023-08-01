7d38d7dae64cdd9e1ec7142f8d81286220f2a91c
Chris Brown <drbyte@zen-cart.com>
2023-08-01 19:11:06 -0400

    Update polyfills for PHP 8.3 functions ... and some older as well

--- includes/functions/php_polyfills.php
+++ includes/functions/php_polyfills.php
@@ -3,17 +3,88 @@
  * polyfills to accommodate older PHP versions, adapted from https://github.com/symfony/polyfill/
  * @copyright Portions  (c) Fabien Potencier <fabien@symfony.com>
  *
- * @version $Id: DrByte 2021 Jan 11 New in v1.5.8-alpha $
+ * @version $Id: DrByte 2023 Aug 01  Modified in v2.0.0 $
  */
 
+if (\PHP_VERSION_ID >= 80300) {
+    return;
+}
+if (!function_exists('json_validate')) {
+    if (!defined('JSON_MAX_DEPTH')) define('JSON_MAX_DEPTH', 0x7FFFFFFF); // see https://www.php.net/manual/en/function.json-decode.php
 
-if (PHP_VERSION_ID >= 80100) {
+    function json_validate(string $json, int $depth = 512, int $flags = 0): bool
+    {
+        if (0 !== $flags && \defined('JSON_INVALID_UTF8_IGNORE') && \JSON_INVALID_UTF8_IGNORE !== $flags) {
+            throw new \ValueError('json_validate(): Argument #3 ($flags) must be a valid flag (allowed flags: JSON_INVALID_UTF8_IGNORE)');
+        }
+
+        if ($depth <= 0) {
+            throw new \ValueError('json_validate(): Argument #2 ($depth) must be greater than 0');
+        }
+
+        if ($depth > JSON_MAX_DEPTH) {
+            throw new \ValueError(sprintf('json_validate(): Argument #2 ($depth) must be less than %d', JSON_MAX_DEPTH));
+        }
+
+        json_decode($json, null, $depth, $flags);
+
+        return \JSON_ERROR_NONE === json_last_error();
+    }
+}
+
+if (!function_exists('mb_str_pad') && function_exists('mb_substr')) {
+    function mb_str_pad(string $string, int $length, string $pad_string = ' ', int $pad_type = \STR_PAD_RIGHT, string $encoding = null): string
+    {
+        if (!\in_array($pad_type, [\STR_PAD_RIGHT, \STR_PAD_LEFT, \STR_PAD_BOTH], true)) {
+            throw new \ValueError('mb_str_pad(): Argument #4 ($pad_type) must be STR_PAD_LEFT, STR_PAD_RIGHT, or STR_PAD_BOTH');
+        }
+
+        if (null === $encoding) {
+            $encoding = mb_internal_encoding();
+        }
+
+        try {
+            $validEncoding = @mb_check_encoding('', $encoding);
+        } catch (\ValueError $e) {
+            throw new \ValueError(sprintf('mb_str_pad(): Argument #5 ($encoding) must be a valid encoding, "%s" given', $encoding));
+        }
+
+        // BC for PHP 7.3 and lower
+        if (!$validEncoding) {
+            throw new \ValueError(sprintf('mb_str_pad(): Argument #5 ($encoding) must be a valid encoding, "%s" given', $encoding));
+        }
+
+        if (mb_strlen($pad_string, $encoding) <= 0) {
+            throw new \ValueError('mb_str_pad(): Argument #3 ($pad_string) must be a non-empty string');
+        }
+
+        $paddingRequired = $length - mb_strlen($string, $encoding);
+
+        if ($paddingRequired < 1) {
+            return $string;
+        }
+
+        switch ($pad_type) {
+            case \STR_PAD_LEFT:
+                return mb_substr(str_repeat($pad_string, $paddingRequired), 0, $paddingRequired, $encoding).$string;
+            case \STR_PAD_RIGHT:
+                return $string.mb_substr(str_repeat($pad_string, $paddingRequired), 0, $paddingRequired, $encoding);
+            default:
+                $leftPaddingLength = floor($paddingRequired / 2);
+                $rightPaddingLength = $paddingRequired - $leftPaddingLength;
+
+                return mb_substr(str_repeat($pad_string, $leftPaddingLength), 0, $leftPaddingLength, $encoding).$string.mb_substr(str_repeat($pad_string, $rightPaddingLength), 0, $rightPaddingLength, $encoding);
+        }
+    }
+}
+
+if (\PHP_VERSION_ID >= 80100) {
     return;
 }
 if (!function_exists('array_is_list')) {
     function array_is_list(array $array): bool
     {
-        if ([] === $array) {
+        if ([] === $array || $array === array_values($array)) {
             return true;
         }
 
@@ -28,10 +99,12 @@ if (!function_exists('array_is_list')) {
         return true;
     }
 }
+if (!function_exists('enum_exists')) {
+    function enum_exists(string $enum, bool $autoload = true): bool { return $autoload && class_exists($enum) && false; }
+}
 
 
-
-if (PHP_VERSION_ID >= 80000) {
+if (\PHP_VERSION_ID >= 80000) {
     return;
 }
 if (!defined('FILTER_VALIDATE_BOOL') && defined('FILTER_VALIDATE_BOOLEAN')) {
@@ -130,7 +203,7 @@ if (!function_exists('get_resource_id')) {
 
 
 
-if (PHP_VERSION_ID >= 70300) {
+if (\PHP_VERSION_ID >= 70300) {
     return;
 }
 if (!function_exists('is_countable')) {
