8be9c1af1e91fb0e07a4cc7feda547239bba5e97
Chris Brown <drbyte@zen-cart.com>
2023-08-01 00:13:53 -0400

    file header

--- zc_install/sql/updates/mysql_upgrade_zencart_200.sql
+++ zc_install/sql/updates/mysql_upgrade_zencart_200.sql
@@ -5,7 +5,7 @@
 # * @copyright Copyright 2003-2023 Zen Cart Development Team
 # * @copyright Portions Copyright 2003 osCommerce
 # * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
-# * @version $Id: Scott C Wilson 2023 Mar 18 Modified in v1.5.8a $
+# * @version $Id: Modified in v2.0.0 $
 #
 
 ############ IMPORTANT INSTRUCTIONS ###############
