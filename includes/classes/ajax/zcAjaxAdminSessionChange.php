9ae0b145fff3d89bbeafdae83b1f9c00dbdf6788
lat9 <cindy@vinosdefrutastropicales.com>
2023-11-01 11:28:11 -0400

    Admin session-changer AJAX, limit to 'known' variable names (#6032)
    * Admin session-changer AJAX, limit to 'known' variable names
    
    * Enable the session-change AJAX to accept additional supported names
    
    ... and don't process unless running under the admin.

--- includes/classes/ajax/zcAjaxAdminSessionChange.php
+++ includes/classes/ajax/zcAjaxAdminSessionChange.php
@@ -1,9 +1,38 @@
 <?php
-
 class zcAjaxAdminSessionChange extends base
 {
+    protected $supportedNames = [
+        'imageView',
+    ];
+
     public function change()
     {
+        // -----
+        // Deny access unless running under the admin.
+        //
+        if (!defined('IS_ADMIN_FLAG') || IS_ADMIN_FLAG !== true) {
+            return '';
+        }
+
+        // -----
+        // Give an observer the opportunity to add other supported names.  Each
+        // name can contain *only* alphanumeric characters.
+        //
+        $other_names = [];
+        $this->notify('NOTIFY_AJAX_ADMIN_NOTIFICATIONS', '', $other_names);
+        foreach ($other_names as $name) {
+            if (ctype_alnum((string)$name) === true) {
+                $this->supportedNames[] = (string)$name;
+            }
+        }
+
+        // -----
+        // No action if the 'name' isn't recognized.
+        //
+        if (!in_array($_POST['name'], $this->supportedNames)) {
+            return '';
+        }
+
         if (!isset($_SESSION[$_POST['name']])) {
             $_SESSION[$_POST['name']] = true;
 
@@ -15,5 +44,4 @@ class zcAjaxAdminSessionChange extends base
         $_SESSION[$_POST['name']] = true;
         return $_POST['name'] . ' set to true!';
     }
-
 }
