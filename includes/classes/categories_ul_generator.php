743ccbc7abd9622eb7d00b96dc50f5fb8faea023
Chris Brown <drbyte@zen-cart.com>
2023-12-09 15:11:07 -0500

    Update header

--- includes/classes/categories_ul_generator.php
+++ includes/classes/categories_ul_generator.php
@@ -1,25 +1,10 @@
 <?php
-// +----------------------------------------------------------------------+
-// |zen-cart Open Source E-commerce                                       |
-// +----------------------------------------------------------------------+
-// | Copyright(c) 2003 The zen-cart developers                            |
-// |                                                                      |
-// | http://www.zen-cart.com/index.php                                    |
-// |                                                                      |
-// | Portions Copyright(c) 2003 osCommerce                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the GPL license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.zen-cart.com/license/2_0.txt.                             |
-// | If you did not receive a copy of the zen-cart license and are unable |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@zen-cart.com so we can mail you a copy immediately.          |
-// +----------------------------------------------------------------------+
-// $Id: categories_ul_generator.php 2004-07-11  DrByteZen $
-//      based on site_map.php v1.0.1 by networkdad 2004-06-04
-//  Modified by Anne, www.picaflor-azul.com
-//
+/**
+ * @copyright Copyright 2003-2023 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id:   $
+ */
 
 class zen_categories_ul_generator {
     protected $root_category_id = TOPMOST_CATEGORY_PARENT_ID,
