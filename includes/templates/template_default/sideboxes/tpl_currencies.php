f8ceb92beed21b50d6fa0de53bc8eb7fac509fcf
OldNGreyBMR <73047349+OldNGreyBMR@users.noreply.github.com>
2024-01-30 12:41:24 +1000

    add label (accessibility) to currencies sidebox
    fixes #6016

--- includes/templates/template_default/sideboxes/tpl_currencies.php
+++ includes/templates/template_default/sideboxes/tpl_currencies.php
@@ -9,8 +9,10 @@
  */
 $content = '';
 $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent centeredContent">';
+$content .= '<label for="Currency">';
 $content .= zen_draw_form('currencies_form', zen_href_link(basename(preg_replace('/.php/','', $PHP_SELF)), '', $request_type, false), 'get');
 $content .= zen_draw_pull_down_menu('currency', $currencies_array, $_SESSION['currency']) . $hidden_get_variables . zen_hide_session_id();
 $content .= zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_GO_ALT);
 $content .= '</form>';
+$content .= '</label>';
 $content .= '</div>';
