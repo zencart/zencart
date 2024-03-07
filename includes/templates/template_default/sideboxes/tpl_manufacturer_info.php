e4a68fda19107a1a5c182eac1b234722f05e99ff
Scott Wilson <scottcwilson@hey.com>
2024-03-07 14:40:14 -0500

    Manufacturer images are not scaled in sidebox.

--- includes/templates/template_default/sideboxes/tpl_manufacturer_info.php
+++ includes/templates/template_default/sideboxes/tpl_manufacturer_info.php
@@ -10,7 +10,7 @@
   $content = "";
   $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">';
       if (!empty($manufacturer_info_sidebox->fields['manufacturers_image']))
-  $content .= '<div class="centeredContent">' . zen_image(DIR_WS_IMAGES . $manufacturer_info_sidebox->fields['manufacturers_image'], $manufacturer_info_sidebox->fields['manufacturers_name']) . '</div>';
+  $content .= '<div class="centeredContent">' . zen_image(DIR_WS_IMAGES . $manufacturer_info_sidebox->fields['manufacturers_image'], $manufacturer_info_sidebox->fields['manufacturers_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</div>';
   $content .= '<ul style="margin: 0; padding: 0; list-style-type: none;">' . "\n" ;
       if (!empty($manufacturer_info_sidebox->fields['manufacturers_url']))
   $content .= '<li><a href="' . zen_href_link(FILENAME_REDIRECT, 'action=manufacturer&manufacturers_id=' . $manufacturer_info_sidebox->fields['manufacturers_id']) . '" rel="noopener" target="_blank">' . sprintf(BOX_MANUFACTURER_INFO_HOMEPAGE, $manufacturer_info_sidebox->fields['manufacturers_name']) . '</a></li>' . "\n" ;
