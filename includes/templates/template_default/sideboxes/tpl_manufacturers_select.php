ef38ab6df70be480308926ba6cde572f4af78dfa
Scott Wilson <scottcwilson@hey.com>
2024-03-09 08:27:33 -0500

    add aria-label to select blocks

--- includes/templates/template_default/sideboxes/tpl_manufacturers_select.php
+++ includes/templates/template_default/sideboxes/tpl_manufacturers_select.php
@@ -12,7 +12,7 @@ $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class=
 $content .= zen_draw_form('manufacturers_form', zen_href_link(FILENAME_DEFAULT, '', $request_type, false), 'get', 'class="sidebox-select-form"');
 $content .= zen_draw_hidden_field('main_page', FILENAME_DEFAULT);
 $content .= zen_draw_label(PLEASE_SELECT, 'select-manufacturers_id', 'class="sr-only"');
-$content .= zen_draw_pull_down_menu('manufacturers_id', $manufacturer_sidebox_array, $default_selection, 'size="' . MAX_MANUFACTURERS_LIST . '"' . $required) . zen_hide_session_id();
+$content .= zen_draw_pull_down_menu('manufacturers_id', $manufacturer_sidebox_array, $default_selection, 'aria-label="' . BOX_HEADING_MANUFACTURERS  . '" size="' . MAX_MANUFACTURERS_LIST . '"' . $required) . zen_hide_session_id();
 $content .= zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_GO_ALT);
 $content .= '</form>';
 $content .= '</div>';
