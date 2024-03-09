a95df164ce879d87f0661245620920859893b4e9
Scott Wilson <scottcwilson@hey.com>
2024-03-09 06:43:39 -0500

    Version stamping 2.0.0-rc1

--- includes/init_includes/init_common_elements.php
+++ includes/init_includes/init_common_elements.php
@@ -3,7 +3,7 @@
  * Set some common processing flags, overridable via site-specific /extra_datafiles processing.  See
  * /includes/extra_datafiles/dist-site_specific_overrides.php.
  *
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  */
--- includes/modules/pages/accessibility/header_php.php
+++ includes/modules/pages/accessibility/header_php.php
@@ -2,10 +2,10 @@
 /**
  * Accessibility Page
  * 
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2020 Jul 10 Modified in v1.5.8-alpha $
+ * @version $Id: Scott C Wilson 2024 Mar 01 New in v2.0.0-rc1 $
  */
 require(DIR_WS_MODULES . zen_get_module_directory('require_languages.php'));
 
