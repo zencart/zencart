b0cc448669f017469f83f76f87a429f8172362b4
Ian Wilson <ian@zen-cart.com>
2024-03-23 09:33:54 +0000

    missing config file (#6351)

---
+++ not_for_release/testFramework/Support/configs/runner.main.configure.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'use-mailserver' => false,
+    'mailserver-port' => 1025,
+    'mailserver-host' => 'localhost',
+    'mailserver-user' => 'ddev',
+    'mailserver-password' => 'mailpit',
+];
