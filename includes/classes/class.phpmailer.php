527106690e4beade2a2aae0a7a7c0b809868a07c
Chris Brown <drbyte@zen-cart.com>
2024-04-10 13:25:36 -0400

    Add file header

--- includes/classes/class.phpmailer.php
+++ includes/classes/class.phpmailer.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id:  $
+ */
 
 require 'vendors/PHPMailer/src/PHPMailer.php';
 require 'vendors/PHPMailer/src/Exception.php';
