7cd4d6d8be810a1f72e1cdaf0e552b0753b3fb29
lat9 <cindy@vinosdefrutastropicales.com>
2024-05-14 18:57:36 -0400

    Notifier to insert "More Information" and "Important Links" sidebox links (#6451)
    * Notifier to insert "More Information" sidebox links
    
    Similar to PR #6443, but for the "More Information" sidebox.  Includes minor reformatting/refactoring.
    
    * Might as well include the ezpages sidebox, too

--- includes/modules/sideboxes/more_information.php
+++ includes/modules/sideboxes/more_information.php
@@ -9,29 +9,34 @@
  */
 
 // initialize
-$more_information = array();
+$more_information = [];
 
 // test if links should display
-  if (DEFINE_PAGE_2_STATUS <= 1) {
+if (DEFINE_PAGE_2_STATUS <= 1) {
     $more_information[] = '<a href="' . zen_href_link(FILENAME_PAGE_2) . '">' . BOX_INFORMATION_PAGE_2 . '</a>';
-  }
-  if (DEFINE_PAGE_3_STATUS <= 1) {
+}
+if (DEFINE_PAGE_3_STATUS <= 1) {
     $more_information[] = '<a href="' . zen_href_link(FILENAME_PAGE_3) . '">' . BOX_INFORMATION_PAGE_3 . '</a>';
-  }
-  if (DEFINE_PAGE_4_STATUS <= 1) {
+}
+if (DEFINE_PAGE_4_STATUS <= 1) {
     $more_information[] = '<a href="' . zen_href_link(FILENAME_PAGE_4) . '">' . BOX_INFORMATION_PAGE_4 . '</a>';
-  }
+}
 
 // insert additional links below to add to the more_information box
 // Example:
 //    $more_information[] = '<a href="' . zen_href_link(FILENAME_DEFAULT) . '">' . 'TESTING' . '</a>';
 
+// -----
+// ... or create an observer-class file that monitors the following notification.
+//
+$zco_notifier->notify('NOTIFY_MORE_INFORMATION_SIDEBOX_ADDITIONS', [], $more_information);
+
 
 // only show if links are active
-  if (sizeof($more_information) > 0) {
-    require($template->get_template_dir('tpl_more_information.php',DIR_WS_TEMPLATE, $current_page_base,'sideboxes'). '/tpl_more_information.php');
+if (count($more_information) > 0) {
+    require $template->get_template_dir('tpl_more_information.php', DIR_WS_TEMPLATE, $current_page_base, 'sideboxes') . '/tpl_more_information.php';
 
     $title =  BOX_HEADING_MORE_INFORMATION;
     $title_link = false;
-    require($template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base,'common') . '/' . $column_box_default);
-  }
+    require $template->get_template_dir($column_box_default, DIR_WS_TEMPLATE, $current_page_base, 'common') . '/' . $column_box_default;
+}
