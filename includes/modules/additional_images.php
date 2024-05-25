68e630d134c92d60b291dad20cefeabe7e465bcb
Chris Brown <drbyte@zen-cart.com>
2024-05-25 19:21:18 -0400

    Reformat file before changes

--- includes/modules/additional_images.php
+++ includes/modules/additional_images.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * additional_images module
  *
@@ -14,27 +15,29 @@ if (!defined('IS_ADMIN_FLAG')) {
 }
 $GLOBALS['zco_notifier']->notify('NOTIFY_MODULES_ADDITIONAL_PRODUCT_IMAGES_START');
 
-if (!defined('IMAGE_ADDITIONAL_DISPLAY_LINK_EVEN_WHEN_NO_LARGE')) define('IMAGE_ADDITIONAL_DISPLAY_LINK_EVEN_WHEN_NO_LARGE','Yes');
+if (!defined('IMAGE_ADDITIONAL_DISPLAY_LINK_EVEN_WHEN_NO_LARGE')) {
+    define('IMAGE_ADDITIONAL_DISPLAY_LINK_EVEN_WHEN_NO_LARGE', 'Yes');
+}
 
 $modal_images = [];
 $images_array = [];
 
 // do not check for additional images when turned off
-if ($products_image != '' && $flag_show_product_info_additional_images != 0) {
+if ($products_image !== '' && !empty($flag_show_product_info_additional_images)) {
     // prepare image name
     $products_image_extension = substr($products_image, strrpos($products_image, '.'));
     $products_image_base = str_replace($products_image_extension, '', $products_image);
 
     // if in a subdirectory
     if (strrpos($products_image, '/')) {
-        $products_image_match = substr($products_image, strrpos($products_image, '/')+1);
+        $products_image_match = substr($products_image, strrpos($products_image, '/') + 1);
         //echo 'TEST 1: I match ' . $products_image_match . ' - ' . $file . ' -  base ' . $products_image_base . '<br>';
         $products_image_match = str_replace($products_image_extension, '', $products_image_match) . '_';
         $products_image_base = $products_image_match;
     }
 
     $products_image_directory = str_replace($products_image, '', substr($products_image, strrpos($products_image, '/')));
-    if ($products_image_directory != '') {
+    if ($products_image_directory !== '') {
         $products_image_directory = DIR_WS_IMAGES . str_replace($products_image_directory, '', $products_image) . "/";
     } else {
         $products_image_directory = DIR_WS_IMAGES;
@@ -42,7 +45,7 @@ if ($products_image != '' && $flag_show_product_info_additional_images != 0) {
 
     // Check for additional matching images
     $file_extension = $products_image_extension;
-    $products_image_match_array = array();
+    $products_image_match_array = [];
     if ($dir = @dir($products_image_directory)) {
         while ($file = $dir->read()) {
             if (!is_dir($products_image_directory . $file)) {
@@ -56,18 +59,18 @@ if ($products_image != '' && $flag_show_product_info_additional_images != 0) {
                 $current_image_match = false;
                 $GLOBALS['zco_notifier']->notify(
                     'NOTIFY_MODULES_ADDITIONAL_IMAGES_FILE_MATCH',
-                    array(
+                    [
                         'file' => $file,
                         'file_extension' => $file_extension,
                         'products_image' => $products_image,
-                        'products_image_base' => $products_image_base
-                    ),
+                        'products_image_base' => $products_image_base,
+                    ],
                     $current_image_match
                 );
-                if ($current_image_match || substr($file, strrpos($file, '.')) == $file_extension) {
-                    if ($current_image_match || preg_match('/' . preg_quote($products_image_base, '/') . '/i', $file) == 1) {
-                        if ($current_image_match || $file != $products_image) {
-                            if ($products_image_base . str_replace($products_image_base, '', $file) == $file) {
+                if ($current_image_match || substr($file, strrpos($file, '.')) === $file_extension) {
+                    if ($current_image_match || preg_match('/' . preg_quote($products_image_base, '/') . '/i', $file) === 1) {
+                        if ($current_image_match || $file !== $products_image) {
+                            if ($products_image_base . str_replace($products_image_base, '', $file) === $file) {
                                 //  echo 'I AM A MATCH ' . $file . '<br>';
                                 $images_array[] = $file;
                             } else {
@@ -78,19 +81,19 @@ if ($products_image != '' && $flag_show_product_info_additional_images != 0) {
                 }
             }
         }
-        if (count($images_array) > 0) {
+        if (count($images_array)) {
             sort($images_array);
         }
         $dir->close();
     }
 }
 
-$GLOBALS['zco_notifier']->notify('NOTIFY_MODULES_ADDITIONAL_PRODUCT_IMAGES_LIST', NULL, $images_array);
+$GLOBALS['zco_notifier']->notify('NOTIFY_MODULES_ADDITIONAL_PRODUCT_IMAGES_LIST', null, $images_array);
 
 
 // Build output based on images found
 $num_images = count($images_array);
-$list_box_contents = array();
+$list_box_contents = [];
 $title = '';
 
 $max_image_grid_columns = (int)IMAGES_AUTO_ADDED; // "Number of additional-images per row"
@@ -98,13 +101,13 @@ $max_image_grid_columns = (int)IMAGES_AUTO_ADDED; // "Number of additional-image
 if ($num_images > 0) {
     $row = 0;
     $col = 0;
-    if ($num_images < $max_image_grid_columns || $max_image_grid_columns === 0 ) {
-        $col_width = floor(100/$num_images);
+    if ($num_images < $max_image_grid_columns || $max_image_grid_columns === 0) {
+        $col_width = floor(100 / $num_images);
     } else {
         $col_width = floor(100 / $max_image_grid_columns);
     }
 
-    for ($i=0, $n=$num_images; $i<$n; $i++) {
+    for ($i = 0, $n = $num_images; $i < $n; $i++) {
         $file = $images_array[$i];
         $products_image_large = str_replace(DIR_WS_IMAGES, DIR_WS_IMAGES . 'large/', $products_image_directory) . str_replace($products_image_extension, '', $file) . IMAGE_SUFFIX_LARGE . $products_image_extension;
 
@@ -118,7 +121,7 @@ if ($num_images > 0) {
 
         $flag_has_large = file_exists($products_image_large);
         $products_image_large = ($flag_has_large ? $products_image_large : $products_image_directory . $file);
-        $flag_display_large = (IMAGE_ADDITIONAL_DISPLAY_LINK_EVEN_WHEN_NO_LARGE == 'Yes' || $flag_has_large);
+        $flag_display_large = (IMAGE_ADDITIONAL_DISPLAY_LINK_EVEN_WHEN_NO_LARGE === 'Yes' || $flag_has_large);
         $base_image = $products_image_directory . $file;
         $thumb_slashes = zen_image(addslashes($base_image), addslashes($products_name), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);
 
@@ -129,7 +132,7 @@ if ($num_images > 0) {
         // $p1 ... (n/a) ... An empty array, not applicable.
         // $p2 ... (r/w) ... A reference to the "slashed" thumbnail image name.
         //
-        $GLOBALS['zco_notifier']->notify('NOTIFY_MODULES_ADDITIONAL_IMAGES_THUMB_SLASHES', array(), $thumb_slashes);
+        $GLOBALS['zco_notifier']->notify('NOTIFY_MODULES_ADDITIONAL_IMAGES_THUMB_SLASHES', [], $thumb_slashes);
 
         $thumb_regular = zen_image($base_image, $products_name, SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);
         $large_link = zen_href_link(FILENAME_POPUP_IMAGE_ADDITIONAL, 'pID=' . $_GET['products_id'] . '&pic=' . $i . '&products_image_large_additional=' . $products_image_large);
@@ -149,33 +152,40 @@ if ($num_images > 0) {
         $link_parameters = 'class="additionalImages centeredContent back"' . ' ' . 'style="width:' . $col_width . '%;"';
         $GLOBALS['zco_notifier']->notify(
             'NOTIFY_MODULES_ADDITIONAL_IMAGES_SCRIPT_LINK',
-            array(
+            [
                 'flag_display_large' => $flag_display_large,
                 'products_name' => $products_name,
                 'products_image_large' => $products_image_large,
                 'thumb_slashes' => $thumb_slashes,
                 'large_link' => $large_link,
-                'index' => $i
-            ),
+                'index' => $i,
+            ],
             $script_link,
             $link_parameters
         );
         if ($script_link === false) {
-            $script_link = '<script>' . "\n" . 'document.write(\'' . ($flag_display_large ? '<a href="javascript:popupWindow(\\\'' . str_replace($products_image_large, urlencode(addslashes($products_image_large)), $large_link) . '\\\')">' . $thumb_slashes . '<br>' . TEXT_CLICK_TO_ENLARGE . '</a>' : $thumb_slashes) . '\');' . "\n" . '</script>';
+            $script_link = '<script>' . "\n" . 'document.write(\'' .
+                ($flag_display_large
+                    ? '<a href="javascript:popupWindow(\\\'' . str_replace($products_image_large, urlencode(addslashes($products_image_large)), $large_link) . '\\\')">' . $thumb_slashes . '<br>' . TEXT_CLICK_TO_ENLARGE . '</a>'
+                    : $thumb_slashes)
+                . '\');' . "\n" . '</script>';
         }
 
-        $noscript_link = '<noscript>' . ($flag_display_large ? '<a href="' . zen_href_link(FILENAME_POPUP_IMAGE_ADDITIONAL, 'pID=' . $_GET['products_id'] . '&pic=' . $i . '&products_image_large_additional=' . $products_image_large) . '" target="_blank">' . $thumb_regular . '<br><span class="imgLinkAdditional">' . TEXT_CLICK_TO_ENLARGE . '</span></a>' : $thumb_regular ) . '</noscript>';
+        $noscript_link = '<noscript>' . ($flag_display_large
+                ? '<a href="' . zen_href_link(FILENAME_POPUP_IMAGE_ADDITIONAL, 'pID=' . $_GET['products_id'] . '&pic=' . $i . '&products_image_large_additional=' . $products_image_large) . '" target="_blank">' . $thumb_regular . '<br><span class="imgLinkAdditional">' . TEXT_CLICK_TO_ENLARGE . '</span></a>'
+                : $thumb_regular)
+            . '</noscript>';
 
         //      $alternate_link = '<a href="' . $products_image_large . '" onclick="javascript:popupWindow(\''. $large_link . '\') return false;" title="' . $products_name . '" target="_blank">' . $thumb_regular . '<br>' . TEXT_CLICK_TO_ENLARGE . '</a>';
 
         $link = $script_link . "\n      " . $noscript_link;
-        //      $link = $alternate_link;
+        // $link = $alternate_link;
 
         // List Box array generation:
-        $list_box_contents[$row][$col] = array(
+        $list_box_contents[$row][$col] = [
             'params' => $link_parameters,
-             'text' => "\n      " . $link
-        );
+            'text' => "\n      " . $link,
+        ];
 
         $modal_images[] = [
             'image' => $file,
