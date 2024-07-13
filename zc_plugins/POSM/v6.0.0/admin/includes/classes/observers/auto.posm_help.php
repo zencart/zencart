af9848f93293352b3105b72d3f71e0c26b5cfbbc
Scott C Wilson <scottcwilson@gmail.com>
2024-07-13 16:47:41 -0400

    Add help to two new POSM pages (#6577)

---
+++ zc_plugins/POSM/v6.0.0/admin/includes/classes/observers/auto.posm_help.php
@@ -0,0 +1,18 @@
+<?php
+
+class zcObserverPosmHelp extends base
+{
+    public function __construct()
+    {
+        $this->attach($this, ['NOTIFIER_PLUGIN_HELP_PAGE_URL_LOOKUP']);
+    }
+
+    protected function update(&$class, $eventID, $page, &$help_page)
+    {
+        if ($page === FILENAME_PRODUCTS_OPTIONS_STOCK) {
+            $help_page = 'https://docs.zen-cart.com/user/admin_pages/catalog/options_stock_manager/'; 
+        } elseif ($page === FILENAME_PRODUCTS_OPTIONS_STOCK_VIEW_ALL) {
+            $help_page = 'https://docs.zen-cart.com/user/admin_pages/catalog/options_stock_view_all/'; 
+        }
+    }
+}
