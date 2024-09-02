dac2fb7b868b6fb3814d6ba33fbeb901cf762bdb
torvista <4391026+torvista@users.noreply.github.com>
2024-09-02 11:18:59 +0200

    formatting

--- includes/modules/categories_tabs.php
+++ includes/modules/categories_tabs.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * categories_tabs.php module
  *
@@ -8,10 +9,9 @@
  * @version $Id: DrByte 2020 Dec 28 Modified in v1.5.8-alpha $
  */
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
 $order_by = " ORDER BY c.sort_order, cd.categories_name";
-
 $sql = "SELECT c.sort_order, c.categories_id, cd.categories_name
         FROM " . TABLE_CATEGORIES . " c
         LEFT JOIN " . TABLE_CATEGORIES_DESCRIPTION . " cd ON (c.categories_id = cd.categories_id AND cd.language_id = " . (int)$_SESSION['languages_id'] . ")
@@ -22,16 +22,14 @@ $categories_tab = $db->Execute($sql);
 
 $links_list = [];
 foreach ($categories_tab as $category_tab) {
-
-  // currently selected category
-  if ((int)$cPath == $category_tab['categories_id']) {
-    $new_style = 'category-top';
-    $categories_tab_current = '<span class="category-subs-selected">' . $category_tab['categories_name'] . '</span>';
-  } else {
-    $new_style = 'category-top';
-    $categories_tab_current = $category_tab['categories_name'];
-  }
-
-  // create link to top level category
-  $links_list[] = '<a class="' . $new_style . '" href="' . zen_href_link(FILENAME_DEFAULT, 'cPath=' . (int)$category_tab['categories_id']) . '">' . $categories_tab_current . '</a> ';
+    // currently selected category
+    if ((int)$cPath == $category_tab['categories_id']) {
+        $new_style = 'category-top';
+        $categories_tab_current = '<span class="category-subs-selected">' . $category_tab['categories_name'] . '</span>';
+    } else {
+        $new_style = 'category-top';
+        $categories_tab_current = $category_tab['categories_name'];
+    }
+    // create link to top level category
+    $links_list[] = '<a class="' . $new_style . '" href="' . zen_href_link(FILENAME_DEFAULT, 'cPath=' . (int)$category_tab['categories_id']) . '">' . $categories_tab_current . '</a> ';
 }
