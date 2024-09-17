e79f03b123bc09359fdc642deef9121e0dc1c68d
lat9 <cindy@vinosdefrutastropicales.com>
2024-09-17 15:34:12 -0400

    Sync product-info templates (#6741)
    * Initial 'split'
    
    * Reformat tpl_product_info_display.php
    
    - Align PHP conditionals and HTML start/end tags
    - Remove redundant start/end PHP clauses
    - Remove enclosing parentheses from `require` statements
    - Use short-array syntax
    - Use `&&` and `||` instead of `and` and `or` for conditionals
    
    * tpl_product_info_display.php refactoring
    
    - Use `===` where warranted
    - Additional `and` to `&&` in conditionals
    - Split long lines for readability
    
    * tpl_product_info_display.php, use short-echo tags
    
    * template_default: Initial changes
    
    * template_default/tpl_product_info_display.php: Reformatting
    
    - Line up PHP conditionals and HTML start/end tags
    - Convert `and`/`or` conditionals to `&&`/`||`
    - Remove redundant PHP start/end directives
    
    * template_default/tpl_product_info_display.php: Refactoring
    
    - Use short-echo tags
    - Split long lines for readability
    - Use `===` where appropriate
    
    * Remove RC templates that are identical to template_default

---
+++ includes/templates/template_default/templates/tpl_product_music_info_display_details.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Page Template, displays the product-details list for a music product.
+ *
+ * Loaded automatically by index.php?main_page=product_music_info.
+ *
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: lat9 2024 May 16 Modified in v2.0.1 $
+ */
+$display_product_model = ($flag_show_product_info_model === '1' && $products_model !== '');
+$display_product_weight = ($flag_show_product_info_weight === '1' && $products_weight != 0);
+$display_product_quantity = ($flag_show_product_info_quantity === '1');
+$display_product_manufacturer = ($flag_show_product_info_manufacturer === '1' && !empty($manufacturers_name));
+$display_product_music_artist = ($flag_show_product_music_info_artist === '1' && !empty($products_artist_name));
+$display_product_music_genre = ($flag_show_product_music_info_genre === '1' && !empty($products_music_genre_name));
+if ($display_product_model || $display_product_weight || $display_product_quantity || $display_product_manufacturer || $display_product_music_artist || $display_product_music_genre) {
+?>
+<ul id="productDetailsList" class="floatingBox back">
+    <?= (($display_product_model === true) ? '<li>' . TEXT_PRODUCT_MODEL . $products_model . '</li>' : '') . "\n" ?>
+    <?= (($display_product_weight === true) ? '<li>' . TEXT_PRODUCT_WEIGHT .  $products_weight . TEXT_PRODUCT_WEIGHT_UNIT . '</li>'  : '') . "\n" ?>
+    <?= (($display_product_quantity === true) ? '<li>' . $products_quantity . TEXT_PRODUCT_QUANTITY . '</li>'  : '') . "\n" ?>
+    <?= (($display_product_manufacturer === true) ? '<li>' . TEXT_PRODUCT_MANUFACTURER . $manufacturers_name . '</li>' : '') . "\n" ?>
+    <?= (($display_product_music_artist === true) ? '<li>' . TEXT_PRODUCT_ARTIST . $products_artist_name . '</li>' : '') . "\n" ?>
+    <?= (($display_product_music_genre === true) ? '<li>' . TEXT_PRODUCT_MUSIC_GENRE . $products_music_genre_name . '</li>' : '') . "\n" ?>
+</ul>
+<br class="clearBoth">
+<?php
+}
