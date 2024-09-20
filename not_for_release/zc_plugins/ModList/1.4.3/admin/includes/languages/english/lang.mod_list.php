7b48e6f7537ebefb17a77c76259f02e14e9892a0
Chris Brown <drbyte@zen-cart.com>
2024-09-20 16:04:44 -0400

    Update mod_list lang files format

---
+++ not_for_release/zc_plugins/ModList/1.4.3/admin/includes/languages/english/lang.mod_list.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Mod List
+ * @copyright That Software Guy, Inc., 2015
+*/
+return [
+    'HEADING_TITLE' => 'Modifications To This Installation of Zen Cart',
+    'PAGES_TABLE' => 'New Admin Pages',
+    'DB_LIST' => 'New Database Tables',
+    'MODULE_LIST' => 'New Modules',
+    'MISSING_ADMIN_PAGES' => 'Missing Admin Configuration Pages',
+    'HEADING_PAGE_NAME' => 'Page Name',
+    'HEADING_PAGE_MENU_KEY' => 'Menu Key',
+    'HEADING_PAGE_LINK' => 'Link',
+    'HEADING_DISPLAY' => 'Display',
+
+    'NO_EXTRAS' => 'No additional modules',
+    'NO_LINK' => 'Link cannot be created',
+    'NO_PAGES_TABLE_FOUND' => 'Admin Pages table not found ',
+    'NO_INFORMATION_SCHEMA_TABLE_FOUND' => 'INFORMATION_SCHEMA.TABLES table not found ',
+    'MISSING_ADMIN_PAGES_WHY' => 'This is caused by (migrating pre 1.5.0 mods which have) entries in Admin->Configuration, without creating admin_pages entries for them.',
+    'NO_MISSING_ADMIN_PAGES' => 'No missing admin pages found.',
+];
