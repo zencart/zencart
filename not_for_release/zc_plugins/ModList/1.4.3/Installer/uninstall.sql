e08f9278daf8fe3128dc967c44c9eefcb60b20a8
Chris Brown <drbyte@zen-cart.com>
2024-09-20 21:11:31 -0400

    Include mod_list in core, as an encapsulated plugin, named "System Inspection"

--- not_for_release/zc_plugins/ModList/1.4.3/Installer/uninstall.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM admin_pages WHERE page_key = 'mod_list'; 
-DELETE FROM admin_pages_to_profiles WHERE page_key = 'mod_list'; 
---
+++ zc_plugins/System Inspection/1.5.0/manifest.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'pluginVersion' => 'v1.5.0',
+    'pluginName' => 'System Inspection',
+    'pluginDescription' => 'Lists database changes that have been made to a base Zen Cart installation. (formerly called ModList)',
+    'pluginAuthor' => 'That Software Guy (swguy)',
+    'pluginId' => 0, // ID from Zen Cart forum
+    'zcVersions' => ['v157', 'v158', 'v200', 'v201', 'v210'],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => '', // url
+    'pluginGroups' => [],
+];
