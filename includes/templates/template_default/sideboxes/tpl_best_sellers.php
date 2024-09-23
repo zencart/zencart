4fddddf73339ef8774e76468c08ec319a1785767
lat9 <lat9@vinosdefrutastropicales.com>
2024-09-23 12:08:25 -0400

    Correct sidebox template; reformatting coming ...

--- includes/templates/template_default/sideboxes/tpl_best_sellers.php
+++ includes/templates/template_default/sideboxes/tpl_best_sellers.php
@@ -10,7 +10,7 @@
   $content = '';
   $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent">' . "\n";
   $content .= '<div class="wrapper">' . "\n" . '<ul class="list-links">' . "\n";
-  for ($i=1, $j=sizeof($bestsellers_list); $i<$j; $i++) {
+  for ($i=1, $j=sizeof($bestsellers_list); $i<=$j; $i++) {
     $content .= '<li><a href="' . zen_href_link(zen_get_info_page($bestsellers_list[$i]['id']), 'products_id=' . $bestsellers_list[$i]['id']) . '">' . zen_trunc_string($bestsellers_list[$i]['name'], BEST_SELLERS_TRUNCATE, BEST_SELLERS_TRUNCATE_MORE) . '</a></li>' . "\n";
   }
   $content .= '</ul>' . "\n";
