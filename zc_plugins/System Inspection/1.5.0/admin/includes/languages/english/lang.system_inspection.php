1c98d9514dc6d1ff5af2f886d4805e9ff2cbcad1
Chris Brown <drbyte@zen-cart.com>
2024-09-26 13:14:47 -0400

    Fix syntax of replaceable sprintf params
    Fixes #6777

--- zc_plugins/System Inspection/1.5.0/admin/includes/languages/english/lang.system_inspection.php
+++ zc_plugins/System Inspection/1.5.0/admin/includes/languages/english/lang.system_inspection.php
@@ -33,7 +33,7 @@ return [
     'MISSING_TABLE' => 'Missing table: %s',
     'NEW_MODULE_FOUND' => 'New Module: %s',
     'OBSOLETE_MODULE' => 'Module %s is obsolete. Recommended to remove it.',
-    'REPLACE_MODULE_WITH' => 'Module %1s should be replaced with %2s.',
-    'PLUGIN_FOUND' => '%1s (%2s), version %3s (Author: %4s)',
+    'REPLACE_MODULE_WITH' => 'Module %1$s should be replaced with %2$s.',
+    'PLUGIN_FOUND' => '%1$s (%2$s), version %3$s (Author: %4$s)',
     'PAGES_PLUGIN_FOOTNOTE' => "NOTE: Some of the 'new' pages above may be controlled by the Plugins listed below:",
 ];
