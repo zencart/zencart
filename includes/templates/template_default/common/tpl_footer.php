13f3b03340170f9481f51d2a56d315bc84cc4ad0
lat9 <lat9@vinosdefrutastropicales.com>
2024-09-30 14:52:53 -0400

    Reformatting/refactoring
    - Removing unneeded parentheses on `require` statements.
    - Remove redundant start/stop PHP tags
    - Align PHP conditional; indent HTML tags
    - Use `===` where warranted.
    - Use PHP short-echo tags.
    - Use `||` instead of `or`

--- includes/templates/template_default/common/tpl_footer.php
+++ includes/templates/template_default/common/tpl_footer.php
@@ -15,23 +15,24 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: DrByte 2020 Dec 29 Modified in v1.5.8-alpha $
  */
-require(DIR_WS_MODULES . zen_get_module_directory('footer.php'));
-?>
+require DIR_WS_MODULES . zen_get_module_directory('footer.php');
 
-<?php
 if (!isset($flag_disable_footer) || !$flag_disable_footer) {
 ?>
-
 <!--bof-navigation display -->
 <div id="navSuppWrapper">
-<div id="navSupp">
-<ul>
-<li><?php echo '<a href="' . HTTP_SERVER . DIR_WS_CATALOG . '">'; ?><?php echo HEADER_TITLE_CATALOG; ?></a></li>
-<?php if (EZPAGES_STATUS_FOOTER == '1' or (EZPAGES_STATUS_FOOTER == '2' && zen_is_whitelisted_admin_ip())) { ?>
-<li><?php require($template->get_template_dir('tpl_ezpages_bar_footer.php',DIR_WS_TEMPLATE, $current_page_base,'templates'). '/tpl_ezpages_bar_footer.php'); ?></li>
-<?php } ?>
-</ul>
-</div>
+    <div id="navSupp">
+        <ul>
+            <li><a href="<?= HTTP_SERVER . DIR_WS_CATALOG ?>"><?= HEADER_TITLE_CATALOG ?></a></li>
+<?php
+    if (EZPAGES_STATUS_FOOTER === '1' || (EZPAGES_STATUS_FOOTER === '2' && zen_is_whitelisted_admin_ip())) {
+?>
+            <li><?php require $template->get_template_dir('tpl_ezpages_bar_footer.php', DIR_WS_TEMPLATE, $current_page_base, 'templates') . '/tpl_ezpages_bar_footer.php'; ?></li>
+<?php
+    }
+?>
+        </ul>
+    </div>
 </div>
 <!--eof-navigation display -->
 <?php
@@ -42,34 +43,35 @@ if (!isset($flag_disable_footer) || !$flag_disable_footer) {
 ?>
 <!--bof-ip address display -->
 <?php
-if (SHOW_FOOTER_IP == '1') {
+    if (SHOW_FOOTER_IP === '1') {
 ?>
-<div id="siteinfoIP"><?php echo TEXT_YOUR_IP_ADDRESS . '  ' . $_SERVER['REMOTE_ADDR']; ?></div>
+<div id="siteinfoIP"><?= TEXT_YOUR_IP_ADDRESS . '  ' . $_SERVER['REMOTE_ADDR'] ?></div>
 <?php
-}
+    }
 ?>
 <!--eof-ip address display -->
 
 <!--bof-banner #5 display -->
 <?php
-  if (SHOW_BANNERS_GROUP_SET5 != '' && $banner = zen_banner_exists('dynamic', SHOW_BANNERS_GROUP_SET5)) {
-    if ($banner->RecordCount() > 0) {
+    if (SHOW_BANNERS_GROUP_SET5 != '' && $banner = zen_banner_exists('dynamic', SHOW_BANNERS_GROUP_SET5)) {
+        if (!$banner->EOF) {
 ?>
-<div id="bannerFive" class="banners"><?php echo zen_display_banner('static', $banner); ?></div>
+<div id="bannerFive" class="banners"><?= zen_display_banner('static', $banner) ?></div>
 <?php
+        }
     }
-  }
 ?>
 <!--eof-banner #5 display -->
 
 <!--bof- site copyright display -->
-<div id="siteinfoLegal" class="legalCopyright"><?php echo FOOTER_TEXT_BODY; ?></div>
+<div id="siteinfoLegal" class="legalCopyright"><?= FOOTER_TEXT_BODY ?></div>
 <!--eof- site copyright display -->
 
 <?php
 } // flag_disable_footer
-?>
 
-<?php if (false || (isset($showValidatorLink) && $showValidatorLink == true)) { ?>
-<a href="https://validator.w3.org/nu/?doc=<?php echo urlencode('http' . ($request_type == 'SSL' ? 's' : '') . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . (strstr($_SERVER['REQUEST_URI'], '?') ? '&' : '?') . zen_session_name() . '=' . zen_session_id()); ?>" rel="noopener" target="_blank">VALIDATOR</a>
-<?php } ?>
+if (false || !empty($showValidatorLink)) {
+?>
+<a href="https://validator.w3.org/nu/?doc=<?= urlencode('http' . ($request_type == 'SSL' ? 's' : '') . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . (strstr($_SERVER['REQUEST_URI'], '?') ? '&' : '?') . zen_session_name() . '=' . zen_session_id()) ?>" rel="noopener" target="_blank">VALIDATOR</a>
+<?php
+}
