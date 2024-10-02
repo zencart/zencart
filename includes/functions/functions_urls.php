f9ac0857243e2fcfe74d7dad0f7aa18a4ce41f56
Leonard <info@zenexpert.com>
2024-10-02 21:02:00 +0200

    Add $params to zen_back_link() and add zen_datetime_without_seconds() (#6792)
    * Add zen_datetime_without_seconds() to return date and time without seconds. useful for order status history to show time when updated
    * Add DATE_TIME_FORMAT_WITHOUT_SECONDS
    * Add optional $parameters for the <a> element in zen_back_link(), ie. adding classes to the link
    
    ---------
    
    Co-authored-by: Chris Brown <code@drbyte.dev>

--- includes/functions/functions_urls.php
+++ includes/functions/functions_urls.php
@@ -113,8 +113,10 @@ function zen_get_top_level_domain(string $url) {
 }
 
 
-// Set back button
-function zen_back_link($link_only = false)
+/**
+  * Generate A HREF link for an HTML-based "Back" button, determined from user's session browsing history
+  */
+function zen_back_link(bool $link_only = false, string $parameters = ''): string
 {
     if (count($_SESSION['navigation']->path) - 2 >= 0) {
         $back = count($_SESSION['navigation']->path) - 2;
@@ -129,9 +131,9 @@ function zen_back_link($link_only = false)
         $_SESSION['navigation'] = new navigationHistory;
     }
 
-    if ($link_only == true) {
+    if ($link_only) {
         return $link;
     } else {
-        return '<a href="' . $link . '">';
+        return '<a href="' . $link . '"' . $parameters . '>';
     }
 }
