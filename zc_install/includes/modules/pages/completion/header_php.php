6ed9c555ef4e842aa58089b57e9f5768c17a1b2c
Chris Brown <drbyte@zen-cart.com>
2024-10-16 13:56:29 -0400

    Prevent double-slashes in URL

--- zc_install/includes/modules/pages/completion/header_php.php
+++ zc_install/includes/modules/pages/completion/header_php.php
@@ -38,6 +38,9 @@ if (!isset($_POST['admin_directory']) || !file_exists(DIR_FS_ROOT . $_POST['admi
 $adminLink = zen_output_string_protected($adminServer) . zen_output_string_protected($dir_ws_http_catalog) . zen_output_string_protected($adminDir);
 $catalogLink = zen_output_string_protected($catalogHttpServer) . zen_output_string_protected($dir_ws_http_catalog);
 
+$adminLink = preg_replace('~(?<!:)/+~', '/', $adminLink);
+$catalogLink = preg_replace('~(?<!:)/+~', '/', $catalogLink);
+
 if (isset($_POST['upgrade_mode']) && $_POST['upgrade_mode'] === 'yes') {
     $isUpgrade = true;
 }
