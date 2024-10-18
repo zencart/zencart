30598f684e286cc443d0ec3c99ee3d019a91ee79
Chris Brown <drbyte@zen-cart.com>
2024-10-17 23:02:24 -0400

    PSR reformat and update assertion method names

--- not_for_release/testFramework/Unit/testsPasswordHash/PasswordHashTest.php
+++ not_for_release/testFramework/Unit/testsPasswordHash/PasswordHashTest.php
@@ -21,7 +21,7 @@ class PasswordHashTest extends zcUnitTestCase
         $pass = zcPassword::getInstance(PHP_VERSION);
     }
 
-    public function testPasswordHashFunctionsExist()
+    public function testPasswordHashFunctionsExist(): void
     {
         $this->assertTrue(class_exists('zcPassword', false));
         $this->assertTrue(function_exists('password_hash'));
@@ -29,83 +29,95 @@ class PasswordHashTest extends zcUnitTestCase
         $this->assertTrue(function_exists('password_needs_rehash'));
     }
 
-    public function testPasswordHashResult()
+    public function testPasswordHashResult(): void
     {
         $result = password_hash('testpass1', PASSWORD_DEFAULT);
         $tmp = explode(':', $result);
-        $this->assertTrue($result [0] == '$');
+        $this->assertSame('$', $result[0]);
     }
 
-    public function testPasswordVerify()
+    public function testPasswordVerify(): void
     {
         $this->assertTrue(password_verify('testpass1', '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC'));
     }
 
-    public function testPasswordNeedsRehash()
+    public function testPasswordNeedsRehash(): void
     {
-        if (version_compare(PHP_VERSION, '5.5.0', '>=')) {
-            $hash = 'd95e8fa7f20a009372eb3477473fcd34:1c';
-            $this->assertTrue(password_needs_rehash($hash, PASSWORD_DEFAULT));
-            $hash = 'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751';
-            $this->assertTrue(password_needs_rehash($hash, PASSWORD_DEFAULT));
-            $hash = '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC';
-            $this->assertTrue(!password_needs_rehash($hash, PASSWORD_DEFAULT));
-        }
+        $hash = 'd95e8fa7f20a009372eb3477473fcd34:1c';
+        $this->assertTrue(password_needs_rehash($hash, PASSWORD_DEFAULT));
+        $hash = 'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751';
+        $this->assertTrue(password_needs_rehash($hash, PASSWORD_DEFAULT));
+        $hash = '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC';
+        $this->assertNotTrue(password_needs_rehash($hash, PASSWORD_DEFAULT));
     }
 
-    public function testDetectPasswordType()
+    public function testDetectPasswordType(): void
     {
         $result = zcPassword::getInstance(PHP_VERSION)->detectPasswordType('d95e8fa7f20a009372eb3477473fcd34:1c');
-        $this->assertTrue($result == 'oldMd5');
+        $this->assertSame('oldMd5', $result);
         $result = zcPassword::getInstance(PHP_VERSION)->detectPasswordType('c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751');
-        $this->assertTrue($result == 'compatSha256');
+        $this->assertSame('compatSha256', $result);
         $result = zcPassword::getInstance(PHP_VERSION)->detectPasswordType('$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC');
-        $this->assertTrue($result == 'unknown');
+        $this->assertSame('unknown', $result);
     }
 
-    public function testPasswordValidate()
+    public function testPasswordValidate(): void
     {
         $result = zcPassword::getInstance(PHP_VERSION)->validatePassword('password', 'd95e8fa7f20a009372eb3477473fcd34:1c');
-        $this->assertTrue($result == true);
-        $result = zcPassword::getInstance(PHP_VERSION)->validatePassword('testpass1',
-            'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751');
-        $this->assertTrue($result == true);
-        $result = zcPassword::getInstance(PHP_VERSION)->validatePassword('testpass1',
-            '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC');
-        $this->assertTrue($result == true);
+        $this->assertTrue($result);
+        $result = zcPassword::getInstance(PHP_VERSION)->validatePassword(
+            'testpass1',
+            'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751'
+        );
+        $this->assertTrue($result);
+        $result = zcPassword::getInstance(PHP_VERSION)->validatePassword(
+            'testpass1',
+            '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC'
+        );
+        $this->assertTrue($result);
     }
 
-    public function testValidatePasswordOldMd5()
+    public function testValidatePasswordOldMd5(): void
     {
         $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordOldMd5('password', 'd95e8fa7f20a009372eb3477473fcd34:1c');
-        $this->assertTrue($result == true);
-        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordOldMd5('testpass1',
-            'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751');
-        $this->assertTrue($result == false);
-        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordOldMd5('testpass1',
-            '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC');
-        $this->assertTrue($result == false);
+        $this->assertTrue($result);
+        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordOldMd5(
+            'testpass1',
+            'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751'
+        );
+        $this->assertFalse($result);
+        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordOldMd5(
+            'testpass1',
+            '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC'
+        );
+        $this->assertFalse($result);
     }
 
-    public function testValidatePasswordCompatSha256()
+    public function testValidatePasswordCompatSha256(): void
     {
-        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordCompatSha256('password',
-            'd95e8fa7f20a009372eb3477473fcd34:1c');
-        $this->assertTrue($result == false);
-        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordCompatSha256('testpass1',
-            'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751');
-        $this->assertTrue($result == true);
-        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordCompatSha256('testpass1',
-            '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC');
-        $this->assertTrue($result == false);
+        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordCompatSha256(
+            'password',
+            'd95e8fa7f20a009372eb3477473fcd34:1c'
+        );
+        $this->assertFalse($result);
+        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordCompatSha256(
+            'testpass1',
+            'c7d6976483032e03d48c1255cc9714838915e58007952f9f5f9c2af6f81f20d7:4972adcbae0c13a8bf77560479341f0beb2fb200ff21c16fc1ade1d467208751'
+        );
+        $this->assertTrue($result);
+        $result = zcPassword::getInstance(PHP_VERSION)->validatePasswordCompatSha256(
+            'testpass1',
+            '$2y$10$XP.PqzC8/M.NbVIRVVael.WU8YxBss.qBUIzXtoIuWPbFHYxjGySC'
+        );
+        $this->assertFalse($result);
     }
 
     /**
      * Test password entropy / duplication risks
      */
-    public function testPasswordGeneration()
+    public function testPasswordGeneration(): void
     {
-        $passwordList = array();
+        $passwordList = [];
         $loopCount = 10000;
         if (defined('BIG_LOOPS_BYPASS')) {
             $loopCount = 100;
@@ -121,136 +133,132 @@ class PasswordHashTest extends zcUnitTestCase
     }
 
 
-    public function testExtensions()
+    public function testExtensions(): void
     {
         $this->assertTrue(function_exists('crypt'));
         $this->assertTrue(defined("CRYPT_BLOWFISH"));
     }
 
-    public function testBlowfishCrypt()
+    public function testBlowfishCrypt(): void
     {
-        $tests = array(
-            array(
+        $tests = [
+            [
                 '$2a$05$CCCCCCCCCCCCCCCCCCCCC.E5YPO9kmyuRGyh0XouQYb4YMJKvyOeW',
-                'U*U'
-            ),
-            array(
+                'U*U',
+            ],
+            [
                 '$2a$05$CCCCCCCCCCCCCCCCCCCCC.VGOzA784oUp/Z0DY336zx7pLYAy0lwK',
-                'U*U*'
-            ),
-            array(
+                'U*U*',
+            ],
+            [
                 '$2a$05$XXXXXXXXXXXXXXXXXXXXXOAcXxm9kjPGEMsLznoKqmqw7tc8WCx4a',
-                'U*U*U'
-            ),
-            array(
+                'U*U*U',
+            ],
+            [
                 '$2a$05$abcdefghijklmnopqrstuu5s2v8.iXieOjg/.AySBTTZIIVFJeBui',
-                '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789chars after 72 are ignored'
-            ),
-            array(
+                '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789chars after 72 are ignored',
+            ],
+            [
                 '$2x$05$/OK.fbVrR/bpIqNJ5ianF.CE5elHaaO4EbggVDjb8P19RukzXSM3e',
-                "\xa3"
-            ),
-            array(
+                "\xa3",
+            ],
+            [
                 '$2x$05$/OK.fbVrR/bpIqNJ5ianF.CE5elHaaO4EbggVDjb8P19RukzXSM3e',
-                "\xff\xff\xa3"
-            ),
-            array(
+                "\xff\xff\xa3",
+            ],
+            [
                 '$2y$05$/OK.fbVrR/bpIqNJ5ianF.CE5elHaaO4EbggVDjb8P19RukzXSM3e',
-                "\xff\xff\xa3"
-            ),
-            array(
+                "\xff\xff\xa3",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.nqd1wy.pTMdcvrRWxyiGL2eMz.2a85.',
-                "\xff\xff\xa3"
-            ),
-            array(
+                "\xff\xff\xa3",
+            ],
+            [
                 '$2y$05$/OK.fbVrR/bpIqNJ5ianF.Sa7shbm4.OzKpvFnX1pQLmQW96oUlCq',
-                "\xa3"
-            ),
-            array(
+                "\xa3",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.Sa7shbm4.OzKpvFnX1pQLmQW96oUlCq',
-                "\xa3"
-            ),
-            array(
+                "\xa3",
+            ],
+            [
                 '$2x$05$/OK.fbVrR/bpIqNJ5ianF.o./n25XVfn6oAPaUvHe.Csk4zRfsYPi',
-                "1\xa3345"
-            ),
-            array(
+                "1\xa3345",
+            ],
+            [
                 '$2x$05$/OK.fbVrR/bpIqNJ5ianF.o./n25XVfn6oAPaUvHe.Csk4zRfsYPi',
-                "\xff\xa3345"
-            ),
-            array(
+                "\xff\xa3345",
+            ],
+            [
                 '$2x$05$/OK.fbVrR/bpIqNJ5ianF.o./n25XVfn6oAPaUvHe.Csk4zRfsYPi',
-                "\xff\xa334\xff\xff\xff\xa3345"
-            ),
-            array(
+                "\xff\xa334\xff\xff\xff\xa3345",
+            ],
+            [
                 '$2y$05$/OK.fbVrR/bpIqNJ5ianF.o./n25XVfn6oAPaUvHe.Csk4zRfsYPi',
-                "\xff\xa334\xff\xff\xff\xa3345"
-            ),
-            array(
+                "\xff\xa334\xff\xff\xff\xa3345",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.ZC1JEJ8Z4gPfpe1JOr/oyPXTWl9EFd.',
-                "\xff\xa334\xff\xff\xff\xa3345"
-            ),
-            array(
+                "\xff\xa334\xff\xff\xff\xa3345",
+            ],
+            [
                 '$2y$05$/OK.fbVrR/bpIqNJ5ianF.nRht2l/HRhr6zmCp9vYUvvsqynflf9e',
-                "\xff\xa3345"
-            ),
-            array(
+                "\xff\xa3345",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.nRht2l/HRhr6zmCp9vYUvvsqynflf9e',
-                "\xff\xa3345"
-            ),
-            array(
+                "\xff\xa3345",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.6IflQkJytoRVc1yuaNtHfiuq.FRlSIS',
-                "\xa3ab"
-            ),
-            array(
+                "\xa3ab",
+            ],
+            [
                 '$2x$05$/OK.fbVrR/bpIqNJ5ianF.6IflQkJytoRVc1yuaNtHfiuq.FRlSIS',
-                "\xa3ab"
-            ),
-            array(
+                "\xa3ab",
+            ],
+            [
                 '$2y$05$/OK.fbVrR/bpIqNJ5ianF.6IflQkJytoRVc1yuaNtHfiuq.FRlSIS',
-                "\xa3ab"
-            ),
-            array(
+                "\xa3ab",
+            ],
+            [
                 '$2x$05$6bNw2HLQYeqHYyBfLMsv/OiwqTymGIGzFsA4hOTWebfehXHNprcAS',
-                "\xd1\x91"
-            ),
-            array(
+                "\xd1\x91",
+            ],
+            [
                 '$2x$05$6bNw2HLQYeqHYyBfLMsv/O9LIGgn8OMzuDoHfof8AQimSGfcSWxnS',
-                "\xd0\xc1\xd2\xcf\xcc\xd8"
-            ),
-            array(
+                "\xd0\xc1\xd2\xcf\xcc\xd8",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.swQOIzjOiJ9GHEPuhEkvqrUyvWhEMx6',
-                "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaachars after 72 are ignored as usual"
-            ),
-            array(
+                "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaachars after 72 are ignored as usual",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.R9xrDjiycxMbQE2bp.vgqlYpW5wx2yy',
-                "\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55"
-            ),
-            array(
+                "\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55\xaa\x55",
+            ],
+            [
                 '$2a$05$/OK.fbVrR/bpIqNJ5ianF.9tQZzcJfm3uj2NvJ/n5xkhpqLrMpWCe',
-                "\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff"
-            ),
-            array(
+                "\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff\x55\xaa\xff",
+            ],
+            [
                 '$2a$05$CCCCCCCCCCCCCCCCCCCCC.7uG0VCzI2bS7j6ymqJi9CdcdxiRTWNy',
-                ''
-            )
-        );
+                '',
+            ],
+        ];
         foreach ($tests as $test) {
-            $this->assertTrue(crypt($test [1], $test [0]) == $test [0]);
+            $this->assertEquals(crypt($test[1], $test[0]), $test[0]);
         }
     }
 
-    public function testPasswordHashing()
+    public function testPasswordHashing(): void
     {
-        if (version_compare(PHP_VERSION, '5.5.0', '>=')) {
-            $hashLength = strlen(password_hash("foo", PASSWORD_BCRYPT));
-            $passwordHash1 = password_hash("rasmuslerdorf", PASSWORD_BCRYPT, array(
-                "cost" => 7,
-            ));
-            $passwordHash2 = password_hash("test", PASSWORD_BCRYPT);
-            $this->assertTrue($hashLength == 60);
-            $this->assertTrue(password_verify("rasmuslerdorf", $passwordHash1));
-            $this->assertTrue(password_verify("test", $passwordHash2));
-        }
+        $hashLength = strlen(password_hash('foo', PASSWORD_BCRYPT));
+        $passwordHash1 = password_hash('rasmuslerdorf', PASSWORD_BCRYPT, ['cost' => 7]);
+        $passwordHash2 = password_hash('test', PASSWORD_BCRYPT);
+        $this->assertSame(60, $hashLength);
+        $this->assertTrue(password_verify('rasmuslerdorf', $passwordHash1));
+        $this->assertTrue(password_verify('test', $passwordHash2));
     }
 }
