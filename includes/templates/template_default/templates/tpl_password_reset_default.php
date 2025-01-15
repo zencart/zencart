9806d3078114e4ab1f4e7a09c92a8b2dcaf56424
Chris Brown <drbyte@zen-cart.com>
2025-01-15 15:49:45 -0500

    Send password-reset URL instead of new password
    When a customer requests a password reset, the old behavior was to generate a new password for them, reset it to that password, and email that password to the customer. These emails often got lost in spam traps, and if the email never arrived then the customer could never login again even if they remembered their old password because their old password had been replaced.
    
    This updated logic now generates a temporary reset token and emails that token in the form of a URL to click to take them to the password_reset page where they can choose their own new password. Once the token is validated and the user selects a (suitable-length) password, their account is updated to use that new password, and the token is deleted.
    
    Tokens also auto-expire after a configurable timeframe, so that old links in old emails don't present a security risk.
    
    NOTE: Retires notifier `NOTIFY_PASSWORD_FORGOTTEN_CHANGED` which is superceded by `NOTIFY_PASSWORD_RESET_URL_SENT`
    But the same former notifier hooks used with the account_password page (when user chooses their own new password) still fire, for the benefit of 3rd party integrations.

---
+++ includes/templates/template_default/templates/tpl_password_reset_default.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Loaded automatically by index.php?main_page=password_reset.<br />
+ * Allows customer to change their password via a requested reset_token
+ *
+ * @copyright Copyright 2003-2025 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: tpl_password_reset_default.php $
+ *
+ *  NOTE: changes made to this file should also be made in tpl_account_password_default.php where relevant
+ */
+
+?>
+<div class="centerColumn" id="passwordReset">
+    <h1><?= HEADING_TITLE ?></h1>
+    <?php
+    if ($messageStack->size('reset_password') > 0) {
+        echo $messageStack->output('reset_password');
+    } ?>
+
+    <?php
+    if (!$token_error) {
+    ?>
+    <?= zen_draw_form('account_password', zen_href_link(FILENAME_PASSWORD_RESET, '', 'SSL'), 'post', 'onsubmit="return check_form(account_password);"') ?>
+    <?= zen_draw_hidden_field('action', 'process') ?>
+    <?= zen_draw_hidden_field('reset_token', $reset_token) ?>
+
+    <fieldset>
+        <div class="alert forward"><?= FORM_REQUIRED_INFORMATION ?></div>
+        <br class="clearBoth">
+
+        <label class="inputLabel" for="password-new"><?= ENTRY_PASSWORD_NEW ?></label>
+        <?= zen_draw_password_field('password_new', '', 'id="password-new" autocomplete="new-password" placeholder="' . ENTRY_PASSWORD_NEW_TEXT . '" required') ?>
+        <br class="clearBoth">
+
+        <label class="inputLabel" for="password-confirm"><?= ENTRY_PASSWORD_CONFIRMATION ?></label>
+        <?= zen_draw_password_field('password_confirmation', '', 'id="password-confirm" autocomplete="new-password" placeholder="' . ENTRY_PASSWORD_CONFIRMATION_TEXT . '" required') ?>
+        <br class="clearBoth">
+
+    </fieldset>
+    <div class="buttonRow forward"><?= zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT) ?></div>
+    <div class="buttonRow back"><?= '<a href="' . zen_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_BACK, BUTTON_BACK_ALT) . '</a>' ?></div>
+    </form>
+<?php
+}
+?>
+</div>
