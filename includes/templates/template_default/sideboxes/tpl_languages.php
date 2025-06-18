c42c101185bc9454117d4c2e7c62e1816cb7fb6c
Chris Brown <drbyte@zen-cart.com>
2025-06-17 21:07:07 -0400

    Refactor last remaining use of $lng->catalog_language from storefront code

--- includes/templates/template_default/sideboxes/tpl_languages.php
+++ includes/templates/template_default/sideboxes/tpl_languages.php
@@ -11,7 +11,7 @@
   $content .= '<div id="' . str_replace('_', '-', $box_id . 'Content') . '" class="sideBoxContent centeredContent">';
 
   $lng_cnt = 0;
-  foreach($lng->catalog_languages as $key => $value) {
+  foreach($lng->get_languages_by_code() as $key => $value) {
     $content .= '<a href="' . zen_href_link($_GET['main_page'], zen_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . zen_image(DIR_WS_LANGUAGES .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a>&nbsp;&nbsp;';
     $lng_cnt ++;
     if ($lng_cnt >= MAX_LANGUAGE_FLAGS_COLUMNS) {
