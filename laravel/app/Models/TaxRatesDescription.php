34adf15f34577d8324fe73175ff050a703dc90e0
Chris Brown <drbyte@zen-cart.com>
2025-07-23 19:47:09 -0400

    Add TaxRatesDescription model

---
+++ laravel/app/Models/TaxRatesDescription.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright 2003-2025 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version GIT: $Id: $
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class TaxRatesDescription extends Eloquent
+{
+    protected $table = TABLE_TAX_RATES_DESCRIPTION;
+    protected $primaryKey = ['id'];
+    public $timestamps = false;
+    protected $guarded = [];
+
+    public function rate()
+    {
+        return $this->hasOne(TaxRate::class, 'tax_rates_id', 'tax_rates_id');
+    }
+}
