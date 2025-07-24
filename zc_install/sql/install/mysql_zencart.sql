31d431e7a20c3475a1a2d3d3f856db4478841ce6
Chris Brown <drbyte@zen-cart.com>
2025-07-23 20:14:58 -0400

    Specify non-auto-increment fields

--- zc_install/sql/install/mysql_zencart.sql
+++ zc_install/sql/install/mysql_zencart.sql
@@ -3352,7 +3352,7 @@ INSERT INTO products_options_values (products_options_values_id, language_id, pr
 
 # USA/Florida
 INSERT INTO tax_rates VALUES (1, 1, 1, 1, 7.0, now(), now());
-INSERT INTO tax_rates_description VALUES (1, 1, 'FL TAX 7.0%');
+INSERT INTO tax_rates_description (tax_rates_id, language_id, tax_description) VALUES (1, 1, 'FL TAX 7.0%');
 INSERT INTO geo_zones (geo_zone_id,geo_zone_name,geo_zone_description,date_added) VALUES (1,'Florida','Florida local sales tax zone',now());
 INSERT INTO zones_to_geo_zones (association_id,zone_country_id,zone_id,geo_zone_id,date_added) VALUES (1,223,18,1,now());
 INSERT INTO tax_class (tax_class_id, tax_class_title, tax_class_description, date_added) VALUES (1, 'Taxable Goods', 'The following types of products are included: non-food, services, etc', now());
