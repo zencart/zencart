d9eee28ae07a2feac0e43568dcb0a9766faab54f
pRose on amour <prose@mxworks.cc>
2025-08-16 06:46:42 -0700

    remove unneeded overrides

--- includes/languages/english/responsive_classic/lang.index.php
+++ includes/languages/english/responsive_classic/lang.index.php
@@ -1,19 +1,6 @@
 <?php
-$define = [
-    'TEXT_GREETING_GUEST' => 'Welcome <span class="greetUser">Guest!</span> Would you like to <a href="%s">log yourself in</a>?',
-    'TEXT_GREETING_GUEST_SHOWCASE' => 'Welcome, please enjoy our online showcase.',
-    'TEXT_GREETING_PERSONAL' => 'Hello <span class="greetUser">%1$s</span>! Would you like to see our <a href="%2$s">newest additions</a>?',
-    'TEXT_INFORMATION' => 'Define your main Index page copy here.',
-    'TABLE_HEADING_IMAGE' => '',
-    'TEXT_NO_PRODUCTS' => 'There are no products to list in this category.',
-    'TEXT_NO_PRODUCTS2' => 'There is no product available from this manufacturer.',
-    'TEXT_NUMBER_OF_PRODUCTS' => 'Number of Products: ',
-    'TEXT_SHOW' => 'Filter by:',
-    'TEXT_BUY' => 'Buy 1 \'',
-    'TEXT_NOW' => '\' now',
-    'HEADING_TITLE_PRODUCTS' => 'Available Products',
-    'HEADING_TITLE' => 'Congratulations! You have successfully installed your Zen Cart&reg; E-Commerce Solution.',
-    'HEADING_TITLE_NESTED' => 'Congratulations! You have successfully installed your Zen Cart&reg; E-Commerce Solution.',
-];
+    $define = [
+        'TABLE_HEADING_IMAGE' => '',
+    ];
 
-return $define;
+    return $define;
