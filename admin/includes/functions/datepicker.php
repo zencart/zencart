b03b3f829721cb71fb93ed92cebfa4302adc4e40
piloujp <117885587+piloujp@users.noreply.github.com>
2025-08-18 02:57:01 +0900

    Products available - Time setting

--- admin/includes/functions/datepicker.php
+++ admin/includes/functions/datepicker.php
@@ -29,6 +29,15 @@ function zen_datepicker_format_fordate()
 }
 
 /**
+ * Add hours & minutes to date format
+ * @return string
+ */
+function zen_datetimepicker_format_fordate(): string
+{
+    return zen_datepicker_format_fordate() . ' H:i';
+}
+
+/**
  * converts mm-dd-yy to %m-%d-%Y
  * @return string
  */
@@ -42,28 +51,38 @@ function zen_datepicker_format_forsql()
 }
 
 /**
+ * Add hours & minutes to date format
+ * @return string
+ */
+function zen_datetimepicker_format_forsql(): string
+{
+    return zen_datepicker_format_forsql() . ' %H:%i';
+}
+
+/**
  * Format a date for database storage when date is blank or is in the past.
  *
  * @param string $raw_date Date to check against today
  * @param string $past_date Date to use if $raw_date is empty or in the past. Normal options are '' or '0001-01-01'.
  * @return string
  */
-function zen_prepare_date(string $raw_date, string $past_date = ''): string
+function zen_prepare_date(string $raw_date, string $past_date = '', bool $usetime = false): string
 {
     if (empty($raw_date)) {
         return $past_date;
     }
+    $useformat = $usetime ? 'Y-m-d H:i' : 'Y-m-d';
     $date = zen_db_prepare_input($raw_date);
     if (DATE_FORMAT_DATE_PICKER !== 'yy-mm-dd' && !empty($date)) {
-        $local_fmt = zen_datepicker_format_fordate();
+        $local_fmt = $usetime ? zen_datetimepicker_format_fordate() : zen_datepicker_format_fordate();
         $dt = DateTime::createFromFormat($local_fmt, $date);
         $date = '';
         if (!empty($dt)) {
-            $date = $dt->format('Y-m-d');
+            $date = $dt->format($useformat);
         }
     }
     if (!empty($past_date)) {
-        $date = (date('Y-m-d') < $date) ? $date : $past_date;
+        $date = (date($useformat) < $date) ? $date : $past_date;
     }
     return $date;
 }
