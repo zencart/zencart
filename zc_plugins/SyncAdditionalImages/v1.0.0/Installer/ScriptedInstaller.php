8ad62b23fd0b54df3e44eb84554a9cb86f997fed
Chris Brown <code@drbyte.dev>
2025-09-08 19:32:42 -0400

    Adjust sort order

--- zc_plugins/SyncAdditionalImages/v1.0.0/Installer/ScriptedInstaller.php
+++ zc_plugins/SyncAdditionalImages/v1.0.0/Installer/ScriptedInstaller.php
@@ -15,12 +15,12 @@ class ScriptedInstaller extends ScriptedInstallBase
             'configuration_value' => 'legacy',
             'configuration_description' => 'Use Legacy mode for old-school directory scanning and matching filenames. Additional images must be uploaded manually via FTP or using Image Handler.<br>Use Modern mode to add additional images directly from the admin product page. Filenames and extensions do not need to match.<br><strong>NOTE:</strong> if you are switching from Legacy to Modern for the first time, you can use the converter tool found in your Tools menu to update all existing product data with existing additional images. Switching back and forth is not recommended.',
             'configuration_group_id' => 4,
-            'sort_order' => 100,
+            'sort_order' => 80,
             'set_function' => 'zen_cfg_select_option([\'legacy\', \'modern\'], ',
         ];
         $this->addConfigurationKey('ADDITIONAL_IMAGES_APPROACH', $fields);
 
-        //        // alter product table to InnoDB if not already done
+//        // alter product table to InnoDB if not already done
 //        $sql = "ALTER TABLE " . TABLE_PRODUCTS . " ENGINE=InnoDB";
 //        $this->executeInstallerSql($sql);
 //
@@ -34,6 +34,7 @@ class ScriptedInstaller extends ScriptedInstallBase
 //            FOREIGN KEY (products_id) REFERENCES " . TABLE_PRODUCTS . "(products_id) ON DELETE CASCADE
 //        ) ENGINE=InnoDB";
 //        $this->executeInstallerSql($sql);
+
         // create products_additional_images table
         $sql = "CREATE TABLE IF NOT EXISTS " . TABLE_PRODUCTS_ADDITIONAL_IMAGES . " (
             id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -49,6 +50,5 @@ class ScriptedInstaller extends ScriptedInstallBase
     protected function executeUninstall()
     {
         zen_deregister_admin_pages(['toolsAidba']);
-
     }
 }
