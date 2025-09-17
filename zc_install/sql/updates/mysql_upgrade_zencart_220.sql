486bca419f09a79fa4d98654072d6e6cff54644f
Chris Brown <code@drbyte.dev>
2025-09-16 23:43:23 -0400

    Shorten field

--- zc_install/sql/updates/mysql_upgrade_zencart_220.sql
+++ zc_install/sql/updates/mysql_upgrade_zencart_220.sql
@@ -104,15 +104,15 @@ EXECUTE stmt2;
 DEALLOCATE PREPARE stmt2;
 
 #PROGRESS_FEEDBACK:!TEXT=Creating new table products_additional_images...
-INSERT IGNORE INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES ('Additional Images Approach', 'ADDITIONAL_IMAGES_APPROACH', 'legacy', 'Use Legacy mode for old-school directory scanning and matching filenames. Additional images must be uploaded manually via FTP or using Image Handler.<br>Use Modern mode to add additional images directly from the admin product page. Filenames and extensions do not need to match.<br><strong>NOTE:</strong> if you are switching from Legacy to Modern for the first time, you can use the converter tool found in your Tools menu to update all existing product data with existing additional images. Switching back and forth is not recommended.', '4', '80', 'zen_cfg_select_option([\'legacy\', \'modern\'], ', NOW());
+INSERT IGNORE INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES ('Additional Images Approach', 'ADDITIONAL_IMAGES_APPROACH', 'legacy', 'Use Legacy mode for old-school directory scanning and matching filenames. Additional images must be uploaded manually via FTP or using Image Handler.<br>Use Modern mode to add additional images directly from the admin product page. Filenames and extensions do not need to match.<br><strong>NOTE:</strong> if you are switching from Legacy to Modern for the first time, you can use the converter tool found in your Tools menu to update all existing product data with existing additional images. Switching back and forth is not recommended.', '4', '26', 'zen_cfg_select_option([\'legacy\', \'modern\'], ', NOW());
 CREATE TABLE IF NOT EXISTS products_additional_images (
-  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  products_id INT(11) NOT NULL,
-  additional_image VARCHAR(255) NOT NULL,
-  sort_order INT(11) DEFAULT 0,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  products_id INT NOT NULL,
+  additional_image VARCHAR(191) NOT NULL,
+  sort_order INT DEFAULT 0,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY idx_pid_img_zen (products_id, additional_image)
-) ENGINE=MyISAM
+) ENGINE=MyISAM;
 
 
 #PROGRESS_FEEDBACK:!TEXT=Finalizing ... Done!
