fa0e7df81733bf860a44d28c7b4dde0b37da3afc
Chris Brown <drbyte@zen-cart.com>
2025-09-19 20:48:40 -0400

    Revised naming and wording

--- zc_plugins/ScanAdditionalImages/v1.0.0/manifest.php
+++ zc_plugins/ScanAdditionalImages/v1.0.0/manifest.php
@@ -2,12 +2,12 @@
 
 return [
     'pluginVersion' => 'v1.0.0',
-    'pluginName' => 'Scan Additional Product Images from Files to Database',
-    'pluginDescription' => 'Tool to scan additional product images from filesystem into database, and keep the db in sync.',
+    'pluginName' => 'Scan For Additional Product Image Files for Database',
+    'pluginDescription' => 'Tool to link a product with its additional images found on server and matching legacy filename patterns.',
     'pluginAuthor' => 'ZenExpert (balihr)',
-    'pluginId' => '0', // ID from Zen Cart forum
-    'zcVersions' => ['v2.0.0', 'v2.0.1', 'v2.1.0'], // compatible versions of Zen Cart
-    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
-    'github_repo' => '', // url
+    'pluginId' => '0', // none
+    'zcVersions' => ['v2.2.0'], // compatible versions of Zen Cart
+    'changelog' => '', // none
+    'github_repo' => '', // none
     'pluginGroups' => [],
 ];
