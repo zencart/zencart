1295dc691fdd660b0f7cc8caa92780eb4f5183ae
Chris Brown <drbyte@zen-cart.com>
2025-09-27 15:20:31 -0400

    Update ajax response codes (#7315)
    * Update ajax response codes
    
    * Add developer-mode flag

--- ajax.php
+++ ajax.php
@@ -7,6 +7,7 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: lat9 2024 Aug 13 Modified in v2.1.0-alpha2 $
  */
+
 // Abort if the request was not an AJAX call
 if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'], $_SERVER['REMOTE_ADDR']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
     http_response_code(400); // "Bad Request"
@@ -22,7 +23,7 @@ if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'], $_SERVER['REMOTE_ADDR']) || strtol
 //
 $class_method_regex = '/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/';
 if (!isset($_GET['act'], $_GET['method']) || $_GET['act'] === '_' || !preg_match($class_method_regex, $_GET['act']) || !preg_match($class_method_regex, $_GET['method'])) {
-    ajaxAbort();
+    ajaxAbort(403);
 }
 
 // -----
@@ -36,14 +37,16 @@ if (empty($zc_ajax_base_dir)) {
 require $zc_ajax_base_dir . 'includes/application_top.php';
 
 // deny ajax requests from spiders
-if (isset($spider_flag) && $spider_flag === true) ajaxAbort();
+if (isset($spider_flag) && $spider_flag === true) {
+    ajaxAbort(inDeveloperMode() ? 410 : 400);
+}
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
 
-// --- support functions ------------------
+// --- begin support functions ------------------
 function ajaxAbort($status = 400, $msg = null)
 {
     global $zc_ajax_base_dir;
@@ -54,30 +57,34 @@ function ajaxAbort($status = 400, $msg = null)
     require $zc_ajax_base_dir . 'includes/application_bottom.php';
     exit();
 }
-// --- support functions ------------------
+function inDeveloperMode(): bool
+{
+    return (defined('DEVELOPER_MODE') && DEVELOPER_MODE === true);
+}
+// --- end support functions ------------------
 
 $language_page_directory = DIR_WS_LANGUAGES . $_SESSION['language'] . '/';
 
 $className = 'zc' . ucfirst($_GET['act']);
 $classFile = basename($className . '.php');
 $classPath = DIR_WS_CLASSES . 'ajax/';
-$basePath  = DIR_FS_CATALOG;
+$basePath = DIR_FS_CATALOG;
 $file = realpath($basePath . $classPath . $classFile);
 if (!empty($file) && file_exists($file)) {
     require $file;
 } else {
     $fs->loadFilesFromPluginsDirectory($installedPlugins, 'catalog/' . $classPath, '~^' . $classFile . '$~');
     if (!class_exists($className)) {
-        ajaxAbort();
+        ajaxAbort(inDeveloperMode() ? 422 : 400);
     }
 }
 
 $class = new $className();
 if (!method_exists($class, $_GET['method'])) {
-    ajaxAbort(400, 'class method error');
+    ajaxAbort(inDeveloperMode() ? 422 : 400, 'class method error');
 }
 
 // Accepted request, so execute and return appropriate response:
-$result = call_user_func(array($class, $_GET['method']));
+$result = call_user_func([$class, $_GET['method']]);
 echo json_encode($result);
 require $zc_ajax_base_dir . 'includes/application_bottom.php';
