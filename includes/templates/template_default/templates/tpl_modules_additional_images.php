68ba8e786bfaa03dff5f96c00f92a9a900aa89ff
torvista <4391026+torvista@users.noreply.github.com>
2025-10-19 12:39:19 +0200

    remove href from img tag, remove duplicate file

--- includes/templates/template_default/templates/tpl_modules_additional_images.php
+++ includes/templates/template_default/templates/tpl_modules_additional_images.php
@@ -37,8 +37,8 @@ if (empty($flag_show_product_info_additional_images) || empty($modal_images)) {
         $modal_content_id = 'modalContent' . $i;
         $modal_link_id = 'modalLink' . $i;
         $modal_link_js = 'openModal(\'' . $modal_id . '\')';
-        $modal_link_attributes = 'href="javascript:void(0);" onclick="' . $modal_link_js . '"';
-        $modal_link_img = zen_image($image['base_image'], $image['products_name'], MEDIUM_IMAGE_WIDTH, MEDIUM_IMAGE_HEIGHT, $modal_link_attributes);
+        $modal_link_attributes = 'href="#" onclick="' . $modal_link_js . '; return false;"';
+        $modal_link_img = zen_image($image['base_image'], $image['products_name'], MEDIUM_IMAGE_WIDTH, MEDIUM_IMAGE_HEIGHT);
 
         // Call the modal template to render the image and its modal
         require $template->get_template_dir('tpl_image_additional.php', DIR_WS_TEMPLATE, $current_page_base, 'modalboxes') . '/tpl_image_additional.php';
