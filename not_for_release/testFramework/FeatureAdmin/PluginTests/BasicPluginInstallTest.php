6d38712985cc73f8e86e5e5796db93e4700e5289
Chris Brown <drbyte@zen-cart.com>
2025-10-25 22:45:26 -0400

    Update plugin tests

--- not_for_release/testFramework/FeatureAdmin/PluginTests/BasicPluginInstallTest.php
+++ not_for_release/testFramework/FeatureAdmin/PluginTests/BasicPluginInstallTest.php
@@ -4,7 +4,7 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  */
 
-namespace Tests\FeatureAdmin\AdminEndpoints;
+namespace Tests\FeatureAdmin\PluginTests;
 
 use Tests\Support\zcFeatureTestCaseAdmin;
 use App\Models\PluginControl;
@@ -15,9 +15,8 @@ class BasicPluginInstallTest extends zcFeatureTestCaseAdmin
     public const TEST_PLUGIN_NAME = 'zenTestPlugin';
     public const TEST_PLUGIN_VERSION = 'v1.0.0';
 
-    public function testInstallPlugin()
+    public function testInstallPlugin(): void
     {
-
         $this->browserAdminLogin();
         $this->installPluginToFilesystem(self::TEST_PLUGIN_NAME, self::TEST_PLUGIN_VERSION);
         $this->browser->request('GET', HTTP_SERVER . '/admin/index.php?cmd=plugin_manager');
@@ -32,5 +31,54 @@ class BasicPluginInstallTest extends zcFeatureTestCaseAdmin
         $response = $this->browser->getResponse();
         $this->assertStringContainsString('Test plugin for testing purposes', (string)$response->getContent() );
         $this->assertStringContainsString('Help on Zen Cart Documentation Site', (string)$response->getContent() );
+    }
+
+    public function testDisableEnablePlugin(): void
+    {
+        $this->browserAdminLogin();
+        $this->installPluginToFilesystem(self::TEST_PLUGIN_NAME, self::TEST_PLUGIN_VERSION);
+        $this->browser->request('GET', HTTP_SERVER . '/admin/index.php?cmd=plugin_manager');
+        $this->browser->request('GET', HTTP_SERVER . '/admin/index.php?cmd=plugin_manager&page=1&colKey=zenTestPlugin&action=install');
+        $response = $this->browser->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Test plugin for testing purposes', (string)$response->getContent() );
+        $this->browser->submitForm('Install', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Version Installed: v1.0.0', \strip_tags((string)$response->getContent()) );
+        $this->browser->request('GET', HTTP_SERVER . '/admin/index.php?cmd=plugin_manager&page=1&colKey=zenTestPlugin');
+        $this->browser->clickLink('Disable', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Are you sure you want to disable this plugin?', \strip_tags((string)$response->getContent()) );
+        $this->browser->submitForm('Disable', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Plugin disabled successfully', \strip_tags((string)$response->getContent()) );
+        $this->browser->clickLink('Enable', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Are you sure you want to enable this plugin?', \strip_tags((string)$response->getContent()) );
+        $this->browser->submitForm('Enable', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Plugin enabled successfully', \strip_tags((string)$response->getContent()) );
+    }
+
+    public function testUninstallPlugin(): void
+    {
+        $this->browserAdminLogin();
+        $this->installPluginToFilesystem(self::TEST_PLUGIN_NAME, self::TEST_PLUGIN_VERSION);
+        $this->browser->request('GET', HTTP_SERVER . '/admin/index.php?cmd=plugin_manager');
+        $this->browser->request('GET', HTTP_SERVER . '/admin/index.php?cmd=plugin_manager&page=1&colKey=zenTestPlugin&action=install');
+        $response = $this->browser->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Test plugin for testing purposes', (string)$response->getContent() );
+        $this->browser->submitForm('Install', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Version Installed: v1.0.0', \strip_tags((string)$response->getContent()) );
+
+        $this->browser->request('GET', HTTP_SERVER . '/admin/index.php?cmd=plugin_manager&page=1&colKey=zenTestPlugin');
+        $this->browser->clickLink('Un-Install', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Are you sure you want to uninstall this plugin?', \strip_tags((string)$response->getContent()) );
+        $this->browser->submitForm('Un-Install', []);
+        $response = $this->browser->getResponse();
+        $this->assertStringContainsString('Plugin un-installed successfully', \strip_tags((string)$response->getContent()) );
     }
 }
