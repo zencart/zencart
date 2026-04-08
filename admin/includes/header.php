aa0ac9a36d06689fd8fd67b65e1976d71766a665
Chris Brown <drbyte@zen-cart.com>
2026-04-08 16:55:00 -0400

    Fix stray `li` and disable added searches

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -121,7 +121,7 @@ foreach ($upperMenuArray as $menuItem) {
 
             <div class="collapse navbar-collapse" id="top-bar-collapse">
                 <ul class="nav navbar-nav navbar-left">
-                    <?php if (zen_is_superuser() || check_page(FILENAME_ADMIN_ACTIVITY, '')) { ?>
+                    <?php if (zen_is_superuser() || check_page(FILENAME_ORDERS, '')) { ?>
                     <li class="hidden-xs">
                         <?= zen_draw_form('order_search', FILENAME_ORDERS, '', 'get', 'class="navbar-form"', true) ?>
                         <div class="form-group header-search">
@@ -132,7 +132,7 @@ foreach ($upperMenuArray as $menuItem) {
                     </li>
                     <?php } ?>
 
-                    <?php if (zen_is_superuser() || check_page(FILENAME_ADMIN_ACTIVITY, '')) { ?>
+                    <?php if (false && zen_is_superuser() || check_page(FILENAME_CUSTOMERS, '')) { ?>
                     <li class="hidden-xs">
                         <?= zen_draw_form('customer_search', FILENAME_CUSTOMERS, '', 'get', 'class="navbar-form"', true); ?>
                         <div class="form-group header-search">
@@ -142,7 +142,7 @@ foreach ($upperMenuArray as $menuItem) {
                     </li>
                     <?php } ?>
 
-                    <?php if (zen_is_superuser() || check_page(FILENAME_ADMIN_ACTIVITY, '')) { ?>
+                    <?php if (false && zen_is_superuser() || check_page(FILENAME_CATEGORY_PRODUCT_LISTING, '')) { ?>
                     <li class="hidden-xs">
                         <?= zen_draw_form('goto', FILENAME_CATEGORY_PRODUCT_LISTING, '', 'get', 'class="navbar-form"') ?>
                         <div class="form-group header-search goto-category">
@@ -227,7 +227,7 @@ foreach ($upperMenuArray as $menuItem) {
                             <?php if (!empty($plugin_menu_items)) { ?>
                                 <li class="divider"></li>
                                 <?php foreach ($plugin_menu_items as $item) { ?>
-                                <li <?= $item['id'] ? 'id="' . $item['id'] . '"' : '' ?>  <?= $item['li-class'] ? 'class="' . $item['li-class'] . '"' : '' ?>></li><a href="<?= $item['a'] ?>" <?= $item['params'] ?? '' ?>><i class="fa <?= $item['icon'] ?? 'fa-plug' ?>"></i> <?= $item['title'] ?></a></li>
+                                <li <?= $item['id'] ? 'id="' . $item['id'] . '"' : '' ?>  <?= $item['li-class'] ? 'class="' . $item['li-class'] . '"' : '' ?>><a href="<?= $item['a'] ?>" <?= $item['params'] ?? '' ?>><i class="fa <?= $item['icon'] ?? 'fa-plug' ?>"></i> <?= $item['title'] ?></a></li>
                                 <?php } ?>
                             <?php } ?>
 
