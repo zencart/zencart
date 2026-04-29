79427a13140af08f3eb65f417df2e995766de6a1
Tony <91156564+highburyeye@users.noreply.github.com>
2026-04-30 07:18:50 +1200

    Update password reset email content and styling
    Provides the HTML version sep as the previous version had poor formatting and not respecting line breaks. Improved the email to a more modern friendly tone.

--- includes/languages/english/lang.password_forgotten.php
+++ includes/languages/english/lang.password_forgotten.php
@@ -1,14 +1,40 @@
 <?php
+
+$password_reset_button_colour = '#00BCE4';//change to stores theme colour
+
 $define = [
     'NAVBAR_TITLE_1' => 'Login',
     'NAVBAR_TITLE_2' => 'Password Forgotten',
     'HEADING_TITLE' => 'Forgotten Password',
+
     'TEXT_MAIN' => "Enter your email address below and we'll send you instructions on how to reset your password.",
 
     'EMAIL_PASSWORD_RESET_SUBJECT' => STORE_NAME . ' - Password Reset',
-    'EMAIL_PASSWORD_RESET_BODY' => "A password reset was requested from %1\$s.\n\nTo reset your password for '%2\$s', please visit the following URL:\n\n%3\$s\n\n",
 
-    'SUCCESS_PASSWORD_RESET_SENT' => 'Thank you. If that email address is in our system, we will send password recovery instructions to that email address (remember to check your Spam folder)',
+    'EMAIL_PASSWORD_RESET_BODY' =>
+        "Hello,\n\n" .
+        "We received a request to reset the password for your %2\$s account.\n\n" .
+        "To choose a new password, please click the link below:\n\n" .
+        "%3\$s\n\n" .
+        "This link is for password reset only. If you did not request this, you can safely ignore this email and your password will not be changed.\n\n" .
+        "For your security, this request was made from IP address: %1\$s\n\n" .
+        "Kind regards,\n" .
+        STORE_NAME . "\n",
+
+    'EMAIL_PASSWORD_RESET_HTML' =>
+        '<p>Hello,</p>' .
+        '<p>We received a request to reset the password for your %2$s account.</p>' .
+        '<p>To choose a new password, please click the button below:</p>' .
+        '<p><a href="%3$s" style="display:inline-block;padding:10px 16px;background:' . $password_reset_button_colour . ';color:#ffffff;text-decoration:none;border-radius:4px;font-weight:bold;">Reset your password</a></p>' .
+        '<p>Or copy and paste this link into your browser:<br><a href="%3$s">%3$s</a></p>' .
+        '<p>This link is for password reset only. If you did not request this, you can safely ignore this email and your password will not be changed.</p>' .
+        '<p>For your security, this request was made from IP address: %1$s</p>' .
+        '<p>Kind regards,<br>' .
+        STORE_NAME .
+        '</p>',
+
+    'SUCCESS_PASSWORD_RESET_SENT' =>
+        'Thank you. If that email address is in our system, we will send password recovery instructions to that email address. Please check your Spam folder if it does not arrive shortly.',
 ];
 
 return $define;
