64a1abf4f3bb9328f8b2ee4556ffac4a2b6c4e52
Chris Brown <drbyte@zen-cart.com>
2026-05-20 16:59:42 -0400

    PSR-12 formatting

--- includes/classes/class.notifier.php
+++ includes/classes/class.notifier.php
@@ -1,22 +1,27 @@
 <?php
-/** 
+/**
  * File contains just the notifier class
  *
  * @copyright Copyright 2003-2025 Zen Cart Development Team
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: DrByte 2025 Sep 18 Modified in v2.2.0 $
  */
+
 /**
+ * NOTE: It is preferred to use Zencart\Traits\NotifierManager trait instead of extending this class.
+ *
  * class notifier is a concrete implemetation of the abstract base class
  *
- * it can be used in procedural (non OOP) code to set up an observer
+ * it can be used in procedural (non OOP) code to set up an observer.
+ *
  * see the observer/notifier tutorial for more details.
  *
  * @since ZC v1.3.0
  */
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
-class notifier extends base {
+
+class notifier extends base
+{
 }
-?>
