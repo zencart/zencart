7894ce7ae9f5bf879a064587dec6f862bc032b40
Chris Brown <code@drbyte.dev>
2026-05-29 11:57:16 -0400

    Remove default

--- admin/products_price_manager.php
+++ admin/products_price_manager.php
@@ -820,7 +820,7 @@ if (!empty($action)) {
                     if (empty($pInfo->products_model)) {
                         $pInfo->products_model = '';
                     }
-                    if ((substr($pInfo->products_model, 0, 4) != 'GIFT') || (substr($pInfo->products_model, 0, 4) == 'GIFT' && (zen_config('MODULE_ORDER_TOTAL_GV_SPECIAL', 'true') === 'true'))) {
+                    if ((substr($pInfo->products_model, 0, 4) != 'GIFT') || (substr($pInfo->products_model, 0, 4) == 'GIFT' && (zen_config('MODULE_ORDER_TOTAL_GV_SPECIAL') === 'true'))) {
                       ?>
                       <a href="<?php echo zen_href_link(FILENAME_SPECIALS, 'add_products_id=' . $_GET['products_filter'] . '&action=new' . (isset($sInfo->specials_id) ? '&sID=' . $sInfo->specials_id : '') . '&go_back=ON' . '&current_category_id=' . $current_category_id); ?>" class="btn btn-info" role="button"><i class="fa-solid fa-plus"></i> <?php echo IMAGE_INSTALL_SPECIAL; ?></a>
                     <?php } else { ?>
