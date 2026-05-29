dfb5edf1f0519bffeb053be86dddfc3f695cc269
Jeff Rutt <tmc@interrutt.com>
2026-05-28 23:20:53 -0500

    Update header_php.php

--- includes/modules/pages/index/header_php.php
+++ includes/modules/pages/index/header_php.php
@@ -119,7 +119,7 @@ if (isset($cPath) && zen_not_null($cPath)) {
 }
 
 // include template specific file name defines
-$define_page = zen_get_file_directory(DIR_WS_LANGUAGES . $_SESSION['language'] . '/html_includes/', FILENAME_DEFINE_MAIN_PAGE);
+$define_page = zen_get_file_directory(DIR_WS_LANGUAGES . $_SESSION['language'] . '/html_includes/', FILENAME_DEFINE_MAIN_PAGE, 'false');
 require DIR_WS_MODULES . zen_get_module_directory('require_languages.php');
 
 // set the product filters according to selected product type
