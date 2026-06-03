3f190fcfe895703e7e1ac908c7631d0937dd8292
Chris Brown <drbyte@zen-cart.com>
2026-06-03 18:04:20 -0400

    Update admin/customers.php

--- admin/customers.php
+++ admin/customers.php
@@ -1710,7 +1710,7 @@ if ($action === 'edit' || $action === 'update') {
                 if ($place_order_override === false && zen_admin_authorized_to_place_order()) {
                     $login_form_start =
                         '<form rel="noopener" target="_blank" name="login" action="' .
-                        zen_catalog_href_link(FILENAME_LOGIN, '', 'SSL') .
+                        zen_catalog_href_link(FILENAME_LOGIN) .
                         '" method="post">';
                     $hiddenFields = zen_draw_hidden_field('email_address', $cInfo->customers_email_address);
                     if (zen_config('EMP_LOGIN_AUTOMATIC') === 'true' && str_starts_with(HTTP_CATALOG_SERVER, 'https')) {
