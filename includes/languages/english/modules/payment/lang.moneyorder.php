8fd8ce7d1dbeda823144f5bbe8496b4a3e0a0a61
piloujp <117885587+piloujp@users.noreply.github.com>
2026-06-05 23:12:04 +0900

    Moneyorder_hard_coded_message
    When module is installed but account information are not set, an alert message is displayed next to the title. This message was hard coded in module file.
    This PR adds a constant in language file for this message.

--- includes/languages/english/modules/payment/lang.moneyorder.php
+++ includes/languages/english/modules/payment/lang.moneyorder.php
@@ -3,6 +3,7 @@ $define = [
     'MODULE_PAYMENT_MONEYORDER_TEXT_TITLE' => 'Check/Money Order',
     'MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION' => 'Customers can mail in their payment. Their order confirmation email will ask them to: <br><br>Please make your check or money order payable to:<br>' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO') : '<br>(your store name)') . '<br><br>Mail your payment to:<br>' . nl2br(zen_config('STORE_NAME_ADDRESS'), false) . '<br><br>' . 'Your order will not ship until we receive payment.',
     'MODULE_PAYMENT_MONEYORDER_REMINDER' => 'Please be sure to write your order number on the front of your check or money order.',
+    'MODULE_PAYMENT_MONEYORDER_TEXT_MISSING_INFO' => '(not configured - needs pay-to)',
 ];
 if (defined('MODULE_PAYMENT_MONEYORDER_STATUS')) {
     $define['MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER'] = 'Please make your check or money order payable to:' . "\n\n" . zen_config('MODULE_PAYMENT_MONEYORDER_PAYTO') . "\n\n" . 'Mail your payment to:' . "\n" . zen_config('STORE_NAME_ADDRESS') . "\n\n" . 'Your order will not ship until we receive payment.';
