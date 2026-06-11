1c0072a4e91b812d1e9a144947a5564fe471bcc8
Chris Brown <drbyte@zen-cart.com>
2026-06-11 02:34:36 -0400

    Reformat

--- includes/init_includes/init_db_config_read.php
+++ includes/init_includes/init_db_config_read.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * read the configuration settings from the db
  * see  {@link  https://docs.zen-cart.com/dev/code/init_system/} for more details.
@@ -8,13 +9,14 @@
  * @version $Id: DrByte 2026 Feb 26 Modified in v2.2.1 $
  */
 if (!defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access');
+    die('Illegal Access');
 }
+
 use Zencart\DbRepositories\ConfigurationRepository;
 use Zencart\DbRepositories\ProductTypeLayoutRepository;
 
 // need to enable caching in eloquent. for now, no caching @todo
-$use_cache = (isset($_GET['nocache']) ? false : true ) ;
+$use_cache = !isset($_GET['nocache']);
 global $db;
 
 $configurationRepository = new ConfigurationRepository($db);
@@ -24,8 +26,8 @@ $productTypeLayoutRepository = new ProductTypeLayoutRepository($db);
 $productTypeLayoutRepository->loadConfigSettings();
 
 if (file_exists(DIR_WS_CLASSES . 'db/' . DB_TYPE . '/define_queries.php')) {
-  /**
- * Load the database dependant query defines
- */
-  include(DIR_WS_CLASSES . 'db/' . DB_TYPE . '/define_queries.php');
+    /**
+     * Load the database dependant query defines
+     */
+    include(DIR_WS_CLASSES . 'db/' . DB_TYPE . '/define_queries.php');
 }
