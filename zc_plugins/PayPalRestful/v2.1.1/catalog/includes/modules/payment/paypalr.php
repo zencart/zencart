3ac502019c3c312cff67015aebf44882feafdaf8
lat9 <cindy@vinosdefrutastropicales.com>
2026-06-23 07:40:52 -0400

    Update PayPalRestful to v2.1.1
    Ref PR #7867

--- zc_plugins/PayPalRestful/v2.1.1/catalog/includes/modules/payment/paypalr.php
+++ zc_plugins/PayPalRestful/v2.1.1/catalog/includes/modules/payment/paypalr.php
@@ -6,7 +6,7 @@
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  *
- * Last updated: v2.1.0
+ * Last updated: v2.1.1
  */
 use PayPalRestful\Admin\AdminMain;
 use PayPalRestful\Admin\DoAuthorization;
@@ -31,7 +31,7 @@ class paypalr extends \base
 {
     use InteractsWithPlugins;
 
-    const CURRENT_VERSION = '2.1.0';
+    const CURRENT_VERSION = '2.1.1';
 
     const REDIRECT_LISTENER = HTTP_SERVER . DIR_WS_CATALOG . 'ppr_listener.php';
 
--- zc_plugins/PayPalRestful/v2.1.1/manifest.php
+++ zc_plugins/PayPalRestful/v2.1.1/manifest.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'pluginVersion' => 'v2.1.0',
+    'pluginVersion' => 'v2.1.1',
     'pluginName' => 'PayPal Restful Payment Module',
     'pluginDescription' => "This Zen Cart payment module combines the processing for the <b>PayPal Payments Pro</b> (<var>paypaldp</var>) and <b>PayPal Express Checkout</b> (<var>paypalwpp</var>) payment modules that are currently built into the Zen Cart distribution.  Instead of using the older NVP (<b>N</b>ame <b>V</b>alue <b>P</b>air) methods to communicate with PayPal, this payment module uses PayPal's now-current <a href=\"https://developer.paypal.com/api/rest/\" target=\"_blank\">REST APIs</a> and combines the two legacy methods into one.<br><br>Based on the like-named unencapsulated v1.3.1 version of the payment module.",
     'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
